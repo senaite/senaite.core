cabb3f20e65644e4618448fddc1b42f500b5415d
Godfrey Leibbrandt <godfrey@bikalabs.com>
2011-06-09 12:22:59 +0000

    fixing "Sample edit" screen

--- products/bika/browser/sample.js
+++ products/bika/browser/sample.js
@@ -2,6 +2,7 @@ jQuery( function($) {
 	
 	$(document).ready(function(){
 		$("#DateSampled").datepicker({'dateFormat': 'yy-mm-dd', showAnim: ''});
+        $("#ClientReference").focus();
 		$(".sampletype").autocomplete({ minLength: 0, source: autocomplete_sampletype});
 		$(".samplepoint").autocomplete({ minLength: 0, source: autocomplete_samplepoint});
 	});
