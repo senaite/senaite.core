9aaf104c19d0e8ffa33f902ded3398fdfecac3e7
Godfrey Leibbrandt <godfrey@bikalabs.com>
2011-06-10 08:08:53 +0000

    fixing "Sample edit" screen

--- products/bika/browser/sample.py
+++ products/bika/browser/sample.py
@@ -23,7 +23,10 @@ class SampleEditView(BrowserView):
     template = ViewPageTemplateFile("templates/sample_edit.pt")
 
     def __call__(self):
-        return self.template()
+        if self.request.form.has_key("submitted"):
+            return sample_edit_submit(self.context, self.request)
+        else:
+            return self.template()
 
     def tabindex(self):
         i = 0
@@ -34,5 +37,9 @@ class SampleEditView(BrowserView):
     def fmtDateSampled(self):
         date_sampled = self.request.form.has_key("DateSampled") and self.request.form["DateSampled"] or self.context.getDateSampled().strftime("%Y-%m-%d")
         return date_sampled
+
+def sample_edit_submit(context, request):
+    context.plone_utils.addPortalMessage("that might have worked", 'info')
+    return json.dumps({'success':"xxx"})
 
 
