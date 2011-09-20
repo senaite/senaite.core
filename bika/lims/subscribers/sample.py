e430301fe1d20df5c106333f3b5858e815e61778
Godfrey Leibbrandt <godfrey@bikalabs.com>
2011-09-20 13:09:03 +0000

    Issue ID: 3411591: Cancelling a Sample causes all it's ARs (and analyses) to be cancelled.

--- bika/lims/subscribers/sample.py
+++ bika/lims/subscribers/sample.py
@@ -16,12 +16,13 @@ def ActionSucceededEventHandler(sample, event):
 
     logger.info("Starting: %s on %s" % (event.action, sample))
 
+    workflow = getToolByName(sample, 'portal_workflow')
+
     if event.action == "receive":
         sample.setDateReceived(DateTime())
         sample.reindexObject(idxs = ["review_state", "getDateReceived", ])
         # when a sample is received, all associated
         # AnalysisRequests are also transitioned
-        workflow = getToolByName(sample, 'portal_workflow')
         for ar in sample.getAnalysisRequests():
             if not ar.UID() in sample.REQUEST['workflow_skiplist']:
                 workflow.doActionFor(ar, "receive")
@@ -29,5 +30,29 @@ def ActionSucceededEventHandler(sample, event):
     elif event.action == "expire":
         sample.setDateExpired(DateTime())
         sample.reindexObject(idxs = ["review_state", "getDateExpired", ])
+
+    #---------------------
+    # Secondary workflows:
+    #---------------------
+
+    elif event.action == "reinstate":
+        sample.reindexObject(idxs = ["cancellation_review_state", ])
+        # reinstate all ARs for this sample.
+        ars = sample.getAnalysisRequests()
+        for ar in ars:
+            if not ar.UID in sample.REQUEST['workflow_skiplist']:
+                ar_state = workflow.getInfoFor(ar, 'cancellation_review_state')
+                if ar_state == 'cancelled':
+                    workflow.doActionFor(ar, 'reinstate')
+
+    elif event.action == "cancel":
+        sample.reindexObject(idxs = ["cancellation_review_state", ])
+        # cancel all ARs for this sample.
+        ars = sample.getAnalysisRequests()
+        for ar in ars:
+            if not ar.UID in sample.REQUEST['workflow_skiplist']:
+                ar_state = workflow.getInfoFor(ar, 'cancellation_review_state')
+                if ar_state == 'active':
+                    workflow.doActionFor(ar, 'cancel')
 
     return
