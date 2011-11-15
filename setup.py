fe9c916b09225ac979014c50350163b8b60f8662
Campbell McKellar-Basset <campbell@bikalabs.com>
2011-11-15 13:59:26 +0200

    issue #2 redux - README.txt was still referenced in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '3.0pre-alpha'
 setup(name='bika.lims',
       version=version,
       description="Bika LIMS",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
