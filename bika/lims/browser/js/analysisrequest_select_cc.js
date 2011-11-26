692a514f067fdb52535e5600baefd10abfc9eef9
Campbell McKellar-Basset <campbell@bikalabs.com>
2011-11-26 10:07:30 +0200

    Select CC Contacts window jquery selectors use bika listing properly
    And few other little css fixes

--- bika/lims/browser/js/analysisrequest_select_cc.js
+++ bika/lims/browser/js/analysisrequest_select_cc.js
@@ -1,9 +1,11 @@
 jQuery( function($) {
 $(document).ready(function(){
 
+	// hack the save button into workflow_actions
 	$(".workflow_action_buttons")
 		.append("<input type='submit' value='Save' class='select_cc_save'/>");
 
+	// ##cc_uids is the parent AR form's CC contacts box
 	$.each(window.opener.$("#cc_uids").val().split(","), function(i,e){
 		form_id = $(this).parents("form").attr("id");
 		$("#"+form_id+"_cb_"+e).click();
@@ -13,9 +15,9 @@ $(document).ready(function(){
 	$('.select_cc_save').live('click', function(){
 		uids = [];
 		titles = [];
-		$.each($('input[id^="_cb_"]:checked'), function(i, e){
-			uids.push(e.id.split("_")[1]);
-			titles.push($(e).attr('alt').split('elect ')[1]);
+		$.each($("[name='uids:list']").filter(":checked"), function(i, e){
+			uids.push($(e).val());
+			titles.push($(e).attr('item_title'));
 		});
 		window.opener.$("#cc_titles").val(titles.join(','));
 		window.opener.$("#cc_uids").val(uids.join(','));
