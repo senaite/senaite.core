0b842d68dbdb2766ee91b22b7bfc909e2d8bffcf
Campbell McKellar-Basset <campbell@bikalabs.com>
2011-12-02 12:59:15 +0200

    Reports is from ATFolder

--- bika/lims/content/reports.py
+++ bika/lims/content/reports.py
@@ -2,15 +2,15 @@ from AccessControl import ClassSecurityInfo
 from DateTime import DateTime
 from Products.ATExtensions.ateapi import DateTimeField, DateTimeWidget
 from Products.Archetypes.public import *
-from bika.lims.content.bikaschema import BikaSchema
+from bika.lims import bikaMessageFactory as _
 from bika.lims.config import I18N_DOMAIN, PROJECTNAME
 from bika.lims.interfaces import IReports
-from bika.lims import bikaMessageFactory as _
+from plone.app.folder.folder import ATFolder, ATFolderSchema
 from zope.interface import implements
 
-schema = BikaSchema.copy()
+schema = ATFolderSchema.copy()
 
-class Reports(BaseContent):
+class Reports(ATFolder):
     implements(IReports)
     security = ClassSecurityInfo()
     displayContentsTab = False
