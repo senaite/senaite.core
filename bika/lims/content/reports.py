deeb37e8d7b64277a3c1ff73a4b02ba5cdf9a7fb
Anneline Sweetnam <anneline@bikalabs.com>
2011-12-07 16:18:45 +0200

    reports

--- bika/lims/content/reports.py
+++ bika/lims/content/reports.py
@@ -1,17 +1,15 @@
 from AccessControl import ClassSecurityInfo
-from DateTime import DateTime
-from Products.ATExtensions.ateapi import DateTimeField, DateTimeWidget
 from Products.Archetypes.public import *
 from bika.lims import bikaMessageFactory as _
 from bika.lims.config import I18N_DOMAIN, PROJECTNAME
-from bika.lims.interfaces import IReports
+from bika.lims.interfaces import IReports, IHaveNoBreadCrumbs
 from plone.app.folder.folder import ATFolder, ATFolderSchema
 from zope.interface import implements
 
 schema = ATFolderSchema.copy()
 
 class Reports(ATFolder):
-    implements(IReports)
+    implements(IReports, IHaveNoBreadCrumbs)
     security = ClassSecurityInfo()
     displayContentsTab = False
     schema = schema
