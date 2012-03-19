b7546f61787e21b74ea6862a09553f1c5db6792b
Campbell McKellar-Basset <campbell@bikalabs.com>
2012-03-19 12:33:05 +0200

    analyses tables have no toggles by default

--- bika/lims/browser/analyses.py
+++ bika/lims/browser/analyses.py
@@ -29,6 +29,7 @@ class AnalysesView(BikaListingView):
         self.show_sort_column = False
         self.show_select_row = False
         self.show_select_column = False
+        self.show_column_toggles = False
         self.pagesize = 1000
         self.form_id = 'analyses_form'
 
