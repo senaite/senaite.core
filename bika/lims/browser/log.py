ad7068502aeddac15c04383c532c37e5b274e35b
Anneline Sweetnam <anneline@bikalabs.com>
2012-06-05 15:39:10 +0200

    log tab on all versioned and workflow objects

--- bika/lims/browser/log.py
+++ bika/lims/browser/log.py
@@ -7,7 +7,11 @@ from bika.lims.browser.bika_listing import BikaListingView
 from bika.lims.utils import TimeOrDate
 from operator import itemgetter
 from plone.app.layout.globals.interfaces import IViewView
+from plone.app.layout.viewlets.content import ContentHistoryView, ContentHistoryViewlet
+from AccessControl.SecurityManagement import newSecurityManager
 from zope.interface import implements
+from zope.publisher.browser import TestRequest
+from DateTime import DateTime
 import json
 
 class LogView(BikaListingView):
@@ -56,6 +60,7 @@ class LogView(BikaListingView):
         wf = getToolByName(self.context, 'portal_workflow')
         pr = getToolByName(self.context, 'portal_repository')
 
+
         isVersionable = pr.isVersionable(aq_inner(self.context))
         review_history = wf.getInfoFor(self.context, 'review_history')
         review_history.reverse()
@@ -63,6 +68,8 @@ class LogView(BikaListingView):
         for entry in review_history:
             # this folderitems doesn't subclass from the bika_listing.py
             # so we create items from scratch
+            review_state = entry.get('review_state')
+            state_title = wf.getTitleForStateOnType(review_state, self.context.portal_type)
             item = {
                 'obj': self.context,
                 'id': self.context.id,
@@ -86,9 +93,52 @@ class LogView(BikaListingView):
                 'sortable_date': entry.get('time'),
                 'User': entry.get('actor'),
                 'Action': entry.get('action') and entry.get('action') or 'Create',
-                'Description': "review_state: %s" % entry.get('review_state'),
+                'Description': "review state: %s" % state_title,
             }
             items.append(item)
+
+        if isVersionable:
+            request = TestRequest()
+            chv = ContentHistoryViewlet(self.context, request, None, None)
+            chv.navigation_root_url = chv.site_url = 'http://localhost:8080/bikas'
+            version_history = chv.revisionHistory()
+        else:
+            version_history = []
+
+        for entry in version_history:
+            # this folderitems doesn't subclass from the bika_listing.py
+            # so we create items from scratch
+            # disregard the first entry of version history, as it is 
+            # represented by the first entry in review_history
+            if not entry.get('version_id'):
+                continue
+            item = {
+                'obj': self.context,
+                'id': self.context.id,
+                'uid': self.context.UID(),
+                'title': self.context.title_or_id(),
+                'type_class': '',
+                'url': self.context.absolute_url(),
+                'relative_url': self.context.absolute_url(),
+                'view_url': self.context.absolute_url(),
+                'path': "/".join(self.context.getPhysicalPath()),
+                'replace': {},
+                'before': {},
+                'after': {},
+                'choices':{},
+                'class': {},
+                'state_class': '',
+                'allow_edit': [],
+
+                'Version': entry.get('version_id'),
+                'Date': TimeOrDate(self.context, DateTime(entry.get('time')), long_format=1),
+                'sortable_date': entry.get('time'),
+                'User': entry.get('actor').get('fullname'),
+                'Action': entry.get('action') and entry.get('action') or 'Create',
+                'Description': entry.get('comments'),
+            }
+            items.append(item)
+
         items = sorted(items, key = itemgetter('sortable_date'))
         items.reverse()
 
