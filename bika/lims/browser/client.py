7f3d2559913013de2b5f0fcd2c447681f6b6651f
Campbell McKellar-Basset <campbell@bikalabs.com>
2012-08-09 11:13:08 +0200

    Add client sample points

--- bika/lims/browser/client.py
+++ bika/lims/browser/client.py
@@ -11,13 +11,7 @@ from bika.lims.browser.analysisrequest import AnalysisRequestsView
 from bika.lims.browser.bika_listing import BikaListingView
 from bika.lims.browser.publish import Publish
 from bika.lims.browser.sample import SamplesView
-from bika.lims.permissions import AddAnalysisProfile
-from bika.lims.permissions import AddARTemplate
-from bika.lims.permissions import AddAnalysisRequest
-from bika.lims.permissions import AddAnalysisSpec
-from bika.lims.permissions import ManageClients
-from bika.lims.permissions import SampleSample
-from bika.lims.permissions import PreserveSample
+from bika.lims.permissions import *
 from bika.lims.utils import TimeOrDate
 from bika.lims.utils import isActive
 from bika.lims.subscribers import skip
@@ -456,6 +450,71 @@ class ClientARTemplatesView(BikaListingView):
                 {'url': 'createObject?type_name=ARTemplate',
                  'icon': '++resource++bika.lims.images/add.png'}
         return super(ClientARTemplatesView, self).__call__()
+
+    def folderitems(self):
+        items = BikaListingView.folderitems(self)
+        for x in range(len(items)):
+            if not items[x].has_key('obj'): continue
+            obj = items[x]['obj']
+            items[x]['title'] = obj.Title()
+            items[x]['replace']['title'] = "<a href='%s'>%s</a>" % \
+                 (items[x]['url'], items[x]['title'])
+
+        return items
+
+class ClientSamplePointsView(BikaListingView):
+    """This is displayed in the "Sample Points" tab on each client
+    """
+
+    def __init__(self, context, request):
+        super(ClientSamplePointsView, self).__init__(context, request)
+        self.catalog = "bika_setup_catalog"
+        self.contentFilter = {
+            'portal_type': 'SamplePoint',
+            'path': {
+                "query": "/".join(self.context.getPhysicalPath()),
+                "level" : 0 },
+        }
+        self.show_sort_column = False
+        self.show_select_row = False
+        self.show_select_column = True
+        self.pagesize = 50
+        self.form_id = "SamplePoints"
+        self.icon = "++resource++bika.lims.images/samplepoint_big.png"
+        self.title = _("Sample Points")
+        self.description = ""
+
+        self.columns = {
+            'title': {'title': _('Title'),
+                      'index': 'sortable_title'},
+            'Description': {'title': _('Description'),
+                            'index': 'description'},
+        }
+        self.review_states = [
+            {'id':'default',
+             'title': _('Active'),
+             'contentFilter': {'inactive_state': 'active'},
+             'transitions': [{'id':'deactivate'}, ],
+             'columns': ['title', 'Description']},
+            {'id':'inactive',
+             'title': _('Dormant'),
+             'contentFilter': {'inactive_state': 'inactive'},
+             'transitions': [{'id':'activate'}, ],
+             'columns': ['title', 'Description']},
+            {'id':'all',
+             'title': _('All'),
+             'contentFilter':{},
+             'columns': ['title', 'Description']},
+        ]
+
+    def __call__(self):
+        mtool = getToolByName(self.context, 'portal_membership')
+        checkPermission = mtool.checkPermission
+        if checkPermission(AddSamplePoint, self.context):
+            self.context_actions[_('Add')] = \
+                {'url': 'createObject?type_name=SamplePoint',
+                 'icon': '++resource++bika.lims.images/add.png'}
+        return super(ClientSamplePointsView, self).__call__()
 
     def folderitems(self):
         items = BikaListingView.folderitems(self)
