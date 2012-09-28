227b2d1faf56683ae3dd0ec3bbc90ba86552b65b
Campbell <campbell@bikalabs.com>
2012-09-28 16:49:32 +0200

    Add Month/Year selectors to default datetime widget

--- bika/lims/skins/bika/bika_widgets/datetimewidget.js
+++ bika/lims/skins/bika/bika_widgets/datetimewidget.js
@@ -1,5 +1,10 @@
 jQuery( function($) {
 $(document).ready(function(){
-	$('[datepicker=1]').datepicker({'dateFormat': window.jsi18n_bika('date_format_short_datepicker')});
+	dateFormat = window.jsi18n_bika('date_format_short_datepicker');
+	$('[datepicker=1]').datepicker({
+		dateFormat: dateFormat,
+		changeMonth:true,
+		changeYear:true
+	});
 });
 });
