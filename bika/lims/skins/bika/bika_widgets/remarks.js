00792b6e2128a6a05f26be9137c63b351d613660
Campbell <campbell@bikalabs.com>
2012-10-01 16:59:28 +0200

    Remove remarks from portal_factory screens

--- bika/lims/skins/bika/bika_widgets/remarks.js
+++ bika/lims/skins/bika/bika_widgets/remarks.js
@@ -1,6 +1,11 @@
 jQuery( function($) {
 $(document).ready(function(){
 
+	// Hide remarks field if this object is in portal_factory
+	if(window.location.href.search("portal_factory") > -1) {
+		$("#archetypes-fieldname-Remarks").toggle(false);
+	}
+
 	$('.saveRemarks').live('click', function(event){
 		event.preventDefault();
 		if ($("#Remarks").val() == '' ||
