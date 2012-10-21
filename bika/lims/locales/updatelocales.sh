bebae9d27173393446d86ab0b90754545ae516ed
Campbell <campbell@bikalabs.com>
2012-10-21 14:26:24 +0200

    Update translations

--- bika/lims/locales/updatelocales.sh
+++ bika/lims/locales/updatelocales.sh
@@ -8,18 +8,21 @@ find . -name "*.mo" -delete
 
 ### bika domain
 ### ===========
-$I18NDUDE rebuild-pot --pot bika.pot --exclude "build" --create bika ..
-$I18NDUDE merge --pot bika.pot --merge bika-manual.pot
+$I18NDUDE rebuild-pot --pot i18ndude.pot --exclude "build" --create bika ..
+$I18NDUDE merge --pot i18ndude.pot --merge bika-manual.pot
+$I18NDUDE admix i18ndude.pot bika-manual.pot > bika.pot
 $I18NDUDE sync --pot bika.pot */LC_MESSAGES/bika.po
 
 ### plone domain
 ### ============
 PLONE_POT=~/Plone/zinstance/parts/omelette/plone/app/locales/locales/plone.pot
 $I18NDUDE rebuild-pot --pot i18ndude.pot --create plone ../profiles/
-$I18NDUDE filter i18ndude.pot $PLONE_POT > plone.pot
-$I18NDUDE merge --pot plone.pot --merge plone-manual.pot
+$I18NDUDE filter i18ndude.pot $PLONE_POT > plone-tmp.pot
+$I18NDUDE merge --pot plone-tmp.pot --merge plone-manual.pot
+$I18NDUDE admix plone-tmp.pot plone-manual.pot > plone.pot
 $I18NDUDE sync --pot plone.pot */LC_MESSAGES/plone.po
-rm i18ndude.pot
+
+rm i18ndude.pot plone-tmp.pot
 
 for po in `find . -name "*.po"`; do msgfmt -o ${po/%po/mo} $po; done
 
