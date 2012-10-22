c85fbe15cf92f2cb553c2f8e025856424ace89e1
Campbell <campbell@bikalabs.com>
2012-10-22 11:00:12 +0200

    Update i18n

--- bika/lims/locales/updatelocales.sh
+++ bika/lims/locales/updatelocales.sh
@@ -9,23 +9,20 @@ find . -name "*.mo" -delete
 ### bika domain
 ### ===========
 $I18NDUDE rebuild-pot --pot i18ndude.pot --exclude "build" --create bika ..
-$I18NDUDE merge --pot i18ndude.pot --merge bika-manual.pot
-$I18NDUDE admix i18ndude.pot bika-manual.pot > bika.pot
+msgcat --strict --use-first bika-manual.pot i18ndude.pot > bika.pot
 $I18NDUDE sync --pot bika.pot */LC_MESSAGES/bika.po
+rm i18ndude.pot
 
 ### plone domain
 ### ============
 PLONE_POT=~/Plone/zinstance/parts/omelette/plone/app/locales/locales/plone.pot
 $I18NDUDE rebuild-pot --pot i18ndude.pot --create plone ../profiles/
 $I18NDUDE filter i18ndude.pot $PLONE_POT > plone-tmp.pot
-$I18NDUDE merge --pot plone-tmp.pot --merge plone-manual.pot
-$I18NDUDE admix plone-tmp.pot plone-manual.pot > plone.pot
+msgcat --strict --use-first plone-manual.pot plone-tmp.pot > plone.pot
 $I18NDUDE sync --pot plone.pot */LC_MESSAGES/plone.po
-
 rm i18ndude.pot plone-tmp.pot
 
 for po in `find . -name "*.po"`; do msgfmt -o ${po/%po/mo} $po; done
 
 ### push new strings to transifex
 # tx push -s -t
-
