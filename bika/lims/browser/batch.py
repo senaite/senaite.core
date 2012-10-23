cd637c2ee41572c8a5708c40dcf62656a4da813a
Campbell <campbell@bikalabs.com>
2012-10-23 12:49:31 +0200

    Batch: Show correct list of samples in batch samples view

--- bika/lims/browser/batch.py
+++ bika/lims/browser/batch.py
@@ -45,4 +45,19 @@ class BatchAnalysisRequestsView(AnalysisRequestsView):
 class BatchSamplesView(SamplesView):
     def __init__(self, context, request):
         super(BatchSamplesView, self).__init__(context, request)
-        self.contentFilter['getBatchUID'] = self.context.UID()
+        self.view_url = self.context.absolute_url() + "/samples"
+        if 'path' in self.contentFilter:
+            del(self.contentFilter['path'])
+
+    def contentsMethod(self, contentFilter):
+        tool = getToolByName(self.context, self.catalog)
+        state = [x for x in self.review_states if x['id'] == self.review_state][0]
+        for k,v in state['contentFilter'].items():
+            self.contentFilter[k] = v
+        tool_samples = tool(contentFilter)
+        samples = {}
+        for sample in (p.getObject() for p in tool_samples):
+            for ar in sample.getAnalysisRequests():
+                if ar.getBatchUID() == self.context.UID():
+                    samples[sample.getId()] = sample
+        return samples.values()
