dc8ce5a2be2dec066711e12602be88ea4e2d1b9d
Campbell <campbell@bikalabs.com>
2012-11-28 20:21:53 +0200

    Cleanup javascript

--- bika/lims/browser/js/analysisservice.js
+++ bika/lims/browser/js/analysisservice.js
@@ -1,132 +1,132 @@
 (function( $ ) {
 
-function updateContainers(target,requestdata){
-	$.ajax({
-		type: 'POST',
-		url: window.location.href + "/getUpdatedContainers",
-		data: requestdata,
-		success: function(data,textStatus,$XHR){
-			// keep the current selection if possible
-			option = $(target).val();
-			if (option == null || option == undefined){ option = []; }
-			$(target).empty();
-			$.each(data, function(i,v){
-				if($.inArray(v[0], option) > -1) {
-					$(target).append("<option value='"+v[0]+"' selected='selected'>"+v[1]+"</option>");
-				} else {
-					$(target).append("<option value='"+v[0]+"'>"+v[1]+"</option>");
-				}
+	$(document).ready(function(){
+
+		_ = jarn.i18n.MessageFactory('bika');
+		PMF = jarn.i18n.MessageFactory('plone');
+
+		function updateContainers(target,requestdata){
+			$.ajax({
+				type: 'POST',
+				url: window.location.href + "/getUpdatedContainers",
+				data: requestdata,
+				success: function(data,textStatus,$XHR){
+					// keep the current selection if possible
+					option = $(target).val();
+					if (option == null || option == undefined){ option = []; }
+					$(target).empty();
+					$.each(data, function(i,v){
+						if($.inArray(v[0], option) > -1) {
+							$(target).append("<option value='"+v[0]+"' selected='selected'>"+v[1]+"</option>");
+						} else {
+							$(target).append("<option value='"+v[0]+"'>"+v[1]+"</option>");
+						}
+					});
+				},
+				dataType: "json"
 			});
-		},
-		dataType: "json"
-	});
-}
-
-$(document).ready(function(){
-
-	_ = jarn.i18n.MessageFactory('bika');
-	PMF = jarn.i18n.MessageFactory('plone');
-
-	// service defaults
-	// update defalt Containers
-	$("#RequiredVolume, #Separate").change(function(){
-		separate = $("#Separate").attr('checked');
-		if(!separate){
-			$("[name='Preservation\\:list']").removeAttr('disabled');
 		}
-		requestdata = {
-			'allow_blank':true,
-			'show_container_types':!separate,
-			'show_containers':separate,
-			'_authenticator': $('input[name="_authenticator"]').val()}
-		updateContainers("#Container\\:list", requestdata);
-	});
 
-	// partition table -> separate checkboxes
-	// partition table -> minvol field
-	// update row's containers
-	$("[name^='PartitionSetup.separate'],[name^='PartitionSetup.vol']").change(function(){
-		separate = $(this).parents("tr").find("[name^='PartitionSetup.separate']").attr("checked");
-		if (!separate){
-			$(this).parents("tr").find("[name^='PartitionSetup.preservation']").removeAttr('disabled');
-		}
-		minvol = $(this).parents("tr").find("[name^='PartitionSetup.vol']").val();
-		target = $(this).parents("tr").find("[name^='PartitionSetup.container']");
-		requestdata = {
-			'allow_blank':true,
-			'minvol':minvol,
-			'show_container_types':!separate,
-			'show_containers':separate,
-			'_authenticator': $('input[name="_authenticator"]').val()}
-		updateContainers(target, requestdata);
-	});
-
-	// copy sampletype MinimumVolume to minvol when selecting sampletype
-	$("[name^='PartitionSetup.sampletype']").change(function(){
-		// get sampletype volume from bika_utils
-		option = $(this).children().filter(":selected");
-		if(!option || $(option).val() == '' || option.length == 0){
-			return;
-		}
-		option = option[0];
-		title = $(option).text();
-		minvol = window.bika_utils.data['st_uids'][title]['minvol'];
-		// put value in minvol field
-		target = $(this).parents("tr").find("[name^='PartitionSetup.vol']");
-		$(target).val(minvol);
-		// trigger change on containers, in case SampleType volume rendered
-		// the selected container too small and removed it from the list
-		$(this).parents("tr").find("[name^='PartitionSetup.container']").change();
-
-	});
-
-	// Selecting a pre-preserved container will filter and disable the
-	// Preservations list.
-	$("[name^='Container'],[name^='PartitionSetup.container']").change(function(){
-		if ($(this).attr('name').search("Container") == 0){
-			target = "[name='Preservation\\:list']";
-			separate = $('#Separate').attr("checked");
-			if (!separate){
-				$(target).removeAttr('disabled');
-				return;
-			}
-		} else {
-			separate = $(this).parents("tr").find("[name^='PartitionSetup.separate']").attr("checked");
-			target = $(this).parents("tr").find("[name^='PartitionSetup.preservation']");
+		// service defaults
+		// update defalt Containers
+		$("#RequiredVolume, #Separate").change(function(){
+			separate = $("#Separate").attr('checked');
 			if(!separate){
+				$("[name='Preservation\\:list']").removeAttr('disabled');
+			}
+			requestdata = {
+				'allow_blank':true,
+				'show_container_types':!separate,
+				'show_containers':separate,
+				'_authenticator': $('input[name="_authenticator"]').val()}
+			updateContainers("#Container\\:list", requestdata);
+		});
+
+		// partition table -> separate checkboxes
+		// partition table -> minvol field
+		// update row's containers
+		$("[name^='PartitionSetup.separate'],[name^='PartitionSetup.vol']").change(function(){
+			separate = $(this).parents("tr").find("[name^='PartitionSetup.separate']").attr("checked");
+			if (!separate){
+				$(this).parents("tr").find("[name^='PartitionSetup.preservation']").removeAttr('disabled');
+			}
+			minvol = $(this).parents("tr").find("[name^='PartitionSetup.vol']").val();
+			target = $(this).parents("tr").find("[name^='PartitionSetup.container']");
+			requestdata = {
+				'allow_blank':true,
+				'minvol':minvol,
+				'show_container_types':!separate,
+				'show_containers':separate,
+				'_authenticator': $('input[name="_authenticator"]').val()}
+			updateContainers(target, requestdata);
+		});
+
+		// copy sampletype MinimumVolume to minvol when selecting sampletype
+		$("[name^='PartitionSetup.sampletype']").change(function(){
+			// get sampletype volume from bika_utils
+			option = $(this).children().filter(":selected");
+			if(!option || $(option).val() == '' || option.length == 0){
+				return;
+			}
+			option = option[0];
+			title = $(option).text();
+			minvol = window.bika_utils.data['st_uids'][title]['minvol'];
+			// put value in minvol field
+			target = $(this).parents("tr").find("[name^='PartitionSetup.vol']");
+			$(target).val(minvol);
+			// trigger change on containers, in case SampleType volume rendered
+			// the selected container too small and removed it from the list
+			$(this).parents("tr").find("[name^='PartitionSetup.container']").change();
+
+		});
+
+		// Selecting a pre-preserved container will filter and disable the
+		// Preservations list.
+		$("[name^='Container'],[name^='PartitionSetup.container']").change(function(){
+			if ($(this).attr('name').search("Container") == 0){
+				target = "[name='Preservation\\:list']";
+				separate = $('#Separate').attr("checked");
+				if (!separate){
+					$(target).removeAttr('disabled');
+					return;
+				}
+			} else {
+				separate = $(this).parents("tr").find("[name^='PartitionSetup.separate']").attr("checked");
+				target = $(this).parents("tr").find("[name^='PartitionSetup.preservation']");
+				if(!separate){
+					$(target).removeAttr('disabled');
+					return;
+				}
+			}
+			preservations = window.bika_utils.data.preservations;
+			containers = window.bika_utils.data.containers;
+			selection = $(this).val();
+			if(selection == null || selection == undefined || selection.length == 0) {
 				$(target).removeAttr('disabled');
 				return;
 			}
-		}
-		preservations = window.bika_utils.data.preservations;
-		containers = window.bika_utils.data.containers;
-		selection = $(this).val();
-		if(selection == null || selection == undefined || selection.length == 0) {
-			$(target).removeAttr('disabled');
-			return;
-		}
-		// Only allow first container to be selected.
-		selection = selection[0];
-		$(this).val(selection);
+			// Only allow first container to be selected.
+			selection = selection[0];
+			$(this).val(selection);
 
-		container = containers[selection];
-		if(container!=null
-		   && container!=undefined
-		   && container.prepreserved
-		   && container.preservation){
-				$(target).val(container.preservation);
-				$(target).attr('disabled', true);
-		} else {
-			$(target).removeAttr('disabled');
-		}
+			container = containers[selection];
+			if(container!=null
+			   && container!=undefined
+			   && container.prepreserved
+			   && container.preservation){
+					$(target).val(container.preservation);
+					$(target).attr('disabled', true);
+			} else {
+				$(target).removeAttr('disabled');
+			}
+		});
+
+
+		// update on first load
+		$("[name^='PartitionSetup.separate']").change();
+		$("[name^='Container']").change();
+		$("[name^='PartitionSetup.container']").change();
+
+
 	});
-
-
-	// update on first load
-	$("[name^='PartitionSetup.separate']").change();
-	$("[name^='Container']").change();
-	$("[name^='PartitionSetup.container']").change();
-
-
-});
 }(jQuery));
--- bika/lims/browser/js/artemplate.js
+++ bika/lims/browser/js/artemplate.js
@@ -1,86 +1,87 @@
 (function( $ ) {
 
-function autocomplete_samplepoint(request,callback){
-	$.getJSON(window.portal_url + "/ajax_samplepoints",
-		{'term':request.term,
-		 'sampletype':$("#SampleType").val(),
-		 '_authenticator': $('input[name="_authenticator"]').val()},
-		function(data,textStatus){
-			callback(data);
+	$(document).ready(function(){
+
+		_ = jarn.i18n.MessageFactory('bika');
+		PMF = jarn.i18n.MessageFactory('plone');
+
+		function autocomplete_samplepoint(request,callback){
+			$.getJSON(window.portal_url + "/ajax_samplepoints",
+				{'term':request.term,
+				 'sampletype':$("#SampleType").val(),
+				 '_authenticator': $('input[name="_authenticator"]').val()},
+				function(data,textStatus){
+					callback(data);
+				}
+			);
+			function set_sp(e){
+		  		sp = window.bika_utils.data.sp_uids[$(e).val()];
+		  		if (sp != undefined && sp != null){
+		  			if (sp['sampletypes'].length == 1){
+		  				$("#SampleType").val(sp['sampletypes'][0]);
+		  			}
+		  		}
+			}
+
+			$("#SamplePoint").focus(function(){
+				window._ac_focus = this;
+			});
+			$("#SamplePoint").change(function(){
+				set_sp(this);
+			});
+			$("#SamplePoint").blur(function(){
+				set_sp(this);
+			});
 		}
-	);
-	function set_sp(e){
-  		sp = window.bika_utils.data.sp_uids[$(e).val()];
-  		if (sp != undefined && sp != null){
-  			if (sp['sampletypes'].length == 1){
-  				$("#SampleType").val(sp['sampletypes'][0]);
-  			}
-  		}
-	}
 
-	$("#SamplePoint").focus(function(){
-		window._ac_focus = this;
-	});
-	$("#SamplePoint").change(function(){
-		set_sp(this);
-	});
-	$("#SamplePoint").blur(function(){
-		set_sp(this);
-	});
-}
+		function autocomplete_sampletype(request,callback){
+			$.getJSON(window.portal_url + "/ajax_sampletypes",
+				{'term':request.term,
+				 'samplepoint':$("#SamplePoint").val(),
+				 '_authenticator': $('input[name="_authenticator"]').val(),},
+				function(data,textStatus){
+					callback(data);
+				}
+			);
 
-function autocomplete_sampletype(request,callback){
-	$.getJSON(window.portal_url + "/ajax_sampletypes",
-		{'term':request.term,
-		 'samplepoint':$("#SamplePoint").val(),
-		 '_authenticator': $('input[name="_authenticator"]').val(),},
-		function(data,textStatus){
-			callback(data);
+		  	function set_st(e){
+		  		st = window.bika_utils.data.st_uids[$(e).val()];
+		  		if (st != undefined && st != null){
+					if (st['samplepoints'].length == 1){
+							$("#SamplePoint").val(st['samplepoints'][0]);
+					}
+				}
+			}
+
+			$("#SampleType").focus(function(){
+				window._ac_focus = this;
+			});
+			// also set on .change() though because sometimes we set these manually.
+			$("#SampleType").change( function(){
+				set_st(this);
+			});
+		  	$("#SampleType").blur(function(){
+		  		set_st(this);
+		  	});
 		}
-	);
 
-  	function set_st(e){
-  		st = window.bika_utils.data.st_uids[$(e).val()];
-  		if (st != undefined && st != null){
-			if (st['samplepoints'].length == 1){
-					$("#SamplePoint").val(st['samplepoints'][0]);
+		function clickSaveButton(event){
+			selected_analyses = $('[name^="uids\\:list"]').filter(':checked');
+			if(selected_analyses.length < 1){
+				window.bika_utils.portalMessage("No analyses have been selected");
+				window.scroll(0, 0);
+				return false;
 			}
 		}
-	}
 
-	$("#SampleType").focus(function(){
-		window._ac_focus = this;
+
+		$(".portaltype-artemplate #SampleType").autocomplete({ minLength: 0, source: autocomplete_sampletype});
+		$(".portaltype-artemplate #SamplePoint").autocomplete({ minLength: 0, source: autocomplete_samplepoint});
+
+		$(".portaltype-artemplate input[name$=save]").addClass('allowMultiSubmit');
+		$(".portaltype-artemplate input[name$=save]").click(clickSaveButton);
+
 	});
-	// also set on .change() though because sometimes we set these manually.
-	$("#SampleType").change( function(){
-		set_st(this);
-	});
-  	$("#SampleType").blur(function(){
-  		set_st(this);
-  	});
-}
-
-function clickSaveButton(event){
-	selected_analyses = $('[name^="uids\\:list"]').filter(':checked');
-	if(selected_analyses.length < 1){
-		window.bika_utils.portalMessage("No analyses have been selected");
-		window.scroll(0, 0);
-		return false;
-	}
-}
-
-$(document).ready(function(){
-
-	_ = jarn.i18n.MessageFactory('bika');
-	PMF = jarn.i18n.MessageFactory('plone');
-
-	$("#SampleType").autocomplete({ minLength: 0, source: autocomplete_sampletype});
-	$("#SamplePoint").autocomplete({ minLength: 0, source: autocomplete_samplepoint});
-
-	$("input[name$=save]").addClass('allowMultiSubmit');
-	$("input[name$=save]").click(clickSaveButton);
-
-});
 }(jQuery));
 
 
