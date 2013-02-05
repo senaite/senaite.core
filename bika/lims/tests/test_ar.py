b9184d9a91563263a68ed74b5a75c73a5461debf
Campbell <campbell@bikalabs.com>
2013-02-05 12:41:00 +0200

    Updated tests

--- bika/lims/tests/test_ar.py
+++ bika/lims/tests/test_ar.py
@@ -1,96 +1,88 @@
-from DateTime import DateTime
 from Products.CMFCore.utils import getToolByName
-from bika.lims.testing import BIKA_LIMS_INTEGRATION_TESTING
-from bika.lims.testing import BIKA_LIMS_FIXTURE
 from bika.lims.tests.base import BikaIntegrationTestCase
-from plone.app.testing import *
-import transaction
-import unittest,random
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+from bika.lims.browser.analysisrequest import ajaxAnalysisRequestSubmit
+
 
 class Tests(BikaIntegrationTestCase):
 
-    defaultBases = (BIKA_LIMS_FIXTURE,)
-
     def test_AR(self):
-        """ Make three ARs and run them through various workflows.
+        """ AR wf.
         """
 
-        login(self.portal, TEST_USER_NAME)
-        setRoles(self.portal, TEST_USER_ID, ['LabManager'])
+        wf = getToolByName(self.portal, 'portal_workflow')
+        setRoles(self.portal, TEST_USER_ID, ['Manager', 'LabManager',
+                                             'Member'])
+        wf.setChainForPortalTypes(('Analysis'),
+                                  ('bika_analysis_workflow',
+                                   'bika_worksheetanalysis_workflow',
+                                   'bika_cancellation_workflow'))
+        wf.setChainForPortalTypes(('AnalysisRequest'),
+                                  ('bika_ar_workflow',
+                                   'bika_worksheetanalysis_workflow',
+                                   'bika_cancellation_workflow'))
+        wf.setChainForPortalTypes(('SamplePartition', 'Sample'),
+                                  ('bika_sample_workflow',
+                                   'bika_cancellation_workflow'))
+        setRoles(self.portal, TEST_USER_ID, ['LabManager', 'Member'])
 
-        workflow = self.portal.portal_workflow
-        workflow.setChainForPortalTypes(('Analysis',), ('bika_analysis_workflow', 'bika_worksheetanalysis_workflow', 'bika_cancellation_workflow',))
-        workflow.setChainForPortalTypes(('AnalysisRequest',), ('bika_ar_workflow', 'bika_worksheetanalysis_workflow', 'bika_cancellation_workflow',))
-        workflow.setChainForPortalTypes(('SamplePartition','Sample'), ('bika_sample_workflow', 'bika_cancellation_workflow',))
+        self.portal.bika_setup.setSamplingWorkflowEnabled(False)
 
-        profiles = ['Digestible Energy', 'Micro-Bio check', 'Micro-Bio counts']
-        sampletypes = [p.getObject() for p in self.bsc(portal_type="SampleType")]
-        samplepoints = [p.getObject() for p in self.bsc(portal_type="SamplePoint")]
+        clients = self.portal.clients
+        clients.invokeFactory('Client', 'client1', title="Client One")
+        client1 = self.portal.clients.client1
+        client1.setClientID('CLIENT1')
+        client1.unmarkCreationFlag()
+        client1.invokeFactory('Contact', 'contact1')
+        contact1 = client1.contact1
+        contact1.unmarkCreationFlag()
+        contact1.edit(Firstname="First", Surname="Last")
+        cats = self.portal.bika_setup.bika_analysiscategories
+        cats.invokeFactory('AnalysisCategory', id='cat1', title='Category')
+        cats.cat1.unmarkCreationFlag()
+        services = self.portal.bika_setup.bika_analysisservices
+        services.invokeFactory(
+            'AnalysisService', id='service1', title='Service One')
+        services.service1.unmarkCreationFlag()
+        services.service1.edit(Category=cats.cat1)
+        sampletypes = self.portal.bika_setup.bika_sampletypes
+        sampletypes.invokeFactory('SampleType', id='st1', title="Type AAA 1")
+        sampletypes.st1.unmarkCreationFlag()
+        sampletypes.st1.edit(Prefix="ST1", MinimumVolume="10 ml")
 
-        client = self.portal.clients['client-1'] # happy hills
-        contacts = [c for c in client.objectValues() if c.portal_type == 'Contact']
-        for profile in profiles:
-            profile = self.bsc(portal_type='AnalysisProfile',
-                               Title=profile)[0].getObject()
-            profile_services = profile.getService()
+        # Configure request for ar_add
+        for x in (
+            ('_authenticator', self.getAuthenticator()),
+            ('CCEmails', ''),
+            ('Contact', client1.contact1.UID()),
+            ('Prices.' + services.service1.UID(), '10.00'),
+            ('VAT.' + services.service1.UID(), '14.00'),
+            ('ar.0', {
+                'Analyses': [services.service1.UID()],
+                'ClientID': 'CLIENT1',
+                'ClientUID': client1.UID(),
+                'SampleType': sampletypes.st1.UID(),
+                'SamplingDate': '2013-01-31',
+                'subtotal': '8.50',
+                'total': '9.69',
+                'vat': '1.19'})):
+            self.request[x[0]] = x[1]
 
-            _ars = []
-            sample_id = client.invokeFactory(type_name = 'Sample', id = 'tmp')
-            sample = client[sample_id]
-            sample.edit(
-                SampleID = sample_id,
-                SampleType = random.choice(sampletypes).Title(),
-                SamplePoint = random.choice(samplepoints).Title(),
-                ClientReference = chr(random.randint(70,90))*5,
-                ClientSampleID = chr(random.randint(70,90))*5,
-                SamplingDate = DateTime()
-            )
-            sample.processForm()
-            self.assertEqual(len(sample.getId().split("-")), 2)
-            ar_id = client.invokeFactory("AnalysisRequest", id = 'tmp')
-            ar = client[ar_id]
-            _ars.append(ar)
-            ar.edit(
-                RequestID = ar_id,
-                Contact = contacts[0],
-                CCContact = contacts[1],
-                CCEmails = "",
-                Sample = sample,
-                Profile = profile,
-                ClientOrderNumber = chr(random.randint(70,90))*10
-            )
-            ar.processForm()
-            self.assertEqual(len(ar.getId().split("-")), 3)
-            prices = {}
-            service_uids = []
-            for service in profile_services:
-                service_uids.append(service.UID())
-                prices[service.UID()] = service.getPrice()
-            ar.setAnalyses(service_uids, prices = prices)
-
-        wf = self.workflow
+        # Create AR, samples, analyses
+        ajaxAnalysisRequestSubmit(client1, self.request)
 
         def check_state(ar, desired_state):
             sample = ar.getSample()
-            self.assertEqual(wf.getInfoFor(sample, 'review_state'), desired_state)
+            self.assertEqual(wf.getInfoFor(sample, 'review_state'),
+                             desired_state)
             part = sample.objectValues('SamplePartition')[0]
-            self.assertEqual(wf.getInfoFor(part, 'review_state'), desired_state)
+            self.assertEqual(wf.getInfoFor(part, 'review_state'),
+                             desired_state)
             analyses = ar.getAnalyses(full_objects=True)
             for a in analyses:
-                self.assertEqual(wf.getInfoFor(a, 'review_state'), desired_state)
+                self.assertEqual(wf.getInfoFor(a, 'review_state'),
+                                 desired_state)
 
-        # Sampling workflow enabled: Digestible Energy AR
-        # ===============================================
-        ar = _ars[0]
-        wf.doActionFor(ar, 'sampling_workflow')
-
-        ## XXX I don't know how to test cascading subscribers properly.
-        ## check_state(ar, 'to_be_sampled')
-
-        transaction.get().commit()
-
-def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(Tests))
-    suite.layer = BIKA_LIMS_INTEGRATION_TESTING
-    return suite
+        ar = client1.objectValues('AnalysisRequest')[0]
+        wf.doActionFor(ar, 'receive')
