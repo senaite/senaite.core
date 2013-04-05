ac6f3d8ed492bb9e21ab93ef1a12f724a6e71a91
Campbell <campbell@bikalabs.com>
2013-04-05 14:47:58 +0200

    fixed #695: Missing portlets (metadata version bump: 1117)

---
+++ bika/lims/upgrade/to1117.py
@@ -0,0 +1,12 @@
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from Products.CMFCore.utils import getToolByName
+
+
+def upgrade(tool):
+    """ Enable portlets for key=/ (re-import portlets.xml): issue #695
+    """
+    portal = aq_parent(aq_inner(tool))
+    setup = portal.portal_setup
+
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'portlets')
