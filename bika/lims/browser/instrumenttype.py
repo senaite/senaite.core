7bab79298d5f3f801b76a279c0cf404ba0242498
Campbell <campbell@bikalabs.com>
2013-05-28 17:04:48 +0200

    issue #883 invalid add link url in instrumenttypes/instruments

--- bika/lims/browser/instrumenttype.py
+++ bika/lims/browser/instrumenttype.py
@@ -1,7 +1,14 @@
 from bika.lims.controlpanel.bika_instruments import InstrumentsView
+from bika.lims import bikaMessageFactory as _
 
 class InstrumentTypeInstrumentsView(InstrumentsView):
 
     def __init__(self, context, request):
         super(InstrumentTypeInstrumentsView, self).__init__(context, request)
         self.contentFilter['getInstrumentTypeUID']=self.context.UID()
+        url = self.portal.absolute_url()
+        url += "/bika_setup/bika_instruments/"
+        self.context_actions = {_('Add'):
+                                {'url': url+'createObject?type_name=Instrument',
+                                 'icon': '++resource++bika.lims.images/add.png'}}
+        
