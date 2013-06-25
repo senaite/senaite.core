bbbb53b7acfcd13226815f9641f8f7dc1de10a86
Campbell <campbell@bikalabs.com>
2013-06-25 12:39:37 +0200

    ARTemplatePartitionsWidget and ARTemplateAnalysesWidget better manage form contents

--- bika/lims/skins/bika/bika_widgets/artemplatepartitionswidget.js
+++ bika/lims/skins/bika/bika_widgets/artemplatepartitionswidget.js
@@ -3,9 +3,28 @@
 $(document).ready(function(){
 
 	function renumber(){
+		// Read list of valid, existing parts from the ARTemplatePartitionsWidget
+		var valid_parts = [];
 		$.each($("input[id*='Partitions-part_id-']"), function(i,e){
-			p = i+1;
-			$(this).val('part-'+p)
+			var p = i+1;
+			$(this).val('part-'+p);
+			valid_parts.push('part-'+p);
+		});
+		// Remove deleted parts from ARTemplateAnalysesWidget
+		$.each($(".listing_select_entry[field='Partition'] option"), function(i,e){
+			if($.inArray($(e).val(), valid_parts) == -1 ){
+				$(e).remove();
+			}
+		});
+		// Add new parts from ARTemplateAnalysesWidget
+		$.each($(".listing_select_entry[field='Partition']"), function(x,e){
+			for (var i = 0; i < valid_parts.length; i++) {
+				var p = valid_parts[i];
+				var opt = $(e).find("option[value='"+p+"']");
+				if (opt.length == 0) {
+					$(e).append("<option value='"+p+"'>"+p+"</option>");
+				};
+			};
 		});
 	}
 
@@ -18,8 +37,8 @@ $(document).ready(function(){
 	});
 
     // On first load, remove extra empty item placed here by recordswidget
-    rows = $(".records_row_Partitions");
-    last = rows.length;
+    var rows = $(".records_row_Partitions");
+    var last = rows.length;
     $($(".records_row_Partitions")[last-1]).remove();
 });
 
