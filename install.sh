2851f56dc95526478119870b99473e093806c7d0
Alfonso de la Guarda Reyes <alfonsodg@gmail.com>
2013-08-01 12:37:10 -0500

    Again fix the URl to download 4.2.5 Plone

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ fi
 # to use the master branch instead.
 
 mkdir -p $BIKA_HOME
-wget -nc https://launchpad.net/plone/4.3/4.3.1/+download/Plone-4.2.5-UnifiedInstaller.tgz 
+wget -nc https://launchpad.net/plone/4.2/4.2.5/+download/Plone-4.2.5-UnifiedInstaller.tgz 
 tar xzf Plone-4.2.5-UnifiedInstaller.tgz 
 cd Plone-4.2.5-UnifiedInstaller/
 ./install.sh --target=${BIKA_HOME} standalone 
