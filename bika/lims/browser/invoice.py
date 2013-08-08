b73208f2b4e54ba8fb0c98e5b55cbacd4eb9238e
Jurie-Jan Botha <juriejanbotha@gmail.com>
2013-08-08 17:38:52 +0100

    Print display for individual invoices fo invoice batch

---
+++ bika/lims/browser/invoice.py
@@ -0,0 +1,59 @@
+from bika.lims.browser import BrowserView
+from bika.lims.interfaces import IInvoiceView
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.interface import implements
+
+
+class InvoiceView(BrowserView):
+
+    implements(IInvoiceView)
+
+    template = ViewPageTemplateFile("templates/invoice.pt")
+    content = ViewPageTemplateFile("templates/invoice_print.pt")
+
+    def __call__(self):
+        context = self.context
+        workflow = getToolByName(context, 'portal_workflow')
+        # Gather relted objects
+        batch = context.aq_parent
+        client = context.getClient()
+        # Gather general data
+        self.invoiceNumber = context.getId()
+        self.invoiceDate = self.ulocalized_time(context.getInvoiceDate())
+        self.subtotal = context.getSubtotal()
+        self.vatTotal = context.getVATTotal()
+        self.total = context.getTotal()
+        # Create the batch range
+        start = self.ulocalized_time(batch.getBatchStartDate())
+        end = self.ulocalized_time(batch.getBatchEndDate())
+        self.batchRange = "%s to %s" % (start, end)
+        # Gather client data
+        self.clientName = client.Title()
+        self.clientPhone = client.getPhone()
+        self.clientFax = client.getFax()
+        self.clientEmail = client.getEmailAddress()
+        self.clientAccountNumber = client.getAccountNumber()
+        # Get an available client address in a preferred order
+        self.clientAddress = None
+        addresses = (
+            client.getBillingAddress(),
+            client.getPostalAddress(),
+            client.getPhysicalAddress(),
+        )
+        for address in addresses:
+            if address['address']:
+                self.clientAddress = address
+                break
+        # Gather the line items
+        items = context.objectValues('InvoiceLineItem')
+        self.items = [{
+            'invoiceDate': self.ulocalized_time(item.getItemDate()),
+            'description': item.getItemDescription(),
+            'orderNo': item.getClientOrderNumber(),
+            'subtotal': item.getSubtotal(),
+            'vatTotal': item.getVATTotal(),
+            'total': item.getTotal(),
+        } for item in items]
+        # Render the template
+        return self.template()
