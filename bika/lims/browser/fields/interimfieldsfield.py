5fd5253830013fabeb0fed4bf7cdd56bbd9cb824
Campbell <campbell@bikalabs.com>
2013-08-13 16:11:56 +0200

    issue 1004, calculation interims field: make sure the 'Add More' button is displayed

--- bika/lims/browser/fields/interimfieldsfield.py
+++ bika/lims/browser/fields/interimfieldsfield.py
@@ -4,24 +4,29 @@ from Products.Archetypes.Registry import registerField
 from Products.Archetypes.public import *
 from bika.lims import bikaMessageFactory as _
 
+
 class InterimFieldsField(RecordsField):
+
     """a list of InterimFields for calculations """
     _properties = RecordsField._properties.copy()
     _properties.update({
-        'type' : 'InterimFields',
-        'subfields' : ('keyword', 'title', 'value', 'unit', 'hidden'),
-        'required_subfields' : ('keyword', 'title'),
-        'subfield_labels' : {'keyword': _('Keyword'),
+        'fixedSize': 0,
+        'minimalSize': 0,
+        'maximalSize': 9999,
+        'type': 'InterimFields',
+        'subfields': ('keyword', 'title', 'value', 'unit', 'hidden'),
+        'required_subfields': ('keyword', 'title'),
+        'subfield_labels': {'keyword': _('Keyword'),
                              'title': _('Field Title'),
                              'value': _('Default value'),
                              'unit': _('Unit'),
                              'hidden': _('Hidden Field')},
-        'subfield_types' : {'hidden': 'boolean', 'value': 'float'},
-        'subfield_sizes' : {'keyword': 20,
+        'subfield_types': {'hidden': 'boolean', 'value': 'float'},
+        'subfield_sizes': {'keyword': 20,
                             'title': 20,
                             'value': 10,
                             'unit': 10},
-        'subfield_validators' : {'keyword': 'interimfieldsvalidator',
+        'subfield_validators': {'keyword': 'interimfieldsvalidator',
                                  'title': 'interimfieldsvalidator',
                                  'value': 'interimfieldsvalidator',
                                  'unit': 'interimfieldsvalidator'},
@@ -29,6 +34,6 @@ class InterimFieldsField(RecordsField):
     security = ClassSecurityInfo()
 
 registerField(InterimFieldsField,
-              title = "Interim Fields",
-              description = "Used for storing Interim Fields or Interim Results.",
+              title="Interim Fields",
+              description="Used for storing Interim Fields or Interim Results.",
               )
