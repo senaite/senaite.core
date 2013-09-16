bfbcc0fe871b127fdd69e6ac1ecd7d0e45b468bc
Campbell <campbell@bikalabs.com>
2013-09-16 10:31:03 +0200

    Cleanup monkey patches

---
+++ bika/lims/monkey/contentmenu.py
@@ -0,0 +1,38 @@
+def contentmenu_factories_available(self):
+    """These types will have their Add New... factories dropdown menu removed.
+    """
+    if hasattr(self._addContext(), 'portal_type') \
+    and self._addContext().portal_type in [
+        'Batch',
+        'Client',
+        'AnalysisRequest',
+        'Worksheet',
+        'Sample',
+        'AnalysisCategory',
+        'AnalysisProfile',
+        'ARTemplate',
+        'AnalysisService',
+        'AnalysisSpec',
+        'Attachment',
+        'Calculation',
+        'Instrument',
+        'LabContact',
+        'Laboratory',
+        'Method',
+        'Department',
+        'ReferenceDefinition',
+        'ReportFolder',
+        'SampleType',
+        'SamplePoint',
+        'WorksheetTemplate',
+        'LabProduct',
+        'ReferenceSample',
+        'Preservation'
+    ]:
+        return False
+    else:
+        itemsToAdd = self._itemsToAdd()
+        showConstrainOptions = self._showConstrainOptions()
+        if self._addingToParent() and not self.context_state.is_default_page():
+            return False
+        return (len(itemsToAdd) > 0 or showConstrainOptions)
---
+++ bika/lims/monkey/controlpanel.py
@@ -0,0 +1,13 @@
+from bika.lims import PMF
+from bika.lims import bikaMessageFactory as _
+
+
+def overview_controlpanel_categories(self):
+    """see ../configure.zcml
+    This adds the bika.lims category to controlpanel-overview.
+    """
+    return [
+        {'id': 'Plone', 'title': PMF(u'Plone Configuration')},
+        {'id': 'bika', 'title': _(u'Bika LIMS Configuration')},
+        {'id': 'Products', 'title': PMF(u'Add-on Configuration')},
+    ]
