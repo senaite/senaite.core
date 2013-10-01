f6e8c7ccf52468c39e3a3df1d868580376700bb0
Campbell <campbell@bikalabs.com>
2013-10-02 01:35:49 +0200

    Case statuses do not track ARs

--- bika/lims/subscribers/analysisrequest.py
+++ bika/lims/subscribers/analysisrequest.py
@@ -22,16 +22,7 @@ def AfterTransitionEventHandler(instance, event):
 
     workflow = getToolByName(instance, 'portal_workflow')
 
-    if action_id in('sampling_workflow', 'no_sampling_workflow'):
-        # Possibly "open" the AR's batch (retraction of batch state)
-        batch = instance.getBatch()
-        if batch:
-            try:
-                workflow.doActionFor(batch, 'open')
-            except:
-                pass
-
-    elif action_id == "attach":
+    if action_id == "attach":
         instance.reindexObject(idxs = ["review_state", ])
         # Don't cascade. Shouldn't be attaching ARs for now (if ever).
         return
@@ -71,24 +62,8 @@ def AfterTransitionEventHandler(instance, event):
             if not skip(analysis, action_id):
                 workflow.doActionFor(analysis.getObject(), 'receive')
 
-        # Possibly receive the AR's batch
-        batch = instance.getBatch()
-        if batch:
-            try:
-                workflow.doActionFor(batch, action_id)
-            except:
-                pass
-
     elif action_id == "submit":
         instance.reindexObject(idxs = ["review_state", ])
-
-        # Possibly submit the AR's batch
-        batch = instance.getBatch()
-        if batch:
-            try:
-                workflow.doActionFor(batch, action_id)
-            except:
-                pass
 
     elif action_id == "retract":
         instance.reindexObject(idxs = ["review_state", ])
@@ -98,13 +73,6 @@ def AfterTransitionEventHandler(instance, event):
             analyses = instance.getAnalyses(review_state = ('attachment_due', 'to_be_verified',))
             for analysis in analyses:
                 doActionFor(analysis.getObject(), 'retract')
-        # Pull the Batch state back to Received
-        batch = instance.getBatch()
-        if batch:
-            try:
-                workflow.doActionFor(batch, 'receive')
-            except:
-                pass
 
     elif action_id == "revert":
         instance.reindexObject(idxs = ["review_state", ])
@@ -122,14 +90,6 @@ def AfterTransitionEventHandler(instance, event):
             analyses = instance.getAnalyses(review_state = 'to_be_verified')
             for analysis in analyses:
                 doActionFor(analysis.getObject(), "verify")
-
-        # Possibly verify the AR's batch
-        batch = instance.getBatch()
-        if batch:
-            try:
-                workflow.doActionFor(batch, action_id)
-            except:
-                pass
 
     elif action_id == "publish":
         instance.reindexObject(idxs = ["review_state", "getDatePublished", ])
