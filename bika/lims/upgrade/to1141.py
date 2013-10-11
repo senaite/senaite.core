dbb82a65b3645d8d869823bb84326026d4cb90d6
Campbell <campbell@bikalabs.com>
2013-10-11 12:40:24 +0200

    Remove un-necessary versioning and upgrade (metadata 1141)

---
+++ bika/lims/upgrade/to1141.py
@@ -0,0 +1,21 @@
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+
+from Products.CMFCore import permissions
+from bika.lims.permissions import *
+
+from Products.CMFCore.utils import getToolByName
+
+
+def upgrade(tool):
+    portal = aq_parent(aq_inner(tool))
+    setup = portal.portal_setup
+
+    # update affected tools
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'typeinfo')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'repositorytool')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'workflow-csv')
+
+    wf.updateRoleMappings()
+
+    return True
