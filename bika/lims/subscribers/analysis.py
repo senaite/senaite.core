f42c1cb42bb0bb4d04fd2ea14f89219dd58514a0
Jordi Puiggené <jordi.puiggene@gmail.com>
2013-12-10 14:17:32 +0100

    Fix error when retracting analysis from a worksheet
    The system throws an 'AttributeError: guard_retract_transition'
    exception after initializing the new analysis cloned from
    the one to be retracted.

--- bika/lims/subscribers/analysis.py
+++ bika/lims/subscribers/analysis.py
@@ -334,7 +334,7 @@ def AfterTransitionEventHandler(instance, event):
             Instrument=instance.getInstrument(),
             SamplePartition=instance.getSamplePartition())
         analysis.unmarkCreationFlag()
-        zope.event.notify(ObjectInitializedEvent(analysis))
+        # zope.event.notify(ObjectInitializedEvent(analysis))
         changeWorkflowState(analysis,
                             'bika_analysis_workflow', 'sample_received')
         if ws:
