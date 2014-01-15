a6ea60a906482f5fac14b1185e7794f74b110968
Campbell <campbell@bikalabs.com>
2014-01-15 16:53:53 +0200

    idserver.py: Get archetypes tool from root portal object

--- bika/lims/idserver.py
+++ bika/lims/idserver.py
@@ -114,11 +114,13 @@ def generateUniqueId(context):
 
         def next_id(prefix):
 
-            # grab the first catalog we are indexed in.
-            at = getToolByName(context, 'archetype_tool')
             plone = context.portal_url.getPortalObject()
-            catalog_name = context.portal_type in at.catalog_map \
-                and at.catalog_map[context.portal_type][0] or 'portal_catalog'
+            # grab the first catalog we are indexed in.
+            at = getToolByName(plone, 'archetype_tool')
+            if context.portal_type in at.catalog_map:
+                catalog_name = at.catalog_map[context.portal_type][0]
+            else:
+                'portal_catalog'
             catalog = getToolByName(plone, catalog_name)
 
             # get all IDS that start with prefix
