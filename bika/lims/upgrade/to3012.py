5ce592795a4c294e3163b7f1d0cfe314ed73406f
Jordi Puiggené <jordi.puiggene@gmail.com>
2014-03-04 13:05:30 +0100

    Fix #1134 #1135 Hide Maintenance, Validations, Calibrations and Schedule
    Hide Maintenance, Validations, Calibrations and Schedule tabs from inside
    Instrument view. All that stuff is currently under refactoring.

---
+++ bika/lims/upgrade/to3012.py
@@ -0,0 +1,15 @@
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from Products.CMFCore.utils import getToolByName
+from Products.Archetypes.config import REFERENCE_CATALOG
+
+def upgrade(tool):
+    """ Remove Maintenance, Validations, Calibrations ans Schedule
+        https://github.com/bikalabs/Bika-LIMS/issues/1134
+    """
+    portal = aq_parent(aq_inner(tool))
+    setup = portal.portal_setup
+
+    # update affected tools
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'typeinfo')
+    return True
