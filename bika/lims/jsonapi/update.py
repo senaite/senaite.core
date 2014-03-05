4ed71e7f18720691b91e34b593d8fd42eee27465
Campbell <campbell@bikalabs.com>
2014-03-05 22:54:35 +0200

    Add @@API/updates method for updating multiple objects in a single request

--- bika/lims/jsonapi/update.py
+++ bika/lims/jsonapi/update.py
@@ -3,7 +3,9 @@ from plone.jsonapi.core import router
 from plone.jsonapi.core.interfaces import IRouteProvider
 from zExceptions import BadRequest
 from zope import interface
+import json
 import transaction
+
 
 class Update(object):
     interface.implements(IRouteProvider)
@@ -15,6 +17,7 @@ class Update(object):
     def routes(self):
         return (
             ("/update", "update", self.update, dict(methods=['GET', 'POST'])),
+            ("/updates", "updates", self.update, dict(methods=['GET', 'POST'])),
         )
 
     def update(self, context, request):
@@ -39,7 +42,6 @@ class Update(object):
             "url": router.url_for("update", force_external=True),
             "success": False,
             "error": True,
-            "objects": [],
         }
         obj_path = request.get("obj_path", "")
         if not obj_path:
@@ -58,4 +60,59 @@ class Update(object):
         ret['success'] = True
         ret['error'] = False
 
+        return ret
+
+    def updates(self, context, request):
+        """/@@API/updates: Update existing object values
+
+        This is a wrapper around the update method, allowing multiple updates
+        to be combined into a single request.
+
+        required parameters:
+
+            - input_values: A json-encoded dictionary.
+              Each key is an obj_path, and each value is a dictionary
+              containing key/value pairs to be set on the object.
+
+        Return value:
+
+        {
+            runtime: Function running time.
+            error: true or string(message) if error. false if no error.
+            success: true or string(message) if success. false if no success.
+            updates: return values from update
+        }
+        """
+        ret = {
+            "url": router.url_for("updates", force_external=True),
+            "success": False,
+            "error": True,
+            "updates": [],
+        }
+
+        input_values = json.loads(request.get('input_values', '[]'))
+        if not input_values:
+            raise BadRequest("missing input_values")
+        site_path = request['PATH_INFO'].replace("/@@API/updates", "")
+
+        for obj_path, i in input_values.items():
+            savepoint = transaction.savepoint()
+            if not obj_path.startswith("/"):
+                obj_path = "/" + obj_path
+            obj = context.restrictedTraverse(site_path + obj_path)
+            this_ret = {
+                "url": router.url_for("updates", force_external=True),
+                "success": False,
+                "error": True,
+            }
+            try:
+                set_fields_from_request(obj, i)
+            except:
+                savepoint.rollback()
+                raise
+            this_ret['success'] = True
+            this_ret['error'] = False
+            ret['updates'].append(this_ret)
+        ret['success'] = True
+        ret['error'] = False
         return ret
