9dd9b86f5446ddf3b5b0205dbe978d10fa9410af
Campbell <campbell@bikalabs.com>
2014-03-10 17:04:45 +0200

    Add ARPriorities upgrade

---
+++ bika/lims/upgrade/to3016.py
@@ -0,0 +1,39 @@
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from bika.lims.permissions import *
+from Products.CMFCore import permissions
+from Products.CMFCore.utils import getToolByName
+from zExceptions import BadRequest
+
+
+def upgrade(tool):
+    portal = aq_parent(aq_inner(tool))
+    setup = portal.portal_setup
+    typestool = getToolByName(portal, 'portal_types')
+
+    # update affected tools
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'typeinfo')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'controlpanel')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'factorytool')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'propertiestool')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'workflow')
+
+    # Add the object to bika_setup
+    try:
+        typestool.constructContent(type_name="SupplyOrderFolder",
+                               container=portal.bika_setup,
+                               id='bika_arpriorities',
+                               title='AR Priorities')
+        obj = portal.bika_setup.bika_arpriorities
+        obj.unmarkCreationFlag()
+        obj.reindexObject()
+    except BadRequest:
+        # folder already exists
+        pass
+
+    # Some catalog indexes were added or modified
+    bsc = getToolByName(portal, 'bika_setup_catalog')
+    bsc.addIndex('sortKey', 'FieldIndex')
+    bsc.clearFindAndRebuild()
+
+    return True
