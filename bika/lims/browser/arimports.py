0635396e939e5eb291a6c99ae167324b404f54fd
Mike Metcalfe <mike@webtide.co.za>
2014-03-26 07:49:45 +0200

    Fixed ARImport issues: Removed Add button on GlobalView, config of CLientView, added SamplingDate on sample creation

--- bika/lims/browser/arimports.py
+++ bika/lims/browser/arimports.py
@@ -13,6 +13,7 @@ from Products.statusmessages.interfaces import IStatusMessage
 from bika.lims import PMF, logger, bikaMessageFactory as _
 from bika.lims.browser import BrowserView
 from bika.lims.browser.bika_listing import BikaListingView
+from bika.lims.interfaces import IClient
 from bika.lims.permissions import *
 from bika.lims.utils import tmpID
 from plone.app.layout.globals.interfaces import IViewView
@@ -71,7 +72,7 @@ class GlobalARImportsView(BikaListingView):
                 'sort_on':'sortable_title',
                 }
         self.context_actions = {}
-        if self.context.portal_type == 'Client':
+        if IClient.providedBy(self.context):
             self.context_actions = \
                 {_('AR Import'):
                            {'url': 'arimport_add',
@@ -164,7 +165,9 @@ class GlobalARImportsView(BikaListingView):
     def getAR(self):
         import pdb; pdb.set_trace()
 
+
 class ClientARImportsView(GlobalARImportsView):
+
     def __init__(self, context, request):
         super(ClientARImportsView, self).__init__(context, request)
         self.contentFilter = {
