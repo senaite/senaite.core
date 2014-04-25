0176f5763a129cbcbaec97950162d6ecdb799cc6
Mike Metcalfe <mike@webtide.co.za>
2014-04-25 16:32:27 +0200

    Used registry to hide fields on a class

--- bika/lims/adapters/widgetvisibility.py
+++ bika/lims/adapters/widgetvisibility.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 from bika.lims.interfaces import IWidgetVisibility
+from plone.registry.interfaces import IRegistry
+from zope.component import queryUtility
 from zope.interface import implements
 
 
@@ -14,7 +16,22 @@ class WidgetVisibility(object):
 
         fields = list(self.context.Schema().fields())
 
+        # Get optional fields for class
+        registry = queryUtility(IRegistry)
+        if not registry:
+            raise RuntimeError('Registry not found')
+
+        hiddenattributes = registry.get('bika.lims.hiddenattributes', ())
+        optionally_disabled_fields = ()
+        for alist in hiddenattributes:
+            if alist[0] == self.context.portal_type:
+                optionally_disabled_fields = alist[1:]
+                break
+
         for field in fields:
+            if optionally_disabled_fields and \
+               field.__name__ in optionally_disabled_fields:
+                continue
             if field.widget.visible and isinstance(field.widget.visible, dict):
                 for k, v in field.widget.visible.items():
                     if k not in ret:
