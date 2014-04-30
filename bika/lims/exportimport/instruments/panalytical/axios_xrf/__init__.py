96d0843552e66e95dd6fc90c365d3afce808671c
Pau Soliva <psoliva@naralabs.com>
2014-04-30 13:11:45 +0200

    Firt header's parser implementation

--- bika/lims/exportimport/instruments/panalytical/axios_xrf/__init__.py
+++ bika/lims/exportimport/instruments/panalytical/axios_xrf/__init__.py
@@ -13,51 +13,211 @@ class AxiosXrfCSVParser(InstrumentCSVResultsFileParser):
         self._columns = []
 
     def _parseline(self, line):
-        sline = line.replace('"', '').strip()
+        #sline = line.replace('"', '').strip()
         if self._end_header == False:
+            #sline = line.replace('"', '').strip()
+            sline = line.strip(',')
+            #sline = sline.strip(',')
             return self.parse_headerline(sline)
         else:
             return self.parse_resultline(sline)
 
     def splitLine(self, line):
+        if self._end_header == False:#Només per la bateria de headers, no pel uantification
+            sline = split(':')
+            return [token.strip(',') for token in sline]
+        
         return [token.strip() for token in line.split('\t')]
 
     def parse_headerline(self, line):
         """ Parses header lines
-
-            Header example:
-            Date    2012/11/15    User    anonymous
-            Time    06:07:08PM    Software version: 4.0
-
-            Example laboratory
-            Arizona
+29/11/2013 10:15:44
+PANalytical
+Quantification of sample ESFERA CINZA - 1g H3BO3 -  1:0,5 - NO PPC
+            
         """
-        if line.startswith('Date'):
-            splitted = self.splitLine(line)
-            if len(splitted) > 1:
-                self._header['Date'] = splitted[1]
-                if len(splitted) > 2 and splitted[2] == 'User':
-                    self._header['Date'] = splitted[1]
-                    self._header['User'] = splitted[3] \
-                        if len(splitted) > 3 else ''
-                else:
-                    self.warn(_('Unexpected header format'))
+        
+        if line.startswith('"Quantification of sample'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+            
+            self.line = self.line.strip('"')
+            splitted = self.line.split('-')
+
+            if len(splitted) > 3:
+                splitted[0] = splitted[0].strip()
+                sample_name_l = splitted[0].split(' ')[3:]
+                sample_name = str()
+                for token in sample_name_l:
+                    sample_name += " " + token
+                self._header['Sample'] = sample_name
+                self._header['Quantity'] = splitted[1]
+                self._header['????'] = splitted[3]
+                self._header['PPC'] = splitted[4]
+                
             else:
                 self.warn(_('Unexpected header format'))
 
-            return 0
+            return 1 #Llegia la segona linia amb 1?
 
-        if line.startswith('Time'):
+        if line.startswith('R.M.S.'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
             splitted = self.splitLine(line)
             if len(splitted) > 1:
-                self._header['Time'] = splitted[1]
+                self._header['Time'] = splitted[1].replace('"', '').strip()
             else:
                 self.warn(_('Unexpected header format'), self._numline)
 
             return 0
 
-        if line.startswith('Sample/ctrl'):
-            # Sample/ctrl ID    Pat/Ctr/cAl    Test name    Test type
+        if line.startswith('Result status'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Time'] = splitted[1].replace('"', '').strip()
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 0
+
+        if line.startswith('Sum before normalization'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Time'] = splitted[1].replace('"', '').strip()
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 0
+
+        if line.startswith('Normalised to'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Time'] = splitted[1].replace('"', '').strip()
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 0
+
+        if line.startswith('Sample type'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Time'] = splitted[1].strip()
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 0
+
+        if line.startswith('Initial sample weight (g)'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Time'] = splitted[1].replace('"', '').strip()
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 0
+
+        if line.startswith('Weight after pressing (g)'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Time'] = splitted[1].replace('"', '').strip()
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 0
+
+        if line.startswith('Correction applied for medium'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Time'] = splitted[1].replace('"', '').strip()
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 0
+
+        if line.startswith('Correction applied for film'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Time'] = splitted[1].replace('"', '').strip()
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 0
+
+        if line.startswith('Used Compound list'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Time'] = splitted[1].replace('"', '').strip()
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 0
+        if line.startswith('Results database'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Time'] = splitted[1].replace('"', '').strip()
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 0
+        
+        if line.startswith('Results database in'):
+            if len(self._header) == 0:
+                self.err(_("No header found"), self._numline)
+                return -1
+
+            splitted = self.splitLine(line)
+            if len(splitted) > 1:
+                self._header['Database path'] = splitted[1]+plitted[2]
+            else:
+                self.warn(_('Unexpected header format'), self._numline)
+
+            return 1
+
+        if line.startswith('Analyte'):
+            # Analyte,Calibration,Compound,Concentration,Unit,Calculation,Status
             if len(self._header) == 0:
                 self.err(_("No header found"), self._numline)
                 return -1
@@ -66,6 +226,9 @@ class AxiosXrfCSVParser(InstrumentCSVResultsFileParser):
             self._end_header = True
             self._columns = self.splitLine(line)
             return 1
+
+        else:
+            #Agafar la data
 
     def parse_resultline(self, line):
         # Sample/ctrl ID    Pat/Ctr/cAl    Test name    Test type
