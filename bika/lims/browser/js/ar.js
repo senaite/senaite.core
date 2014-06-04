5365c7a1576422eadf759049d1b399dd7879e3e5
Mike Metcalfe <mike@webtide.co.za>
2014-06-04 10:03:16 +0200

    Improved (and moved) ar_count onchange callback

--- bika/lims/browser/js/ar.js
+++ bika/lims/browser/js/ar.js
@@ -65,7 +65,56 @@ function workflow_transition_republish(event){
 	window.location.href = href;
 }
 
+function addParam(address, param, val){
+    var argparts;
+    var parts = address.split("?");
+    var url = parts[0];
+    var args = [];
+    if (parts.length > 1) {
+        var found = false;
+        args = parts[1].split("&");
+        for (var arg=0; arg<args.length; arg++) {
+            argparts = args[arg].split('=');
+            if (argparts[0] == param) {
+                args[arg] = param + '=' + val;
+                found = true;
+            }
+        };
+        if (found == false) {
+            args.push(param + '=' + val);
+        };
+    } else {
+        args.push(param + '=' + val);
+    };
+    return url + "?" + args.join('&');
+};
+
 $(document).ready(function(){
+
+    $('#layout').change(function(){
+        var address = $(".context_action_link").attr('href');
+        var href = addParam(address, 'layout', $(this).val())
+        $(".context_action_link").attr('href', href);
+    });
+    $('#ar_count').each(function() {
+        var elem = $(this);
+
+        // Save current value of element
+        elem.data('oldVal', elem.val());
+
+        // Look for changes in the value
+        elem.bind("propertychange keyup input paste", function(event){
+            var new_val = elem.val();
+            // If value has changed...
+            if (new_val != '' && elem.data('oldVal') != new_val) {
+                // Updated stored value
+                elem.data('oldVal', new_val);
+                var address = $(".context_action_link").attr('href');
+                var href = addParam(address, 'ar_count', $(this).val())
+                $(".context_action_link").attr('href', href);
+            }
+        });
+    });
 
     // jarn.i18n.loadCatalog('bika');
 	// var _ = window.jarn.i18n.MessageFactory("bika");
