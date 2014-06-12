9d34ae021b04533d7fe598a99d49dd4895112aa6
Jordi Puiggené <jordi.puiggene@gmail.com>
2014-06-12 22:23:03 +0200

    Fix #1265 Improvements

--- bika/lims/browser/js/ar.js
+++ bika/lims/browser/js/ar.js
@@ -36,13 +36,17 @@ $(document).ready(function(){
 	$("#workflow-transition-republish").click(workflow_transition_publish);
 
     // Set the analyst automatically when selected in the picklist
-    $('.bika-listing-table td.Analyst select').change(function() {
+    $('.portaltype-analysisrequest .bika-listing-table td.Analyst select').change(function() {
         var analyst = $(this).val();
         var key = $(this).closest('tr').attr('keyword');
         var obj_path = window.location.href.replace(window.portal_url, '');
         var obj_path_split = obj_path.split('/');
-        obj_path_split[obj_path_split.length-1] = key;
-        obj_path = obj_path_split.join('/')
+        if (obj_path_split.length > 4) {
+            obj_path_split[obj_path_split.length-1] = key;
+        } else {
+            obj_path_split.push(key);
+        }
+        obj_path = obj_path_split.join('/');
         $.ajax({
             type: "POST",
             url: window.portal_url+"/@@API/update",
