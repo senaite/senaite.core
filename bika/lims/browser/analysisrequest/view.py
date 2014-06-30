a7012c92a72b685018d28684625d0cf9f0c3537e
Pau Soliva <psoliva@naralabs.com>
2014-06-30 17:55:27 +0200

    Fix #1256 Sample Point jQuery widget does not filter by client. Worked before

--- bika/lims/browser/analysisrequest/view.py
+++ bika/lims/browser/analysisrequest/view.py
@@ -408,6 +408,3 @@ class AnalysisRequestViewView(BrowserView):
             icon = priority.getBigIcon()
             if icon:
                 return '/'.join(icon.getPhysicalPath())
-
-    def getClientUID(self):
-        return self.aq_parent.UID()
