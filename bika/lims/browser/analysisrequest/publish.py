123e3ad63e44b91f3f2cb9f27ea59f60e75549d3
Jordi Puiggené <jordi.puiggene@gmail.com>
2014-07-01 23:01:56 +0200

    Default DIN-A4 results report, generic reports view

--- bika/lims/browser/analysisrequest/publish.py
+++ bika/lims/browser/analysisrequest/publish.py
@@ -3,7 +3,10 @@ from bika.lims.utils import to_utf8
 from Products.CMFCore.utils import getToolByName
 from bika.lims import logger
 from bika.lims.browser import BrowserView
+from bika.lims.config import POINTS_OF_CAPTURE
+from bika.lims.interfaces import IResultOutOfRange
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getAdapters
 import sys, traceback
 
 class AnalysisRequestPublishView(BrowserView):
@@ -52,10 +55,11 @@ class AnalysisRequestPublishView(BrowserView):
                 'composite': ar.getComposite(),
                 'report_drymatter': ar.getReportDryMatter(),
                 'invoice_exclude': ar.getInvoiceExclude(),
-                'date_received': ar.getDateReceived(),
+                'date_received': self.ulocalized_time(ar.getDateReceived(), long_format=1),
                 'remarks': ar.getRemarks(),
                 'member_discount': ar.getMemberDiscount(),
-                'date_sampled': ar.getDateSampled(),
+                'date_sampled': self.ulocalized_time(ar.getDateSampled(), long_format=1),
+                'date_published': self.ulocalized_time(ar.getDatePublished(), long_format=1),
                 'invoiced': ar.getInvoiced(),
                 'late': ar.getLate(),
                 'subtotal': ar.getSubtotal(),
@@ -63,6 +67,8 @@ class AnalysisRequestPublishView(BrowserView):
                 'totalprice': ar.getTotalPrice(),
                 'invalid': ar.isInvalid(),
                 'url': ar.absolute_url(),
+                'remarks': to_utf8(ar.getRemarks()),
+                'footer': to_utf8(self.context.bika_setup.getResultFooter()),
                 'child_analysisrequest': None,
                 'parent_analysisrequest': None}
 
@@ -74,8 +80,24 @@ class AnalysisRequestPublishView(BrowserView):
 
         data['contact'] = self._contact_data(ar)
         data['client'] = self._client_data(ar)
-        data['sample'] = self._sample_data(ar.getSample())
-        data['batch'] = self._batch_data(ar.getBatch())
+        data['sample'] = self._sample_data(ar)
+        data['batch'] = self._batch_data(ar)
+        data['specifications'] = self._specs_data(ar)
+        data['analyses'] = self._analyses_data(ar, ['verified', 'published'])
+        data['points_of_capture'] = sorted(set([an['point_of_capture'] for an in data['analyses']]))
+        data['categories'] = sorted(set([an['category'] for an in data['analyses']]))
+        # Categorized analyses
+        data['categorized_analyses'] = {}
+        for an in data['analyses']:
+            poc = an['point_of_capture']
+            cat = an['category']
+            pocdict = data['categorized_analyses'].get(poc, {})
+            catlist = pocdict.get(cat, [])
+            catlist.append(an)
+            pocdict[poc] = catlist
+            data['categorized_analyses'][poc] = pocdict
+        data['haspreviousresults'] = len([an['previous_results'] for an in data['analyses'] if an['previous_results']]) > 0
+        data['reporter'] = self._reporter_data(ar)
 
         portal = self.context.portal_url.getPortalObject()
         data['portal'] = {'obj': portal,
@@ -84,53 +106,64 @@ class AnalysisRequestPublishView(BrowserView):
 
         return data
 
-    def _batch_data(self, batch=None):
-        if not batch:
-            return {}
-        data = {'obj': batch,
-                'id': batch.id,
-                'title': batch.Title(),
-                'date': batch.getBatchDate(),
-                'client_batchid': batch.getClientBatchID(),
-                'remarks': batch.getRemarks()}
+    def _batch_data(self, ar):
+        data = {}
+        batch = ar.getBatch()
+        if batch:
+            data = {'obj': batch,
+                    'id': batch.id,
+                    'title': to_utf8(batch.Title()),
+                    'date': batch.getBatchDate(),
+                    'client_batchid': to_utf8(batch.getClientBatchID()),
+                    'remarks': to_utf8(batch.getRemarks())}
+
+            uids = batch.getBatchLabels()
+            uc = getToolByName(self.context, 'uid_catalog')
+            data['labels'] = [to_utf8(p.getObject().Title()) for p in uc(UID=uids)]
+
         return data
 
-    def _sample_data(self, sample):
-        data = {'obj': sample,
-                'id': sample.id,
-                'url': sample.absolute_url(),
-                'client_sampleid': sample.getClientSampleID(),
-                'date_sampled': sample.getDateSampled(),
-                'sampling_date': sample.getSamplingDate(),
-                'sampler': sample.getSampler(),
-                'date_received': sample.getDateReceived(),
-                'composite': sample.getComposite(),
-                'date_expired': sample.getDateExpired(),
-                'date_disposal': sample.getDisposalDate(),
-                'date_disposed': sample.getDateDisposed(),
-                'adhoc': sample.getAdHoc(),
-                'remarks': sample.getRemarks() }
+    def _sample_data(self, ar):
+        data = {}
+        sample = ar.getSample()
+        if sample:
+            data = {'obj': sample,
+                    'id': sample.id,
+                    'url': sample.absolute_url(),
+                    'client_sampleid': sample.getClientSampleID(),
+                    'date_sampled': sample.getDateSampled(),
+                    'sampling_date': sample.getSamplingDate(),
+                    'sampler': sample.getSampler(),
+                    'date_received': sample.getDateReceived(),
+                    'composite': sample.getComposite(),
+                    'date_expired': sample.getDateExpired(),
+                    'date_disposal': sample.getDisposalDate(),
+                    'date_disposed': sample.getDateDisposed(),
+                    'adhoc': sample.getAdHoc(),
+                    'remarks': sample.getRemarks() }
 
-        data['sample_type'] = self._sample_type(sample.getSampleType())
-        data['sample_point'] = self._sample_point(sample.getSamplePoint())
+            data['sample_type'] = self._sample_type(sample)
+            data['sample_point'] = self._sample_point(sample)
         return data
 
-    def _sample_type(self, sampletype=None):
-        if not sampletype:
-            return {}
-        data = {'obj': sampletype,
-                'id': sampletype.id,
-                'title': sampletype.Title(),
-                'url': sampletype.absolute_url()}
+    def _sample_type(self, sample=None):
+        data = {}
+        sampletype = sample.getSampleType() if sample else None
+        if sampletype:
+            data = {'obj': sampletype,
+                    'id': sampletype.id,
+                    'title': sampletype.Title(),
+                    'url': sampletype.absolute_url()}
         return data
 
-    def _sample_point(self, samplepoint=None):
-        if not samplepoint:
-            return {}
-        data = {'obj': samplepoint,
-                'id': samplepoint.id,
-                'title': samplepoint.Title(),
-                'url': samplepoint.absolute_url()}
+    def _sample_point(self, sample=None):
+        samplepoint = sample.getSamplePoint() if sample else None
+        data = {}
+        if samplepoint:
+            data = {'obj': samplepoint,
+                    'id': samplepoint.id,
+                    'title': samplepoint.Title(),
+                    'url': samplepoint.absolute_url()}
         return data
 
     def _lab_data(self):
@@ -151,33 +184,24 @@ class AnalysisRequestPublishView(BrowserView):
                 'title': to_utf8(lab.Title()),
                 'url': to_utf8(lab.getLabURL()),
                 'address': to_utf8(lab_address),
+                'confidence': lab.getConfidence(),
+                'accredited': lab.getLaboratoryAccredited(),
                 'accreditation_body': to_utf8(lab.getAccreditationBody()),
+                'accreditation_logo': lab.getAccreditationBodyLogo(),
                 'logo': "%s/logo_print.jpg" % portal.absolute_url()}
 
     def _contact_data(self, ar):
-        data = {'obj': None,
-                'fullname': '',
-                'email': '',
-                'pubpref': '',}
-
+        data = {}
         contact = ar.getContact()
         if contact:
-            data['obj'] = contact
-            data['fullname'] = to_utf8(contact.getFullname())
-            data['email'] = to_utf8(contact.getEmailAddress())
-            data['pubpref'] = contact.getPublicationPreference()
-
+            data = {'obj': contact,
+                    'fullname': to_utf8(contact.getFullname()),
+                    'email': to_utf8(contact.getEmailAddress()),
+                    'pubpref': contact.getPublicationPreference()}
         return data
 
     def _client_data(self, ar):
-        data = {'obj': None,
-                'id': '',
-                'url': '',
-                'name': '',
-                'phone': '',
-                'fax': '',
-                'address': '',}
-
+        data = {}
         client = ar.aq_parent
         if client:
             data['obj'] = client
@@ -204,5 +228,123 @@ class AnalysisRequestPublishView(BrowserView):
             else:
                 client_address = ''
             data['address'] = to_utf8(client_address)
+        return data
+
+    def _specs_data(self, ar):
+        data = {}
+        specs = ar.getPublicationSpecification()
+        if not specs:
+            specs = ar.getSpecification()
+
+        if specs:
+            data['obj'] = specs
+            data['id'] = specs.id
+            data['url'] = specs.absolute_url()
+            data['title'] = to_utf8(specs.Title())
+            data['resultsrange'] = specs.getResultsRangeDict()
+
+        return data
+
+    def _analyses_data(self, ar, analysis_states=['verified', 'published']):
+        analyses = []
+        batch = ar.getBatch()
+        workflow = getToolByName(self.context, 'portal_workflow')
+        for an in ar.getAnalyses(full_objects=True,
+                                 review_state=analysis_states):
+
+            # Build the analysis-specific dict
+            andict = self._analysis_data(an)
+
+            # Are there previous results for the same AS and batch?
+            andict['previous'] = []
+            andict['previous_results'] = ""
+            if batch:
+                keyword = an.getKeyword()
+                bars = [bar for bar in batch.getAnalysisRequests() \
+                            if an.aq_parent.UID() != bar.UID() \
+                            and keyword in bar]
+                for bar in bars:
+                    pan = bar[keyword]
+                    pan_state = workflow.getInfoFor(pan, 'review_state')
+                    if pan.getResult() and pan_state in analysis_states:
+                        pandict = self._analysis_data(pan)
+                        andict['previous'].append(pandict)
+
+                andict['previous'] = sorted(andict['previous'], key=itemgetter("capture_date"))
+                andict['previous_results'] = ", ".join([p['formatted_result'] for p in andict['previous'][-5:]])
+
+            analyses.append(andict)
+        analyses.sort(lambda x, y: cmp(x.get('title').lower(), y.get('title').lower()))
+        return analyses
+
+    def _analysis_data(self, analysis):
+        keyword = analysis.getKeyword()
+        service = analysis.getService()
+        andict = {'obj': analysis,
+                  'id': analysis.id,
+                  'title': analysis.Title(),
+                  'keyword': keyword,
+                  'accredited': service.getAccredited(),
+                  'point_of_capture': to_utf8(POINTS_OF_CAPTURE.getValue(service.getPointOfCapture())),
+                  'category': to_utf8(service.getCategoryTitle()),
+                  'result': analysis.getResult(),
+                  'unit': to_utf8(service.getUnit()),
+                  'capture_date': analysis.getResultCaptureDate(),
+                  'request_id': analysis.aq_parent.getId(),
+                  'formatted_result': '',
+                  'uncertainty': analysis.getUncertainty(),
+                  'retested': analysis.getRetested(),
+                  'remarks': to_utf8(analysis.getRemarks()),
+                  'resultdm': to_utf8(analysis.getResultDM()),
+                  'outofrange': False,
+                  'specs': {},
+                  'formatted_specs': ''}
+
+        # Which analysis specs must be used?
+        # Try first with those defined at AR Publish Specs level
+        ar = analysis.aq_parent
+        specs = ar.getPublicationSpecification()
+        if not specs or keyword not in specs.getResultsRangeDict():
+            specs = analysis.getAnalysisSpecs()
+
+        specs = specs.getResultsRangeDict().get(keyword, {}) \
+                if specs else {}
+
+        andict['specs'] = specs
+        andict['formatted_result'] = analysis.getFormattedResult(specs)
+
+        if specs.get('min', None) and specs.get('max', None):
+            andict['formatted_specs'] = '%s - %s' % (specs['min'], specs['max'])
+        elif specs.get('min', None):
+            andict['formatted_specs'] = '> %s' % specs['min']
+        elif specs.get('max', None):
+            andict['formatted_specs'] = '< %s' % specs['max']
+
+        # Out of range?
+        if specs:
+            adapters = getAdapters((analysis, ), IResultOutOfRange)
+            bsc = getToolByName(self.context, "bika_setup_catalog")
+            for name, adapter in adapters:
+                ret = adapter(specification=specs)
+                if ret and ret['out_of_range']:
+                    andict['outofrange'] = True
+                    break
+        return andict
+
+    def _reporter_data(self, ar):
+        data = {}
+        member = self.context.portal_membership.getAuthenticatedMember()
+        if member:
+            username = member.getUserName()
+            data['username'] = username
+            data['fullname'] = to_utf8(self.user_fullname(username))
+            data['email'] = to_utf8(self.user_email(username))
+
+            c = [x for x in self.bika_setup_catalog(portal_type='LabContact')
+                 if x.getObject().getUsername() == username]
+            if c:
+                sf = c[0].getObject().getSignature()
+                if sf:
+                    data['signature'] = sf.absolute_url() + "/Signature"
 
         return data
