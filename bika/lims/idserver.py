e08246df64b823567190fb40a6d65bb0b6c19335
Campbell <campbell@bikalabs.com>
2014-08-05 11:09:11 +0200

    IDServer: IIDNormalizer for new AR and Sample IDs.

--- bika/lims/idserver.py
+++ bika/lims/idserver.py
@@ -53,7 +53,7 @@ def generateUniqueId(context):
           used as a prefix instead.
     """
 
-    norm = getUtility(IIDNormalizer).normalize
+    normalize = getUtility(IIDNormalizer).normalize
     prefixes = context.bika_setup.getPrefixes()
 
     year = context.bika_setup.getYearInPrefix() and \
@@ -62,25 +62,29 @@ def generateUniqueId(context):
     # Analysis Request IDs
     if context.portal_type == "AnalysisRequest":
         sample = context.getSample()
-        s_prefix = sample.getSampleType().getPrefix()
+        s_prefix = normalize(sample.getSampleType().getPrefix())
         sample_padding = context.bika_setup.getSampleIDPadding()
         ar_padding = context.bika_setup.getARIDPadding()
         sample_id = sample.getId()
         sample_number = sample_id.split(s_prefix)[1]
         ar_number = sample.getLastARNumber()
         ar_number = ar_number and ar_number + 1 or 1
-        return "%s%s-R%s" % (s_prefix,
-                           str(sample_number).zfill(sample_padding),
-                           str(ar_number).zfill(ar_padding))
+        return normalize(
+            "%s%s-R%s" % (s_prefix,
+                          str(sample_number).zfill(sample_padding),
+                          str(ar_number).zfill(ar_padding))
+        )
 
     # Sample Partition IDs
     if context.portal_type == "SamplePartition":
-        matches = [p for p in prefixes if p['portal_type'] == 'SamplePartition']
-        prefix = matches and matches[0]['prefix'] or 'samplepartition'
-        padding = int(matches and matches[0]['padding'] or '0')
+        # We do not use prefixes.  There are actually codes that require the 'P'.
+        # matches = [p for p in prefixes if p['portal_type'] == 'SamplePartition']
+        # prefix = matches and matches[0]['prefix'] or 'samplepartition'
+        # padding = int(matches and matches[0]['padding'] or '0')
+
         # at this time the part exists, so +1 would be 1 too many
         partnr = str(len(context.aq_parent.objectValues('SamplePartition')))
-        return "%s-P%s" % (context.aq_parent.id, partnr)
+        return normalize("%s-P%s" % (context.aq_parent.id, partnr))
 
     if context.bika_setup.getExternalIDServer():
 
@@ -106,8 +110,8 @@ def generateUniqueId(context):
                 return '%s%s-%s' % (prefix, year, new_id)
         # no prefix; use portal_type
         # year is not inserted here
-        new_id = str(idserver_generate_id(context, norm(context.portal_type) + "-"))
-        return '%s-%s' % (norm(context.portal_type), new_id)
+        new_id = str(idserver_generate_id(context, normalize(context.portal_type) + "-"))
+        return '%s-%s' % (normalize(context.portal_type), new_id)
 
     else:
 
@@ -157,13 +161,14 @@ def generateUniqueId(context):
 
         # no prefix; use portal_type
         # no year inserted here
-        prefix = norm(context.portal_type);
+        prefix = normalize(context.portal_type);
         new_id = next_id(prefix)
         return '%s-%s' % (prefix, new_id)
 
 def renameAfterCreation(obj):
+    normalize = getUtility(IIDNormalizer).normalize
     # Can't rename without a subtransaction commit when using portal_factory
     transaction.savepoint(optimistic=True)
-    new_id = generateUniqueId(obj)
+    new_id = normalize(generateUniqueId(obj))
     obj.aq_inner.aq_parent.manage_renameObject(obj.id, new_id)
     return new_id
