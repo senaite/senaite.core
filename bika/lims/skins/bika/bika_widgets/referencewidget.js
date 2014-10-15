senaite/senaite.core

commit ebee5009fa506d2ba72a11c2b1463a84c7b879d5

Now able to unselect selected clients from batch/edit

bika/lims/skins/bika/bika_widgets/referencewidget.js
@@ -13,13 +13,14 @@ $(document).ready(function(){
         $(this).parent().remove();
     });
 
-    // handle custom event "unselected" from jquery.ui.combogrid
-    $(".ArchetypesReferenceWidget").live("unselected", function(){
+    $(".ArchetypesReferenceWidget").bind("selected blur change", function(){
         var e = $(this).children("input.referencewidget");
-        fieldName = $(e).attr("name").split(":")[0];
-        $(e).attr("uid", "");
-        $("input[name^='"+fieldName+"_uid']").val("");
-        $("div[name='"+fieldName+"-listing']").empty();
+        if (e.val() == '') {
+            fieldName = $(e).attr("name").split(":")[0];
+            $(e).attr("uid", "");
+            $("input[name^='"+fieldName+"_uid']").val("");
+            $("div[name='"+fieldName+"-listing']").empty();
+        }
     });
 
 });

commit 26abef5932d221d63fe066c9a7123f0525904796

Fix for ebee500 mistakenly clearing field values

bika/lims/skins/bika/bika_widgets/referencewidget.js
@@ -13,15 +13,14 @@ $(document).ready(function(){
         $(this).parent().remove();
     });
 
-    $(".ArchetypesReferenceWidget").bind("selected blur change", function(){
-        var e = $(this).children("input.referencewidget");
-        if (e.val() == '') {
-            fieldName = $(e).attr("name").split(":")[0];
-            $(e).attr("uid", "");
-            $("input[name^='"+fieldName+"_uid']").val("");
-            $("div[name='"+fieldName+"-listing']").empty();
-        }
-    });
+		$(".ArchetypesReferenceWidget").bind("change", function(){
+			var e = $(this).children("input.referencewidget");
+			if (e.val() == '') {
+				fieldName = $(e).attr("name").split(":")[0];
+				$(e).attr("uid", "");
+				$("input[name^='"+fieldName+"_uid']").val("");
+			}
+		});
 
 });
 