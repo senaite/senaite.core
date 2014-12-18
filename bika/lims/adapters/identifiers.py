d3179c2be1de5116e1d6756136ebd45506dbafc9
Campbell <campbell@bikalabs.com>
2014-12-18 22:41:59 +0200

    Fix 'Identifiers' KeywordIndex
    Indexes Title and Identifier for each additional identifier for all
    objects in IBikaCatalog and IBikaSetupCatalog

--- bika/lims/adapters/identifiers.py
+++ bika/lims/adapters/identifiers.py
@@ -3,14 +3,38 @@ import json
 
 from archetypes.schemaextender.interfaces import IOrderableSchemaExtender, \
     ISchemaModifier
-from bika.lims import bikaMessageFactory as _
+from bika.lims import bikaMessageFactory as _, to_utf8
 from bika.lims.browser import BrowserView
 from bika.lims.browser.widgets import RecordsWidget
 from bika.lims.fields import ExtRecordsField
-from bika.lims.interfaces import IHaveIdentifiers
+from bika.lims.interfaces import IHaveIdentifiers, IBikaCatalog, \
+    IBikaSetupCatalog
+from plone.indexer import indexer
 from zope.component import adapts
 import plone
 from zope.interface import implements
+
+
+@indexer(IHaveIdentifiers, IBikaCatalog)
+def IBikaCatalogIdentifiersIndexer(instance):
+    try:
+        Identifiers = instance.Schema()['Identifiers'].get(instance)
+    except:
+        return None
+    if Identifiers:
+        return [to_utf8(i['Identifier']) for i in Identifiers] + \
+               [to_utf8(i['Title']) for i in Identifiers]
+
+
+@indexer(IHaveIdentifiers, IBikaSetupCatalog)
+def IBikaSetupCatalogIdentifiersIndexer(instance):
+    try:
+        Identifiers = instance.Schema()['Identifiers'].get(instance)
+    except:
+        return None
+    if Identifiers:
+        return [to_utf8(i['Identifier']) for i in Identifiers] + \
+               [to_utf8(i['Title']) for i in Identifiers]
 
 
 Identifiers = ExtRecordsField(
@@ -32,15 +56,18 @@ Identifiers = ExtRecordsField(
                     'Description': 32},
     widget=RecordsWidget(
         label=_('Identifiers for this object'),
-        description=_("Select identifiers by which this object can be referenced."),
+        description=_(
+            "Select identifiers by which this object can be referenced."),
         visible={'view': 'visible',
                  'edit': 'visible'},
         combogrid_options={
             'IdentifierType': {
                 'colModel': [
                     {'columnName': 'identifiertype_uid', 'hidden': True},
-                    {'columnName': 'IdentifierType', 'width': '30', 'label': _('Identifier Type')},
-                    {'columnName': 'Description', 'width': '70', 'label': _('Description')}
+                    {'columnName': 'IdentifierType', 'width': '30',
+                     'label': _('Identifier Type')},
+                    {'columnName': 'Description', 'width': '70',
+                     'label': _('Description')}
                 ],
                 'url': 'getidentifiertypes',
                 'showOn': True,
@@ -88,9 +115,11 @@ class IHaveIdentifiersSchemaModifier(object):
         schema.moveField('Identifiers', after='description')
         return schema
 
+
 class ajaxGetIdentifierTypes(BrowserView):
     """ Identifier types vocabulary source for jquery combo dropdown box
     """
+
     def __call__(self):
         plone.protect.CheckAuthenticator(self.request)
         searchTerm = 'searchTerm' in self.request and self.request[
@@ -121,5 +150,6 @@ class ajaxGetIdentifierTypes(BrowserView):
         ret = {'page': page,
                'total': pages,
                'records': len(rows),
-               'rows': rows[(int(page) - 1) * int(nr_rows): int(page) * int(nr_rows)]}
+               'rows': rows[(int(page) - 1) * int(nr_rows): int(page) * int(
+                   nr_rows)]}
         return json.dumps(ret)
