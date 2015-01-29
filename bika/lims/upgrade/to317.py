a138c68f18c49120c645278dd341c30bdd38fdcb
Jordi Puiggené <jpuiggene@naralabs.com>
2015-01-29 11:01:57 +0100

    LIMS-1520 Allow to invalidate verified ARs

--- bika/lims/upgrade/to317.py
+++ bika/lims/upgrade/to317.py
@@ -9,5 +9,8 @@ def upgrade(tool):
     """
     Upgrade step required for Bika LIMS 3.1.7
     """
+    portal = aq_parent(aq_inner(tool))
+    setup = portal.portal_setup
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'workflow-csv')
 
     return True
