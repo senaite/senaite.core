741ddb6c072aa2dcd01dd305c2591fffa647372e
Pau Soliva <psoliva@naralabs.com>
2015-02-23 16:27:10 +0100

    import done, parser pending

--- bika/lims/exportimport/instruments/beckmancoulter/access/__init__.py
+++ bika/lims/exportimport/instruments/beckmancoulter/access/__init__.py
@@ -1,1 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+""" Beckman Couter Access
+"""
+from bika.lims.exportimport.instruments.resultsimport import \
+    AnalysisResultsImporter, InstrumentCSVResultsFileParser
+
+class BeckmancoulterAccessCSVParser(InstrumentCSVResultsFileParser):
+    def __init__(self, csv):
+        InstrumentCSVResultsFileParser.__init__(self, csv)
+        self._columns = {} #The diferents data columns names
+        self._linedata = {}#The line with the data
+        self._rownum = None
+        self._isFirst = True #Used to know if is the first linedata
+
+
+
+class BeckmancoulterAccessImporter(AnalysisResultsImporter):
+    def __init__(self, parser, context, idsearchcriteria, override,
+                 allowed_ar_states=None, allowed_analysis_states=None,
+                 instrument_uid=None):
+        AnalysisResultsImporter.__init__(self, parser, context,
+                                         idsearchcriteria, override,
+                                         allowed_ar_states,
+                                         allowed_analysis_states,
+                                         instrument_uid)
