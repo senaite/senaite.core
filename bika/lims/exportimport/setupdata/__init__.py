81e65560283a902b2e4c0a220f1251f83cca27dc
Pau Soliva <psoliva@naralabs.com>
2015-04-14 18:38:03 +0200

    Added the 'none' sperator option.

--- bika/lims/exportimport/setupdata/__init__.py
+++ bika/lims/exportimport/setupdata/__init__.py
@@ -1547,6 +1547,9 @@ class ID_Prefixes(WorksheetImporter):
             # remove existing prefix from list
             prefixes = [p for p in prefixes
                         if p['portal_type'] != row['portal_type']]
+            # The spreadsheet will contain 'none' for user's visual stuff, but it means 'no separator'
+            if row['separator'] == 'none':
+                row['separator'] = ''
             # add new prefix to list
             prefixes.append({'portal_type': row['portal_type'],
                              'padding': row['padding'],
