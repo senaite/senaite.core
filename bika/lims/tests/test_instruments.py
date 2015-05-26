9198791872973628154a9c2f2cd9ecce05861be6
Pau Soliva <psoliva@naralabs.com>
2015-05-26 11:25:12 +0200

    test for instrument warnings

--- bika/lims/tests/test_instruments.py
+++ bika/lims/tests/test_instruments.py
@@ -1,10 +1,11 @@
-from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import _createObjectByType
+from bika.lims.utils import tmpID
 from bika.lims.testing import BIKA_FUNCTIONAL_TESTING
 from bika.lims.tests.base import BikaFunctionalTestCase
+from bika.lims.idserver import renameAfterCreation
 from plone.app.testing import login, logout
 from plone.app.testing import TEST_USER_NAME
 from datetime import date
-from DateTime import DateTime
 import unittest
 
 try:
@@ -22,57 +23,82 @@ class TestInstrumentAlerts(BikaFunctionalTestCase):
 
     def test_instrument_validation(self):
         # Getting all instruments
-        bsc = getToolByName(self, 'bika_setup_catalog')
-        instruments = bsc(portal_type='Instrument', inactive_state='active')
+        instrument_names = self.portal.bika_setup.bika_instruments.keys()
         # Setting validation dates
-        for instrument in instruments:
+        for instrument_name in instrument_names:
+            instrument = self.portal.bika_setup.bika_instruments[instrument_name]
             today = date.today()
-            instrument.setDownTo(today)
-            instrument.setDownFrom(today)
-        try:
-            for instrument in instruments:
-                if not(instrument.isValidationInProgress()):
-                    raise ValidationError('Instrument should be under validation progress')
-        except ValidationError as e:
-            print e.value
+            # Getting last valid validation
+            lastval = instrument.getLatestValidValidation()
+            if not lastval:
+                #  Creating a new validation
+                cal_obj = _createObjectByType("InstrumentValidation", instrument, tmpID())
+                cal_obj.edit(
+                    title='test',
+                    DownFrom=today.strftime("%Y/%m/%d"),
+                    DownTo=today.strftime("%Y/%m/%d"),
+                    Instrument=instrument
+                )
+                cal_obj.unmarkCreationFlag()
+                renameAfterCreation(cal_obj)
+            else:
+                #  Updating last validation
+                lastval.setDownTo(today)
+                lastval.setDownFrom(today)
 
-        for instrument in instruments:
-            anotherday = DateTime('2014/11/27')
-            instrument.setDownTo(anotherday)
-            instrument.setDownFrom(anotherday)
-        try:
-            for instrument in instruments:
-                if instrument.isValidationInProgress():
-                    raise ValidationError('Instrument should not be under validation progress')
-        except ValidationError as e:
-            print e.value
+        for instrument_name in instrument_names:
+            instrument = self.portal.bika_setup.bika_instruments[instrument_name]
+            self.assertTrue(instrument.isValidationInProgress())
+
+        for instrument_name in instrument_names:
+            instrument = self.portal.bika_setup.bika_instruments[instrument_name]
+            anotherday = '2014/11/27'
+            lastval = instrument.getLatestValidValidation()
+            lastval.setDownTo(anotherday)
+            lastval.setDownFrom(anotherday)
+        for instrument_name in instrument_names:
+            instrument = self.portal.bika_setup.bika_instruments[instrument_name]
+            self.assertFalse(instrument.isValidationInProgress())
 
     def test_instrument_calibration(self):
         # Getting all instruments
-        bsc = getToolByName(self, 'bika_setup_catalog')
-        instruments = bsc(portal_type='Instrument', inactive_state='active')
+        instrument_names = self.portal.bika_setup.bika_instruments.keys()
         # Setting calibration dates
-        for instrument in instruments:
+        for instrument_name in instrument_names:
+            # Getting each instrument
+            instrument = self.portal.bika_setup.bika_instruments[instrument_name]
             today = date.today()
-            instrument.setDownTo(today)
-            instrument.setDownFrom(today)
-        try:
-            for instrument in instruments:
-                if not(instrument.isCalibrationInProgress()):
-                    raise CalibrationError('Instrument should be under calibration progress')
-        except CalibrationError as e:
-            print e.value
+            # Getting last valid calibration
+            lastcal = instrument.getLatestValidCalibration()
+            if not lastcal:
+                #  Creating a new calibration
+                cal_obj = _createObjectByType("InstrumentCalibration", instrument, tmpID())
+                cal_obj.edit(
+                    title='test',
+                    DownFrom=today.strftime("%Y/%m/%d"),
+                    DownTo=today.strftime("%Y/%m/%d"),
+                    Instrument=instrument
+                )
+                cal_obj.unmarkCreationFlag()
+                renameAfterCreation(cal_obj)
+            else:
+                #  Updating last calibration
+                lastcal.setDownTo(today)
+                lastcal.setDownFrom(today)
+        #  Testing calibration state
+        for instrument_name in instrument_names:
+            instrument = self.portal.bika_setup.bika_instruments[instrument_name]
+            self.assertTrue(instrument.isCalibrationInProgress())
 
-        for instrument in instruments:
-            anotherday = DateTime('2014/11/27')
-            instrument.setDownTo(anotherday)
-            instrument.setDownFrom(anotherday)
-        try:
-            for instrument in instruments:
-                if instrument.isCalibrationInProgress():
-                    raise CalibrationError('Instrument should not be under calibration progress')
-        except CalibrationError as e:
-            print e.value
+        for instrument_name in instrument_names:
+            instrument = self.portal.bika_setup.bika_instruments[instrument_name]
+            anotherday = '2014/11/27'
+            lastcal = instrument.getLatestValidCalibration()
+            lastcal.setDownTo(anotherday)
+            lastcal.setDownFrom(anotherday)
+        for instrument_name in instrument_names:
+            instrument = self.portal.bika_setup.bika_instruments[instrument_name]
+            self.assertFalse(instrument.isCalibrationInProgress())
 
     def tearDown(self):
         logout()
