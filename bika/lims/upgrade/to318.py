e5651f00272bb2e12618dd011b6c406693c9fa0d
Jordi Puiggené <jpuiggene@naralabs.com>
2015-05-28 09:40:24 +0200

    Log upgrade step info

--- bika/lims/upgrade/to318.py
+++ bika/lims/upgrade/to318.py
@@ -2,13 +2,16 @@ from Acquisition import aq_inner
 from Acquisition import aq_parent
 from Products.Archetypes.BaseContent import BaseContent
 from bika.lims.upgrade import stub
-
+from bika.lims import logger
 
 def upgrade(tool):
     """Upgrade step required for Bika LIMS 3.1.8
     """
     portal = aq_parent(aq_inner(tool))
     setup = portal.portal_setup
+    qi = portal.portal_quickinstaller
+    ufrom = qi.upgradeInfo('bika.lims')['installedVersion']
+    logger.info("Upgrading Bika LIMS: %s -> %s" % (ufrom, '318'))
 
     # Reread typeinfo to update/add the modified/added types
     setup.runImportStepFromProfile('profile-bika.lims:default', 'typeinfo')
