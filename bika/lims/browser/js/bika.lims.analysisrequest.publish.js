f62339d58a131ec4064b36b54e2cd2a83a2b6366
Jordi Puiggené <jpuiggene@naralabs.com>
2015-06-01 21:26:57 +0200

    First approach

--- bika/lims/browser/js/bika.lims.analysisrequest.publish.js
+++ bika/lims/browser/js/bika.lims.analysisrequest.publish.js
@@ -10,10 +10,26 @@ function AnalysisRequestPublishView() {
 
     var referrer_cookie_name = '_arpv';
 
+    var layouts_size = {layout_a4: 'A4', layout_letter: 'letter'};
+
+    var layouts_mm = {layout_a4:     [210, 297],
+                      layout_letter: [216, 279]};
+
+    var layouts_margin = {layout_a4:     [15, 15, 20, 15],
+                          layout_letter: [15, 15, 20, 15]};
+    var header_height = 0;
+    var footer_height = 0;
+    var headerhtml = '';
+    var footerhtml = '';
+    var current_layout = 'layout_a4';
+
+
     /**
      * Entry-point method for AnalysisRequestPublishView
      */
     that.load = function() {
+
+        load_barcodes();
 
         // Store referrer in cookie in case it is lost due to a page reload
         var backurl = document.referrer;
@@ -59,6 +75,13 @@ function AnalysisRequestPublishView() {
             reloadReport();
         });
 
+        $('#sel_layout').change(function(e) {
+            $('body').removeClass($('body').attr('data-layout'));
+            $('body').attr('data-layout', $(this).val());
+            $('body').addClass($(this).val());
+            reloadReport();
+        });
+
         $('#publish_button').click(function(e) {
             var url = window.location.href;
             var qcvisible = $('#qcvisible').is(':checked') ? 1 : 0;
@@ -94,8 +117,7 @@ function AnalysisRequestPublishView() {
             location.href=backurl;
         });
 
-        load_barcodes();
-        load_pagination();
+        //load_pagination();
 
         var invalidbackurl = window.portal_url + '/++resource++bika.lims.images/report_invalid_back.png';
         $('.ar-invalid').css('background', "url('"+invalidbackurl+"') repeat scroll #ffffff");
@@ -107,7 +129,7 @@ function AnalysisRequestPublishView() {
             e.preventDefault();
             $('#sel_format_info_pane').toggle();
         });
-
+        reloadReport();
     }
 
     function get(name){
@@ -131,6 +153,15 @@ function AnalysisRequestPublishView() {
     }
 
     function load_pagination() {
+        $(".paginated-report").each(function(i) {
+            var layout = layouts[$('#sel_layout').val()];
+            if ($('.page-footer').length > 0) {
+                var hpf = $('.page-footer').css('margin-top',0).height();
+            } else {
+                $(this).css('height', layout[1]);
+            }
+        });
+
         $(".paginated-report").each(function(i) {
             var numpages = $(this).find('.page-footer').length;
             $(this).find('.page-total-count').html(numpages);
@@ -163,7 +194,159 @@ function AnalysisRequestPublishView() {
             $('#report').html(htmldata);
             $('#report').fadeTo('fast', 1);
             load_barcodes();
-            load_pagination();
+            load_layout();
         });
     }
+
+    function load_layout() {
+        // Store footer and header heights for further pagination and
+        // page layout changes.
+        header_height = 0;
+        if ($('.page-header').length > 0) {
+            var phe = $('.page-header').first();
+            header_height = $(phe).outerHeight();
+            header_height += parseFloat($(phe).css('marginTop'));
+            header_height += parseFloat($(phe).css('marginBottom'));
+        }
+        footer_height = 0;
+        if ($('.page-footer').length > 0) {
+            var pfe = $('.page-footer').first();
+            footer_height = $(pfe).outerHeight();
+            footer_height += parseFloat($(pfe).css('marginTop'));
+            footer_height += parseFloat($(pfe).css('marginBottom'));
+        }
+        // Remove page-header and page-footer. load_pagination() will do
+        // it dynamically later.
+        headerhtml = $('.page-header').length > 0 ? $('.page-header').first().html() : "";
+        footerhtml = $('.page-footer').length > 0 ? $('.page-footer').first().html(): "";
+        $('.page-header').remove();
+        $('.page-footer').remove();
+
+        currentlayout = $('#sel_layout').val();
+        var layout_style = '@page { size: '+layouts_size[currentlayout]+' !important;';
+        layout_style += 'width: '+layouts_mm[currentlayout][0]+'mm !important;'
+        layout_style += 'height: '+layouts_mm[currentlayout][1]+'mm !important;'
+        layout_style += 'margin: '+layouts_margin[currentlayout][0]+'mm ';
+        layout_style +=  layouts_margin[currentlayout][1]+'mm ';
+        layout_style +=  layouts_margin[currentlayout][2]+'mm ';
+        layout_style +=  layouts_margin[currentlayout][3]+'mm !important; }';
+        $('#layout-style').html(layout_style);
+
+        // Calculate pagination according to the selected Layout.
+        // DIN-A4, Letter US Size, etc.
+        // Iterate for each report body and apply the dimensions
+        var footadded = false;
+        var prevarid = '';
+        var prevheight = 0;
+        var layheight = parseFloat(mmTopx(layouts_mm[currentlayout][1]));
+        layheight -= parseFloat(mmTopx(layouts_margin[currentlayout][0]));
+        layheight -= parseFloat(mmTopx(layouts_margin[currentlayout][2]));
+        var maxheight = parseFloat(layheight) - parseFloat(footer_height) - parseFloat(header_height);
+        console.log(layheight+" : "+maxheight);
+        $('div.ar_publish_body > div').each(function(i) {
+            var arbody = $(this).closest('div.ar_publish_body');
+            var id_ar = $(arbody).attr('id');
+            if (id_ar != prevarid) {
+                // New AR report, new page
+                if (prevarid != '') {
+                    $("<div class='page-break'></div><div class='page-header'>"+headerhtml+"</div>").prependTo($(arbody));
+                } else {
+                    $("<div class='page-header'>"+headerhtml+"</div>").prependTo($(arbody));
+                }
+                var marginTop = parseFloat($(arbody).css("marginTop"));
+                var marginBot = parseFloat($(arbody).css("marginBottom"));
+                prevheight = header_height + marginTop + marginBot;
+                prevarid = id_ar;
+            }
+            var elheight = parseFloat($(this).outerHeight()) + parseFloat($(this).css('marginTop')) + parseFloat($(this).css('marginBottom'));
+            var currheight = parseFloat(prevheight) + elheight;
+            console.log($(this).attr('id')+": "+$(this).outerHeight()+" -> "+currheight);
+            if (currheight > maxheight || $(this).hasClass('manual-page-break')) {
+                // Add page-break, hader and footer
+                var margin = maxheight - prevheight;
+                $("<div class='page-footer' style='padding-top:"+pxTomm(margin)+"mm'>"+footerhtml+"</div>").insertBefore($(this));
+                $("<div class='page-break'></div><div class='page-header'>"+headerhtml+"</div>").insertBefore($(this));
+                console.log("---> "+ prevheight +" + " + margin + " + "+ footer_height + " = " +(prevheight + margin + footer_height));
+                prevheight = elheight;
+                footadded = true;
+            } else {
+                prevheight = currheight;
+            }
+        });
+        if (footadded == false) {
+            var margin = 0;//maxheight-prevheight;
+            $("<div class='page-footer' style='padding-top:"+margin+"px'>"+footerhtml+"</div>").appendTo($('div.ar_publish_body').last());
+        }
+        $('.manual-page-break').hide();
+
+        // Page numbering
+        $('div.ar_publish_body').each(function(i) {
+                console.log($(this).attr('id')+": "+$(this).first().outerHeight());
+            var pagesnum = $(this).find('div.page-footer').length;
+            $(this).find('.page-total-count').html(pagesnum);
+            var currnum = 1;
+            $(this).find('div.page-footer .page-current-num').each(function(j) {
+                $(this).html(currnum);
+                currnum += 1;
+            });
+            var currnum = 1;
+            $(this).find('div.page-header .page-current-num').each(function(j) {
+                $(this).html(currnum);
+                currnum += 1;
+            });
+        });
+    }
+
+    function paginate_old(element, reportheight, prevheight) {
+        var baseheight = reportheight - prevheight;
+        var repheight = header_height + baseheight + footer_height;
+        var layheight = mmTopx(layouts_mm[currentlayout][1]);
+        console.info(repheight + " > " + layheight);
+        var rhtml = '';//'<hr/>'+$('.runtime').html();
+        $('.runtime').html('<p>'+repheight+'</p>'+$(element).html()+rhtml);
+        setTimeout(function() {
+        if (repheight > layheight) {
+            //console.warn("Needs pagination: "+repheight+ " > "+layheight);
+            var next = null;
+            if ($(element).hasClass('ar_publish_body')) {
+                next = $(element).children().last();
+                paginate($(next), reportheight, $(next).height());
+            } else if ($(element).prev().length > 0) {
+                next = $(element).prev();
+                paginate($(next), reportheight, $(next).height() + prevheight);
+            }
+            /*
+            if ($(element).children().length > 0) {
+                var last = $(element).children().last();
+                paginate($(last), reportheight, $(last).height());
+            } else {
+                parent = $(element).parent();
+                if ($(parent).hasClass('ar_publish_body')) {
+                    console.log("End of iterations. Cannot paginate");
+                    $('.runtime').html('<p>End of iterations. Cannot paginate</p>'+$(element).html()+rhtml);
+                } else if ($(parent).prev().length > 0) {
+                    var prev = $(parent).prev();
+                    paginate($(prev), reportheight, $(element).height() + $(prev).height());
+                } else {
+                    console.log("First item reached. Cannot paginate");
+                    $('.runtime').html('<p>First item reached. Cannot paginate</p>'+$(element).html()+rhtml);
+                }
+            }*/
+        } else if ($(element).hasClass('ar_publish_body')) {
+            $("<div class='page-break-before'></div><div class='page-header'>"+headerhtml+"</div>").prependTo($(element));
+            $("<div class='page-footer'>"+footerhtml+"</div><div class='page-break-after'></div>").appendTo($(element));
+        } else {
+            $("<div class='page-break-before'></div><div class='page-header'>"+headerhtml+"</div>").insertBefore($(element));
+            $("<div class='page-footer'>"+footerhtml+"</div><div class='page-break-after'></div>").insertAfter($(element));
+        }
+        }, 1000);
+    }
+
+
 }
+var mmTopx = function(mm) {
+    return Math.floor(mm*$('#my_mm').height());
+}
+var pxTomm = function(px){
+    return Math.floor(px/$('#my_mm').height());
+};
