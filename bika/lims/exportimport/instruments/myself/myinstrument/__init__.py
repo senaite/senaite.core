4c256da8a463e86c270eb25e0761c5bf26f93ced
Jordi Puiggené <jpuiggene@naralabs.com>
2015-11-12 17:08:17 -0800

    Myself.Myinstrument instrument (instrument interface creation example)

---
+++ bika/lims/exportimport/instruments/myself/myinstrument/__init__.py
@@ -0,0 +1,24 @@
+from bika.lims.exportimport.instruments.resultsimport import InstrumentCSVResultsFileParser
+from bika.lims.exportimport.instruments.instrument import GenericImport, getResultsInputFile
+
+title = "My Instrument"
+
+class MyInstrumentCSVParser(InstrumentCSVResultsFileParser):
+
+    def __init__(self, csv):
+        InstrumentCSVResultsFileParser.__init__(self, csv)
+
+    def _parseline(self, line):
+        if not line.startswith("SampleID"):
+            sline = self.splitLine(line)
+            raw = {sline[1]: {'DefaultResult': 'result',
+                              'result': sline[2],
+                              'DateTime': sline[3]}}
+            self._addRawResult(sline[0], raw)
+            return 0
+
+def Import(context, request):
+    infile = getResultsInputFile(request)
+    parser = MyInstrumentCSVParser(infile)
+    res = GenericImport(context, request, parser)
+    return res
