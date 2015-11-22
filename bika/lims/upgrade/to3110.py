b3d5750a5a189a06f5a3bbdc90d671fe543c3625
Campbell <campbell@bikalabs.com>
2015-11-22 19:34:02 +0200

    LIMS-2124: Add missing to3110 import step.

---
+++ bika/lims/upgrade/to3110.py
@@ -0,0 +1,39 @@
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from Products.CMFCore.utils import getToolByName
+from bika.lims.permissions import AddMultifile
+from Products.Archetypes.BaseContent import BaseContent
+from bika.lims.upgrade import stub
+from bika.lims import logger
+
+
+def upgrade(tool):
+    """Upgrade step required for Bika LIMS 3.1.10
+    """
+    portal = aq_parent(aq_inner(tool))
+    setup = portal.portal_setup
+    # Updated profile steps
+    # list of the generic setup import step names: portal.portal_setup.getSortedImportSteps() <---
+    # if you want more metadata use this: portal.portal_setup.getImportStepMetadata('jsregistry') <---
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'typeinfo')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'jsregistry')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'cssregistry')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'workflow-csv')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'factorytool')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'controlpanel')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'catalog')
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'propertiestool')
+    # important info about upgrade steps in
+    # http://stackoverflow.com/questions/7821498/is-there-a-good-reference-list-for-the-names-of-the-genericsetup-import-steps
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'skins')
+    # Update workflow permissions
+    wf = getToolByName(portal, 'portal_workflow')
+    wf.updateRoleMappings()
+
+    qi = portal.portal_quickinstaller
+    ufrom = qi.upgradeInfo('bika.lims')['installedVersion']
+    logger.info("Upgrading Bika LIMS: %s -> %s" % (ufrom, '319'))
+
+    # Migrations
+
+    return True
