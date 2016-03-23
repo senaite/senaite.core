9e28e58bd1777735d75ca9e517f7df97a6e9dfc1
Pau Soliva <psoliva@naralabs.com>
2016-03-23 19:06:28 +0100

    adding the action button

---
+++ bika/lims/browser/samplinground/print.py
@@ -0,0 +1,8 @@
+from bika.lims import bikaMessageFactory as _, t
+from bika.lims.browser import BrowserView
+
+
+class PrintForm(BrowserView):
+
+    def __call__(self):
+        import pdb; pdb.set_trace()
