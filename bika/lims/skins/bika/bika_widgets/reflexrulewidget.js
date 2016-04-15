f3caa0f909756cbcdf99b2a283b5c5770f3f7d84
Pau Soliva <psoliva@naralabs.com>
2016-04-15 12:54:14 +0200

    JS ony sets options for the first field

--- bika/lims/skins/bika/bika_widgets/reflexrulewidget.js
+++ bika/lims/skins/bika/bika_widgets/reflexrulewidget.js
@@ -1,13 +1,13 @@
 jQuery(function($){
     $(document).ready(function(){
         // This will be a global variable to be used everywhere
-        method_controller();
+        var method_analysis = $.parseJSON($('#methods-analysis').html());
+        method_controller(method_analysis);
         $('select#Method').bind("change", function () {
             method_controller();
         });
     });
-    var method_analysis = $.parseJSON($('#methods-analysis').html());
-    function method_controller(){
+    function method_controller(method_analysis){
         /**
         This function updates the options in the analysis service selectors
         according to the selected method
@@ -16,12 +16,12 @@ jQuery(function($){
         var ass = method_analysis[method].analysisservices;
         var ass_keys = method_analysis[method].as_keys;
         // Remove selection options
-        $('select[id="ReflexRules-analysisservice-0"]')
+        $('select[id^="ReflexRules-analysisservice-"]')
             .find("option").remove();
         // Create an option for each analysis service obtained for
         // the current method
         for (var i=0; ass_keys.length > i; i++){
-            $('select[id="ReflexRules-analysisservice-0"]').append(
+            $('select[id^="ReflexRules-analysisservice-"]').append(
                 '<option value="' + ass_keys[i] +
                 '">' + ass[ass_keys[i]].as_title + '</option>'
             );
