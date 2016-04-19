ce88122635653f5b65fc46271852189c81fdb3fd
Pau Soliva <psoliva@naralabs.com>
2016-04-19 18:48:24 +0200

    Last code javascript done. dictionary builder moved from field to widget recordswidget.js improved

--- bika/lims/browser/widgets/reflexrulewidget.py
+++ bika/lims/browser/widgets/reflexrulewidget.py
@@ -2,9 +2,11 @@ from AccessControl import ClassSecurityInfo
 from Products.Archetypes.Registry import registerWidget
 from Products.Archetypes.interfaces import IVocabulary
 from zope.interface import implements
+from Products.CMFCore.utils import getToolByName
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.vocabulary import SimpleTerm
 from bika.lims.browser.widgets import RecordsWidget
+import json
 
 try:
     from zope.component.hooks import getSite
@@ -32,6 +34,43 @@ class ReflexRuleWidget(RecordsWidget):
 
     security = ClassSecurityInfo()
 
+    def getReflexRuleSetup(self):
+        """
+        Return a json dict with all the setup data necessary to build the
+        relations:
+        - Relations between methods and analysis services options.
+        - The current saved data
+        """
+        relations = {}
+        # Getting all the methods from the system
+        pc = getToolByName(self, 'portal_catalog')
+        methods = [obj.getObject() for obj in pc(
+                    portal_type='Method',
+                    inactive_state='active')]
+        for method in methods:
+            # Get the analysis related to each method
+            br = method.getBackReferences('AnalysisServiceMethods')
+            analysiservices = {}
+            for analysiservice in br:
+                analysiservices[analysiservice.UID()] = {
+                    'as_id': analysiservice.id,
+                    'as_title': analysiservice.Title(),
+                }
+            # Make the json dict
+            relations[method.UID()] = {
+                'method_id': method.id,
+                'method_tile': method.Title(),
+                'analysisservices': analysiservices,
+                'as_keys': analysiservices.keys(),
+            }
+        # Get the data saved in the object
+        relations['saved_actions'] = {
+            'method_uid': self.aq_parent.getMethod().UID(),
+            'method_id': self.aq_parent.getMethod().id,
+            'method_tile': self.aq_parent.getMethod().Title(),
+            'actions': self.aq_parent.getReflexRules(),
+            }
+        return json.dumps(relations)
 
 registerWidget(
     ReflexRuleWidget,
