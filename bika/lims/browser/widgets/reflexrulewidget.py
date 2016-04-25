e5eec9fba36d7c4a96a16bfa59a6f8e69c3522c1
Pau Soliva <psoliva@naralabs.com>
2016-04-25 19:52:42 +0200

    Working on saving and getting the widget's data

--- bika/lims/browser/widgets/reflexrulewidget.py
+++ bika/lims/browser/widgets/reflexrulewidget.py
@@ -2,6 +2,7 @@ from AccessControl import ClassSecurityInfo
 from Products.Archetypes.Registry import registerWidget
 from Products.Archetypes.interfaces import IVocabulary
 from zope.interface import implements
+from Products.Archetypes.public import DisplayList
 from Products.CMFCore.utils import getToolByName
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.vocabulary import SimpleTerm
@@ -34,12 +35,137 @@ class ReflexRuleWidget(RecordsWidget):
 
     security = ClassSecurityInfo()
 
+    def process_form(self, instance, field, form, empty_marker=None,
+                     emptyReturnsMarker=False):
+        """
+        Gets the values from the ReflexRule section and returns them in the
+        correct way to be saved:
+        [
+            {'range1': 'X', 'range0': 'X', 'analysisservice': '<as_uid>', 'value': '',
+                'actions':[{'action':'<action_name>', 'act_row_idx':'X'},
+                          {'action':'<action_name>', 'act_row_idx':'X'}
+                    ]
+            },
+            ...
+        ]
+        """
+        if field.getName() != 'ReflexRules':
+            return RecordsWidget.process_form(
+                self, instance, field, form, empty_marker, emptyReturnsMarker)
+        raw_data = RecordsWidget.process_form(
+            self, instance, field, form, empty_marker, emptyReturnsMarker)
+        # 'value' is a list which will be saved
+        value = []
+        # Building the actions list
+        for action_row in raw_data[0]:
+            value.append(self._format_actions_row(action_row))
+        return value, {}
+
+    def _build_actions_list(self, action_row):
+        """
+        This function gets a raw action row with the following format:
+        {'analysisservice': '<as_uid>',
+        'value': '',
+        'range1': '3',
+        'range0': '1',
+        'action-1': 'replace',
+        'action-0': 'replace'}
+
+        and returns a formatted row like this one:
+        {'range1': '3', 'range0': '1', 'analysisservice': '<as_uid>', 'value': '',
+            'actions':[
+                {'action':'replace', 'act_row_idx':'0'},
+                {'action':'replace', 'act_row_idx':'1'}
+            ]
+        }
+        """
+        keys = action_row.keys()
+        # 'formatted_action_row' is the dict which will be added in the
+        # 'value' list
+        formatted_action_row = {}
+        # Filling the
+        # Filling the dict with the none action values
+        for key in keys:
+            if key.startswith('action-'):
+                pass
+            else:
+                formatted_action_row[key] = action_row[key]
+        # Adding the actions list to the final dictionary
+        formatted_action_row['actions'] = self._get_sorted_actions_list(
+            keys, action_row
+        )
+        return formatted_action_row
+
+    def _get_sorted_actions_list(self, keys_list, raw_dict):
+        """
+        This function takes advantatge of the yet sorted 'keys_list'
+        and returns a list of dictionaries with the actions from the raw_dict
+        following the keys_list order.
+        :keys_list: is a list with the keys starting with 'action-' in the
+            'raw_dict' sorted by its index.
+        :raw_dict: is the dict representing an actions set
+        """
+        actions_dicts_l = []
+        a_count = 0
+        # a_keys_list will contain the keys starting with 'action-' but sorted
+        # by their index
+        actions_list = self._get_sorted_action_keys(keys)
+        for key in actions_list:
+            # Building the action dict
+            action_dict = {
+                'action': raw_dict[key], 'act_row_idx': a_count}
+            # Saves the action as a new dict inside the actions list
+            actions_dicts_l.append(action_dict)
+            a_cont += 1
+        return actions_dicts_l
+
+    def _get_sorted_action_keys(self, keys_list):
+        """
+        This function returns only the elements starting with 'action-' in
+        'keys_list'. The returned list is sorted by the index appended to
+        the end of each element
+        """
+        action_list = []
+        for key in keys_list:
+            if key.startswith('action-'):
+                action_list.append(key)
+        return action_list.sort()
+
     def getReflexRuleSetup(self):
         """
         Return a json dict with all the setup data necessary to build the
         relations:
         - Relations between methods and analysis services options.
         - The current saved data
+        the functions returns:
+        {'<method_uid>': {'analysisservices': {'<as_uid>': {'as_id': '<as_id>',
+                                                            'as_title':'<as_title>'}
+                                               '<as_uid>': {'as_id': '<as_id>',
+                                                            'as_title':'<as_title>'}
+                                            },
+                          'as_keys': ['<as_uid>', '<as_uid>'],
+                          'method_id': '<method_id>',
+                          'method_tile': '<method_tile>'
+                          },
+        '<method_uid>': {'analysisservices': {'<as_uid>': {'as_id': '<as_id>',
+                                                            'as_title':'<as_title>'}
+                                               '<as_uid>': {'as_id': '<as_id>',
+                                                            'as_title':'<as_title>'}
+                                            },
+                          'as_keys': ['<as_uid>', '<as_uid>'],
+                          'method_id': '<method_id>',
+                          'method_tile': '<method_tile>'
+                          },
+         'saved_actions': {'rules': [{'analysisservice': '<as_uid>',
+                                        'range0': 'xx',
+                                        'range1': 'xx',
+                                        'value': ''
+                                    }],
+                           'method_id': '<method_uid>',
+                           'method_tile': '<method_tile>',
+                           'method_uid': '<method_uid>'
+                           }
+        }
         """
         relations = {}
         # Getting all the methods from the system
@@ -71,20 +197,56 @@ class ReflexRuleWidget(RecordsWidget):
             self.aq_parent.getMethod() else '',
             'method_tile': self.aq_parent.getMethod().Title() if
             self.aq_parent.getMethod() else '',
-            'actions': self.aq_parent.getReflexRules(),
+            'rules': self.aq_parent.getReflexRules(),
             }
         return json.dumps(relations)
 
-    def getReflexRuleValue(self, idx=0, element=''):
+    def getActionVoc(self):
         """
-        Return the expected value saved
-        :idx: it is a integer with the position of the reflex rules set in the
-        widget's list
-        :element: a string with the elemen's name to obtain
+        Return the different action available
+        """
+        return DisplayList(
+            [('replace', 'Remplace'), ('duplicate', 'Duplicate')])
+
+    def getReflexRuleElement(self, idx=0, element=''):
+        """
+        Returns the expected value saved in the object.
+        :idx: it is an integer with the position of the reflex rules set in the
+        widget's list.
+        :element: a string with the elemen's name to obtain:
+            'range0/1', 'actions', 'analysisservice',
+
+        The widget is going to return a list like this:
+        [
+            {'range1': 'X', 'range0': '1', 'analysisservice': '<as_uid>', 'value': '',
+                'actions':[{'action':'<action_name>', 'act_row_idx':'X'},
+                          {'action':'<action_name>', 'act_row_idx':'X'}
+                    ]
+            },
+            {'range1': 'X', 'range0': '1', 'analysisservice': '<as_uid>', 'value': '',
+                'actions':[{'action':'<action_name>', 'act_row_idx':'X'},
+                          {'action':'<action_name>', 'act_row_idx':'X'}
+                    ]
+            },
+            {'range1': 'X', 'range0': '1', 'analysisservice': '<as_uid>', 'value': '',
+                'actions':[{'action':'<action_name>', 'act_row_idx':'X'},
+                          {'action':'<action_name>', 'act_row_idx':'X'}
+                    ]
+            },
+        ]
+        - The list is the abstraction of the rules section in a Reflex
+        Rule obj.
+        - Each dictionary inside the list is an abstraction of a set of rules
+        binded to an analysis service.
+        - The 'action' element from the dictionary is a list (its order is
+        important) with dictionaries where each dict represents a simple
+        action.
+        - act_row_idx: it is used to know the position numeber of the action
+        inside the list.
         """
         rules_list = self.aq_parent.getReflexRules()
         if len(rules_list) > idx:
-            return self.aq_parent.getReflexRules()[idx].get(element, '')
+            return rules_list[idx].get(element, [{'action':'<action_name1>', 'act_row_idx':'0'},{'action':'<action_name>2', 'act_row_idx':'1'}])
         return ''
 
 registerWidget(
