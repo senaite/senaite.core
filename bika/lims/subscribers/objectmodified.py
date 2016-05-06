2ad14f9a2f517ff17d0ae177b00352ad0e7ea844
Campbell <campbell@bikalabs.com>
2016-05-06 09:44:11 +0200

    WINE-166: Synchronise Contact and Member email addresses

--- bika/lims/subscribers/objectmodified.py
+++ bika/lims/subscribers/objectmodified.py
@@ -41,9 +41,22 @@ def ObjectModifiedEventHandler(obj, event):
         mp('Access contents information', ['Manager', 'LabManager', 'Member', 'LabClerk', 'Analyst', 'Sampler', 'Preserver', 'Owner'], 0)
 
     elif obj.portal_type == 'Contact':
+        # Contacts need to be given "Owner" local-role on their Client.
         mp = obj.manage_permission
         mp(permissions.View, ['Manager', 'LabManager', 'LabClerk', 'Owner', 'Analyst', 'Sampler', 'Preserver'], 0)
-        mp(permissions.ModifyPortalContent, ['Manager', 'LabManager', 'Owner'], 0)
+        mp(permissions.ModifyPortalContent, ['Manager', 'LabManager', 'Owner', 'LabClerk'], 0)
+        mp(ManageLoginDetails, ['Manager', 'LabManager', 'LabClerk'], 0)
+        # Verify that the Contact details are the same as the Plone user.
+        contact_username = obj.Schema()['Username'].get(obj)
+        contact_email = obj.Schema()['EmailAddress'].get(obj)
+        contact_fullname = obj.Schema()['Fullname'].get(obj)
+        mt = getToolByName(obj, 'portal_membership')
+        member = mt.getMemberById(contact_username)
+        properties = {'username':contact_username,
+                      'email': contact_email,
+                      'fullname': contact_fullname}
+        member.setMemberProperties(properties)
+
     elif obj.portal_type == 'AnalysisCategory':
         for analysis in obj.getBackReferences('AnalysisServiceAnalysisCategory'):
             analysis.reindexObject(idxs=["getCategoryTitle", "getCategoryUID", ])
