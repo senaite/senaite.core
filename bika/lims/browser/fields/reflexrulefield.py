730d172377141ca1e68322ae2d8e608cbe098295
Pau Soliva <psoliva@naralabs.com>
2016-07-11 17:42:00 +0200

    simple test done

--- bika/lims/browser/fields/reflexrulefield.py
+++ bika/lims/browser/fields/reflexrulefield.py
@@ -3,6 +3,7 @@ from Products.ATExtensions.ateapi import RecordsField
 from Products.Archetypes.Registry import registerField
 from Products.Archetypes.interfaces import IVocabulary
 from Products.Archetypes.public import DisplayList
+from Products.CMFCore.utils import getToolByName
 from zope.interface import implements
 from bika.lims import bikaMessageFactory as _
 from bika.lims.utils import t
@@ -26,71 +27,6 @@ class ReflexRuleField(RecordsField):
         'subfield_validators': {'rulesset': 'reflexrulevalidator', },
         })
     security = ClassSecurityInfo()
-
-    def _check_set_values(self, dic):
-        """
-        This function check if the dict values are correct. It doesn't look
-        into the action list.
-        :dict: is a dictionary with the following format:
-        {
-        'range1': 'X', 'range0': 'X', 'discreteresult': 'X',
-        'trigger': 'xxx',
-        'analysisservice': '<as_uid>', 'value': '',
-            'actions':[{'action':'<action_name>', 'act_row_idx':'X',
-                        'otherWS':Bool, 'analyst': '<analyst_id>'},
-                      {'action':'<action_name>', 'act_row_idx':'X',
-                        'otherWS':Bool, 'analyst': '<analyst_id>'},
-                ]
-        }
-        These are the checking rules:
-            :range0/range1: string or number.
-        They are the numeric range within the action will be
-        carried on. It is needed to keep it as None or '' if the discreteresult
-        is going to be used instead.
-            :discreteresult: string
-        If discreteresult is not Null, ranges have to be Null.
-            :trigger: string.
-        So far there are only two options: 'submit'/'verify'. They are defined
-        in browser/widgets/reflexrulewidget.py/ReflexRuleWidget/getTriggerVoc.
-            :analysisservice: it is the uid of an analysis service
-            :actions: It is a list of dictionaries with the following format:
-        [{'action':'<action_name>', 'act_row_idx':'X',
-                    'otherWS':Bool, 'analyst': '<analyst_id>'},
-                  {'action':'<action_name>', 'act_row_idx':'X',
-                    'otherWS':Bool, 'analyst': '<analyst_id>'},
-            ]
-        <action_name> options are found in
-        browser/widgets/reflexrulewidget.py/ReflexRuleWidget/getActionVoc
-        so far.
-        """
-        range0 = dic.get('range0', None)
-        range1 = dic.get('range1', None)
-        discreteresult = dic.get('discreteresult', None)
-        trigger = dic.get('trigger', 'submit')
-        analysisservice = dic.get('analysisservice', None)
-        actions = dic.get('actions', [])
-        if (not discreteresult and (not range0 or not range1)) or \
-                (discreteresult and range0 and range1):
-            logger.warn(_(
-                'If range values are empty, discreteresult must contain a '
-                'value, and if discreteresult has a value, ranges must be '
-                'empty. But ranges or discreteresult must conatin a value.'
-                'The given values are: '
-                'discreteresult: %s, range0: %s, range1: %s'
-                % (discreteresult, range0, range1)))
-            return False
-        if trigger not in ['submit', 'verify']:
-            logger.warn('Only available triggers are "verify" or "submit". '
-                        '%s has been introduced.' % (trigger))
-            return False
-        uc = getToolByName(instance, 'uid_catalog')
-        as_brain = uc(UID=analysisservice)
-        if not as_brain:
-            logger.warn('Not correct analysis service UID.')
-            return False
-        if type(actions) not in (list):
-            logger.warn('actions must be a list.')
-            return False
 
     def set(self, instance, rules_list, **kwargs):
         """
@@ -116,16 +52,18 @@ class ReflexRuleField(RecordsField):
                         'otherWS':Bool, 'analyst': '<analyst_id>'},
                 ]
         ...]
+        This list of dictionaries is how the system will store the reflexrule
+        field info. This dictionaries must be in sync with the
+        browser/widgets/reflexrulewidget.py/process_form() dictionaries format.
         """
-        import pdb; pdb.set_trace()
         final_list = []
         for d in rules_list:
             # Checking if all dictionary items are correct
-            if self._check_set_values(d):
+            if _check_set_values(instance, d):
                 # checking actions
                 action_idx = 0
                 for action in d['actions']:
-                    if type(action) not in (dict):
+                    if type(action) not in (dict,):
                         logger.warn('Each action must be a dict.')
                         return False
                     if action.get('action', '') not in ('repeat', 'duplicate'):
@@ -139,10 +77,72 @@ class ReflexRuleField(RecordsField):
                     action['act_row_idx'] = str(action['act_row_idx'])
                     action_idx += 1
                 final_list.append(d)
-        return final_list
+        RecordsField.set(self, instance, final_list, **kwargs)
 
 
-registerField(ReflexRuleField,
-              title="Reflex Rule Field",
-              description="Used for storing Reflex Rules.",
-              )
+def _check_set_values(instance, dic):
+    """
+    This function check if the dict values are correct. It doesn't look
+    into the action list.
+    :instance: the object instance. Used for querying
+    :dic: is a dictionary with the following format:
+    {
+    'range1': 'X', 'range0': 'X', 'discreteresult': 'X',
+    'trigger': 'xxx',
+    'analysisservice': '<as_uid>', 'value': '',
+        'actions':[{'action':'<action_name>', 'act_row_idx':'X',
+                    'otherWS':Bool, 'analyst': '<analyst_id>'},
+                  {'action':'<action_name>', 'act_row_idx':'X',
+                    'otherWS':Bool, 'analyst': '<analyst_id>'},
+            ]
+    }
+    These are the checking rules:
+        :range0/range1: string or number.
+    They are the numeric range within the action will be
+    carried on. It is needed to keep it as None or '' if the discreteresult
+    is going to be used instead.
+        :discreteresult: string
+    If discreteresult is not Null, ranges have to be Null.
+        :trigger: string.
+    So far there are only two options: 'submit'/'verify'. They are defined
+    in browser/widgets/reflexrulewidget.py/ReflexRuleWidget/getTriggerVoc.
+        :analysisservice: it is the uid of an analysis service
+        :actions: It is a list of dictionaries with the following format:
+    [{'action':'<action_name>', 'act_row_idx':'X',
+                'otherWS':Bool, 'analyst': '<analyst_id>'},
+              {'action':'<action_name>', 'act_row_idx':'X',
+                'otherWS':Bool, 'analyst': '<analyst_id>'},
+        ]
+    <action_name> options are found in
+    browser/widgets/reflexrulewidget.py/ReflexRuleWidget/getActionVoc
+    so far.
+    """
+    range0 = dic.get('range0', None)
+    range1 = dic.get('range1', None)
+    discreteresult = dic.get('discreteresult', None)
+    trigger = dic.get('trigger', 'submit')
+    analysisservice = dic.get('analysisservice', None)
+    actions = dic.get('actions', [])
+    if (not discreteresult and (not range0 or not range1)) or \
+            (discreteresult and range0 and range1):
+        logger.warn(_(
+            'If range values are empty, discreteresult must contain a '
+            'value, and if discreteresult has a value, ranges must be '
+            'empty. But ranges or discreteresult must conatin a value.'
+            'The given values are: '
+            'discreteresult: %s, range0: %s, range1: %s'
+            % (discreteresult, range0, range1)))
+        return False
+    if trigger not in ['submit', 'verify']:
+        logger.warn('Only available triggers are "verify" or "submit". '
+                    '%s has been introduced.' % (trigger))
+        return False
+    uc = getToolByName(instance, 'uid_catalog')
+    as_brain = uc(UID=analysisservice)
+    if not as_brain:
+        logger.warn('Not correct analysis service UID.')
+        return False
+    if type(actions) not in (list,):
+        logger.warn('actions must be a list.')
+        return False
+    return True
