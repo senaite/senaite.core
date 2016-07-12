5ea3490588b6e62ebb94929b2ee007044b959962
Pau Soliva <psoliva@naralabs.com>
2016-07-12 17:45:37 +0200

    pdb

--- bika/lims/vocabularies/__init__.py
+++ bika/lims/vocabularies/__init__.py
@@ -109,7 +109,6 @@ class BikaContentVocabulary(object):
                 xitems = [(t(item.Title()), item.Title()) for item in objects]
                 xitems = [SimpleTerm(i[1], i[1], i[0]) for i in xitems]
                 items += xitems
-        import pdb; pdb.set_trace()
         return SimpleVocabulary(items)
 
 
