e0346a432aa542a8ba70e298db2ad41fce4a62de
Campbell <campbell@bikalabs.com>
2016-09-07 15:54:55 +0200

    LIMS-2355: missing review_state values in samplingdeviations listing
    Could not view dormant/active lists.

--- bika/lims/controlpanel/bika_samplingdeviations.py
+++ bika/lims/controlpanel/bika_samplingdeviations.py
@@ -1,21 +1,14 @@
-from AccessControl import ClassSecurityInfo
 from Products.ATContentTypes.content import schemata
 from Products.Archetypes import atapi
-from Products.Archetypes.ArchetypeTool import registerType
-from Products.CMFCore import permissions
-from Products.CMFCore.utils import getToolByName
-from bika.lims.browser import BrowserView
+from bika.lims import bikaMessageFactory as _
 from bika.lims.browser.bika_listing import BikaListingView
 from bika.lims.config import PROJECTNAME
 from bika.lims.interfaces import ISamplingDeviations
-from plone.app.layout.globals.interfaces import IViewView
-from bika.lims import bikaMessageFactory as _
-from bika.lims.utils import t
-from bika.lims.content.bikaschema import BikaFolderSchema
-from bika.lims.permissions import AddSamplingDeviation, ManageBika
 from plone.app.content.browser.interfaces import IFolderContentsView
 from plone.app.folder.folder import ATFolder, ATFolderSchema
+from plone.app.layout.globals.interfaces import IViewView
 from zope.interface.declarations import implements
+
 
 class SamplingDeviationsView(BikaListingView):
     implements(IFolderContentsView, IViewView)
@@ -30,12 +23,13 @@ class SamplingDeviationsView(BikaListingView):
             'icon': '++resource++bika.lims.images/add.png'
         }}
         self.title = self.context.translate(_("Sampling Deviations"))
-        self.icon = self.portal_url + "/++resource++bika.lims.images/samplingdeviation_big.png"
+        self.icon = self.portal_url + \
+                    "/++resource++bika.lims.images/samplingdeviation_big.png"
         self.description = ""
         self.show_sort_column = False
         self.show_select_row = False
-        self.show_select_column = False
         self.pagesize = 25
+        self.show_select_column = True
 
         self.columns = {
             'Title': {'title': _('Sampling Deviation'),
@@ -46,44 +40,42 @@ class SamplingDeviationsView(BikaListingView):
         }
 
         self.review_states = [
-            {'id':'default',
+            {'id': 'default',
              'title': _('All'),
-             'contentFilter':{},
-             'transitions':[{'id':'empty'},],
+             'contentFilter': {},
+             'transitions': [{'id': 'empty'}, ],
              'columns': ['Title', 'Description']},
+            {'id': 'active',
+             'title': _('Active'),
+             'contentFilter': {'inactive_state': 'active'},
+             'transitions': [{'id': 'deactivate'}, ],
+             'columns': ['Title', 'Description']},
+            {'id': 'inactive',
+             'title': _('Dormant'),
+             'contentFilter': {'inactive_state': 'inactive'},
+             'transitions': [{'id': 'activate'}, ],
+             'columns': ['Title', 'Description']}
         ]
 
     def folderitems(self):
-        mtool = getToolByName(self.context, 'portal_membership')
-        if mtool.checkPermission(ManageBika, self.context):
-            del self.review_states[0]['transitions']
-            self.show_select_column = True
-            self.review_states.append(
-                {'id':'active',
-                 'title': _('Active'),
-                 'contentFilter': {'inactive_state': 'active'},
-                 'transitions': [{'id':'deactivate'}, ],
-                 'columns': ['Title', 'Description']})
-            self.review_states.append(
-                {'id':'inactive',
-                 'title': _('Dormant'),
-                 'contentFilter': {'inactive_state': 'inactive'},
-                 'transitions': [{'id':'activate'}, ],
-                 'columns': ['Title', 'Description']})
 
         items = BikaListingView.folderitems(self)
         for x in range(len(items)):
-            if not items[x].has_key('obj'): continue
+            if not items[x].has_key('obj'):
+                continue
             items[x]['replace']['Title'] = "<a href='%s'>%s</a>" % \
-                 (items[x]['url'], items[x]['Title'])
-
+                                           (items[x]['url'], items[x]['Title'])
         return items
 
+
 schema = ATFolderSchema.copy()
+
+
 class SamplingDeviations(ATFolder):
     implements(ISamplingDeviations)
     displayContentsTab = False
     schema = schema
 
-schemata.finalizeATCTSchema(schema, folderish = True, moveDiscussion = False)
+
+schemata.finalizeATCTSchema(schema, folderish=True, moveDiscussion=False)
 atapi.registerType(SamplingDeviations, PROJECTNAME)
