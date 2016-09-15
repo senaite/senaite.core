senaite/senaite.core

commit 1a28009617bc2f61a518afa02d46c55ad49af9ed

LIMS-2133 Once in a while, specs var is going empty in results reports

bika/lims/browser/calcs.py
@@ -254,28 +254,35 @@ class ajaxCalculateAnalysisEntry(BrowserView):
                 else:
                     self.alerts[uid] = [alert, ]
 
+        if analysis.portal_type == 'ReferenceAnalysis':
+            # The analysis is a Control or Blank. We might use the
+            # reference results instead other specs
+            uid = analysis.getServiceUID()
+            specs = analysis.aq_parent.getResultsRangeDict().get(uid, {})
+
+        else:
+            # Get the specs directly from the analysis. The getResultsRange
+            # function already takes care about which are the specs to be used:
+            # AR, client or lab.
+            specs = analysis.getResultsRange()
+
         # format result
         belowmin = False
         abovemax = False
-        # Some analyses will not have AnalysisSpecs, eg, ReferenceAnalysis
-        if hasattr(analysis, 'getAnalysisSpecs'):
-            specs = analysis.getAnalysisSpecs()
-            specs = specs.getResultsRangeDict() if specs is not None else {}
-            specs = specs.get(analysis.getKeyword(), {})
-            hidemin = specs.get('hidemin', '')
-            hidemax = specs.get('hidemax', '')
-            if Result.get('result', ''):
-                fresult = Result['result']
-                try:
-                    belowmin = hidemin and fresult < float(hidemin) or False
-                except ValueError:
-                    belowmin = False
-                    pass
-                try:
-                    abovemax = hidemax and fresult > float(hidemax) or False
-                except ValueError:
-                    abovemax = False
-                    pass
+        hidemin = specs.get('hidemin', '')
+        hidemax = specs.get('hidemax', '')
+        if Result.get('result', ''):
+            fresult = Result['result']
+            try:
+                belowmin = hidemin and fresult < float(hidemin) or False
+            except ValueError:
+                belowmin = False
+                pass
+            try:
+                abovemax = hidemax and fresult > float(hidemax) or False
+            except ValueError:
+                abovemax = False
+                pass
 
         if belowmin is True:
             Result['formatted_result'] = '< %s' % hidemin

bika/lims/browser/analysisrequest/publish.py
@@ -529,24 +529,17 @@ class AnalysisRequestPublishView(BrowserView):
                             if analysis.portal_type == 'Analysis' \
                             else '%s - %s' % (analysis.aq_parent.id, analysis.aq_parent.Title())
 
-        # Which analysis specs must be used?
-        # Try first with those defined at AR Publish Specs level
         if analysis.portal_type == 'ReferenceAnalysis':
             # The analysis is a Control or Blank. We might use the
             # reference results instead other specs
             uid = analysis.getServiceUID()
             specs = analysis.aq_parent.getResultsRangeDict().get(uid, {})
 
-        elif analysis.portal_type == 'DuplicateAnalysis':
-            specs = analysis.getAnalysisSpecs();
-
         else:
-            ar = analysis.aq_parent
-            specs = ar.getPublicationSpecification()
-            if not specs or keyword not in specs.getResultsRangeDict():
-                specs = analysis.getAnalysisSpecs()
-            specs = specs.getResultsRangeDict().get(keyword, {}) \
-                    if specs else {}
+            # Get the specs directly from the analysis. The getResultsRange
+            # function already takes care about which are the specs to be used:
+            # AR, client or lab.
+            specs = analysis.getResultsRange()
 
         andict['specs'] = specs
         scinot = self.context.bika_setup.getScientificNotationReport()
@@ -561,9 +554,6 @@ class AnalysisRequestPublishView(BrowserView):
             fs = '< %s' % specs['max']
         andict['formatted_specs'] = formatDecimalMark(fs, decimalmark)
         andict['formatted_uncertainty'] = format_uncertainty(analysis, analysis.getResult(), decimalmark=decimalmark, sciformat=int(scinot))
-
-        # Return specs of current analysis
-        andict['specs_dict'] = analysis.getSpecification().getResultsRangeDict().get(analysis.id)
 
         # Out of range?
         if specs:

bika/lims/browser/worksheet/views/printview.py
@@ -407,16 +407,11 @@ class PrintView(BrowserView):
             uid = analysis.getServiceUID()
             specs = analysis.aq_parent.getResultsRangeDict().get(uid, {})
 
-        elif analysis.portal_type == 'DuplicateAnalysis':
-            specs = analysis.getAnalysisSpecs();
-
         else:
-            ar = analysis.aq_parent
-            specs = ar.getPublicationSpecification()
-            if not specs or keyword not in specs.getResultsRangeDict():
-                specs = analysis.getAnalysisSpecs()
-            specs = specs.getResultsRangeDict().get(keyword, {}) \
-                    if specs else {}
+            # Get the specs directly from the analysis. The getResultsRange
+            # function already takes care about which are the specs to be used:
+            # AR, client or lab.
+            specs = analysis.getResultsRange()
 
         andict['specs'] = specs
         scinot = self.context.bika_setup.getScientificNotationReport()

bika/lims/content/analysis.py
@@ -533,15 +533,15 @@ class Analysis(BaseContent):
                 rr = an.aq_parent.getResultsRange()
                 rr = [r for r in rr if r.get('keyword', '') == an.getKeyword()]
                 rr = rr[0] if rr and len(rr) > 0 else {}
-            if specification == 'ar' or rr:
+                if rr:
+                    rr['uid'] = self.UID()
+        if not rr:
+            # Let's try to retrieve the specs from client and/or lab
+            specs = an.getAnalysisSpecs(specification)
+            rr = specs.getResultsRangeDict() if specs else {}
+            rr = rr.get(an.getKeyword(), {}) if rr else {}
+            if rr:
                 rr['uid'] = self.UID()
-                return rr
-
-        specs = an.getAnalysisSpecs(specification)
-        rr = specs.getResultsRangeDict() if specs else {}
-        rr = rr.get(an.getKeyword(), {}) if rr else {}
-        if rr:
-            rr['uid'] = self.UID()
         return rr
 
     def getAnalysisSpecs(self, specification=None):
@@ -1500,4 +1500,3 @@ class Analysis(BaseContent):
 
 
 atapi.registerType(Analysis, PROJECTNAME)
-

commit 9c559501511f781060a8013794ea27e3432e99a5

LIMS-2394 appears to be caused by 1a28009

bika/lims/browser/calcs.py
@@ -262,8 +262,8 @@ class ajaxCalculateAnalysisEntry(BrowserView):
         if analysis.portal_type == 'ReferenceAnalysis':
             # The analysis is a Control or Blank. We might use the
             # reference results instead other specs
-            uid = analysis.getServiceUID()
-            specs = analysis.aq_parent.getResultsRangeDict().get(uid, {})
+            _uid = analysis.getServiceUID()
+            specs = analysis.aq_parent.getResultsRangeDict().get(_uid, {})
 
         else:
             # Get the specs directly from the analysis. The getResultsRange