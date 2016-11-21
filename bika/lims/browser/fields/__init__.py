f4df721fc0cd85014c6501ca330927bc9843a33c
Campbell <campbell@bikalabs.com>
2016-11-21 17:26:57 +0200

    PEP8 and fix warnings

--- bika/lims/browser/fields/__init__.py
+++ bika/lims/browser/fields/__init__.py
@@ -3,11 +3,11 @@
 # Copyright 2011-2016 by it's authors.
 # Some rights reserved. See LICENSE.txt, AUTHORS.txt.
 
-from addressfield import AddressField
-from datetimefield import DateTimeField
-from durationfield import DurationField
-from aranalysesfield import ARAnalysesField
-from interimfieldsfield import InterimFieldsField
-from referenceresultsfield import ReferenceResultsField
-from historyawarereferencefield import HistoryAwareReferenceField
-from coordinatefield import CoordinateField
+from .addressfield import AddressField
+from .datetimefield import DateTimeField
+from .durationfield import DurationField
+from .aranalysesfield import ARAnalysesField
+from .interimfieldsfield import InterimFieldsField
+from .referenceresultsfield import ReferenceResultsField
+from .historyawarereferencefield import HistoryAwareReferenceField
+from .coordinatefield import CoordinateField
