3e6fafc2f9c9f3fdc43ff86711ee89ef4c691add
Nihad Mammadli <nihad@tesla>
2016-11-30 11:56:17 +0100

    NMRL-51- Department Filtering enabled in first login by setting cookie value.
    Conflicts:
    	bika/lims/browser/js/bika.lims.site.js

--- bika/lims/browser/js/bika.lims.site.js
+++ bika/lims/browser/js/bika.lims.site.js
@@ -15,7 +15,7 @@ function SiteView() {
 
         loadReferenceDefinitionEvents();
 
-        loadFilterByDepartmentCookie();
+        loadFilterByDepartment();
 
     }
 
@@ -360,10 +360,17 @@ function SiteView() {
         $('#admin_dep_filter_enabled').change(function() {
             var cookiename = 'filter_by_department_info';
             if($(this).is(":checked")) {
-                createCookie(cookiename, "disabled");
+                var deps=[];
+                var all_dep_ids = document.getElementById('department_filter_portlet').options;
+                for (var i = 0; i < all_dep_ids.length; i++) {
+                  deps.push(all_dep_ids[i].value);
+                }
+                createCookie(cookiename, deps);
+                createCookie('dep_filter_disabled','true');
                 $('#department_filter_portlet').prop("disabled",true);
                 location.reload();
               }else{
+                createCookie('dep_filter_disabled','false');
                 $('#department_filter_portlet').prop("disabled",false);
                 $('#department_filter_submit').click();
               }
@@ -377,19 +384,20 @@ function SiteView() {
         available. If the cookie exists, do nothing, if the cookie has not been
         created yet, checks the selected department in the 'filter by department
         cookie' and creates the cookie with the UID of the department.
-        If cookie value is "disabled", it means the user is admin and filtering is disabled.
+        If cookie value "dep_filter_disabled" is true, it means the user is admin and filtering is disabled.
         */
         // Gettin the cookie
         var cookiename = 'filter_by_department_info';
         var cookie_val = readCookie(cookiename);
-        if (cookie_val === null){
+        if (cookie_val === null || document.cookie.indexOf(cookiename)<1){
             // we need to create a cookie with the default value in selected
             // the portlet
             var dep_uid = $('.portlet#portletfilter_by_department form ' +
                 'select#department_filter_portlet').find(":selected").val();
             createCookie(cookiename, dep_uid);
         }
-        if (cookie_val === "disabled"){
+        var dep_filter_disabled=readCookie('dep_filter_disabled');
+        if (dep_filter_disabled=="true" || dep_filter_disabled=='"true"'){
             $('#admin_dep_filter_enabled').prop("checked",true);
             $('#department_filter_portlet').prop("disabled",true);
         }
