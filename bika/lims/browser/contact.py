7f0e11457166c42322d9c4141b26d7d146a48b8a
Ramon Bartl <rb@ridingbytes.com>
2016-12-01 21:31:51 +0100

    First prototype of new Contact/User linkage handling

--- bika/lims/browser/contact.py
+++ bika/lims/browser/contact.py
@@ -1,117 +1,246 @@
+# -*- coding: utf-8 -*-
+#
 # This file is part of Bika LIMS
 #
 # Copyright 2011-2016 by it's authors.
 # Some rights reserved. See LICENSE.txt, AUTHORS.txt.
 
-from Acquisition import aq_parent, aq_inner, aq_base
-from Products.CMFCore.utils import getToolByName
+import re
+from Acquisition import aq_base
 from Products.CMFPlone.utils import safe_unicode
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.Archetypes.utils import DisplayList
-from bika.lims import PMF, bikaMessageFactory as _
+
+from plone import api
+from plone.protect import CheckAuthenticator
+
+from bika.lims import PMF
 from bika.lims.browser import BrowserView
-import json
+from bika.lims import logger
+from bika.lims import bikaMessageFactory as _
+
 
 class ContactLoginDetailsView(BrowserView):
-    """ The contact login details edit
+    """Contact Login View
     """
     template = ViewPageTemplateFile("templates/login_details.pt")
+    searchstring = ""
 
     def __call__(self):
+        request = self.request
+        form = request.form
+        CheckAuthenticator(form)
 
-        if self.request.form.has_key("submitted"):
+        if form.get("submitted"):
+            logger.info("Form Submitted: {}".format(form))
+            if form.get("unlink_button", False):
+                self._unlink_user()
+            elif form.get("delete_button", False):
+                self._unlink_user(delete=True)
+            elif form.get("deactivate_button", False):
+                self._deactivate_user()
+            elif form.get("search_button", False):
+                logger.info("Search User")
+            elif form.get("link_button", False):
+                logger.info("Link User")
+                self._link_user(form.get("userid"))
+            elif form.get("save_button", False):
+                logger.info("Create User")
+                self._create_user()
 
-            def error(field, message):
-                if field:
-                    message = "%s: %s" % (field, message)
-                self.context.plone_utils.addPortalMessage(message, 'error')
-                return self.template()
+        return self.template()
 
-            form = self.request.form
-            contact = self.context
+    def _link_user(self, userid):
+        """Link user to the current Contact
+        """
+        if userid:
+            self.context.setUser(userid)
+            self.add_status_message(_("Linked System User"), "success")
 
-            password = safe_unicode(form.get('password', '')).encode('utf-8')
-            username = safe_unicode(form.get('username', '')).encode('utf-8')
-            confirm = form.get('confirm', '')
-            email = safe_unicode(form.get('email', '')).encode('utf-8')
+    def _unlink_user(self, delete=False):
+        """Unlink/Delete the User from the current Contact
+        """
+        if delete:
+            self.add_status_message(_("Unlinked and deleted System User"), "warning")
+            self.context.unlinkUser(delete=True)
+        else:
+            self.add_status_message(_("Unlinked System User"), "info")
+            self.context.unlinkUser()
 
-            if not username:
-                return error('username', PMF("Input is required but not given."))
+    def _deactivate_user(self):
+        """Deactivate User Login
+        """
+        self.add_status_message(_("Deactivated System User"), "info")
 
-            if not email:
-                return error('email', PMF("Input is required but not given."))
+    def add_status_message(self, message, severity="info"):
+        """Set a portal message
+        """
+        self.context.plone_utils.addPortalMessage(message, severity)
 
-            reg_tool = self.context.portal_registration
-            properties = self.context.portal_properties.site_properties
+    def linkable_users(self):
+        """Users which are not linked to a contact
+        """
 
-##            if properties.validate_email:
-##                password = reg_tool.generatePassword()
-##            else:
-            if password!=confirm:
-                return error('password', PMF("Passwords do not match."))
+        users = api.user.get_users()
+        pc = api.portal.get_tool("portal_catalog")
+        self.searchstring = self.request.form.get("searchstring")
 
-            if not password:
-                return error('password', PMF("Input is required but not given."))
+        out = []
+        for user in users:
+            userid = user.getId()
 
-            if not confirm:
-                return error('password', PMF("Passwords do not match."))
+            # Skip users which are already linked to a Contact
+            results = pc(portal_type="Contact", getUsername=userid)
+            if results:
+                logger.info("User {} is already linked to {}".format(
+                    userid, ",".join(map(lambda x: x.Title, results))))
+                if len(results) > 1:
+                    logger.error("User {} is linked to multiple Contacts: {}".format(
+                        userid, ",".join(map(lambda x: x.Title, results))))
+                continue
 
-            if len(password) < 5:
-                return error('password', PMF("Passwords must contain at least 5 letters."))
+            userdata = {
+                "userid": user.getId(),
+                "email": user.getProperty("email"),
+                "fullname": user.getProperty("fullname"),
+            }
 
-            try:
-                reg_tool.addMember(username,
-                                   password,
-                                   properties = {
-                                       'username': username,
-                                       'email': email,
-                                       'fullname': username})
-            except ValueError, msg:
-                return error(None, msg)
+            # filter out users which do not match the searchstring
+            if self.searchstring:
+                s = self.searchstring.lower()
+                if not any(map(lambda v: re.search(v, s), userdata.values())):
+                    continue
 
-            contact.setUsername(username)
-            contact.setEmailAddress(email)
+            # Append the userdata for the results
+            out.append(userdata)
+        return out
 
-            # If we're being created in a Client context, then give
-            # the contact an Owner local role on client.
-            if contact.aq_parent.portal_type == 'Client':
-                contact.aq_parent.manage_setLocalRoles( username, ['Owner',] )
-                if hasattr(aq_base(contact.aq_parent), 'reindexObjectSecurity'):
-                    contact.aq_parent.reindexObjectSecurity()
+    def _create_user(self):
+        def error(field, message):
+            if field:
+                message = "%s: %s" % (field, message)
+            self.context.plone_utils.addPortalMessage(message, 'error')
+            return self.template()
 
-                # add user to Clients group
-                group=self.context.portal_groups.getGroupById('Clients')
+        form = self.request.form
+        contact = self.context
+
+        password = safe_unicode(form.get('password', '')).encode('utf-8')
+        username = safe_unicode(form.get('username', '')).encode('utf-8')
+        confirm = form.get('confirm', '')
+        email = safe_unicode(form.get('email', '')).encode('utf-8')
+
+        if not username:
+            return error('username', PMF("Input is required but not given."))
+
+        if not email:
+            return error('email', PMF("Input is required but not given."))
+
+        reg_tool = self.context.portal_registration
+        properties = self.context.portal_properties.site_properties
+
+        # if properties.validate_email:
+        #     password = reg_tool.generatePassword()
+        # else:
+        if password != confirm:
+            return error('password', PMF("Passwords do not match."))
+
+        if not password:
+            return error('password', PMF("Input is required but not given."))
+
+        if not confirm:
+            return error('password', PMF("Passwords do not match."))
+
+        if len(password) < 5:
+            return error('password', PMF("Passwords must contain at least 5 letters."))
+
+        try:
+            reg_tool.addMember(username,
+                               password,
+                               properties={
+                                   'username': username,
+                                   'email': email,
+                                   'fullname': username})
+        except ValueError, msg:
+            return error(None, msg)
+
+        contact.setUsername(username)
+        contact.setEmailAddress(email)
+
+        # If we're being created in a Client context, then give
+        # the contact an Owner local role on client.
+        if contact.aq_parent.portal_type == 'Client':
+            contact.aq_parent.manage_setLocalRoles(username, ['Owner', ])
+            if hasattr(aq_base(contact.aq_parent), 'reindexObjectSecurity'):
+                contact.aq_parent.reindexObjectSecurity()
+
+            # add user to Clients group
+            group = self.context.portal_groups.getGroupById('Clients')
+            group.addMember(username)
+
+        # Additional groups for LabContact users.
+        # not required (not available for client Contact)
+        if 'groups' in self.request and self.request['groups']:
+            groups = self.request['groups']
+            if not type(groups) in (list, tuple):
+                groups = [groups, ]
+            for group in groups:
+                group = self.portal_groups.getGroupById(group)
                 group.addMember(username)
 
-            # Additional groups for LabContact users.
-            # not required (not available for client Contact)
-            if 'groups' in self.request and self.request['groups']:
-                groups = self.request['groups']
-                if not type(groups) in (list,tuple):
-                    groups = [groups,]
-                for group in groups:
-                    group = self.portal_groups.getGroupById(group)
-                    group.addMember(username)
+        contact.reindexObject()
 
-            contact.reindexObject()
+        if properties.validate_email or self.request.get('mail_me', 0):
+            try:
+                reg_tool.registeredNotify(username)
+            except:
+                import transaction
+                transaction.abort()
+                return error(
+                    None, PMF("SMTP server disconnected."))
 
-            if properties.validate_email or self.request.get('mail_me', 0):
-                try:
-                    reg_tool.registeredNotify(username)
-                except:
-                    import transaction
-                    transaction.abort()
-                    return error(
-                        None, PMF("SMTP server disconnected."))
-
-            message = PMF("Member registered.")
-            self.context.plone_utils.addPortalMessage(message, 'info')
-            return self.template()
-        else:
-            return self.template()
+        message = PMF("Member registered.")
+        self.context.plone_utils.addPortalMessage(message, 'info')
+        return self.template()
 
     def tabindex(self):
         i = 0
         while True:
             i += 1
             yield i
+
+    def get_user_properties(self):
+        """Return the properties of the System User
+        """
+
+        user = self.context.getUser()
+
+        # No User linked, nothing to do
+        if user is None:
+            return {}
+
+        out = {}
+        plone_user = user.getUser()
+        userid = plone_user.getId()
+        for sheet in plone_user.listPropertysheets():
+            ps = plone_user.getPropertysheet(sheet)
+            out.update(dict(ps.propertyItems()))
+
+        portal = api.portal.get()
+        mtool = api.portal.get_tool(name='portal_membership')
+
+        out["id"] = userid
+        out["portrait"] = mtool.getPersonalPortrait(id=userid)
+        out["edit_url"] = "{}/@@user-information?userid={}".format(
+            portal.absolute_url(), userid)
+
+        return out
+
+    def get_contact_properties(self):
+        """Return the properties of the Contact
+        """
+        contact = self.context
+
+        return {
+            "fullname": contact.getFullname(),
+            "username": contact.getUsername(),
+        }
--- bika/lims/content/contact.py
+++ bika/lims/content/contact.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+#
 # This file is part of Bika LIMS
 #
 # Copyright 2011-2016 by it's authors.
@@ -5,54 +7,56 @@
 
 """The contact person at an organisation.
 """
-from AccessControl import ClassSecurityInfo
-from AccessControl.Permissions import manage_users
-from Products.ATContentTypes.content import schemata
-from Products.Archetypes import atapi
-from Products.Archetypes.public import *
-from Products.CMFCore import permissions
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.utils import safe_unicode
-from bika.lims.config import ManageClients, PROJECTNAME
-from bika.lims.content.person import Person
-from bika.lims import PMF, bikaMessageFactory as _
-from bika.lims.interfaces import IContact
-from zope.component import getAdapters
-from zope.interface import implements
-from bika.lims.utils import isActive
-from bika.lims.vocabularies import CustomPubPrefVocabularyFactory
+import types
 
-schema = Person.schema.copy() + Schema((
-    LinesField('PublicationPreference',
-        vocabulary_factory = 'bika.lims.vocabularies.CustomPubPrefVocabularyFactory',
-        schemata = 'Publication preference',
-        widget = MultiSelectionWidget(
-            label=_("Publication preference"),
-        ),
-    ),
-    BooleanField('AttachmentsPermitted',
-        default = False,
-        schemata = 'Publication preference',
-        widget = BooleanWidget(
-            label=_("Results attachments permitted"),
-            description = _(
-                "File attachments to results, e.g. microscope "
-                "photos, will be included in emails to recipients "
-                "if this option is enabled")
-        ),
-    ),
-    ReferenceField('CCContact',
-        schemata = 'Publication preference',
-        vocabulary = 'getContacts',
-        multiValued = 1,
-        allowed_types = ('Contact',),
-        relationship = 'ContactContact',
-        widget = ReferenceWidget(
-            checkbox_bound = 0,
-            label=_("Contacts to CC"),
-        ),
-    ),
+from AccessControl import ClassSecurityInfo
+
+from Products.Archetypes import atapi
+from Products.CMFPlone.utils import safe_unicode
+from Products.Archetypes.utils import DisplayList
+
+from zope.interface import implements
+
+from plone import api
+
+from bika.lims.utils import isActive
+from bika.lims.interfaces import IContact
+from bika.lims.content.person import Person
+from bika.lims.config import PROJECTNAME
+from bika.lims.config import ManageClients
+from bika.lims import logger
+from bika.lims import bikaMessageFactory as _
+
+
+schema = Person.schema.copy() + atapi.Schema((
+    atapi.LinesField('PublicationPreference',
+                     vocabulary_factory='bika.lims.vocabularies.CustomPubPrefVocabularyFactory',
+                     schemata='Publication preference',
+                     widget=atapi.MultiSelectionWidget(
+                         label=_("Publication preference"),
+                     )),
+    atapi.BooleanField('AttachmentsPermitted',
+                       default=False,
+                       schemata='Publication preference',
+                       widget=atapi.BooleanWidget(
+                           label=_("Results attachments permitted"),
+                           description=_(
+                               "File attachments to results, e.g. microscope "
+                               "photos, will be included in emails to recipients "
+                               "if this option is enabled")
+                       )),
+    atapi.ReferenceField('CCContact',
+                         schemata='Publication preference',
+                         vocabulary='getContacts',
+                         multiValued=1,
+                         allowed_types=('Contact',),
+                         relationship='ContactContact',
+                         widget=atapi.ReferenceWidget(
+                             checkbox_bound=0,
+                             label=_("Contacts to CC"),
+                         )),
 ))
+
 
 schema['JobTitle'].schemata = 'default'
 schema['Department'].schemata = 'default'
@@ -61,26 +65,84 @@ schema['title'].required = 0
 schema['title'].widget.visible = False
 schema.moveField('CCContact', before='AttachmentsPermitted')
 
+
 class Contact(Person):
+    """A Contact of a Client which can be linked to a System User
+    """
     implements(IContact)
+
     security = ClassSecurityInfo()
     displayContentsTab = False
     schema = schema
-
     _at_rename_after_creation = True
+
     def _renameAfterCreation(self, check_auto_id=False):
         from bika.lims.idserver import renameAfterCreation
         renameAfterCreation(self)
 
     def Title(self):
-        """ Return the contact's Fullname as title """
+        """Return the contact's Fullname as title
+        """
         return safe_unicode(self.getFullname()).encode('utf-8')
+
+    security.declareProtected(ManageClients, 'getUser')
+    def getUser(self):
+        """Returns the linked Plone User or None
+        """
+        username = self.getUsername()
+        if not username:
+            return None
+        user = api.user.get(userid=username)
+        return user
+
+    security.declareProtected(ManageClients, 'getUser')
+    def setUser(self, user_or_username):
+        """Link the user to the Contact
+        """
+        user = None
+        userid = None
+
+        # Handle User IDs (strings)
+        if isinstance(user_or_username, types.StringTypes):
+            userid = user_or_username
+            user = api.user.get(userid)
+        # Handle User Objects (MemberData/PloneUser)
+        if hasattr(user_or_username, "getId"):
+            userid = user_or_username.getId()
+            user = user_or_username
+
+        # Not a valid user
+        if user is None:
+            return False
+
+        # Set the Username
+        self.setUsername(userid)
+        return True
+
+    security.declareProtected(ManageClients, 'unlinkUser')
+    def unlinkUser(self, delete=False):
+        """Unlink the user to the Contact
+        """
+        userid = self.getUsername()
+        if self.hasUser():
+            logger.debug("Unlinking User '{}' from Contact '{}'".format(
+                userid, self.Title()))
+            self.setUsername(None)
+            # Also remove the Plone User (caution)
+            if delete:
+                logger.debug("Removing Plone User '{}'".format(userid))
+                api.user.delete(username=userid)
+            return True
+        return False
 
     security.declareProtected(ManageClients, 'hasUser')
     def hasUser(self):
-        """ check if contact has user """
-        return self.portal_membership.getMemberById(
-            self.getUsername()) is not None
+        """Check if Contact has a linked a System User
+        """
+        user = self.getUser()
+        if user is None:
+            return False
+        return True
 
     def getContacts(self, dl=True):
         pairs = []
@@ -94,6 +156,6 @@ class Contact(Person):
         return dl and DisplayList(pairs) or objects
 
     def getParentUID(self):
-        return self.aq_parent.UID();
+        return self.aq_parent.UID()
 
 atapi.registerType(Contact, PROJECTNAME)
