4af6ba2252c8feb538bfeef7d24916906518f104
Nihad Mammadli <1992.nihad@gmail.com>
2016-12-06 12:24:59 +0100

    NMRL-58-Warning Icon will appear if analysis was submitted and verified by the same person.

--- bika/lims/browser/analyses.py
+++ bika/lims/browser/analyses.py
@@ -762,6 +762,13 @@ class AnalysesView(BikaListingView):
                         "<img src='++resource++bika.lims.images/warning.png' title='%s'/>" %
                         (t(_("Can verify, but submitted by current user")))
                         )
+            #If analysis Submitted and Verified by the same person, then warning icon will appear.
+            if obj.getSubmittedBy() and obj.getVerifiedBy() \
+                    and obj.getSubmittedBy() == obj.getVerifiedBy():
+                after_icons.append(
+                    "<img src='++resource++bika.lims.images/warning.png' title='%s'/>" %
+                    (t(_("Submited and verified by the same user- "+obj.getVerifiedBy())))
+                    )
 
             # add icon for assigned analyses in AR views
             if self.context.portal_type == 'AnalysisRequest':
