0dc2b6315aad23db7e94283113a947ed3c9e39c5
Nihad Mammadli <1992.nihad@gmail.com>
2016-12-12 13:17:52 +0100

    Nothing special...

--- bika/lims/browser/bika_listing.py
+++ bika/lims/browser/bika_listing.py
@@ -219,12 +219,10 @@ class WorkflowAction:
                         nmvers = item.getNumberOfVerifications()
                         username=getToolByName(self.context,'portal_membership').getAuthenticatedMember().getUserName()
                         item.addVerificator(username)
-                        #item.setNumberOfVerifications(nmvers+1)
                         if revers-nmvers <= 1:
                             success, message = doActionFor(item, action)
                             if not success:
-                                # If failed, restore to the previous number
-                                #item.setNumberOfVerifications(numvers)
+                                # If failed, remove the last verification
                                 item.deleteLastVerificator()
                     else:
                         success, message = doActionFor(item, action)
