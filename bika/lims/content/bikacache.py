7bbc4c4c14235d09b5174ba35b1e3c88b9ca7fa7
Nihadness <1992.nihad@gmail.com>
2017-01-30 10:57:12 +0100

    Added some comments

--- bika/lims/content/bikacache.py
+++ bika/lims/content/bikacache.py
@@ -12,8 +12,10 @@ from bika.lims import config
 
 
 schema = BikaSchema.copy() + atapi.Schema((
-
+    #'Key' field is name of the Cache object, must be Unique
     atapi.StringField('Key',default=''),
+
+    # 'Value' is ID of the last created object. Must be inscreased before using.
     atapi.StringField('Value',default='')
 
 ))
@@ -21,6 +23,13 @@ schema = BikaSchema.copy() + atapi.Schema((
 schema['title'].widget.visible = False
 
 class BikaCache(BaseContent):
+    """
+    BikaCache objects stores information about 'Last Created ID's of different
+    types. For each object type, there must be only one Cache object, and the ID
+    of its Last Created Object.
+    It is used to avoid querying whole catalog just to get the highest ID for any
+    kind of object.
+    """
     schema = schema
 
 # Activating the content type in Archetypes' internal types registry
