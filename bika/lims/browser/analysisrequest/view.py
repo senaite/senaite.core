a750569f4870993bfa82cf6929d954c06ab970b0
Pau Soliva <psoliva@naralabs.com>
2017-02-10 11:39:32 +0100

    warning log removed

--- bika/lims/browser/analysisrequest/view.py
+++ bika/lims/browser/analysisrequest/view.py
@@ -45,9 +45,12 @@ class AnalysisRequestViewView(BrowserView):
         ar = self.context
         if 'check_edit' in self.request and\
                 self.request.get('check_edit') == '1':
-                # It mens we should redirect to manage_results
-                redirect = self.context.absolute_url() + '/manage_results'
-                self.request.response.redirect(redirect)
+                # Another check, here to increase performance, is it stupid?
+                state = ar.getObjectWorkflowStates().get('review_state', '')
+                if state not in ['to_be_sampled', ]:
+                    # It mens we should redirect to manage_results
+                    redirect = self.context.absolute_url() + '/manage_results'
+                    self.request.response.redirect(redirect)
         workflow = getToolByName(self.context, 'portal_workflow')
         if 'transition' in self.request.form:
             doActionFor(self.context, self.request.form['transition'])
