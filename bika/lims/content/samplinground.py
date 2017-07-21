95487e97ecdac23516071b0721156d7eaf75be63
Espurna <psoliva@naralabs.com>
2017-07-21 13:15:27 +0200

    getting the contacts from client

--- bika/lims/content/samplinground.py
+++ bika/lims/content/samplinground.py
@@ -116,7 +116,7 @@ class ClientContacts(object):
     implements(IContextSourceBinder)
 
     def __call__(self, context):
-        container = context.aq_parent
+        container = context
         terms = []
         # Show only the client's
         if container.portal_type == 'Client':
