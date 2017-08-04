8f56953747e80f4939192029a9f9fb8b21d27a1c
Nihadness <1992.nihad@gmail.com>
2017-08-04 12:12:19 +0200

    Added JS null validator of Sampling Date field on Secondary AR creation.

--- bika/lims/browser/js/bika.lims.analysisrequest.add_by_col.js
+++ bika/lims/browser/js/bika.lims.analysisrequest.add_by_col.js
@@ -1504,7 +1504,7 @@ function AnalysisRequestAddByCol() {
                                   console.log('Selector #' + fieldname + '-' + arnum + ' not present in form')
                                   continue
                               }
-                              if(element.getAttribute("datetimepicker") && fieldvalue.indexOf(" ")==-1){
+                              if(element.getAttribute("datetimepicker") && fieldvalue && fieldvalue.indexOf(" ")==-1){
                                 fieldvalue = fieldvalue + " 00:00";
                               }
                               // here we go
