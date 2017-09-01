e604521e2833fab1675c230f136e6f39d4a68c37
Nihadness <1992.nihad@gmail.com>
2017-09-01 12:16:09 +0200

    Get strings from message factory

--- bika/lims/browser/widgets/reflexrulewidget.py
+++ bika/lims/browser/widgets/reflexrulewidget.py
@@ -1,23 +1,13 @@
-from AccessControl import ClassSecurityInfo
-from Products.Archetypes.Registry import registerWidget
-from Products.Archetypes.interfaces import IVocabulary
-from zope.interface import implements
-from Products.Archetypes.public import DisplayList
-from Products.CMFCore.utils import getToolByName
-from zope.schema.vocabulary import SimpleVocabulary
-from zope.schema.vocabulary import SimpleTerm
-from bika.lims.utils import getUsers
-from bika.lims.browser.widgets import RecordsWidget
-from bika.lims.browser.widgets.reflexrulewidget_description import description
-from plone.api.portal import get_tool
 import json
 
-try:
-    from zope.component.hooks import getSite
-except:
-    # Plone < 4.3
-    from zope.app.component.hooks import getSite
-
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.Registry import registerWidget
+from Products.Archetypes.public import DisplayList
+from Products.CMFCore.utils import getToolByName
+from bika.lims import bikaMessageFactory as _
+from bika.lims.browser.widgets import RecordsWidget
+from bika.lims.browser.widgets.reflexrulewidget_description import description
+from bika.lims.utils import getUsers
 
 class ReflexRuleWidget(RecordsWidget):
     _properties = RecordsWidget._properties.copy()
@@ -433,19 +423,19 @@ class ReflexRuleWidget(RecordsWidget):
         Return the different action available
         """
         return DisplayList([
-            ('repeat', 'Repeat'),
-            ('duplicate', 'Duplicate'),
-            ('setresult', 'Set result'),
-            ('setvisibility', 'Set Visibility')])
+            ('repeat', _('Repeat')),
+            ('duplicate', _('Duplicate')),
+            ('setresult', _('Set result')),
+            ('setvisibility', _('Set Visibility'))])
 
     def getShowInRepVoc(self):
         """
         Return the different Visibility in Report values.
         """
         return DisplayList([
-            ('default', 'Visibility (default)'),
-            ('visible', 'Show in Report'),
-            ('invisible', 'Hide In Report')])
+            ('default', _('Visibility (default)')),
+            ('visible', _('Show in Report')),
+            ('invisible', _('Hide In Report'))])
 
     def getAndOrVoc(self):
         """
