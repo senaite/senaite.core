70755da57aa34ff6953f4eb65ae78b9a69a3e173
Juan Gallostra <jgallostra@naralabs.com>
2017-09-14 13:33:33 +0200

    - Remove unnecessary indexes and metadata from bika catalog - Uncatalog samples from portal catalog

--- bika/lims/upgrade/v3_2_0_1709.py
+++ bika/lims/upgrade/v3_2_0_1709.py
@@ -38,6 +38,7 @@ def upgrade(tool):
     setup.runImportStepFromProfile('profile-bika.lims:default', 'toolset')
 
     create_sample_catalog(portal, ut)
+    update_bika_catalog(portal, ut)
     ut.refreshCatalogs()
 
     logger.info("{0} upgraded to version {1}".format(product, version))
@@ -50,29 +51,52 @@ def create_sample_catalog(portal, upgrade_utils):
     catalog_dict = bika_catalog_sample_definition.get(CATALOG_SAMPLE_LISTING, {})
     sample_indexes = catalog_dict.get('indexes', {})
     sample_columns = catalog_dict.get('columns', [])
-    # create report catalog indexes
+    # create sample catalog indexes
     for idx in sample_indexes:
         upgrade_utils.addIndex(CATALOG_SAMPLE_LISTING, idx, sample_indexes[idx])
-    # create report catalog columns
+    # create sample catalog columns
     for col in sample_columns:
         upgrade_utils.addColumn(CATALOG_SAMPLE_LISTING, col)
     # define objects to be catalogued
     at.setCatalogsByType('Sample', [CATALOG_SAMPLE_LISTING, ])
-    # retrieve brains of objects to be catalogued from bika catalog
     logger.info('Recovering samples to reindex')
+    # retrieve bika catalog and portal catalog so that samples can be uncatalogued
     bika_catalog = getToolByName(portal, 'bika_catalog')
+    portal_catalog = getToolByName(portal, 'portal_catalog')
+    # get brains of objects to be catalogued from bika catalog
     sample_brains = bika_catalog(portal_type='Sample')
     i = 0  # already indexed objects counter
-    # reindex the found objects in the new sample catalog and uncatalog them from bika_catalog
+    # reindex the found objects in the new sample catalog and uncatalog them from bika catalog
     logger.info('Reindexing samples')
     for brain in sample_brains:
         if i % 100 == 0:
             logger.info('Reindexed {}/{} samples'.format(i, len(sample_brains)))
         sample_obj = brain.getObject()
         sample_obj.reindexObject()
-        # uncatalog samples from bika_catalog
+        # uncatalog samples from bika catalog
         path_uid = '/'.join(sample_obj.getPhysicalPath())
         bika_catalog.uncatalog_object(path_uid)
+        portal_catalog.uncatalog_object(path_uid)
         i += 1
     logger.info('Reindexed {}/{} samples'.format(len(sample_brains), len(sample_brains)))
 
+def update_bika_catalog(portal, upgrade_utils):
+    # remove unnecessary columns and indexes from bika catalog
+    # now that samples have their own catalog
+    logger.info('Updating bika catalog')
+    bika_catalog = getToolByName(portal, 'bika_catalog')
+    unnecessary_cols = ['getSampleID', 'getRequestID', 'getClientOrderNumber', 'getClientReference',
+                        'getClientSampleID', 'getSamplePointTitle', 'getSampleTypeTitle',
+                        'getDatePublished', ]
+    unnecessary_idxs = ['getDepartmentUIDs', 'getAnalysesUIDs', 'getClientOrderNumber', 'getClientReference',
+                        'getClientSampleID', 'getClientTitle', 'getClientUID', 'getDatePublished',
+                        'getInvoiced', 'getRequestID', 'getSampleID', 'getSamplePointTitle',
+                        'getSamplePointUID', 'getSampler', 'getScheduledSamplingSampler',
+                        'getSampleTypeTitle', 'getSampleTypeUID', 'getSampleUID', 'getSamplingDate',
+                        'getWorksheetTemplateTitle', ]
+    for column in unnecessary_cols:
+        upgrade_utils.delColumn(bika_catalog, column)
+    logger.info('Removed unnecessary columns')
+    for index in unnecessary_idxs:
+        upgrade_utils.delIndex(bika_catalog, index)
+    logger.info('Removed unnecessary indexes')
