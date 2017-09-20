7c7692670b19d038d4759373f7cc8400df68fa5b
Nihadness <1992.nihad@gmail.com>
2017-09-20 10:55:56 +0200

    Disabled warning message for Client Contacts..

--- bika/lims/subscribers/dep_cookie.py
+++ bika/lims/subscribers/dep_cookie.py
@@ -26,23 +26,28 @@ def SetDepartmentCookies(event):
                     inactive_state='active')
             for dep in deps:
                 dep_for_cookie+=dep.UID+','
-            response.setCookie('dep_filter_disabled','true',  path = '/', max_age = 24 * 3600)
+            response.setCookie('dep_filter_disabled', 'true',  path = '/', max_age = 24 * 3600)
         else:
-            brain = context.portal_catalog(portal_type='LabContact',
-                                getUsername=username)
+            brain = context.portal_catalog(getUsername=username)
+            # It is possible that current user is created by Plone ZMI.
+            # Just log it as a warning and go on
             if not brain:
-                # It is possible that current user is created by Plone ZMI and
-                # it is not a LabContact. We disable filtering by department because
-                # it can be Client Contact. Also we log it as a warning message in case
-                # it is not a Client Contact but a Zope User.
                 logger.warn("No lab Contact found... Plone user or Client Contact logged in. "
                             + username)
+                response.setCookie('filter_by_department_info', None, path='/', max_age=0)
+                response.setCookie('dep_filter_disabled', None, path='/', max_age=0)
+                return
+
+            # If it is Client Contact, enable all departments no need to filter.
+            elif brain[0].portal_type == 'Contact':
                 deps = context.portal_catalog(portal_type='Department', sort_on='sortable_title',
                                               sort_order='ascending',
                                               inactive_state='active')
                 for dep in deps:
                     dep_for_cookie += dep.UID + ','
                 response.setCookie('dep_filter_disabled', None, path='/', max_age=24 * 3600)
+
+            # It is a LabContact, set up departments.
             else:
                 lab_con = brain[0].getObject()
                 if lab_con.getDefaultDepartment():
