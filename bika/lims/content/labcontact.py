13f1c11991b7fad6ad63bfda0f93801dc5136071
Nihadness <1992.nihad@gmail.com>
2017-10-17 14:01:01 +0200

    Removed 'getDepartmentTitle' from Lab Contact. Added removal to upgrade step.

--- bika/lims/content/labcontact.py
+++ bika/lims/content/labcontact.py
@@ -63,11 +63,6 @@ schema = Person.schema.copy() + atapi.Schema((
             description=_("The laboratory department"),
         ),
     ),
-    atapi.ComputedField('DepartmentTitle',
-                        expression="context.getDepartment() and context.getDepartment().Title() or ''",
-                        widget=atapi.ComputedWidget(
-                            visible=False,
-                        )),
     atapi.ReferenceField('Departments',
         		required = 0,
         		vocabulary_display_path_bound = sys.maxint,
