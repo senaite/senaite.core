a415029368d146071736372e04315c6f99692fd1
Ramon Bartl <rb@ridingbytes.com>
2017-10-26 08:49:37 +0200

    Hotfix: AS sortable_text indexer
    Wrong test for 'None'.

--- bika/lims/catalog/indexers/analysisservice.py
+++ bika/lims/catalog/indexers/analysisservice.py
@@ -6,7 +6,7 @@ from Products.CMFPlone.CatalogTool import sortable_title as _sortable_title
 @indexer(IAnalysisService)
 def sortable_title(instance):
     sort_key = instance.getSortKey()
-    if sort_key is not None:
+    if sort_key is None:
         sort_key = 999999
     title = _sortable_title(instance)
     if callable(title):
