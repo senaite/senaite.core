f4691c12b79c658dc8e61bcd38fefa2de1c8622d
Espurna <psoliva@naralabs.com>
2017-10-27 14:22:48 +0200

    browser user

--- bika/lims/tests/base.py
+++ bika/lims/tests/base.py
@@ -44,15 +44,18 @@ class BikaFunctionalTestCase(unittest.TestCase):
         # During testing, CSRF protection causes failures.
         os.environ["PLONE_CSRF_DISABLED"] = "true"
 
-    def getBrowser(self, loggedIn=True):
+    def getBrowser(self,
+            username=TEST_USER_NAME,
+            password=TEST_USER_PASSWORD,
+            loggedIn=True):
         """ instantiate and return a testbrowser for convenience """
         browser = Browser(self.portal)
         browser.addHeader('Accept-Language', 'en-US')
         browser.handleErrors = False
         if loggedIn:
             browser.open(self.portal.absolute_url())
-            browser.getControl('Login Name').value = TEST_USER_NAME
-            browser.getControl('Password').value = TEST_USER_PASSWORD
+            browser.getControl('Login Name').value = username
+            browser.getControl('Password').value = password
             browser.getControl('Log in').click()
             self.assertTrue('You are now logged in' in browser.contents)
         return browser
