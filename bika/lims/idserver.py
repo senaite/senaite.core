70799e94062eeffa637d3eb664439109838ffb99
Jordi Puiggené <jpuiggene@naralabs.com>
2017-11-11 00:11:10 +0100

    new_seq might be referenced before assignment

--- bika/lims/idserver.py
+++ bika/lims/idserver.py
@@ -136,6 +136,7 @@ def generateUniqueId(context, parent=False, portal_type=''):
             }
 
     # Actual id construction starts here
+    new_seq = 0
     form = config['form']
     if config['sequence_type'] == 'counter':
         new_seq = getLastCounter(
