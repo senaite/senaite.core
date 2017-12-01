cda3767bb3d031709c8aabccfa3408b8bfa3dfb2
Jordi Puiggené <jpuiggene@naralabs.com>
2017-12-01 19:20:59 +0100

    Version bump v1.1.8

---
+++ bika/lims/upgrade/v01_01_008.py
@@ -0,0 +1,27 @@
+from bika.lims import logger
+from bika.lims.catalog import CATALOG_ANALYSIS_REQUEST_LISTING
+from bika.lims.config import PROJECTNAME as product
+from bika.lims.upgrade import upgradestep
+from bika.lims.upgrade.utils import UpgradeUtils
+
+version = '1.1.8'  # Remember version number in metadata.xml and setup.py
+profile = 'profile-{0}:default'.format(product)
+
+@upgradestep(product, version)
+def upgrade(tool):
+    portal = tool.aq_inner.aq_parent
+    ut = UpgradeUtils(portal)
+    ver_from = ut.getInstalledVersion(product)
+
+    if ut.isOlderVersion(product, version):
+        logger.info("Skipping upgrade of {0}: {1} > {2}".format(
+            product, ver_from, version))
+        return True
+
+    logger.info("Upgrading {0}: {1} -> {2}".format(product, ver_from, version))
+
+    # -------- ADD YOUR STUFF HERE --------
+
+    logger.info("{0} upgraded to version {1}".format(product, version))
+
+    return True
