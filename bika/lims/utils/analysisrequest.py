3889b9e695732cf77aaa4b9e2f280999aebe70fd
Lunga Baliwe <lunga001@gmail.com>
2018-01-19 16:16:16 +0200

    Issue-587: Background Process AR Creation

--- bika/lims/utils/analysisrequest.py
+++ bika/lims/utils/analysisrequest.py
@@ -5,8 +5,12 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
+import json
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode
+from Products.CMFPlone.utils import _createObjectByType
+from Products.Five.browser import BrowserView
+from bika.lims import api
 from bika.lims import bikaMessageFactory as _
 from bika.lims import logger
 from bika.lims.idserver import renameAfterCreation
@@ -25,8 +29,7 @@ from copy import deepcopy
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.Utils import formataddr
-from plone import api
-from Products.CMFPlone.utils import _createObjectByType
+from plone import api as ploneapi
 import os
 import tempfile
 
@@ -274,7 +277,7 @@ def _resolve_items_to_service_uids(items):
             continue
 
         # Maybe object UID.
-        portal = portal if portal else api.portal.get()
+        portal = portal if portal else ploneapi.portal.get()
         bsc = bsc if bsc else getToolByName(portal, 'bika_setup_catalog')
         brains = bsc(UID=item)
         if brains:
@@ -382,3 +385,79 @@ def notify_rejection(analysisrequest):
             "Email with subject %s was not sent (SMTP connection error)" % mailsubject)
 
     return True
+
+class Async_AR_Utils(BrowserView):
+
+    def async_create_analysisrequest(self):
+        msgs = []
+        form = self.request.form
+        records = json.loads(form.get('records', '[]'))
+        attachments = json.loads(form.get('attachments', '[]'))
+        ARs = []
+        logger.info('Async create %s records' % len(records))
+        for n, record in enumerate(records):
+            client_uid = record.get("Client")
+            client = api.get_object_by_uid(client_uid)
+
+            if not client:
+                msgs.append("Error: Client {} found".format(client_uid))
+                continue
+
+            # get the specifications and pass them directly to the AR create function.
+            specifications = record.pop("Specifications", {})
+
+
+            ## Create the Analysis Request
+            ar = create_analysisrequest(
+                client,
+                self.request,
+                values=record,
+                specifications=specifications)
+            ARs.append(ar.getId())
+
+        if len(ARs) == 1:
+            msgs.append('Created AR {}'.format(ARs[0]))
+        elif len(ARs) > 1:
+            msgs.append('Created ARs {}'.format(', '.join(ARs)))
+        else:
+            msgs.append('No ARs created')
+        message = '; '.join(msgs)
+        logger.info('AR Creation complete: {}'.format(message))
+        self._email_analyst(message)
+        return
+
+    def queue_count(self):
+        task_queue = queryUtility(ITaskQueue, name='ar-create')
+        if task_queue is None:
+            return 0
+        return 'Len = %s' % len(task_queue)
+
+    def _email_analyst(self, message):
+        mail_template = """
+Dear {name},
+
+Analysis request creation completed with the following messages:
+{message}
+
+Cheers
+Bika LIMS
+"""
+        portal = ploneapi.portal.get()
+        email_charset = portal.getProperty('email_charset')
+        member = ploneapi.user.get_current()
+        mail_host = ploneapi.portal.get_tool(name='MailHost')
+        from_email= mail_host.email_from_address
+        to_email = member.getProperty('email')
+        subject = 'AR Creation Complete'
+        if len(to_email) == 0:
+            to_email = from_email
+            subject = 'AR Creation by admin user is complete'
+        mail_text = mail_template.format(
+                        name=member.getProperty('fullname'),
+                        message=message)
+        try:
+            return mail_host.send(
+                        mail_text, to_email, from_email,
+                        subject=subject, charset="utf-8", immediate=False)
+        except smtplib.SMTPRecipientsRefused:
+            raise smtplib.SMTPRecipientsRefused('Recipient address rejected by server')
