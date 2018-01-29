be8c5724ae11b2a5fd69baa5f402754d05658504
Campbell <campbell@bikalabs.com>
2018-01-29 23:20:36 +0200

    tidy accreditation.py

--- bika/lims/browser/accreditation.py
+++ bika/lims/browser/accreditation.py
@@ -6,56 +6,58 @@
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
 from Products.CMFPlone.utils import safe_unicode
-from bika.lims.controlpanel.bika_analysisservices import AnalysisServicesView
 from bika.lims import bikaMessageFactory as _
+from bika.lims.controlpanel.bika_analysisservices import AnalysisServicesView
 from bika.lims.utils import t
 from plone.app.content.browser.interfaces import IFolderContentsView
 from zope.interface import implements
 
 
 class AccreditationView(AnalysisServicesView):
-    """
-    >>> portal = layer['portal']
-    >>> portal_url = portal.absolute_url()
-    >>> from plone.app.testing import SITE_OWNER_NAME
-    >>> from plone.app.testing import SITE_OWNER_PASSWORD
-
-    >>> browser = layer['getBrowser'](portal)
-    >>> browser.open(portal_url+"/accreditation")
-    >>> 'SAI is the' in browser.contents
-    True
-
-    """
     implements(IFolderContentsView)
 
     def __init__(self, context, request):
         super(AccreditationView, self).__init__(context, request)
-        self.contentFilter = {'portal_type': 'AnalysisService',
-                              'sort_on': 'sortable_title',
-                              'getAccredited': True,
-                              'inactive_state': 'active'}
+        self.contentFilter = {
+            'portal_type': 'AnalysisService',
+            'sort_on': 'sortable_title',
+            'getAccredited': True,
+            'inactive_state': 'active'}
         self.context_actions = {}
-        self.icon = self.portal_url + "/++resource++bika.lims.images/accredited_big.png"
         self.title = self.context.translate(_("Accreditation"))
+        self.icon = self.portal_url + \
+                    "/++resource++bika.lims.images/accredited_big.png"
 
         lab = context.bika_setup.laboratory
         accredited = lab.getLaboratoryAccredited()
-        self.mapping = {'lab_is_accredited': accredited,
-                        'lab_name': safe_unicode(lab.getName()),
-                        'lab_country': safe_unicode(lab.getPhysicalAddress().get('country', '')),
-                        'confidence': safe_unicode(lab.getConfidence()),
-                        'accreditation_body_abbr': safe_unicode(lab.getAccreditationBody()),
-                        'accreditation_body_name': safe_unicode(lab.getAccreditationBodyURL()),
-                        'accreditation_standard': safe_unicode(lab.getAccreditation()),
-                        'accreditation_reference': safe_unicode(lab.getAccreditationReference())
+        self.mapping = {
+            'lab_is_accredited':
+                accredited,
+            'lab_name':
+                safe_unicode(lab.getName()),
+            'lab_country':
+                safe_unicode(lab.getPhysicalAddress().get('country', '')),
+            'confidence':
+                safe_unicode(lab.getConfidence()),
+            'accreditation_body_abbr':
+                safe_unicode(lab.getAccreditationBody()),
+            'accreditation_body_name':
+                safe_unicode(lab.getAccreditationBodyURL()),
+            'accreditation_standard':
+                safe_unicode(lab.getAccreditation()),
+            'accreditation_reference':
+                safe_unicode(lab.getAccreditationReference())
         }
         if accredited:
-            self.description = t(_(safe_unicode(lab.getAccreditationPageHeader()),
-                                   mapping=self.mapping
+            self.description = t(_(
+                safe_unicode(lab.getAccreditationPageHeader()),
+                mapping=self.mapping
             ))
         else:
-            self.description = t(_("The lab is not accredited, or accreditation has "
-                    "not been configured. "))
+            self.description = t(_(
+                "The lab is not accredited, or accreditation has not been "
+                "configured. "
+            ))
         msg = t(_("All Accredited analysis services are listed here."))
         self.description = "%s<p><br/>%s</p>" % (self.description, msg)
 
@@ -86,8 +88,8 @@ class AccreditationView(AnalysisServicesView):
                          'Price',
                          'MaxTimeAllowed',
                          'DuplicateVariation',
-             ],
-            },
+                         ],
+             },
         ]
 
         if not self.context.bika_setup.getShowPrices():
