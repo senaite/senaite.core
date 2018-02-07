765feb28b13391d2ac58f502c60ded61d924bf70
Ramon Bartl <rb@ridingbytes.com>
2018-02-07 19:44:27 +0100

    ARReports are in portal_catalog now

--- bika/lims/browser/analysisrequest/published_results.py
+++ bika/lims/browser/analysisrequest/published_results.py
@@ -27,7 +27,7 @@ class AnalysisRequestPublishedResults(BikaListingView):
 
     def __init__(self, context, request):
         super(AnalysisRequestPublishedResults, self).__init__(context, request)
-        self.catalog = "bika_catalog"
+        self.catalog = "portal_catalog"
         self.contentFilter = {'portal_type': 'ARReport',
                               'sort_order': 'reverse'}
         self.context_actions = {}
