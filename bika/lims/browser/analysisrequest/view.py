78071d0faa7fc632bee421638ce405efbeb3a49d
Nihadness <1992.nihad@gmail.com>
2018-02-16 13:46:32 +0100

    Do not redirect cancelled AR's to manage results view.

--- bika/lims/browser/analysisrequest/view.py
+++ bika/lims/browser/analysisrequest/view.py
@@ -57,10 +57,13 @@ class AnalysisRequestViewView(BrowserView):
             check_permission(EditFieldResults, self.context) and
             wasTransitionPerformed(self.context, 'receive') and
             not wasTransitionPerformed(self.context, 'verify')):
-            # Redirect to manage results view
-            manage_results_url = self.context.absolute_url() + '/manage_results'
-            self.request.response.redirect(manage_results_url)
-            return
+            # Redirect to manage results view if not cancelled
+            workflow = getToolByName(ar, 'portal_workflow')
+            if workflow.getInfoFor(ar, 'cancellation_state') != "cancelled":
+                manage_results_url = "/".join([self.context.absolute_url(),
+                                               'manage_results'])
+                self.request.response.redirect(manage_results_url)
+                return
 
         # Contacts get expanded for view
         contact = self.context.getContact()
