92d98fc3ee3cc787e64a2c1bf1272f85444b07e0
Ramon Bartl <rb@ridingbytes.com>
2018-02-16 19:39:25 +0100

    Return empty list instead of None

--- bika/lims/browser/bika_listing.py
+++ bika/lims/browser/bika_listing.py
@@ -646,9 +646,9 @@ class BikaListingView(BrowserView):
         state_id = self.request.form.get(key, self.default_review_state)
         states = [r for r in self.review_states if r['id'] == state_id]
         if not states:
-            logger.error("%s.review_states does not contains id='%s'." %
+            logger.error("%s.review_states does not contain id='%s'." %
                          (self, state_id))
-            return None
+            return []
         review_state = states[0] if states else self.review_states[0]
         # set selected state into the request
         self.request['%s_review_state' % self.form_id] = review_state['id']
@@ -1145,8 +1145,7 @@ class BikaListingView(BrowserView):
         query = copy.deepcopy(self.contentFilter)
 
         # contentFilter is allowed in every self.review_state.
-        review_state = getattr(self, "review_state", {})
-        for k, v in review_state.get("contentFilter", {}).items():
+        for k, v in self.review_state.get("contentFilter", {}).items():
             query[k] = v
 
         # set the sort_on criteria
