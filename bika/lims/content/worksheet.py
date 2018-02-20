fb322e4e930a3f4437279bc128cd04813e88e035
Jordi Puiggené <jpuiggene@naralabs.com>
2018-02-20 23:28:31 +0100

    QCAnalyses and Samples number not totaled correctly in Worksheets

--- bika/lims/content/worksheet.py
+++ bika/lims/content/worksheet.py
@@ -389,6 +389,7 @@ class Worksheet(BaseFolder, HistoryAwareMixin):
         # Add the duplicate in the worksheet
         self.setAnalyses(self.getAnalyses() + [ref_analysis, ])
         doActionFor(ref_analysis, 'assign')
+        self.reindexObject()
         return ref_analysis
 
     def nextReferenceAnalysesGroupID(self, reference):
@@ -509,6 +510,7 @@ class Worksheet(BaseFolder, HistoryAwareMixin):
         # Add the duplicate in the worksheet
         self.setAnalyses(self.getAnalyses() + [duplicate, ])
         doActionFor(duplicate, 'assign')
+        self.reindexObject()
 
         return duplicate
 
