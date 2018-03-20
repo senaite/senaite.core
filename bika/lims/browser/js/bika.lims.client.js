5ca83ad1012d7e5b3e9dd10af7d5f1d8b729043d
Juan Gallostra <jgallostra@naralabs.com>
2018-03-20 10:47:03 +0100

    Comment JS function

--- bika/lims/browser/js/bika.lims.client.js
+++ bika/lims/browser/js/bika.lims.client.js
@@ -72,7 +72,16 @@ function ClientSamplesView() {
     that.load = function() {
         $('a.context_action_link').click(print_samples_trigger);
     };
-
+    /**
+    * Function fired when Print Sample Sheets is clicked
+    * It makes a POST of the samples form to print_sampling_sheets.
+    *
+    * To do so, it first cancels the default action of the Print
+    * Samples Sheet anchor. It is a GET to print_sampling_sheets
+    * that would override the POST with the consequent data loss.
+    * Secondly, the form action is overridden before submitting
+    * because by default the POST is made to workflow_action
+    */
     function print_samples_trigger(event){
         event.preventDefault();
         $('form#samples')[0].action = 'print_sampling_sheets';
