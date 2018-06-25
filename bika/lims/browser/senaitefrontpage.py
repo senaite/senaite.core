1b5f3f5a1f55659da1db669a16fcedb1c447cd29
Jordi Puiggené <jpuiggene@naralabs.com>
2018-06-25 16:07:19 +0200

    Added #855 in CHANGES.rst

--- bika/lims/browser/senaitefrontpage.py
+++ bika/lims/browser/senaitefrontpage.py
@@ -25,6 +25,7 @@ class FrontPageView(BrowserView):
         bika_setup = getToolByName(self.context, "bika_setup")
         login_url = '{}/{}'.format(self.portal_url, 'login')
         landingpage = bika_setup.getLandingPage()
+
         # Anonymous Users get either redirected to the std. bika-frontpage or
         # to the custom landing page, which is set in bika_setup. If no landing
         # page setup, then redirect to login page.
@@ -56,9 +57,12 @@ class FrontPageView(BrowserView):
         # Second precedence: Dashboard enabled
         if self.is_dashboard_enabled():
             roles = self.get_user_roles()
+            allowed = ['Manager', 'LabManager', 'LabClerk']
+            if set(roles).intersection(allowed):
+                return self.request.response.redirect(
+                    self.portal_url + "/bika-dashboard")
             if 'Sampler' in roles or 'SampleCoordinator' in roles:
                 return self.request.response.redirect(self.portal_url + "/samples?samples_review_state=to_be_sampled")
-            return self.request.response.redirect(self.portal_url + "/bika-dashboard")
 
         # Third precedence: Custom Landing Page
         if landingpage:
