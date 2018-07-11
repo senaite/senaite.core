cf0aa504cfecfd3549353e2efdaacadd346e2ef4
Espurna <psoliva@naralabs.com>
2018-07-11 12:23:18 +0200

    client permission for batch folder

--- bika/lims/upgrade/v01_02_008.py
+++ bika/lims/upgrade/v01_02_008.py
@@ -10,6 +10,7 @@ from bika.lims import api
 from bika.lims.config import PROJECTNAME as product
 from bika.lims.upgrade import upgradestep
 from bika.lims.upgrade.utils import UpgradeUtils
+from bika.lims import permissions
 
 version = '1.2.8'  # Remember version number in metadata.xml and setup.py
 profile = 'profile-{0}:default'.format(product)
@@ -28,8 +29,20 @@ def upgrade(tool):
         return True
 
     logger.info("Upgrading {0}: {1} -> {2}".format(product, ver_from, version))
+    client_contact_permissions_on_batches_folder(portal)
 
     # -------- ADD YOUR STUFF HERE --------
 
     logger.info("{0} upgraded to version {1}".format(product, version))
     return True
+
+
+def client_contact_permissions_on_batches_folder(portal):
+    """
+    Grants view permissions on batches folder in navtree
+    :param portal: portal object
+    :return: None
+    """
+    mp = portal.batches.manage_permission
+    mp(permissions.View, ['Manager', 'LabManager', 'LabClerk', 'Analyst', 'RegulatoryInspector', 'Client'], 0)
+    portal.batches.reindexObject()
