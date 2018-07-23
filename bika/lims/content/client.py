6139f7d8c1a4c1675a96a97b04c65166c88dd89e
Ramon Bartl <rb@ridingbytes.com>
2018-07-23 11:48:07 +0200

    Removed stupid address validator for Clients and PEP8-ified the code

--- bika/lims/content/client.py
+++ bika/lims/content/client.py
@@ -5,241 +5,253 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-"""Client - the main organisational entity in bika.
-"""
+import sys
+
 from AccessControl import ClassSecurityInfo
-from Products.ATContentTypes.content import schemata
-from Products.Archetypes import atapi
-from Products.Archetypes.utils import DisplayList
-from Products.CMFCore import permissions
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.utils import safe_unicode
-from bika.lims import PMF, bikaMessageFactory as _
-from bika.lims import interfaces
-from bika.lims.config import *
+from bika.lims import _
+from bika.lims import api
+from bika.lims.config import ARIMPORT_OPTIONS
+from bika.lims.config import DECIMAL_MARKS
+from bika.lims.config import EMAIL_SUBJECT_OPTIONS
+from bika.lims.config import PROJECTNAME
 from bika.lims.content.organisation import Organisation
 from bika.lims.interfaces import IClient
-from bika.lims.utils import isActive
-from zope.component import getUtility
+from Products.Archetypes.public import BooleanField
+from Products.Archetypes.public import BooleanWidget
+from Products.Archetypes.public import LinesField
+from Products.Archetypes.public import MultiSelectionWidget
+from Products.Archetypes.public import ReferenceField
+from Products.Archetypes.public import ReferenceWidget
+from Products.Archetypes.public import Schema
+from Products.Archetypes.public import SelectionWidget
+from Products.Archetypes.public import StringField
+from Products.Archetypes.public import StringWidget
+from Products.Archetypes.public import registerType
+from Products.Archetypes.utils import DisplayList
+from Products.ATContentTypes.content import schemata
+from Products.CMFPlone.utils import safe_unicode
 from zope.interface import implements
-from zope.interface.declarations import alsoProvides
-import json
-import sys
-from bika.lims.workflow import getCurrentState, StateFlow, InactiveState
 
-schema = Organisation.schema.copy() + atapi.Schema((
-    atapi.StringField('ClientID',
-        required = 1,
-        searchable = True,
-        validators = ('uniquefieldvalidator', 'standard_id_validator'),
-        widget = atapi.StringWidget(
+
+schema = Organisation.schema.copy() + Schema((
+    StringField(
+        "ClientID",
+        required=1,
+        searchable=True,
+        validators=("uniquefieldvalidator", "standard_id_validator"),
+        widget=StringWidget(
             label=_("Client ID"),
         ),
     ),
-    atapi.BooleanField('BulkDiscount',
-        default = False,
-        write_permission = ManageClients,
-        widget = atapi.BooleanWidget(
+
+    BooleanField(
+        "BulkDiscount",
+        default=False,
+        widget=BooleanWidget(
             label=_("Bulk discount applies"),
         ),
     ),
-    atapi.BooleanField('MemberDiscountApplies',
-        default = False,
-        write_permission = ManageClients,
-        widget = atapi.BooleanWidget(
+
+    BooleanField(
+        "MemberDiscountApplies",
+        default=False,
+        widget=BooleanWidget(
             label=_("Member discount applies"),
         ),
     ),
-    atapi.StringField(
-        'CCEmails',
-        schemata = 'Preferences',
+
+    StringField(
+        "CCEmails",
+        schemata="Preferences",
         mode="rw",
-        widget=atapi.StringWidget(
+        widget=StringWidget(
             label=_("CC Emails"),
             description=_(
                 "Default Emails to CC all published ARs for this client"),
             visible={
-                'edit': 'visible',
-                'view': 'visible',
+                "edit": "visible",
+                "view": "visible",
             },
         ),
     ),
-    atapi.LinesField('EmailSubject',
-        schemata = 'Preferences',
-        default = ['ar', ],
-        vocabulary = EMAIL_SUBJECT_OPTIONS,
-        widget = atapi.MultiSelectionWidget(
+
+    LinesField(
+        "EmailSubject",
+        schemata="Preferences",
+        default=["ar", ],
+        vocabulary=EMAIL_SUBJECT_OPTIONS,
+        widget=MultiSelectionWidget(
             description=_("Items to be included in email subject lines"),
             label=_("Email subject line"),
         ),
     ),
-    atapi.ReferenceField('DefaultCategories',
-        schemata = 'Preferences',
-        required = 0,
-        multiValued = 1,
-        vocabulary = 'getAnalysisCategories',
-        vocabulary_display_path_bound = sys.maxint,
-        allowed_types = ('AnalysisCategory',),
-        relationship = 'ClientDefaultCategories',
-        widget = atapi.ReferenceWidget(
-            checkbox_bound = 0,
+
+    ReferenceField(
+        "DefaultCategories",
+        schemata="Preferences",
+        required=0,
+        multiValued=1,
+        vocabulary="getAnalysisCategories",
+        vocabulary_display_path_bound=sys.maxint,
+        allowed_types=("AnalysisCategory",),
+        relationship="ClientDefaultCategories",
+        widget=ReferenceWidget(
+            checkbox_bound=0,
             label=_("Default categories"),
-            description=_("Always expand the selected categories in client views"),
+            description=_(
+                "Always expand the selected categories in client views"),
         ),
     ),
-    atapi.ReferenceField('RestrictedCategories',
-        schemata = 'Preferences',
-        required = 0,
-        multiValued = 1,
-        vocabulary = 'getAnalysisCategories',
-        validators = ('restrictedcategoriesvalidator',),
-        vocabulary_display_path_bound = sys.maxint,
-        allowed_types = ('AnalysisCategory',),
-        relationship = 'ClientRestrictedCategories',
-        widget = atapi.ReferenceWidget(
-            checkbox_bound = 0,
+
+    ReferenceField(
+        "RestrictedCategories",
+        schemata="Preferences",
+        required=0,
+        multiValued=1,
+        vocabulary="getAnalysisCategories",
+        validators=("restrictedcategoriesvalidator",),
+        vocabulary_display_path_bound=sys.maxint,
+        allowed_types=("AnalysisCategory",),
+        relationship="ClientRestrictedCategories",
+        widget=ReferenceWidget(
+            checkbox_bound=0,
             label=_("Restrict categories"),
             description=_("Show only selected categories in client views"),
         ),
     ),
-    atapi.BooleanField('DefaultDecimalMark',
-        schemata = "Preferences",
-        default = True,
-        widget = atapi.BooleanWidget(
+
+    BooleanField(
+        "DefaultDecimalMark",
+        schemata="Preferences",
+        default=True,
+        widget=BooleanWidget(
             label=_("Default decimal mark"),
-            description=_("The decimal mark selected in Bika Setup will be used."),
+            description=_(
+                "The decimal mark selected in Bika Setup will be used."),
         )
     ),
-    atapi.StringField('DecimalMark',
-        schemata = "Preferences",
+
+    StringField(
+        "DecimalMark",
+        schemata="Preferences",
         vocabulary=DECIMAL_MARKS,
-        default = ".",
-        widget = atapi.SelectionWidget(
+        default=".",
+        widget=SelectionWidget(
             label=_("Custom decimal mark"),
-            description=_("Decimal mark to use in the reports from this Client."),
-            format = 'select',
+            description=_(
+                "Decimal mark to use in the reports from this Client."),
+            format="select",
         )
     ),
 ))
 
-schema['AccountNumber'].write_permission = ManageClients
-schema['title'].widget.visible = False
-schema['description'].widget.visible = False
-schema['EmailAddress'].schemata = 'default'
+schema["title"].widget.visible = False
+schema["description"].widget.visible = False
+schema["EmailAddress"].schemata = "default"
 
-schema.moveField('ClientID', after='Name')
+schema.moveField("ClientID", after="Name")
+
 
 class Client(Organisation):
     implements(IClient)
+
     security = ClassSecurityInfo()
     displayContentsTab = False
     schema = schema
-
     _at_rename_after_creation = True
+
     def _renameAfterCreation(self, check_auto_id=False):
         from bika.lims.idserver import renameAfterCreation
         renameAfterCreation(self)
 
     def Title(self):
-        """ Return the Organisation's Name as its title """
-        name = self.schema['Name'].get(self)
-        return safe_unicode(name).encode('utf-8')
+        """Return the Organisation's Name as its title
+        """
+        name = self.schema["Name"].get(self)
+        return safe_unicode(name).encode("utf-8")
 
-    security.declarePublic('getContactFromUsername')
+    security.declarePublic("getContactFromUsername")
+
     def getContactFromUsername(self, username):
-        for contact in self.objectValues('Contact'):
+        for contact in self.objectValues("Contact"):
             if contact.getUsername() == username:
                 return contact.UID()
 
-    security.declarePublic('getContactUIDForUser')
+    security.declarePublic("getContactUIDForUser")
+
     def getContactUIDForUser(self):
-        """ get the UID of the user associated with the authenticated user
+        """Get the UID of the user associated with the authenticated user
         """
-        membership_tool = getToolByName(self, 'portal_membership')
+        membership_tool = api.get_tool("portal_membership")
         member = membership_tool.getAuthenticatedMember()
         username = member.getUserName()
         r = self.portal_catalog(
-            portal_type = 'Contact',
-            getUsername = username
+            portal_type="Contact",
+            getUsername=username
         )
         if len(r) == 1:
             return r[0].UID
 
+    security.declarePublic("getARImportOptions")
 
-    security.declarePublic('getARImportOptions')
     def getARImportOptions(self):
         return ARIMPORT_OPTIONS
 
-    security.declarePublic('getAnalysisCategories')
+    security.declarePublic("getAnalysisCategories")
+
     def getAnalysisCategories(self):
-        """ return all available analysis categories """
-        bsc = getToolByName(self, 'bika_setup_catalog')
+        """Return all available analysis categories
+        """
+        bsc = api.get_tool("bika_setup_catalog")
         cats = []
-        for st in bsc(portal_type = 'AnalysisCategory',
-                      inactive_state = 'active',
-                      sort_on = 'sortable_title'):
+        for st in bsc(portal_type="AnalysisCategory",
+                      inactive_state="active",
+                      sort_on="sortable_title"):
             cats.append((st.UID, st.Title))
         return DisplayList(cats)
 
     def getContacts(self, only_active=True):
-        """ Return an array containing the contacts from this Client
+        """Return an array containing the contacts from this Client
         """
-        contacts = []
+        contacts = self.objectValues("Contact")
         if only_active:
-            contacts = [c for c in self.objectValues('Contact') if
-                        getCurrentState(c, StateFlow.inactive) == InactiveState.active]
-        else:
-            contacts = self.objectValues('Contact')
-        return contacts;
+            contacts = filter(api.is_active, contacts)
+        return contacts
 
     def getDecimalMark(self):
-        """ Return the decimal mark to be used on reports for this
-            client. If the client has DefaultDecimalMark selected, the
-            Default value from Bika Setup will be returned. Otherwise,
-            will return the value of DecimalMark.
+        """Return the decimal mark to be used on reports for this client
+
+        If the client has DefaultDecimalMark selected, the Default value from
+        the LIMS Setup will be returned.
+
+        Otherwise, will return the value of DecimalMark.
         """
-        if self.getDefaultDecimalMark() == False:
-            return self.Schema()['DecimalMark'].get(self)
+        if self.getDefaultDecimalMark() is False:
+            return self.Schema()["DecimalMark"].get(self)
         return self.bika_setup.getDecimalMark()
 
     def getCountry(self, default=None):
-        """ Return the Country from the Physical or Postal Address
+        """Return the Country from the Physical or Postal Address
         """
         physical_address = self.getPhysicalAddress().get("country", default)
         postal_address = self.getPostalAddress().get("country", default)
         return physical_address or postal_address
 
     def getProvince(self, default=None):
-        """ Return the Province from the Physical or Postal Address
+        """Return the Province from the Physical or Postal Address
         """
         physical_address = self.getPhysicalAddress().get("state", default)
         postal_address = self.getPostalAddress().get("state", default)
         return physical_address or postal_address
 
     def getDistrict(self, default=None):
-        """ Return the Province from the Physical or Postal Address
+        """Return the Province from the Physical or Postal Address
         """
         physical_address = self.getPhysicalAddress().get("district", default)
         postal_address = self.getPostalAddress().get("district", default)
         return physical_address or postal_address
 
-    def validate_address(self, request, field, data):
-        """ Validates the Address Fields
 
-        If country is "Zimbabwe", the province and district fields are required.
+schemata.finalizeATCTSchema(schema, folderish=True, moveDiscussion=False)
 
-        :returns: (str) message if validation fails, otherwise (bool) True
-        """
-        country = data.get("country", None)
-        province = data.get("state", None)
-        district = data.get("district", None)
-
-        if country == "Zimbabwe" and not all([province, district]):
-            return "Province and district fields are mandatory"
-
-        return True
-
-
-schemata.finalizeATCTSchema(schema, folderish = True, moveDiscussion = False)
-
-atapi.registerType(Client, PROJECTNAME)
+registerType(Client, PROJECTNAME)
