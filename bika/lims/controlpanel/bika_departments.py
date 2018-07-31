ab2809af9a39d4a8a71aa00b558124f52f7d53fe
Ramon Bartl <rb@ridingbytes.com>
2018-07-31 10:25:17 +0200

    Refactored Departments Listing

--- bika/lims/controlpanel/bika_departments.py
+++ bika/lims/controlpanel/bika_departments.py
@@ -5,125 +5,172 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-from AccessControl.SecurityInfo import ClassSecurityInfo
-from Products.ATContentTypes.content import schemata
-from Products.Archetypes import atapi
-from Products.Archetypes.utils import DisplayList
-from Products.Archetypes.ArchetypeTool import registerType
-from Products.CMFCore.utils import getToolByName
+import collections
+
+from bika.lims import api
+from bika.lims import bikaMessageFactory as _
 from bika.lims.browser.bika_listing import BikaListingView
 from bika.lims.config import PROJECTNAME
-from bika.lims import bikaMessageFactory as _
-from bika.lims.utils import t
-from bika.lims.content.bikaschema import BikaFolderSchema
 from bika.lims.interfaces import IDepartments
-from plone.app.layout.globals.interfaces import IViewView
+from bika.lims.utils import get_email_link
+from bika.lims.utils import get_link
 from plone.app.content.browser.interfaces import IFolderContentsView
-from plone.app.folder.folder import ATFolder, ATFolderSchema
+from plone.app.folder.folder import ATFolder
+from plone.app.folder.folder import ATFolderSchema
+from plone.app.layout.globals.interfaces import IViewView
+from Products.Archetypes import atapi
+from Products.Archetypes.utils import DisplayList
+from Products.ATContentTypes.content import schemata
 from zope.interface.declarations import implements
-from operator import itemgetter
+
 
 class DepartmentsView(BikaListingView):
     implements(IFolderContentsView, IViewView)
 
     def __init__(self, context, request):
         super(DepartmentsView, self).__init__(context, request)
-        self.catalog = 'bika_setup_catalog'
-        self.contentFilter = {'portal_type': 'Department',
-                              'sort_on': 'sortable_title'}
-        self.context_actions = {_('Add'):
-                                {'url': 'createObject?type_name=Department',
-                                 'icon': '++resource++bika.lims.images/add.png'}}
+
+        self.catalog = "bika_setup_catalog"
+
+        self.contentFilter = {
+            "portal_type": "Department",
+            "sort_order": "ascending",
+            "sort_on": "sortable_title"
+        }
+
+        self.context_actions = {
+            _("Add"): {
+                "url": "createObject?type_name=Department",
+                "permission": "Add portal content",
+                "icon": "++resource++bika.lims.images/add.png"}
+        }
+
         self.title = self.context.translate(_("Lab Departments"))
-        self.icon = self.portal_url + "/++resource++bika.lims.images/department_big.png"
-        self.description = ""
+        self.icon = "{}/{}".format(
+            self.portal_url,
+            "/++resource++bika.lims.images/department_big.png"
+        )
+
         self.show_sort_column = False
         self.show_select_row = False
         self.show_select_column = True
         self.pagesize = 25
 
-        self.columns = {
-            'Title': {'title': _('Department'),
-                      'index':'sortable_title'},
-            'Description': {'title': _('Description'),
-                            'index': 'description',
-                            'toggle': True},
-            'Manager': {'title': _('Manager'),
-                        'index': 'getManagerName',
-                        'toggle': True},
-            'ManagerPhone': {'title': _('Manager Phone'),
-                             'index': 'getManagerPhone',
-                             'toggle': True},
-            'ManagerEmail': {'title': _('Manager Email'),
-                             'index': 'getManagerEmail',
-                             'toggle': True},
-        }
+        self.columns = collections.OrderedDict((
+            ("Title", {
+                "title": _("Department"),
+                "index": "sortable_title"}),
+            ("Description", {
+                "title": _("Description"),
+                "index": "Description",
+                "toggle": True}),
+            ("Manager", {
+                "title": _("Manager"),
+                "index": "getManagerName",
+                "toggle": True}),
+            ("ManagerPhone", {
+                "title": _("Manager Phone"),
+                "index": "getManagerPhone",
+                "toggle": True}),
+            ("ManagerEmail", {
+                "title": _("Manager Email"),
+                "index": "getManagerEmail",
+                "toggle": True}),
+        ))
 
         self.review_states = [
-            {'id':'default',
-             'title': _('Active'),
-             'contentFilter': {'inactive_state': 'active'},
-             'transitions': [{'id':'deactivate'}, ],
-             'columns': ['Title',
-                         'Description',
-                         'Manager',
-                         'ManagerPhone',
-                         'ManagerEmail']},
-            {'id':'inactive',
-             'title': _('Dormant'),
-             'contentFilter': {'inactive_state': 'inactive'},
-             'transitions': [{'id':'activate'}, ],
-             'columns': ['Title',
-                         'Description',
-                         'Manager',
-                         'ManagerPhone',
-                         'ManagerEmail']},
-            {'id':'all',
-             'title': _('All'),
-             'contentFilter':{},
-             'columns': ['Title',
-                         'Description',
-                         'Manager',
-                         'ManagerPhone',
-                         'ManagerEmail']},
+            {
+                "id": "default",
+                "title": _("Active"),
+                "contentFilter": {"inactive_state": "active"},
+                "transitions": [{"id": "deactivate"}, ],
+                "columns": self.columns.keys(),
+            }, {
+                "id": "inactive",
+                "title": _("Dormant"),
+                "contentFilter": {"inactive_state": "inactive"},
+                "transitions": [{"id": "activate"}, ],
+                "columns": self.columns.keys(),
+            }, {
+                "id": "all",
+                "title": _("All"),
+                "contentFilter": {},
+                "columns": self.columns.keys(),
+            },
         ]
 
-    def folderitems(self):
-        items = BikaListingView.folderitems(self)
-        for x in range(len(items)):
-            if not items[x].has_key('obj'): continue
-            obj = items[x]['obj']
-            items[x]['Description'] = obj.Description()
-            items[x]['Manager'] = obj.getManagerName()
-            items[x]['ManagerPhone'] = obj.getManagerPhone()
-            items[x]['ManagerEmail'] = obj.getManagerEmail()
+    def before_render(self):
+        """Before template render hook
+        """
+        # Don't allow any context actions
+        self.request.set("disable_border", 1)
 
-            items[x]['replace']['Title'] = "<a href='%s'>%s</a>" % \
-                 (items[x]['url'], items[x]['Title'])
+    def folderitem(self, obj, item, index):
+        """Service triggered each time an item is iterated in folderitems.
+        The use of this service prevents the extra-loops in child objects.
+        :obj: the instance of the class to be foldered
+        :item: dict containing the properties of the object to be used by
+            the template
+        :index: current index of the item
+        """
+        title = obj.Title()
+        description = obj.Description()
+        url = obj.absolute_url()
 
-            if items[x]['ManagerEmail']:
-                items[x]['replace']['ManagerEmail'] = "<a href='%s'>%s</a>"%\
-                     ('mailto:%s' % items[x]['ManagerEmail'],
-                      items[x]['ManagerEmail'])
+        item["replace"]["Title"] = get_link(url, value=title)
+        item["Description"] = description
 
+        manager = obj.getManager()
+        manager_name = obj.getManagerName()
+        manager_phone = obj.getManagerPhone()
+        manager_email = obj.getManagerEmail()
 
-        return items
+        if manager:
+            item["Manager"] = manager_name
+            item["replace"]["Manager"] = get_link(
+                manager.absolute_url(), manager_name)
+        else:
+            item["Manager"] = ""
+
+        if manager_email:
+            item["ManagerEmail"] = manager_email
+            item["replace"]["ManagerEmail"] = get_email_link(
+                manager_email, value=manager_email)
+        else:
+            item["ManagerEmail"] = ""
+
+        item["ManagerPhone"] = manager_phone
+
+        return item
+
 
 schema = ATFolderSchema.copy()
+
+
 class Departments(ATFolder):
     implements(IDepartments)
     displayContentsTab = False
     schema = schema
 
-    def getContacts(self):
-        bsc = getToolByName(self, 'bika_setup_catalog')
-        # fallback - all Lab Contacts
-        pairs = []
-        for contact in bsc(portal_type = 'LabContact',
-                           inactive_state = 'active',
-                           sort_on = 'sortable_title'):
-            pairs.append((contact.UID, contact.Title))
+    def getContacts(self, active_only=True):
+        catalog = api.get_tool("bika_setup_catalog")
+        query = {
+            "portal_type": "LabContact",
+            "sort_on": "sortable_title",
+            "sort_order": "ascending"
+        }
+        results = catalog(query)
+
+        # XXX  Better directly filter in the catalog query as soon as we have
+        #      the active/inactive state in the primary workflow
+        if active_only:
+            results = filter(api.is_active, results)
+
+        pairs = map(
+            lambda brain: (brain.UID, brain.Title), results)
+
         return DisplayList(pairs)
 
-schemata.finalizeATCTSchema(schema, folderish = True, moveDiscussion = False)
+
+schemata.finalizeATCTSchema(schema, folderish=True, moveDiscussion=False)
 atapi.registerType(Departments, PROJECTNAME)
