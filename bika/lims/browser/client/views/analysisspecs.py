3beba4c2340d14fb0e38896cf1e4aa900010690d
Jordi Puiggené <jpuiggene@naralabs.com>
2018-08-11 11:22:18 +0200

    Cleanup. Remove code not used anywhere

--- bika/lims/browser/client/views/analysisspecs.py
+++ bika/lims/browser/client/views/analysisspecs.py
@@ -5,14 +5,10 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.utils import _createObjectByType
 from bika.lims import api
 from bika.lims import bikaMessageFactory as _
-from bika.lims.browser import BrowserView
 from bika.lims.controlpanel.bika_analysisspecs import BaseAnalysisSpecsView
 from bika.lims.permissions import AddAnalysisSpec
-from bika.lims.utils import tmpID
 
 
 class ClientAnalysisSpecsView(BaseAnalysisSpecsView):
@@ -27,37 +23,3 @@ class ClientAnalysisSpecsView(BaseAnalysisSpecsView):
         mtool = api.get_tool("portal_membership")
         if mtool.checkPermission(AddAnalysisSpec, self.context):
             del self.context_actions[_("Add")]
-
-
-class SetSpecsToLabDefaults(BrowserView):
-    """ Remove all client specs, and add copies of all lab specs
-    """
-
-    def __call__(self):
-        form = self.request.form
-        bsc = getToolByName(self.context, 'bika_setup_catalog')
-
-        # find and remove existing specs
-        cs = bsc(portal_type='AnalysisSpec',
-                 getClientUID=self.context.UID())
-        if cs:
-            self.context.manage_delObjects([s.id for s in cs])
-
-        # find and duplicate lab specs
-        ls = bsc(portal_type='AnalysisSpec',
-                 getClientUID=self.context.bika_setup.bika_analysisspecs.UID())
-        ls = [s.getObject() for s in ls]
-        for labspec in ls:
-            clientspec = _createObjectByType(
-                "AnalysisSpec", self.context, tmpID())
-            clientspec.processForm()
-            clientspec.edit(
-                SampleType=labspec.getSampleType(),
-                ResultsRange=labspec.getResultsRange(),
-            )
-        translate = self.context.translate
-        message = _("Analysis specifications reset to lab defaults.")
-        self.context.plone_utils.addPortalMessage(message, 'info')
-        self.request.RESPONSE.redirect(self.context.absolute_url() +
-                                       "/analysisspecs")
-        return
