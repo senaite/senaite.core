c59e32172769138233d9d50c25f831116ba3c3c6
Ramon Bartl <rb@ridingbytes.com>
2018-08-13 14:57:37 +0200

    Removed "Attach" report option from Attachments

--- bika/lims/upgrade/v01_02_009.py
+++ bika/lims/upgrade/v01_02_009.py
@@ -5,6 +5,7 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
+from bika.lims import api
 from bika.lims import logger
 from bika.lims.config import PROJECTNAME as product
 from bika.lims.upgrade import upgradestep
@@ -28,6 +29,24 @@ def upgrade(tool):
     logger.info("Upgrading {0}: {1} -> {2}".format(product, ver_from, version))
 
     # -------- ADD YOUR STUFF HERE --------
+    migrate_attachment_report_options(portal)
 
     logger.info("{0} upgraded to version {1}".format(product, version))
     return True
+
+
+def migrate_attachment_report_options(portal):
+    """Migrate Attachments with the report option "a" (attach in report)
+       to the option to "i" (ignore in report)
+    """
+    attachments = api.search({"portal_type": "Attachment"})
+    total = len(attachments)
+    logger.info("Migrating 'Attach to Report' -> 'Ingore in Report' "
+                "for %d attachments" % total)
+    for num, attachment in enumerate(attachments):
+        obj = api.get_object(attachment)
+
+        if obj.getReportOption() in ["a", ""]:
+            obj.setReportOption("i")
+            obj.reindexObject()
+            logger.info("Migrated Attachment %s" % obj.getTextTitle())
