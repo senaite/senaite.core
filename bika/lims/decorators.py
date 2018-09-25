f6463e41ed695a1e9d916470c38d38703a843eab
Ramon Bartl <rb@ridingbytes.com>
2018-09-25 17:25:08 +0200

    Added profile decorator

--- bika/lims/decorators.py
+++ bika/lims/decorators.py
@@ -5,7 +5,10 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
+import cProfile
 import json
+import os
+from functools import wraps
 
 
 def returns_json(func):
@@ -18,3 +21,26 @@ def returns_json(func):
         result = func(*args, **kwargs)
         return json.dumps(result)
     return decorator
+
+
+def profileit(path=None):
+    """cProfile decorator to profile a function
+
+    :param path: output file path
+    :type path: str
+    :return: Function
+    """
+
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            prof = cProfile.Profile()
+            retval = prof.runcall(func, *args, **kwargs)
+            if path is not None:
+                print prof.print_stats()
+                prof.dump_stats(os.path.expanduser(path))
+            else:
+                print prof.print_stats()
+            return retval
+        return wrapper
+    return inner
