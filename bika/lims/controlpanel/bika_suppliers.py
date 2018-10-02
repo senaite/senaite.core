d7ee0497e7c12bb88648f30d10e2b09b246c4b1a
Ramon Bartl <rb@ridingbytes.com>
2018-10-02 23:26:39 +0200

    Refactored suppliers listing view

--- bika/lims/controlpanel/bika_suppliers.py
+++ bika/lims/controlpanel/bika_suppliers.py
@@ -5,88 +5,132 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-from AccessControl import ClassSecurityInfo
-from Products.ATContentTypes.content import schemata
-from Products.Archetypes import atapi
-from Products.Archetypes.ArchetypeTool import registerType
-from Products.CMFCore import permissions
-from Products.CMFCore.utils import getToolByName
-from bika.lims.browser import BrowserView
+import collections
+
+from bika.lims import bikaMessageFactory as _
 from bika.lims.browser.bika_listing import BikaListingView
 from bika.lims.config import PROJECTNAME
-from bika.lims import bikaMessageFactory as _
-from bika.lims.utils import t
-from plone.app.layout.globals.interfaces import IViewView
-from bika.lims.content.bikaschema import BikaFolderSchema
-from plone.app.content.browser.interfaces import IFolderContentsView
-from plone.app.folder.folder import ATFolder, ATFolderSchema
 from bika.lims.interfaces import ISuppliers
+from bika.lims.utils import get_link
+from plone.app.content.browser.interfaces import IFolderContentsView
+from plone.app.folder.folder import ATFolder
+from plone.app.folder.folder import ATFolderSchema
+from plone.app.layout.globals.interfaces import IViewView
+from Products.Archetypes import atapi
+from Products.ATContentTypes.content import schemata
 from zope.interface.declarations import implements
+
+# TODO: Separate content and view into own modules!
+
 
 class SuppliersView(BikaListingView):
     implements(IFolderContentsView, IViewView)
+
     def __init__(self, context, request):
         super(SuppliersView, self).__init__(context, request)
-        self.catalog = 'bika_setup_catalog'
-        self.contentFilter = {'portal_type': 'Supplier', 
-                              'sort_on': 'sortable_title'}
-        self.context_actions = {_('Add'):
-                                {'url': 'createObject?type_name=Supplier',
-                                 'icon': '++resource++bika.lims.images/add.png'}}
+
+        self.catalog = "bika_setup_catalog"
+
+        self.contentFilter = {
+            "portal_type": "Supplier",
+            "sort_on": "sortable_title",
+            "sort_order": "ascending",
+        }
+
+        self.context_actions = {
+            _("Add"): {
+                "url": "createObject?type_name=Supplier",
+                "permission": "Add portal content",
+                "icon": "++resource++bika.lims.images/add.png"}
+        }
+
         self.title = self.context.translate(_("Suppliers"))
-        self.icon = "++resource++bika.lims.images/supplier_big.png"
+        self.icon = "{}/{}".format(
+            self.portal_url,
+            "/++resource++bika.lims.images/supplier_big.png"
+        )
+
         self.description = ""
         self.show_sort_column = False
         self.show_select_row = False
         self.show_select_column = True
         self.pagesize = 25
 
-        self.columns = {
-            'Name': {'title': _('Name'),
-                     'index': 'getName'},
-            'Email': {'title': _('Email'),
-                      'toggle': True},
-            'Phone': {'title': _('Phone'),
-                      'toggle': True},
-            'Fax': {'title': _('Fax'),
-                    'toggle': True},
-        }
+        self.columns = collections.OrderedDict((
+            ("Name", {
+                "title": _("Name"),
+                "index": "getName"}),
+            ("Email", {
+                "title": _("Email"),
+                "toggle": True}),
+            ("Phone", {
+                "title": _("Phone"),
+                "toggle": True}),
+            ("Fax", {"title": _("Fax"),
+                     "toggle": True}),
+        ))
+
         self.review_states = [
-            {'id':'default',
-             'title': _('Active'),
-             'contentFilter': {'inactive_state': 'active'},
-             'transitions': [{'id':'deactivate'}, ],
-             'columns': ['Name', 'Email', 'Phone', 'Fax']},
-            {'id':'inactive',
-             'title': _('Dormant'),
-             'contentFilter': {'inactive_state': 'inactive'},
-             'transitions': [{'id':'activate'}, ],
-             'columns': ['Name', 'Email', 'Phone', 'Fax']},
-            {'id':'all',
-             'title': _('All'),
-             'contentFilter':{},
-             'columns': ['Name', 'Email', 'Phone', 'Fax']},
+            {
+                "id": "default",
+                "title": _("Active"),
+                "contentFilter": {"inactive_state": "active"},
+                "transitions": [{"id": "deactivate"}, ],
+                "columns": self.columns.keys(),
+            }, {
+                "id": "inactive",
+                "title": _("Dormant"),
+                "contentFilter": {"inactive_state": "inactive"},
+                "transitions": [{"id": "activate"}, ],
+                "columns": self.columns.keys(),
+            }, {
+                "id": "all",
+                "title": _("All"),
+                "contentFilter": {},
+                "columns": self.columns.keys(),
+            },
         ]
 
-    def folderitems(self):
-        items = BikaListingView.folderitems(self)
-        for x in range(len(items)):
-            if not items[x].has_key('obj'): continue
-            obj = items[x]['obj']
-            items[x]['Name'] = obj.getName()
-            items[x]['Email'] = obj.getEmailAddress()
-            items[x]['Phone'] = obj.getPhone()
-            items[x]['Fax'] = obj.getFax()
-            items[x]['replace']['Name'] = "<a href='%s'>%s</a>" % \
-                 (items[x]['url'], items[x]['Name'])
+    def before_render(self):
+        """Before template render hook
+        """
+        # Don't allow any context actions
+        self.request.set("disable_border", 1)
 
-        return items
+    def folderitem(self, obj, item, index):
+        """Service triggered each time an item is iterated in folderitems.
+
+        The use of this service prevents the extra-loops in child objects.
+
+        :obj: the instance of the class to be foldered
+        :item: dict containing the properties of the object to be used by
+            the template
+        :index: current index of the item
+        """
+
+        name = obj.getName()
+        email = obj.getEmailAddress()
+        phone = obj.getPhone()
+        fax = obj.getFax()
+        url = obj.absolute_url()
+
+        item["Name"] = name
+        item["Email"] = email
+        item["Phone"] = phone
+        item["Fax"] = fax
+        item["replace"]["Name"] = get_link(url, value=name)
+
+        return item
+
 
 schema = ATFolderSchema.copy()
+
+
 class Suppliers(ATFolder):
     implements(ISuppliers)
     displayContentsTab = False
     schema = schema
 
-schemata.finalizeATCTSchema(schema, folderish = True, moveDiscussion = False)
+
+schemata.finalizeATCTSchema(schema, folderish=True, moveDiscussion=False)
 atapi.registerType(Suppliers, PROJECTNAME)
