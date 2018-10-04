609a2876203bc1fdc98feb658ad62e91ee855573
Jordi Puiggené <jpuiggene@naralabs.com>
2018-10-04 20:06:44 +0200

    Remove portalMessage related with invalid and/or retest ARs

--- bika/lims/browser/analysisrequest/results_not_requested.py
+++ bika/lims/browser/analysisrequest/results_not_requested.py
@@ -6,21 +6,13 @@
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
 from AccessControl import getSecurityManager
-from bika.lims import bikaMessageFactory as _
-from bika.lims.utils import t
-from bika.lims.permissions import *
-from bika.lims.browser.analysisrequest import AnalysisRequestManageResultsView
-from bika.lims.content.analysisrequest import schema as AnalysisRequestSchema
-from bika.lims.utils import to_utf8
-from bika.lims.workflow import doActionFor
-from plone.app.layout.globals.interfaces import IViewView
-from DateTime import DateTime
-from Products.Archetypes import PloneMessageFactory as PMF
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from bika.lims.browser.analysisrequest import AnalysisRequestManageResultsView
+from bika.lims.permissions import *
+from plone.app.layout.globals.interfaces import IViewView
 from zope.interface import implements
 
-import plone
 
 class AnalysisRequestResultsNotRequestedView(AnalysisRequestManageResultsView):
     implements(IViewView)
@@ -29,26 +21,6 @@ class AnalysisRequestResultsNotRequestedView(AnalysisRequestManageResultsView):
     def __call__(self):
         ar = self.context
         workflow = getToolByName(ar, 'portal_workflow')
-
-        # If is a retracted AR, show the link to child AR and show a warn msg
-        if workflow.getInfoFor(ar, 'review_state') == 'invalid':
-            childar = ar.getRetest() or None
-            childid = childar and childar.getId() or None
-            message = _('This Analysis Request has been withdrawn and is shown '
-                        'for trace-ability purposes only. Retest: ${retest_child_id}.',
-                        mapping={"retest_child_id":childid if childid else ''})
-            self.context.plone_utils.addPortalMessage(message, 'warning')
-
-        # If is an AR automatically generated due to a Retraction, show it's
-        # parent AR information
-        invalidated = ar.getInvalidated()
-        if invalidated:
-            message = _(
-                'This Analysis Request has been generated automatically due to '
-                'the retraction of the Analysis Request ${retracted_request_id}.',
-                mapping={"retracted_request_id": invalidated.getId()})
-            self.context.plone_utils.addPortalMessage(message, 'info')
-
         can_do = getSecurityManager().checkPermission(ResultsNotRequested, ar)
         if workflow.getInfoFor(ar, 'cancellation_state') == "cancelled":
             self.request.response.redirect(ar.absolute_url())
--- bika/lims/browser/analysisrequest/view.py
+++ bika/lims/browser/analysisrequest/view.py
@@ -9,8 +9,8 @@ from AccessControl import getSecurityManager
 from DateTime import DateTime
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from bika.lims import bikaMessageFactory as _
 from bika.lims import api
+from bika.lims import bikaMessageFactory as _
 from bika.lims.browser import BrowserView
 from bika.lims.browser.analyses import AnalysesView
 from bika.lims.browser.analyses import QCAnalysesView
@@ -144,27 +144,6 @@ class AnalysisRequestViewView(BrowserView):
                 message = "General Retract Done.  Submit this AR manually."
                 self.addMessage(message, 'warning')
 
-        # If is a retracted AR, show the link to child AR and show a warn msg
-        if workflow.getInfoFor(ar, 'review_state') == 'invalid':
-            childar = ar.getRetest() or None
-            message = _('These results have been withdrawn and are '
-                        'listed here for trace-ability purposes. Please follow '
-                        'the link to the retest')
-            if childar:
-                message = (message + " %s.") % childar.getId()
-            else:
-                message = message + "."
-            self.addMessage(message, 'warning')
-        # If is an AR automatically generated due to a Retraction, show it's
-        # parent AR information
-        invalidated = ar.getInvalidated()
-        if invalidated:
-            message = _('This Analysis Request has been '
-                        'generated automatically due to '
-                        'the retraction of the Analysis '
-                        'Request ${retracted_request_id}.',
-                        mapping={'retracted_request_id': invalidated.getId()})
-            self.addMessage(message, 'info')
         self.renderMessages()
         return self.template()
 
