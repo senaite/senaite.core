4d3b9464007b5c9cffc8f1ad0a9cf5d8f64e0d9f
Jordi Puiggené <jpuiggene@naralabs.com>
2018-10-04 20:25:29 +0200

    Cleanup

--- bika/lims/browser/viewlets/analysisrequest.py
+++ bika/lims/browser/viewlets/analysisrequest.py
@@ -2,14 +2,13 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets import ViewletBase
 
 
+class InvalidAnalysisRequestViewlet(ViewletBase):
+    """ Current Analysis Request is invalid and display the link to the retest
+    """
+    template = ViewPageTemplateFile("templates/invalid_ar_viewlet.pt")
+
+
 class RetestAnalysisRequestViewlet(ViewletBase):
     """ Current Analysis Request is a retest. Display the link to the invalid
     """
     template = ViewPageTemplateFile("templates/retest_ar_viewlet.pt")
-
-    def __init__(self, context, request, view, manager=None):
-        super(RetestAnalysisRequestViewlet, self).__init__(
-            context, request, view, manager=manager)
-        self.context = context
-        self.request = request
-        self.view = view
