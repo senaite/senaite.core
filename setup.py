0d52cd2bd0f07707b5823b6092e106ac7f308bdb
Jordi Puiggené <jpuiggene@naralabs.com>
2018-10-24 10:54:46 +0200

    Python 2.x is not supported by WeasyPrint v43. Pinned version: 0.42.3

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
         'plone.app.iterate',
         'magnitude',
         'jarn.jsi18n',
-        'WeasyPrint',
         'collective.progressbar',
         'plone.app.dexterity',
         'plone.app.relationfield',
@@ -60,6 +59,8 @@ setup(
         'plone.resource',
         'CairoSVG==1.0.20',
         'zopyx.txng3.ext==3.4.0'
+        # Python 2.x is not supported by WeasyPrint v43
+        'WeasyPrint==0.42.3',
     ],
     extras_require={
         'test': [
