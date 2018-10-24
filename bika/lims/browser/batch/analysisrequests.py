078b9d1c1dfe1c9d30ee2d4b96173e3419c72ca4
Jordi Puiggené <jpuiggene@naralabs.com>
2018-10-24 09:40:25 +0200

    Removal of needless code from Batch/AnalysisRequests listing

--- bika/lims/browser/batch/analysisrequests.py
+++ bika/lims/browser/batch/analysisrequests.py
@@ -5,14 +5,11 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from bika.lims import api
 from bika.lims.browser.analysisrequest import AnalysisRequestsView as BaseView
 
 
 class AnalysisRequestsView(BaseView):
-    template = ViewPageTemplateFile(
-        "../analysisrequest/templates/analysisrequests.pt")
 
     def __init__(self, context, request):
         super(AnalysisRequestsView, self).__init__(context, request)
@@ -21,15 +18,3 @@ class AnalysisRequestsView(BaseView):
                               'sort_on': 'created',
                               'sort_order': 'reverse',
                               'cancellation_state':'active'}
-
-    def getMemberDiscountApplies(self):
-        client = self.context.getClient()
-        return client and client.getMemberDiscountApplies() or False
-
-    def getRestrictedCategories(self):
-        client = self.context.getClient()
-        return client and client.getRestrictedCategories() or []
-
-    def getDefaultCategories(self):
-        client = self.context.getClient()
-        return client and client.getDefaultCategories() or []
