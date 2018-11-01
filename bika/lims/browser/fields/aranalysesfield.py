90e1ca3a6a3bbfda518e40b86c3631713d4bfbf0
Jordi Puiggené <jpuiggene@naralabs.com>
2018-11-01 22:08:17 +0100

    ARAnalysesField.get redux (removal of "retracted" param)
    "retracted" param in kwargs is not used anywhere except in `getLate`
    function. Hence, better to keep the function less complex and just
    add the "retracted" check in `getLate` function.

--- bika/lims/browser/fields/aranalysesfield.py
+++ bika/lims/browser/fields/aranalysesfield.py
@@ -59,43 +59,22 @@ class ARAnalysesField(ObjectField):
         """Returns a list of Analyses assigned to this AR
 
         Return a list of catalog brains unless `full_objects=True` is passed.
-        Overrides "ViewRetractedAnalyses" when `retracted=True` is passed.
         Other keyword arguments are passed to bika_analysis_catalog
 
         :param instance: Analysis Request object
-        :param kwargs: Keyword arguments to be passed to control the output
+        :param kwargs: Keyword arguments to inject in the search query
         :returns: A list of Analysis Objects/Catalog Brains
         """
-        full_objects = kwargs and kwargs.get("full_objects", False)
-        reflexed = kwargs and kwargs.get("get_reflexed", True)
-        retracted = kwargs and kwargs.get("retracted", True)
-        if retracted:
-            mtool = getToolByName(instance, 'portal_membership')
-            retracted = mtool.checkPermission(ViewRetractedAnalyses, instance)
-
         catalog = getToolByName(instance, CATALOG_ANALYSIS_LISTING)
         query = dict(
             [(k, v) for k, v in kwargs.items() if k in catalog.indexes()])
         query['portal_type'] = "Analysis"
         query['getRequestUID'] = api.get_uid(instance)
         analyses = catalog(query)
-        if not full_objects and reflexed and retracted:
+        if not kwargs.get("full_objects", False):
             return analyses
 
-        out_analyses = list()
-        for analysis in analyses:
-            analysis_obj = None
-            if not retracted and analysis.review_state == 'retracted':
-                continue
-            if not reflexed:
-                analysis_obj = api.get_object(analysis)
-                if analysis_obj.getReflexRuleActionsTriggered():
-                    continue
-            if full_objects:
-                analysis = analysis_obj and analysis_obj or api.get_object(
-                    analysis)
-            out_analyses.append(analysis)
-        return out_analyses
+        return map(api.get_object, analyses)
 
     security.declarePrivate('set')
 
