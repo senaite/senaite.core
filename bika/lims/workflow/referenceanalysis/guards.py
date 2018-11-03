a004790aca47f8b670a6297c2f98c38d5652b9f0
Jordi Puiggené <jpuiggene@naralabs.com>
2018-11-03 11:08:30 +0100

    Import the whole guards package from analysis in referenceanalysis/guards

--- bika/lims/workflow/referenceanalysis/guards.py
+++ bika/lims/workflow/referenceanalysis/guards.py
@@ -5,10 +5,10 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-from bika.lims.workflow.analysis.guards import guard_submit as base_submit
+from bika.lims.workflow.analysis import guards as analysis_guards
 
 
 def guard_submit(reference_analysis):
     """Return whether the transition "submit" can be performed or not
     """
-    return base_submit(reference_analysis)
+    return analysis_guards.guard_submit(reference_analysis)
