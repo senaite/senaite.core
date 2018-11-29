4fb517eaeeed9b135773d766868e8c9fe4fd6cda
Ramon Bartl <rb@ridingbytes.com>
2018-11-29 15:06:39 +0100

    Don't allow to edit Analyst and retest in Analyses Listing

--- bika/lims/browser/analyses/view.py
+++ bika/lims/browser/analyses/view.py
@@ -702,10 +702,6 @@ class AnalysesView(BikaListingView):
         calculation_uid = analysis_brain.getCalculationUID
         has_calculation = calculation_uid and True or False
         item['calculation'] = has_calculation
-        if is_editable and (not has_calculation or interim_fields):
-            # If the analysis is editable and doesn't have a calculation or it
-            # does, but has interim fields, it must be re-testable.
-            item['allow_edit'].append('retested')
 
     def _folder_item_method(self, analysis_brain, item):
         """Fills the analysis' method to the item passed in.
@@ -775,10 +771,6 @@ class AnalysesView(BikaListingView):
         # Analyst is editable
         item['Analyst'] = obj.getAnalyst or api.get_current_user().id
         item['choices']['Analyst'] = self.get_analysts()
-
-        # allow edit only if the Analysis is not assigned to a worksheet
-        if api.get_workflow_status_of(obj) == "unassigned":
-            item['allow_edit'].append('Analyst')
 
     def _folder_item_attachments(self, obj, item):
         item['Attachments'] = ''
--- bika/lims/content/abstractanalysis.py
+++ bika/lims/content/abstractanalysis.py
@@ -959,7 +959,7 @@ class AbstractAnalysis(AbstractBaseAnalysis):
         worksheet = self.getWorksheet()
         if worksheet:
             return worksheet.getAnalyst() or ""
-        return self.getField("Analyst").get(self)
+        return ""
 
     @security.public
     def getAnalystName(self):
