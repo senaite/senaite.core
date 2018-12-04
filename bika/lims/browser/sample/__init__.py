cd05ef62dab65aa242998b6cd917adefb8d65947
Jordi Puiggené <jpuiggene@naralabs.com>
2018-12-05 00:16:56 +0100

    Remove ajax's getsampletypeinfo, not used anywhere

--- bika/lims/browser/sample/__init__.py
+++ bika/lims/browser/sample/__init__.py
@@ -5,7 +5,6 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-from .ajax import ajaxGetSampleTypeInfo
 from .analyses import SampleAnalysesView
 from .partitions import SamplePartitionsView
 from .edit import SampleEdit
