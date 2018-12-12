24949fb271fa4bcc2b464466482b5f6c2784e827
Jordi Puiggené <jpuiggene@naralabs.com>
2018-12-12 22:31:30 +0100

    guard signature for rollback_to_open (Worksheet) was wrong

--- bika/lims/workflow/worksheet/events.py
+++ bika/lims/workflow/worksheet/events.py
@@ -20,25 +20,3 @@ def after_submit(obj):
     # all the analyses that contain have been submitted manually after
     # the results input
     wf.doActionFor(obj, 'attach')
-
-
-def after_verify(obj):
-    """Method triggered after a 'verify' transition for the Worksheet
-    passed in is performed. Responsible of triggering cascade actions to
-    associated analyses.
-    This function is called automatically by
-    bika.lims.workflow.AfterTransitionEventHandler
-    :param obj: Worksheet affected by the transition
-    :type obj: Worksheet
-    """
-    pass
-
-
-def after_retract(obj):
-    pass
-
-
-def after_rollback_to_receive(analysis_request):
-    """Function triggered after rollback_to_receive transition finishes
-    """
-    pass
