ab7d9b16ef21f56303204b490c0933cbfa01b9f7
Jordi Puiggené <jpuiggene@naralabs.com>
2018-12-20 21:42:18 +0100

    Add cancellation_state indexer for Analysis Request content type

--- bika/lims/catalog/indexers/__init__.py
+++ bika/lims/catalog/indexers/__init__.py
@@ -4,3 +4,17 @@
 #
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
+
+from bika.lims import api
+from bika.lims.interfaces import IAnalysisRequest
+from bika.lims.interfaces.analysis import IRequestAnalysis
+from plone.indexer import indexer
+
+@indexer(IAnalysisRequest, IRequestAnalysis)
+def cancellation_state(instance):
+    """Acts as a mask for cancellation_workflow for those content types that are
+    not bound to this workflow. Returns 'active' or 'cancelled'
+    """
+    if api.get_workflow_status_of(instance) == "cancelled":
+        return "cancelled"
+    return "active"
--- bika/lims/catalog/indexers/requestanalysis.py
+++ bika/lims/catalog/indexers/requestanalysis.py
@@ -11,13 +11,3 @@ def getAncestorsUIDs(instance):
     request = instance.getRequest()
     parents = map(lambda ar: api.get_uid(ar), request.getAncestors())
     return [api.get_uid(request)] + parents
-
-
-@indexer(IRequestAnalysis)
-def cancellation_state(instance):
-    """Acts as a mask for cancellation_workflow that is not bound to Analysis
-    content type. Returns 'active' or 'cancelled'
-    """
-    if api.get_workflow_status_of(instance) == "cancelled":
-        return "cancelled"
-    return "active"
