b22c0b991e8c9cd1795112fdac42736eb2ce45dc
Jordi Puiggené <jpuiggene@naralabs.com>
2018-12-20 23:40:58 +0100

    Temporary fix for guard_attach_transition (Analysis Request)

--- bika/lims/skins/bika/guard_attach_transition.py
+++ bika/lims/skins/bika/guard_attach_transition.py
@@ -17,19 +17,17 @@
 
 workflow = context.portal_workflow
 
-# Can't do anything to the object if it's cancelled
-# reference and duplicate analyses don't have cancellation_state
-#if context.portal_type == "Analysis":
-if workflow.getInfoFor(context, 'cancellation_state', 'active') == "cancelled":
-    return False
-
 if context.portal_type == "AnalysisRequest":
-    # Allow transition to 'to_be_verified'
+    # Allow transition to 'attach'
     # if all analyses are at least to_be_verified
     for a in context.getAnalyses(full_objects=True):
         review_state = workflow.getInfoFor(a, 'review_state')
         if review_state in ('unassigned', 'assigned', 'attachment_due'):
             return False
+    return True
+
+if workflow.getInfoFor(context, 'cancellation_state', 'active') == "cancelled":
+    return False
 
 if context.portal_type == "Worksheet":
     # Allow transition to 'to_be_verified'
