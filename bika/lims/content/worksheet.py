b81b0d30588cfbf5295eab73e88ca2364f6df88f
Jordi Puiggené <jpuiggene@naralabs.com>
2018-12-21 13:27:50 +0100

    Rely on layout to get the position of a given analysis

--- bika/lims/content/worksheet.py
+++ bika/lims/content/worksheet.py
@@ -747,11 +747,11 @@ class Worksheet(BaseFolder, HistoryAwareMixin):
         """Returns the slot where the instance passed in is located. If not
         found, returns None
         """
-        analysis_type = self.get_analysis_type(instance)
-        container = self.get_container_for(instance)
-        slot = self.get_slot_position(container, analysis_type)
-        analyses = self.get_analyses_at(slot)
-        return instance in analyses and slot or None
+        uid = api.get_uid(instance)
+        slot = filter(lambda s: s['analysis_uid'] == uid, self.getLayout())
+        if not slot:
+            return None
+        return to_int(slot[0]['position'])
 
     def resolve_available_slots(self, worksheet_template, type='a'):
         """Returns the available slots from the current worksheet that fits
