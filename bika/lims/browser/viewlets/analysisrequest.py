0676794af7a41c2e0e6e473da0ea419484a4d78f
Jordi Puiggené <jp@naralabs.com>
2019-01-09 08:55:31 +0100

    Added viewlets for partition and primary ARs (#1193)
    * Added viewlets for partition and primary ARs
    
    * Added changelog

--- bika/lims/browser/viewlets/analysisrequest.py
+++ bika/lims/browser/viewlets/analysisrequest.py
@@ -19,3 +19,15 @@ class RetestAnalysisRequestViewlet(ViewletBase):
     """ Current Analysis Request is a retest. Display the link to the invalid
     """
     template = ViewPageTemplateFile("templates/retest_ar_viewlet.pt")
+
+
+class PrimaryAnalysisRequestViewlet(ViewletBase):
+    """ Current Analysis Request is a primary. Diplay links to partitions
+    """
+    template = ViewPageTemplateFile("templates/primary_ar_viewlet.pt")
+
+
+class PartitionAnalysisRequestViewlet(ViewletBase):
+    """ Current Analysis Request is a partition. Display the link to primary
+    """
+    template = ViewPageTemplateFile("templates/partition_ar_viewlet.pt")
