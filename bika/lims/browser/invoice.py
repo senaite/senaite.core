8dbfa38b5af14aac5d4e862207a42142d6753013
Ramon Bartl <rb@ridingbytes.com>
2019-03-21 22:22:32 +0100

    Removed obsolete code

--- bika/lims/browser/invoice.py
+++ bika/lims/browser/invoice.py
@@ -7,89 +7,13 @@
 
 from bika.lims.browser import BrowserView
 from bika.lims.interfaces import IInvoiceView
-from Products.CMFCore.utils import getToolByName
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.i18n.locales import locales
 from zope.interface import implements
 
 
 class InvoiceView(BrowserView):
-
     implements(IInvoiceView)
-
-    template = ViewPageTemplateFile("templates/invoice.pt")
-    content = ViewPageTemplateFile("templates/invoice_content.pt")
 
     def __init__(self, context, request):
         super(InvoiceView, self).__init__(context, request)
-        request.set('disable_border', 1)
-
-    def __call__(self):
-        context = self.context
-        workflow = getToolByName(context, 'portal_workflow')
-        # Gather relted objects
-        batch = context.aq_parent
-        client = context.getClient()
-        analysis_request = context.getAnalysisRequest() if context.getAnalysisRequest() else None
-        # Gather general data
-        self.invoiceId = context.getId()
-        self.invoiceDate = self.ulocalized_time(context.getInvoiceDate())
-        self.subtotal = '%0.2f' % context.getSubtotal()
-        self.VATAmount = '%0.2f' % context.getVATAmount()
-        self.total = '%0.2f' % context.getTotal()
-        # Create the batch range
-        start = self.ulocalized_time(batch.getBatchStartDate())
-        end = self.ulocalized_time(batch.getBatchEndDate())
-        self.batchRange = "%s to %s" % (start, end)
-        # Gather client data
-        self.clientName = client.Title()
-        self.clientURL = client.absolute_url()
-        self.clientPhone = client.getPhone()
-        self.clientFax = client.getFax()
-        self.clientEmail = client.getEmailAddress()
-        self.clientAccountNumber = client.getAccountNumber()
-        # currency info
-        locale = locales.getLocale('en')
-        self.currency = self.context.bika_setup.getCurrency()
-        self.symbol = locale.numbers.currencies[self.currency].symbol
-        # Get an available client address in a preferred order
-        self.clientAddress = None
-        # A list with the items and its invoice values to render in template
-        self.items = []
-        addresses = (
-            client.getBillingAddress(),
-            client.getPostalAddress(),
-            client.getPhysicalAddress(),
-        )
-        for address in addresses:
-            if address.get('address'):
-                self.clientAddress = address
-                break
-        # Gather the line items
-        items = context.invoice_lineitems
-        for item in items:
-            invoice_data = {
-                'invoiceDate': self.ulocalized_time(item.get('ItemDate', '')),
-                'description': item.get('ItemDescription', ''),
-                'orderNo': item.get('OrderNumber', ''),
-                'subtotal': '%0.2f' % item.get('Subtotal', ''),
-                'VATAmount': '%0.2f' % item.get('VATAmount', ''),
-                'total': '%0.2f' % item.get('Total', ''),
-            }
-            if item.get('AnalysisRequest', ''):
-                    invoice_data['orderNoURL'] = item['AnalysisRequest'].absolute_url()
-            elif item.get('SupplyOrder', ''):
-                    invoice_data['orderNoURL'] = item['SupplyOrder'].absolute_url()
-            else:
-                invoice_data['orderNoURL'] = ''
-            self.items.append(invoice_data)
-        # Render the template
-        return self.template()
-
-
-class InvoicePrintView(InvoiceView):
-
-    template = ViewPageTemplateFile("templates/invoice_print.pt")
-
-    def __call__(self):
-        return InvoiceView.__call__(self)
+        self.context = context
+        self.request = request
--- bika/lims/content/invoicebatch.py
+++ bika/lims/content/invoicebatch.py
@@ -5,113 +5,17 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-from AccessControl import ClassSecurityInfo
-from bika.lims import bikaMessageFactory as _
 from bika.lims.config import PROJECTNAME
-from bika.lims.config import ManageInvoices
-from bika.lims.content.bikaschema import BikaSchema
-from bika.lims.content.invoice import InvoiceLineItem
-from bika.lims.interfaces import ICancellable
 from bika.lims.interfaces import IInvoiceBatch
-from bika.lims.utils import get_invoice_item_description
-from bika.lims.workflow import getTransitionDate
-from DateTime import DateTime
-from Products.Archetypes.public import DateTimeField
-from Products.Archetypes.public import CalendarWidget
 from Products.Archetypes.public import registerType
-from Products.Archetypes.public import Schema
 from Products.Archetypes.public import BaseFolder
-from Products.CMFPlone.utils import _createObjectByType
-from zope.container.contained import ContainerModifiedEvent
 from zope.interface import implements
 
 
-schema = BikaSchema.copy() + Schema((
-    DateTimeField(
-        "BatchStartDate",
-        required=1,
-        default_method="current_date",
-        widget=CalendarWidget(
-            label=_("Start Date"),
-        ),
-    ),
-    DateTimeField(
-        "BatchEndDate",
-        required=1,
-        default_method="current_date",
-        validators=("invoicebatch_EndDate_validator",),
-        widget=CalendarWidget(
-            label=_("End Date"),
-        ),
-    ),
-))
-
-schema["title"].default = DateTime().strftime("%b %Y")
-
-
 class InvoiceBatch(BaseFolder):
-    """Container for Invoice instances
+    """REMOVE AFTER 1.3
     """
-    implements(IInvoiceBatch, ICancellable)
-    security = ClassSecurityInfo()
-    displayContentsTab = False
-    schema = schema
-
-    _at_rename_after_creation = True
-
-    def _renameAfterCreation(self, check_auto_id=False):
-        from bika.lims.idserver import renameAfterCreation
-        renameAfterCreation(self)
-
-    @security.protected(ManageInvoices)
-    def invoices(self):
-        return self.objectValues("Invoice")
-
-    @security.protected(ManageInvoices)
-    def createInvoice(self, client_uid, items):
-        """Creates and invoice for a client and a set of items
-        """
-        plone_view = self.restrictedTraverse('@@plone')
-        invoice_id = self.generateUniqueId('Invoice')
-        invoice = _createObjectByType("Invoice", self, invoice_id)
-        # noinspection PyCallingNonCallable
-        invoice.edit(
-            Client=client_uid,
-            InvoiceDate=DateTime(),
-        )
-
-        invoice.processForm()
-        invoice.invoice_lineitems = []
-        for item in items:
-            lineitem = InvoiceLineItem()
-            if item.portal_type == 'AnalysisRequest':
-                lineitem['ItemDate'] = plone_view.toLocalizedTime(
-                    getTransitionDate(item, 'publish'), long_format=1)
-                lineitem['OrderNumber'] = item.getId()
-                lineitem['AnalysisRequest'] = item
-                lineitem['SupplyOrder'] = ''
-                description = get_invoice_item_description(item)
-                lineitem['ItemDescription'] = description
-            elif item.portal_type == 'SupplyOrder':
-                lineitem['ItemDate'] = item.getDateDispatched()
-                lineitem['OrderNumber'] = item.getOrderNumber()
-                lineitem['AnalysisRequest'] = ''
-                lineitem['SupplyOrder'] = item
-                description = get_invoice_item_description(item)
-                lineitem['ItemDescription'] = description
-            lineitem['Subtotal'] = item.getSubtotal()
-            lineitem['VATAmount'] = item.getVATAmount()
-            lineitem['Total'] = item.getTotal()
-            invoice.invoice_lineitems.append(lineitem)
-        invoice.reindexObject()
-        return invoice
-
-    security.declarePublic('current_date')
-
-    def current_date(self):
-        """ return current date """
-        # noinspection PyCallingNonCallable
-        return DateTime()
+    implements(IInvoiceBatch)
 
 
 registerType(InvoiceBatch, PROJECTNAME)
