c915c757d7a38418a4b6e849ddf5040da6836841
Ramon Bartl <rb@ridingbytes.com>
2019-03-21 20:22:04 +0100

    cleaup imports and PEP8

--- bika/lims/content/invoicefolder.py
+++ bika/lims/content/invoicefolder.py
@@ -6,22 +6,15 @@
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
 from AccessControl import ClassSecurityInfo
-from bika.lims import bikaMessageFactory as _
-from bika.lims.utils import t
-from bika.lims.config import ManageInvoices, PROJECTNAME
+from bika.lims.config import PROJECTNAME
 from bika.lims.interfaces import IInvoiceFolder, IHaveNoBreadCrumbs
 from Products.Archetypes import atapi
-from Products.Archetypes.public import *
 from Products.ATContentTypes.content import folder
-from Products.ATContentTypes.content import schemata
-from Products.CMFCore import permissions
-from Products.CMFCore.utils import UniqueObject
-from ZODB.POSException import ConflictError
 from zope.interface import implements
 
 schema = folder.ATFolderSchema.copy()
-schema['id'].widget.visible = {'edit':'hidden', 'view':'invisible'}
-schema['title'].widget.visible = {'edit':'hidden', 'view':'invisible'}
+schema["id"].widget.visible = {"edit": "hidden", "view": "invisible"}
+schema["title"].widget.visible = {"edit": "hidden", "view": "invisible"}
 
 
 class InvoiceFolder(folder.ATFolder):
