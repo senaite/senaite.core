e58e06feaf9f900f59b714e715bf4dd431fce104
Ramon Bartl <rb@ridingbytes.com>
2019-03-22 09:03:17 +0100

    Show breadcrumbs for Supply Orders

--- bika/lims/content/supplyorderfolder.py
+++ bika/lims/content/supplyorderfolder.py
@@ -5,25 +5,24 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-"""Supply Order Folder contains Supply Orders
-"""
-from Products.ATContentTypes.content import schemata
-from Products.Archetypes import atapi
-from bika.lims.config import PROJECTNAME
 from AccessControl import ClassSecurityInfo
-from bika.lims.interfaces import ISupplyOrderFolder, IHaveNoBreadCrumbs
+from bika.lims.config import PROJECTNAME
+from bika.lims.interfaces import ISupplyOrderFolder
 from plone.app.folder import folder
+from Products.Archetypes import atapi
+from Products.ATContentTypes.content import schemata
 from zope.interface import implements
+
 
 schema = folder.ATFolderSchema.copy()
 
 
 class SupplyOrderFolder(folder.ATFolder):
-    implements(ISupplyOrderFolder, IHaveNoBreadCrumbs)
+    implements(ISupplyOrderFolder)
     schema = schema
     displayContentsTab = False
     security = ClassSecurityInfo()
 
-schemata.finalizeATCTSchema(schema, folderish=True, moveDiscussion=False)
 
+schemata.finalizeATCTSchema(schema, folderish=True, moveDiscussion=False)
 atapi.registerType(SupplyOrderFolder, PROJECTNAME)
