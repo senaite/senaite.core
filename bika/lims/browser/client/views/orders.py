f54d151b769a511145191a7b11298c1ce647f489
Ramon Bartl <rb@ridingbytes.com>
2019-03-22 09:20:57 +0100

    Refactored client orders listing

--- bika/lims/browser/client/views/orders.py
+++ bika/lims/browser/client/views/orders.py
@@ -5,30 +5,36 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
+from bika.lims import api
 from bika.lims import bikaMessageFactory as _
 from bika.lims.browser.supplyorderfolder import SupplyOrderFolderView
-from plone.app.layout.globals.interfaces import IViewView
-from zope.interface import implements
+from bika.lims.permissions import AddSupplyOrder
 
 
 class ClientOrdersView(SupplyOrderFolderView):
-    implements(IViewView)
+    """Client supply order listing
+    """
 
     def __init__(self, context, request):
         super(ClientOrdersView, self).__init__(context, request)
+
         self.contentFilter = {
-            'portal_type': 'SupplyOrder',
-            'sort_on': 'sortable_title',
-            'sort_order': 'reverse',
-            'path': {
-                'query': '/'.join(context.getPhysicalPath()),
-                'level': 0
+            "portal_type": "SupplyOrder",
+            "sort_on": "sortable_title",
+            "sort_order": "ascending",
+            "path": {
+                "query": api.get_path(context),
+                "level": 0
             }
         }
         self.context_actions = {
-            _('Add'): {
-                'url': 'createObject?type_name=SupplyOrder',
-                'permission': 'Add portal content',
-                'icon': '++resource++bika.lims.images/add.png'
+            _("Add"): {
+                "url": "createObject?type_name=SupplyOrder",
+                "permission": AddSupplyOrder,
+                "icon": "++resource++bika.lims.images/add.png"
             }
         }
+
+    def before_render(self):
+        """Before template render hook
+        """
