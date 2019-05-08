e67f4c8f379ca2bb987ad0128702df3d3de9cf31
Ramon Bartl <rb@ridingbytes.com>
2019-05-08 17:52:45 +0200

    Added upgrade step to remove Samples/Partitions

--- bika/lims/upgrade/v01_03_001.py
+++ bika/lims/upgrade/v01_03_001.py
@@ -75,6 +75,9 @@ def upgrade(tool):
     setup.runImportStepFromProfile(profile, "toolset")
     setup.runImportStepFromProfile(profile, "content")
 
+    # Remove Sample Objects
+    remove_samples(portal)
+
     # Convert inline images
     # https://github.com/senaite/senaite.core/issues/1333
     convert_inline_images_to_attachments(portal)
@@ -116,6 +119,25 @@ def convert_inline_images_to_attachments(portal):
         obj.setResultsInterpretationDepts(ri)
 
     # Commit all changes
+    transaction.commit()
+
+
+def remove_samples(portal):
+    """Wipe out samples and their contained partitions
+    """
+    uc = api.get_tool("uid_catalog")
+    brains = uc({"portal_type": "Sample"})
+    total = len(brains)
+    logger.info("Removing {} Samples".format(total))
+    for num, brain in enumerate(brains):
+        obj = api.get_object(brain)
+        parent = obj.aq_parent
+        # bypass security checks
+        parent._delObject(obj.getId())
+        if num and num % 1000 == 0:
+            logger.info("Removed {}/{} Samples".format(num, total))
+            transaction.commit()
+    logger.info("Removed all {} Samples".format(total))
     transaction.commit()
 
 
