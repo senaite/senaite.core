eeab6e5201ebd247cc38241f902c483246b0dd56
Jordi Puiggené <jp@naralabs.com>
2019-06-07 20:46:29 +0200

    Analysts and Labclerks cannot create worksheets (#1389)
    * Allow/disallow worksheet management to Analysts and Labclerks
    
    * Changelog
    
    * Update CHANGES.rst

--- CHANGES.rst
+++ CHANGES.rst
@@ -28,6 +28,7 @@ Changelog
 
 **Fixed**
 
+- #1389 Analysts and Labclerks cannot create worksheets
 - #1386 No auto-rejection of Sample when rejection reasons are set in Add form
 - #1382 Fix double publication of the same sample when using multi-reports
 - #1368 Fix WF state propagation on partition verification
---
+++ bika/lims/subscribers/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.CORE.
+#
+# SENAITE.CORE is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2018-2019 by it's authors.
+# Some rights reserved, see README and LICENSE.
+
+from bika.lims import api
+from bika.lims import permissions
+
+
+def ObjectModifiedEventHandler(instance, event):
+    """Actions to be taken when Setup object is modified
+    """
+    update_worksheet_manage_permissions(instance)
+
+
+def update_worksheet_manage_permissions(senaite_setup):
+    """Updates the permissions 'Manage Worksheets' and 'Edit Worksheet' based
+    on the setting 'RestrictWorksheetManagement' from Setup
+    """
+    roles = ["LabManager", "Manager"]
+    if not senaite_setup.getRestrictWorksheetManagement():
+        # LabManagers, Analysts and LabClerks can create and manage worksheets
+        roles.extend(["Analyst", "LabClerk"])
+
+    worksheets = api.get_portal().worksheets
+    worksheets.manage_permission(permissions.ManageWorksheets, roles, acquire=1)
+    worksheets.manage_permission(permissions.EditWorksheet, roles, acquire=1)
+    worksheets.reindexObject()
