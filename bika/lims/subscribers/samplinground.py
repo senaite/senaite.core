bac74236bc3572ae63346bcba05f92a72ff91830
Ramon Bartl <rb@ridingbytes.com>
2019-09-13 14:58:04 +0200

    Added Auto ID Behavior for Dexterity Contents

--- bika/lims/subscribers/samplinground.py
+++ bika/lims/subscribers/samplinground.py
@@ -18,21 +18,19 @@
 # Copyright 2018-2019 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
-from bika.lims.idserver import renameAfterCreation
 
 def SamplingRoundAddedEventHandler(instance, event):
-    """ Event fired when BikaSetup object gets modified.
-        Since Sampling Round is a dexterity object we have to change the ID by "hand"
-        Then we have to redirect the user to the ar add form
+    """Redirect user to the sample add form
     """
     if instance.portal_type != "SamplingRound":
-        print("How does this happen: type is %s should be SamplingRound" % instance.portal_type)
-        return
-    renameAfterCreation(instance)
+        raise TypeError("Expected portal type 'SamplingRound', got '{}'"
+                        .format(instance.portal_type))
+
+    # renameAfterCreation(instance)
     num_art = len(instance.ar_templates)
     destination_url = instance.aq_parent.absolute_url() + \
-                    "/portal_factory/" + \
-                    "AnalysisRequest/Request new analyses/ar_add?samplinground=" + \
-                    instance.UID() + "&ar_count=" + str(num_art)
+        "/portal_factory/" + \
+        "AnalysisRequest/Request new analyses/ar_add?samplinground=" + \
+        instance.UID() + "&ar_count=" + str(num_art)
     request = getattr(instance, 'REQUEST', None)
     request.response.redirect(destination_url)
