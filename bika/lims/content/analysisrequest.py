d59193e49fe361fbfbbe20f64a6e205b77d9386c
Jordi Puiggené <jp@naralabs.com>
2019-10-16 11:16:12 +0200

    Register indexer for Batch's listing_searchable_text

--- bika/lims/content/analysisrequest.py
+++ bika/lims/content/analysisrequest.py
@@ -327,7 +327,6 @@ schema = BikaSchema.copy() + Schema((
                 {'columnName': 'getClientTitle', 'width': '30',
                  'label': _('Client'), 'align': 'left'},
             ],
-            minLength=3,
             force_all = False,
             ui_item="getId",
             showOn=True,
