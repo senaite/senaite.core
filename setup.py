aa8cfb1334997b5ea75da8cec980547675a8417f
Ramon Bartl <rb@ridingbytes.com>
2020-01-12 07:33:29 +0100

    Pin Pillow for Scrutinizer

--- setup.py
+++ setup.py
@@ -87,6 +87,8 @@ setup(
         # Needed for `IPortalCatalogQueueProcessor`, which will be included in
         # `Products.CMFCore` in Plone 5. Remove after we are on Plone 5!
         'collective.indexing',
+        # https://github.com/python-pillow/Pillow/blob/master/CHANGES.rst#622-2020-01-02
+        'Pillow<7.0.0'
     ],
     extras_require={
         'test': [
