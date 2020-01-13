2b3001e6eacad04d1ffc3d9c7037206aab01a0b4
Ramon Bartl <rb@ridingbytes.com>
2020-01-13 15:39:39 +0100

    Integrated global sections viewlet

---
+++ bika/lims/browser/viewlets/sections.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from cgi import escape
+
+from plone.app.layout.viewlets.common import GlobalSectionsViewlet as Base
+from Products.CMFPlone.utils import safe_unicode
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter
+
+
+class GlobalSectionsViewlet(Base):
+    index = ViewPageTemplateFile(
+        "templates/plone.app.layout.viewlets.sections.pt")
+
+    def update(self):
+        super(GlobalSectionsViewlet, self).update()
+        portal_state = getMultiAdapter((self.context, self.request),
+                                       name=u"plone_portal_state")
+        self.navigation_root_url = portal_state.navigation_root_url()
+        self.portal_title = escape(
+            safe_unicode(portal_state.navigation_root_title()))
+
