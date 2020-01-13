5807177e99cf19092f6c93e1b67625cabdd85501
Ramon Bartl <rb@ridingbytes.com>
2020-01-13 15:46:00 +0100

    Integrated Global Sections Dropdown Viewlet

--- bika/lims/browser/viewlets/sections.py
+++ bika/lims/browser/viewlets/sections.py
@@ -8,6 +8,18 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.component import getMultiAdapter
 
 
+class GlobalSectionsDropdownViewlet(Base):
+    index = ViewPageTemplateFile("templates/sections_dropdown.pt")
+
+    def update(self):
+        super(GlobalSectionsDropdownViewlet, self).update()
+        portal_state = getMultiAdapter((self.context, self.request),
+                                       name=u"plone_portal_state")
+        self.navigation_root_url = portal_state.navigation_root_url()
+        self.portal_title = escape(
+            safe_unicode(portal_state.navigation_root_title()))
+
+
 class GlobalSectionsViewlet(Base):
     index = ViewPageTemplateFile(
         "templates/plone.app.layout.viewlets.sections.pt")
@@ -19,4 +31,3 @@ class GlobalSectionsViewlet(Base):
         self.navigation_root_url = portal_state.navigation_root_url()
         self.portal_title = escape(
             safe_unicode(portal_state.navigation_root_title()))
-
