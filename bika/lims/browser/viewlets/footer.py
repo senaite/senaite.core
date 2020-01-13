c115b11b467fbca46a6f9731fe805f4fdc701ac8
Ramon Bartl <rb@ridingbytes.com>
2020-01-13 13:36:08 +0100

    Integrated Footer Viewlet

---
+++ bika/lims/browser/viewlets/footer.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from plone.app.layout.viewlets.common import FooterViewlet as Base
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class FooterViewlet(Base):
+    index = ViewPageTemplateFile(
+        "templates/plone.app.layout.viewlets.footer.pt")
