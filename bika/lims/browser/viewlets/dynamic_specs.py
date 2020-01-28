63d0e5aa7be5bccbed4227dd40cfdcb5aa8d6a26
Jordi Puiggené <jp@naralabs.com>
2020-01-28 12:01:24 +0100

    Add a viewlet for when a spec has a dynamic spec assigned

---
+++ bika/lims/browser/viewlets/dynamic_specs.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.CORE.
+#
+# SENAITE.CORE is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2018-2019 by it's authors.
+# Some rights reserved, see README and LICENSE.
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets import ViewletBase
+from bika.lims import api
+
+
+class DynamicSpecsViewlet(ViewletBase):
+    """ Displays an informative message when the specification has a dynamic
+    specification assigned, so ranges might be overriden by the ranges provided
+    in the xls file from the Dynamic Specification
+    """
+    template = ViewPageTemplateFile("templates/dynamic_specs_viewlet.pt")
