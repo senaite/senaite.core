448899f07b711c396ec28cedcc3a056c4e70c106
Jordi Puiggené <jp@naralabs.com>
2020-02-04 15:58:14 +0100

    Remove unused import

--- bika/lims/upgrade/v01_03_003.py
+++ bika/lims/upgrade/v01_03_003.py
@@ -28,7 +28,6 @@ from bika.lims.catalog import CATALOG_ANALYSIS_LISTING
 from bika.lims.catalog import CATALOG_ANALYSIS_REQUEST_LISTING
 from bika.lims.catalog.bikasetup_catalog import SETUP_CATALOG
 from bika.lims.config import PROJECTNAME as product
-from bika.lims.exportimport.setupdata import Analysis_Categories
 from bika.lims.interfaces import IAnalysisRequestWithPartitions
 from bika.lims.interfaces import ISubmitted
 from bika.lims.interfaces import IVerified
