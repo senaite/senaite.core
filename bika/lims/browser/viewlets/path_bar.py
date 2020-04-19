c334be9dcb43b885daf0a8c4d44d166f5e4199d2
Jordi Puiggené <jp@naralabs.com>
2020-04-20 00:07:07 +0200

    Tracbeack when rendering breadcumbs for non-object types

--- bika/lims/browser/viewlets/path_bar.py
+++ bika/lims/browser/viewlets/path_bar.py
@@ -44,7 +44,11 @@ class PathBarViewlet(Base):
         hierarchy = []
         current = self.context
         while not api.is_portal(current):
-            if check_permission(View, current):
+            if api.is_object(current):
+                if check_permission(View, current):
+                    hierarchy.append(current)
+            else:
+                # Some objects (e.g. portal_registry) are not supported
                 hierarchy.append(current)
             current = current.aq_parent
         hierarchy = sorted(hierarchy, reverse=True)
@@ -54,11 +58,15 @@ class PathBarViewlet(Base):
         """Converts the object to a breadcrumb for the template consumption
         """
         return {"Title": self.get_obj_title(obj),
-                "absolute_url": api.get_url(obj)}
+                "absolute_url": self.get_obj_url(obj)}
 
     def get_obj_title(self, obj):
         """Returns the title of the object to be displayed as breadcrumb
         """
+        if not api.is_object(obj):
+            # Some objects (e.g. portal_registry) are not supported
+            return obj.Title()
+
         title = api.get_title(obj)
         if IClient.providedBy(obj.aq_parent):
             # Objects from inside Client folder are always stored directly, w/o
@@ -69,6 +77,15 @@ class PathBarViewlet(Base):
             if pt_title:
                 title = "{} ({})".format(title, _(pt_title))
         return title
+
+    def get_obj_url(self, obj):
+        """Returns the absolute url of the object passed-in
+        """
+        if not api.is_object(obj):
+            # Some objects (e.g. portal_registry) are not supported
+            return obj.absolute_url()
+
+        return api.get_url(obj)
 
     def get_portal_type_title(self, obj):
         """Returns the title of the portal type of the obj passed-in
