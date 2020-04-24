5e1963c913721f042e977855f7812c7a69248147
Ramon Bartl <rb@ridingbytes.com>
2020-04-24 21:05:06 +0200

    Updated build system for Plone 5

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@
 
 from setuptools import setup, find_packages
 
-version = '1.3.4'
+version = "1.3.4"
 
 setup(
-    name='senaite.core',
+    name="senaite.core",
     version=version,
     description="SENAITE Core",
     long_description=open("README.rst").read() + "\n" +
@@ -38,72 +38,70 @@ setup(
         "Framework :: Zope2",
         "Programming Language :: Python",
     ],
-    keywords=['senaite', 'lims', 'opensource'],
+    keywords=["senaite", "lims", "opensource"],
     author="RIDING BYTES & NARALABS",
     author_email="senaite@senaite.com",
-    url='https://github.com/senaite/senaite.core',
-    license='GPLv2',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['bika'],
+    url="https://github.com/senaite/senaite.core",
+    license="GPLv2",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["bika"],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'setuptools',
-        'plone.api',
-        'plone.subrequest',
-        'plone.jsonapi.core',
-        'Products.ATExtensions>=1.1a3',
-        'Products.CMFEditions',
-        'Products.DataGridField',
-        'Products.TinyMCE',
-        'Products.TextIndexNG3',
-        'collective.monkeypatcher',
-        'collective.js.jqueryui',
-        'plone.app.z3cform',
-        'openpyxl==1.5.8',
-        'plone.app.iterate',
-        'magnitude',
-        'jarn.jsi18n',
-        'collective.progressbar',
-        'plone.app.dexterity',
-        'plone.app.relationfield',
-        'plone.app.referenceablebehavior',
-        'five.pt==2.2.4',
-        'z3c.jbot',
-        'plone.resource',
-        'CairoSVG==1.0.20',
-        'cairocffi<1.0.0',
-        'zopyx.txng3.ext==3.4.0',
-        "senaite.core.supermodel>=1.2.0",
-        "senaite.core.listing>=1.1.0",
-        "senaite.core.spotlight",
-        # Python 2.x is not supported by WeasyPrint v43
-        'WeasyPrint==0.42.3',
+        "setuptools",
+        "plone.api",
+        "plone.subrequest",
+        "plone.jsonapi.core",
+        "Products.ATExtensions",
+        "Products.CMFEditions",
+        "Products.DataGridField",
+        # XXX P5: Do we need it? Fails during installation
+        # "Products.TinyMCE",
+        "Products.TextIndexNG3",
+        "archetypes.referencebrowserwidget",
+        "collective.monkeypatcher",
+        "collective.js.jqueryui",
+        "plone.app.z3cform",
+        "openpyxl==1.5.8",
+        "plone.app.iterate",
+        "magnitude",
+        "jarn.jsi18n",
+        "collective.progressbar",
+        "plone.app.dexterity",
+        "plone.app.relationfield",
+        "plone.app.referenceablebehavior",
+        "z3c.jbot",
+        "plone.resource",
+        "zopyx.txng3.ext==3.4.0",
+        "senaite.core.supermodel",
+        "senaite.core.listing",
+        "plonetheme.sunburst",
+        # XXX P5: Fix senaite.jsonapi
+        # "senaite.core.spotlight",
+        "senaite.impress",
         # tinycss2 >= 1.0.0 does not support Python 2.x anymore
-        'tinycss2<1.0.0',
-        # Add this line *after* senaite.impress 1.2.0 was realeased!
-        'senaite.impress>=1.2.0',
+        "tinycss2<1.0.0",
         # Python 2/3 compatibility library: https://six.readthedocs.io/
-        'six',
+        "six",
         # Needed for `IPortalCatalogQueueProcessor`, which will be included in
         # `Products.CMFCore` in Plone 5. Remove after we are on Plone 5!
-        'collective.indexing',
+        "collective.indexing",
         # Fix Scrutinizer (remove after we migrated to Python 3)
         # https://github.com/python-pillow/Pillow/blob/master/CHANGES.rst#622-2020-01-02
-        'Pillow<7.0.0',
+        "Pillow<7.0.0",
         # https://pypi.org/project/more-itertools/
-        'more-itertools<6.0.0',
+        "more-itertools<6.0.0",
         # cssselect2 0.3.0 does not support Python 2.x anymore
-        'cssselect2<0.3.0',
-        # beautifulsoup4 4.9.0 requires 'soupsieve<2.0'
-        'soupsieve<2.0.0',
+        "cssselect2<0.3.0",
+        # beautifulsoup4 4.9.0 requires "soupsieve<2.0"
+        "soupsieve<2.0.0",
         # dependency for jsonapi
-        'simplejson',
+        "simplejson",
     ],
     extras_require={
-        'test': [
-            'unittest2',
-            'plone.app.testing',
+        "test": [
+            "unittest2",
+            "plone.app.testing",
         ]
     },
     entry_points="""
