ae9e879d81c1b46f549d07f369030c4892470a2b
Ramon Bartl <rb@ridingbytes.com>
2020-04-24 21:08:40 +0200

    Refactored Bootstrap View

--- bika/lims/browser/bootstrap/bootstrap.py
+++ bika/lims/browser/bootstrap/bootstrap.py
@@ -24,7 +24,7 @@ from plone.memoize.ram import cache
 from Products.Five import BrowserView
 from zope.component import getMultiAdapter
 from zope.interface import Interface
-from zope.interface import implements
+from zope.interface import implementer
 
 
 class IBootstrapView(Interface):
@@ -56,10 +56,10 @@ def icon_cache_key(method, self, brain_or_object):
     return key
 
 
+@implementer(IBootstrapView)
 class BootstrapView(BrowserView):
     """Twitter Bootstrap helper view for SENAITE LIMS
     """
-    implements(IBootstrapView)
 
     def __init__(self, context, request):
         super(BrowserView, self).__init__(context, request)
@@ -88,63 +88,59 @@ class BootstrapView(BrowserView):
             api.get_path(brain_or_object), html_tag))
         return html_tag
 
-    def getViewportValues(self, view=None):
+    def get_viewport_values(self, view=None):
         """Determine the value of the viewport meta-tag
         """
         values = {
-            'width': 'device-width',
-            'initial-scale': '1.0',
+            "width": "device-width",
+            "initial-scale": "1.0",
         }
 
-        return ','.join('%s=%s' % (k, v) for k, v in values.items())
+        return ",".join("%s=%s" % (k, v) for k, v in values.items())
 
-    def getColumnsClasses(self, view=None):
+    def get_columns_classes(self, view=None):
         """Determine whether a column should be shown. The left column is
            called plone.leftcolumn; the right column is called
            plone.rightcolumn.
         """
 
-        plone_view = getMultiAdapter(
-            (self.context, self.request), name=u'plone')
-        portal_state = getMultiAdapter(
-            (self.context, self.request), name=u'plone_portal_state')
+        layout = getMultiAdapter(
+            (self.context, self.request), name=u"plone_layout")
+        state = getMultiAdapter(
+            (self.context, self.request), name=u"plone_portal_state")
 
-        sl = plone_view.have_portlets('plone.leftcolumn', view=view)
-        sr = plone_view.have_portlets('plone.rightcolumn', view=view)
+        sl = layout.have_portlets("plone.leftcolumn", view=view)
+        sr = layout.have_portlets("plone.rightcolumn", view=view)
 
-        isRTL = portal_state.is_rtl()
+        isRTL = state.is_rtl()
 
         # pre-fill dictionary
         columns = dict(one="", content="", two="")
 
         if not sl and not sr:
-            # we don't have columns, thus conten takes the whole width
-            columns['content'] = "col-md-12"
+            # we don"t have columns, thus conten takes the whole width
+            columns["content"] = "col-md-12"
 
         elif sl and sr:
             # In case we have both columns, content takes 50% of the whole
             # width and the rest 50% is spread between the columns
-            columns['one'] = "col-xs-12 col-md-2"
-            columns['content'] = "col-xs-12 col-md-8"
-            columns['two'] = "col-xs-12 col-md-2"
+            columns["one"] = "col-xs-12 col-md-2"
+            columns["content"] = "col-xs-12 col-md-8"
+            columns["two"] = "col-xs-12 col-md-2"
 
         elif (sr and not sl) and not isRTL:
             # We have right column and we are NOT in RTL language
-            columns['content'] = "col-xs-12 col-md-10"
-            columns['two'] = "col-xs-12 col-md-2"
+            columns["content"] = "col-xs-12 col-md-10"
+            columns["two"] = "col-xs-12 col-md-2"
 
         elif (sl and not sr) and isRTL:
             # We have left column and we are in RTL language
-            columns['one'] = "col-xs-12 col-md-2"
-            columns['content'] = "col-xs-12 col-md-10"
+            columns["one"] = "col-xs-12 col-md-2"
+            columns["content"] = "col-xs-12 col-md-10"
 
         elif (sl and not sr) and not isRTL:
             # We have left column and we are in NOT RTL language
-            columns['one'] = "col-xs-12 col-md-2"
-            columns['content'] = "col-xs-12 col-md-10"
-
-        # # append cell to each css-string
-        # for key, value in columns.items():
-        #     columns[key] = "cell " + value
+            columns["one"] = "col-xs-12 col-md-2"
+            columns["content"] = "col-xs-12 col-md-10"
 
         return columns
