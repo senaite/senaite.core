933b04c3f9adcce837e0e395f236dc1514a66309
Ramon Bartl <rb@ridingbytes.com>
2020-04-24 21:16:43 +0200

    Updated logo viewlet

--- bika/lims/browser/viewlets/logo.py
+++ bika/lims/browser/viewlets/logo.py
@@ -25,18 +25,3 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 class LogoViewlet(Base):
     index = ViewPageTemplateFile(
         "templates/plone.app.layout.viewlets.logo.pt")
-
-    def update(self):
-        super(Base, self).update()
-
-        portal = self.portal_state.portal()
-        bprops = portal.restrictedTraverse("base_properties", None)
-        if bprops is not None:
-            logoName = bprops.logoName
-        else:
-            logoName = "logo.jpg"
-
-        logoTitle = self.portal_state.portal_title()
-        self.logo_tag = portal.restrictedTraverse(logoName).tag(
-            title=logoTitle, alt=logoTitle, scale=0.5)
-        self.navigation_root_title = self.portal_state.navigation_root_title()
