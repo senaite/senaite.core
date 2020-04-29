45093eb55cc0d1c0d24affc2926ac958ab17283b
Ramon Bartl <rb@ridingbytes.com>
2020-04-29 14:45:26 +0200

    Removed template from class

--- src/senaite/core/browser/viewlets/personal_bar.py
+++ src/senaite/core/browser/viewlets/personal_bar.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets.common import PersonalBarViewlet as BaseViewlet
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class PersonalBarViewlet(BaseViewlet):
-    index = ViewPageTemplateFile(
-        "templates/plone.app.layout.viewlets.anontools.pt")
-
     def update(self):
         super(PersonalBarViewlet, self).update()
