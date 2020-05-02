701705b11adccd069f7443bfd24df1f8522798f7
Ramon Bartl <rb@ridingbytes.com>
2020-05-02 12:31:06 +0200

    Removed setupbutton viewlet

--- src/senaite/core/browser/viewlets/personal_bar.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from plone.app.layout.viewlets.common import PersonalBarViewlet as BaseViewlet
-
-
-class PersonalBarViewlet(BaseViewlet):
-    def update(self):
-        super(PersonalBarViewlet, self).update()
