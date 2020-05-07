5bc8ae01a89e951661358d42d5ba4d52a840578e
Ramon Bartl <rb@ridingbytes.com>
2020-05-07 13:29:39 +0200

    Better CSS Styles

--- webpack/app/senaite.core.js
+++ webpack/app/senaite.core.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   console.debug("*** SENAITE JS LOADED ***");
 
   $("#sidebar-header").on("click", function () {
-    $("#sidebar").toggleClass("active");
+    $("#sidebar").toggleClass("minimized");
   });
 
 });
