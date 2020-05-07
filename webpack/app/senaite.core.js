dec2a6c0fb32c72585c93c944a11f8401dee680d
Ramon Bartl <rb@ridingbytes.com>
2020-05-07 13:12:23 +0200

    Webpack resource management

--- webpack/app/senaite.core.js
+++ webpack/app/senaite.core.js
@@ -1,11 +1,7 @@
-import "./senaite.core.scss";
-import "bootstrap";
-
-
 document.addEventListener("DOMContentLoaded", () => {
   console.debug("*** SENAITE JS LOADED ***");
 
-  $('#sidebar-header').on('click', function () {
+  $("#sidebar-header").on("click", function () {
     $("#sidebar").toggleClass("active");
   });
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,80 +1,116 @@
 const path = require("path");
-const childProcess = require("child_process");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
+const childProcess = require("child_process");
+
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 const gitCmd = "git rev-list -1 HEAD -- `pwd`";
-const compilePath = path.resolve(__dirname, "../src/senaite/core/browser/static");
-const publicPath = "++plone++senaite.core.static/"
 let gitHash = childProcess.execSync(gitCmd).toString().substring(0, 7);
 
-// Remove old static resources
-childProcess.execSync(`rm -f ${compilePath}/senaite.*`);
+const staticPath = path.resolve(__dirname, "../src/senaite/core/browser/static");
+
+const devMode = process.env.NODE_ENV !== 'production';
+
 
 module.exports = {
+  context: path.resolve(__dirname, "app"),
   entry: {
-    core: path.resolve(__dirname, "./app/senaite.core.js")
+    main: [
+      // scripts
+      "./senaite.core.js",
+      // styles
+      "./scss/senaite.core.scss",
+    ],
   },
   output: {
-    filename: `senaite.[name]-${gitHash}.js`,
-    path: compilePath,
-    publicPath: publicPath,
+    filename: `[name]-${gitHash}.js`,
+    path: path.resolve(staticPath, "bundles"),
+    publicPath: "++plone++senaite.core.static/bundles"
   },
   module: {
     rules: [
       {
-        test: /\.coffee$/,
-        exclude: [/node_modules/],
-        use: ["babel-loader", "coffee-loader"]
-      }, {
+        // JS
         test: /\.(js|jsx)$/,
         exclude: [/node_modules/],
-        use: ["babel-loader"]
-      }, {
+        use: [
+          {
+            // https://webpack.js.org/loaders/babel-loader/
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        // SCSS
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            // https://webpack.js.org/plugins/mini-css-extract-plugin/
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === "development"
+            },
+          },
+          {
+            // https://webpack.js.org/loaders/css-loader/
+            loader: "css-loader"
+          },
+          {
+            // https://webpack.js.org/loaders/sass-loader/
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: "file-loader",
             options: {
-              name: "[name].[ext]"
+              name: "[name].[ext]",
+              outputPath: "../fonts",
+              publicPath: "++plone++senaite.core.static/fonts"
             }
           }
         ]
-      }, {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }, {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
       }
     ]
   },
-  plugins: [
-    // e.g. https://webpack.js.org/plugins/provide-plugin/
+  plugins: [// e.g. https://webpack.js.org/plugins/provide-plugin/
+
+    // A webpack plugin to remove your build folder(s) before building
+    // https://github.com/johnagan/clean-webpack-plugin
+    new CleanWebpackPlugin(),
+    // https://webpack.js.org/plugins/html-webpack-plugin/
     new HtmlWebpackPlugin({
       inject: false,
-      filename: "resources.pt",
-      template: "app/resources.pt",
-      publicPath: publicPath,
+      filename:  path.resolve(staticPath, "resources.pt"),
+      template: "resources.pt",
     }),
-    // new webpack.ProvidePlugin({
-    //   $: "jquery",
-    //   jQuery: "jquery"
-    // }),
-    // new webpack.ProvidePlugin({
-    //   bootstrap: "bootstrap/dist/js/bootstrap.min.js"
-    // })
+    // https://webpack.js.org/plugins/mini-css-extract-plugin/
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css",
+    }),
+    // https://webpack.js.org/plugins/copy-webpack-plugin/
+    new CopyPlugin([
+      { from: "../node_modules/jquery", to: path.resolve(staticPath, "lib/jquery") },
+      { from: "../node_modules/jquery-migrate", to: path.resolve(staticPath, "lib/jquery-migrate") },
+      { from: "../node_modules/bootstrap", to: path.resolve(staticPath, "lib/bootstrap") },
+    ]),
+    // https://webpack.js.org/plugins/provide-plugin/
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
   ],
   externals: {
     // https://webpack.js.org/configuration/externals
     // use jQuery from the outer scope
-    // jquery: "jQuery",
+    jquery: "jQuery",
     // bootstrap: "bootstrap",
   }
 };
