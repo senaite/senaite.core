8182daf6da0f0fd504964875aa5d4fdb1635173b
Ramon Bartl <rb@ridingbytes.com>
2020-05-22 20:29:14 +0200

    PEP8

--- src/senaite/core/browser/viewlets/attachments.py
+++ src/senaite/core/browser/viewlets/attachments.py
@@ -1,22 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# This file is part of SENAITE.CORE.
-#
-# SENAITE.CORE is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, version 2.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc., 51
-# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-# Copyright 2018-2020 by it's authors.
-# Some rights reserved, see README and LICENSE.
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from bika.lims import api
@@ -47,7 +29,7 @@ class AttachmentsViewlet(ViewletBase):
         if not any(map(url.endswith, ["base_view", "manage_results"])):
             return False
         return self.attachments_view.user_can_add_attachments() or \
-               self.attachments_view.user_can_update_attachments()
+            self.attachments_view.user_can_update_attachments()
 
     def update(self):
         """Called always before render()
