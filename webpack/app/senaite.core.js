5288bba30c37c37fcd58747a9ae00f46d0ee98a2
Ramon Bartl <rb@ridingbytes.com>
2020-06-06 14:18:16 +0200

    Remove autoresize plugin from TinyMCE

--- webpack/app/senaite.core.js
+++ webpack/app/senaite.core.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // TinyMCE
   tinymce.init({
     selector: "textarea.mce_editable,div.ArchetypesRichWidget textarea,textarea[name='form.widgets.IRichTextBehavior.text'],textarea.richTextWidget",
-    plugins: ["paste", "link", "autoresize", "fullscreen", "table", "code"],
+    plugins: ["paste", "link", "fullscreen", "table", "code"],
     content_css : "/++plone++senaite.core.static/bundles/main.css",
   })
   // /TinyMCE
