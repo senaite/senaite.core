75a1696f31d33e0192fa46472d509eb6eb8d4808
Jordi Puiggené <jp@naralabs.com>
2020-06-07 19:15:10 +0200

    Remove redirect to manage_results for AnalysisRequest
    This commit removes the redirection to sample's manage results view when the
    sample is in received status and user has Edit privileges. Currently, when these
    conditions are met, the manage results view is displayed, but this view is
    always displayed, even when "View" link is clicked. For sake of simplicity,
    better to handle this in e.g. listing rather than modifying the expression url
    for AnalysisRequest's action view.

--- src/bika/lims/browser/analysisrequest/view.py
+++ src/bika/lims/browser/analysisrequest/view.py
@@ -44,20 +44,6 @@ class AnalysisRequestViewView(BrowserView):
         )
 
     def __call__(self):
-        # If the analysis request has been received and hasn't been yet
-        # verified yet, redirect the user to manage_results view, but only if
-        # the user has privileges to Edit(Field)Results, cause otherwise she/he
-        # will receive an InsufficientPrivileges error!
-        if (self.request.PATH_TRANSLATED.endswith(self.context.id) and
-            self.can_edit_results() and self.can_edit_field_results() and
-           self.is_received() and not self.is_verified()):
-
-            # Redirect to manage results view if not cancelled
-            if not self.is_cancelled():
-                manage_results_url = "{}/{}".format(
-                    self.context.absolute_url(), "manage_results")
-                return self.request.response.redirect(manage_results_url)
-
         # render header table
         self.header_table = HeaderTableView(self.context, self.request)()
 
