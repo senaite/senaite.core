7e4279626b7f46c74a58761fb786af7bff81d30c
Jordi Puiggené <jp@naralabs.com>
2020-06-07 20:05:53 +0200

    Remove nuts from AR "manage_analyses" req

--- src/senaite/core/browser/static/js/bika.lims.analysisrequest.js
+++ src/senaite/core/browser/static/js/bika.lims.analysisrequest.js
@@ -140,44 +140,6 @@
     that.load = function() {};
   };
 
-
-  /**
-   * Controller class for Analysis Request Manage Results view
-   */
-
-  window.AnalysisRequestManageResultsView = function() {
-    var that;
-    that = this;
-
-    /**
-     * Entry-point method for AnalysisRequestManageResultsView
-     */
-    that.load = function() {
-      $('.portaltype-analysisrequest .bika-listing-table td.Analyst select').change(function() {
-        var analyst, key, obj_path, obj_path_split;
-        analyst = $(this).val();
-        key = $(this).closest('tr').attr('keyword');
-        obj_path = window.location.href.replace(window.portal_url, '');
-        obj_path_split = obj_path.split('/');
-        if (obj_path_split.length > 4) {
-          obj_path_split[obj_path_split.length - 1] = key;
-        } else {
-          obj_path_split.push(key);
-        }
-        obj_path = obj_path_split.join('/');
-        $.ajax({
-          type: 'POST',
-          url: window.portal_url + '/@@API/update',
-          data: {
-            'obj_path': obj_path,
-            'Analyst': analyst
-          }
-        });
-      });
-    };
-  };
-
-
   /**
    * Controller class for Analysis Request Analyses view
    */
--- src/senaite/core/browser/static/js/coffee/bika.lims.analysisrequest.coffee
+++ src/senaite/core/browser/static/js/coffee/bika.lims.analysisrequest.coffee
@@ -126,40 +126,6 @@ window.AnalysisRequestViewView = ->
   return
 
 ###*
-# Controller class for Analysis Request Manage Results view
-###
-
-window.AnalysisRequestManageResultsView = ->
-  that = this
-
-  ###*
-  # Entry-point method for AnalysisRequestManageResultsView
-  ###
-
-  that.load = ->
-    # Set the analyst automatically when selected in the picklist
-    $('.portaltype-analysisrequest .bika-listing-table td.Analyst select').change ->
-      analyst = $(this).val()
-      key = $(this).closest('tr').attr('keyword')
-      obj_path = window.location.href.replace(window.portal_url, '')
-      obj_path_split = obj_path.split('/')
-      if obj_path_split.length > 4
-        obj_path_split[obj_path_split.length - 1] = key
-      else
-        obj_path_split.push key
-      obj_path = obj_path_split.join('/')
-      $.ajax
-        type: 'POST'
-        url: window.portal_url + '/@@API/update'
-        data:
-          'obj_path': obj_path
-          'Analyst': analyst
-      return
-    return
-
-  return
-
-###*
 # Controller class for Analysis Request Analyses view
 ###
 
