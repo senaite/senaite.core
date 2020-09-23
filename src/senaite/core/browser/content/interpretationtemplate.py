85e17811b7b2873aea685d7d0921df6b4cdbde42
Jordi Puiggené <jp@naralabs.com>
2020-09-23 11:34:12 +0200

    interpretationtemplate.View --> interpretationtemplate.FolderView

--- src/senaite/core/browser/content/interpretationtemplate.py
+++ src/senaite/core/browser/content/interpretationtemplate.py
@@ -27,12 +27,12 @@ from bika.lims.utils import get_link_for
 from senaite.app.listing import ListingView
 
 
-class View(ListingView):
+class FolderView(ListingView):
     """Results Interpretation Templates listing view
     """
 
     def __init__(self, context, request):
-        super(View, self).__init__(context, request)
+        super(FolderView, self).__init__(context, request)
 
         self.catalog = SETUP_CATALOG
 
@@ -83,12 +83,12 @@ class View(ListingView):
     def update(self):
         """Update hook
         """
-        super(View, self).update()
+        super(FolderView, self).update()
 
     def before_render(self):
         """Before template render hook
         """
-        super(View, self).before_render()
+        super(FolderView, self).before_render()
         # Don't allow any context actions
         self.request.set("disable_border", 1)
 
