a21023b0a8b8cfef6e0833361ee842fc54b19f1c
Ramon Bartl <rb@ridingbytes.com>
2020-12-11 11:40:04 +0100

    Fixed create API for dexterity types

--- src/bika/lims/api/__init__.py
+++ src/bika/lims/api/__init__.py
@@ -60,7 +60,6 @@ from zope.component import queryMultiAdapter
 from zope.component.interfaces import IFactory
 from zope.event import notify
 from zope.lifecycleevent import ObjectCreatedEvent
-from zope.lifecycleevent import modified
 from zope.schema import getFieldsInOrder
 from zope.security.interfaces import Unauthorized
 
@@ -132,41 +131,35 @@ def create(container, portal_type, *args, **kwargs):
     :returns: The new created object
     """
     from bika.lims.utils import tmpID
-    if kwargs.get("title") is None:
-        kwargs["title"] = "New {}".format(portal_type)
 
-    # generate a temporary ID
-    tmp_id = tmpID()
+    id = kwargs.pop("id", tmpID())
+    title = kwargs.pop("title", "New {}".format(portal_type))
 
     # get the fti
     types_tool = get_tool("portal_types")
     fti = types_tool.getTypeInfo(portal_type)
 
     if fti.product:
-        obj = _createObjectByType(portal_type, container, tmp_id)
+        obj = _createObjectByType(portal_type, container, id)
+        obj.setTitle(title)
+        obj.processForm(values=kwargs)
     else:
         # newstyle factory
         factory = getUtility(IFactory, fti.factory)
-        obj = factory(tmp_id, *args, **kwargs)
+        obj = factory(id, *args, **kwargs)
         if hasattr(obj, '_setPortalTypeName'):
             obj._setPortalTypeName(fti.getId())
+        # set the title
+        obj.title = safe_unicode(title)
+        # notify that the object was created
         notify(ObjectCreatedEvent(obj))
         # notifies ObjectWillBeAddedEvent, ObjectAddedEvent and
         # ContainerModifiedEvent
-        container._setObject(tmp_id, obj)
+        container._setObject(id, obj)
         # we get the object here with the current object id, as it might be
         # renamed already by an event handler
         obj = container._getOb(obj.getId())
 
-    # handle AT Content
-    if is_at_content(obj):
-        obj.processForm()
-
-    # Edit after processForm; processForm does AT unmarkCreationFlag.
-    obj.edit(**kwargs)
-
-    # explicit notification
-    modified(obj)
     return obj
 
 
