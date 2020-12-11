3d545bc6eb7e5343d3d07caf1a20ef96543f7dd4
Jordi Puiggené <jp@naralabs.com>
2020-12-11 23:01:41 +0100

    IActiveStatus adapter for IAnalysis type
    Returns active if the analysis status is "cancelled", "inactive"
    or the analysis has been verified already

---
+++ src/senaite/core/adapters/analysis.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.CORE.
+#
+# SENAITE.CORE is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2018-2020 by it's authors.
+# Some rights reserved, see README and LICENSE.
+
+from bika.lims import api
+from bika.lims.interfaces import IVerified
+from senaite.core.adapters.interfaces import IActiveStatus
+from zope.interface import implementer
+
+
+@implementer(IActiveStatus)
+class AnalysisActiveStatus(object):
+    """Adapter that resolves if an analysis is active
+    """
+
+    def __init__(self, context):
+        self.context = context
+
+    def is_active(self):
+        """Returns whether the analysis is active
+        """
+        if IVerified.providedBy(self.context):
+            return False
+
+        status = api.get_review_status(self.context)
+        return status not in ["cancelled", "inactive"]
