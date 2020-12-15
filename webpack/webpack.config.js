669a590c9252eced5b0ac0f8b26512d2e6ce7aec
Ramon Bartl <rb@ridingbytes.com>
2020-12-15 21:00:05 +0100

    Updated build system to WP5

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const webpack = require("webpack");
 const childProcess = require("child_process");
 
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const CleanCSS = require("clean-css");
 const CopyPlugin = require("copy-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MergeIntoSingleFilePlugin = require("webpack-merge-and-include-globally");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 const uglifyJS = require("uglify-js");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
@@ -18,10 +19,15 @@ let gitHash = childProcess.execSync(gitCmd).toString().substring(0, 7);
 
 const staticPath = path.resolve(__dirname, "../src/senaite/core/browser/static");
 
-const devMode = process.env.NODE_ENV !== 'production';
+const devMode = process.env.NODE_ENV == "development";
+const mode = devMode ? "development" : "production";
 
 
 module.exports = {
+  // https://webpack.js.org/configuration/devtool
+  devtool: devMode ? "eval" : "source-map",
+  // https://webpack.js.org/configuration/mode/#usage
+  mode: mode,
   context: path.resolve(__dirname, "app"),
   entry: {
     main: [
@@ -56,9 +62,6 @@ module.exports = {
           {
             // https://webpack.js.org/plugins/mini-css-extract-plugin/
             loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: process.env.NODE_ENV === "development"
-            },
           },
           {
             // https://webpack.js.org/loaders/css-loader/
@@ -105,14 +108,16 @@ module.exports = {
     minimizer: [
       // https://v4.webpack.js.org/plugins/terser-webpack-plugin/
       new TerserPlugin({
-        extractComments: true,
-        sourceMap: true, // Must be set to true if using source-maps in production
         exclude: /\/modules/,
         terserOptions: {
           // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-          extractComments: true,
+          sourceMap: true, // Must be set to true if using source-maps in production
+          format: {
+            comments: false
+          },
           compress: {
             drop_console: true,
+            passes: 2,
           },
 	      }
       }),
@@ -130,6 +135,8 @@ module.exports = {
     ],
   },
   plugins: [
+    // https://github.com/webpack-contrib/webpack-bundle-analyzer
+    // new BundleAnalyzerPlugin(),
     // https://github.com/markshapiro/webpack-merge-and-include-globally
     new MergeIntoSingleFilePlugin({
       files: [{
@@ -228,8 +235,7 @@ module.exports = {
     }),
     // https://webpack.js.org/plugins/mini-css-extract-plugin/
     new MiniCssExtractPlugin({
-      filename: devMode ? "[name].css" : "[name].[hash].css",
-      chunkFilename: devMode ? "[id].css" : "[id].[hash].css",
+      filename: devMode ? "[name].css" : `[name]-${gitHash}.css`,
     }),
     // https://webpack.js.org/plugins/copy-webpack-plugin/
     new CopyPlugin({
