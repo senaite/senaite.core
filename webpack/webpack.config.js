55260dfe1d9f1ea85aeeca4217f8ee1989534194
Ramon Bartl <rb@ridingbytes.com>
2020-12-15 23:00:05 +0100

    Fixed development mode issue and improved build performance

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -20,6 +20,7 @@ let gitHash = childProcess.execSync(gitCmd).toString().substring(0, 7);
 const staticPath = path.resolve(__dirname, "../src/senaite/core/browser/static");
 
 const devMode = process.env.mode == "development";
+const prodMode = process.env.mode == "production";
 const mode = process.env.mode;
 console.log(`RUNNING WEBPACK IN '${mode}' MODE`);
 
@@ -39,7 +40,7 @@ module.exports = {
     ],
   },
   output: {
-    filename: `[name]-${gitHash}.js`,
+    filename: devMode ? "[name].js" : `[name]-${gitHash}.js`,
     path: path.resolve(staticPath, "bundles"),
     publicPath: "/++plone++senaite.core.static/bundles"
   },
@@ -105,7 +106,7 @@ module.exports = {
     ]
   },
   optimization: {
-    minimize: true,
+    minimize: prodMode,
     minimizer: [
       // https://v4.webpack.js.org/plugins/terser-webpack-plugin/
       new TerserPlugin({
@@ -136,6 +137,20 @@ module.exports = {
     ],
   },
   plugins: [
+    // https://github.com/johnagan/clean-webpack-plugin
+    new CleanWebpackPlugin({
+      verbose: false,
+      // Workaround in `watch` mode when trying to remove the `resources.pt` in the parent folder:
+      // Error: clean-webpack-plugin: Cannot delete files/folders outside the current working directory.
+      cleanAfterEveryBuildPatterns: ["!../*"],
+    }),
+    // https://webpack.js.org/plugins/html-webpack-plugin/
+    new HtmlWebpackPlugin({
+      inject: false,
+      filename:  path.resolve(staticPath, "resources.pt"),
+      template: "resources.pt",
+    }),
+    new webpack.ProgressPlugin(),
     // https://github.com/webpack-contrib/webpack-bundle-analyzer
     // new BundleAnalyzerPlugin(),
     // https://github.com/markshapiro/webpack-merge-and-include-globally
@@ -171,6 +186,11 @@ module.exports = {
           "../src/senaite/core/browser/static/js/bika.lims.loader.js",
         ],
         dest: code => {
+          if (devMode) {
+            return {
+              "thirdparty.js": code
+            }
+          }
           const min = uglifyJS.minify(code, {sourceMap: {
             filename: "legacy.js",
             url: "legacy.js.map"
@@ -202,6 +222,11 @@ module.exports = {
           "../src/senaite/core/browser/static/thirdparty/d3.js",
         ],
         dest: code => {
+          if (devMode) {
+            return {
+              "thirdparty.js": code
+            }
+          }
           const min = uglifyJS.minify(code, {sourceMap: {
             filename: "thirdparty.js",
             url: "thirdparty.js.map"
@@ -225,14 +250,6 @@ module.exports = {
     ]
     }, filesMap => {
       console.log("generated files: ",filesMap)
-    }),
-    // https://github.com/johnagan/clean-webpack-plugin
-    new CleanWebpackPlugin(),
-    // https://webpack.js.org/plugins/html-webpack-plugin/
-    new HtmlWebpackPlugin({
-      inject: false,
-      filename:  path.resolve(staticPath, "resources.pt"),
-      template: "resources.pt",
     }),
     // https://webpack.js.org/plugins/mini-css-extract-plugin/
     new MiniCssExtractPlugin({
