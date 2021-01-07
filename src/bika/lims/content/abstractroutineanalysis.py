49b731742b2246792fe247a0431db805e958d479
Jordi Puiggené <jp@naralabs.com>
2021-01-07 10:43:42 +0100

    Remove legacy-reflex events

--- src/bika/lims/content/abstractroutineanalysis.py
+++ src/bika/lims/content/abstractroutineanalysis.py
@@ -531,33 +531,3 @@ class AbstractRoutineAnalysis(AbstractAnalysis, ClientAwareMixin):
         if original:
             return original.UID()
         return ''
-
-    @security.private
-    def _reflex_rule_process(self, wf_action):
-        """This function does all the reflex rule process.
-        :param wf_action: is a string containing the workflow action triggered
-        """
-        # Check out if the analysis has any reflex rule bound to it.
-        # First we have get the analysis' method because the Reflex Rule
-        # objects are related to a method.
-        a_method = self.getMethod()
-        if not a_method:
-            return
-        # After getting the analysis' method we have to get all Reflex Rules
-        # related to that method.
-        all_rrs = a_method.getBackReferences('ReflexRuleMethod')
-        if not all_rrs:
-            return
-        # Once we have all the Reflex Rules with the same method as the
-        # analysis has, it is time to get the rules that are bound to the
-        # same analysis service that is using the analysis.
-        for rule in all_rrs:
-            if not api.is_active(rule):
-                continue
-            # Getting the rules to be done from the reflex rule taking
-            # in consideration the analysis service, the result and
-            # the state change
-            action_row = rule.getActionReflexRules(self, wf_action)
-            # Once we have the rules, the system has to execute its
-            # instructions if the result has the expected result.
-            doReflexRuleAction(self, action_row)
