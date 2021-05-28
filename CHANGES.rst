269894b891f8d17ac22aa76a1dd9bd6a675e0a5c
Ramon Bartl <rb@ridingbytes.com>
2021-05-28 19:55:29 +0200

    Added base structure to implement custom dexterity fields and widgets (#1806)
    * Implemented base structure for custom dexterity fields and widgets
    
    * Changelog added

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,6 +4,7 @@ Changelog
 2.0.0 (unreleased)
 ------------------
 
+- #1806 Added base structure to implement custom dexterity fields and widgets
 - #1807 Removal of ACTIONS_TO_INDEXES mapping to ensure data integrity
 - #1804 Adapter hook for confirmation when creating a Sample
 - #1801 Updated openpyxl to latest Python 2.x compatible version
---
+++ src/senaite/core/tests/test_z3c_widgets.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import doctest
+from os.path import join
+
+from pkg_resources import resource_listdir
+
+import unittest2 as unittest
+from senaite.core.config import PROJECTNAME
+from senaite.core.tests.base import BaseTestCase
+from Testing import ZopeTestCase as ztc
+from z3c.form import testing
+
+filenames = [f for f in resource_listdir(PROJECTNAME, "z3cform/widgets")
+             if f.endswith(".txt")]
+
+doctests = [join("../z3cform/widgets", filename) for filename in filenames]
+
+flags = doctest.ELLIPSIS | \
+        doctest.NORMALIZE_WHITESPACE | \
+        doctest.IGNORE_EXCEPTION_DETAIL
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    for doctestfile in doctests:
+        suite.addTests([
+            ztc.ZopeDocFileSuite(
+                doctestfile,
+                test_class=BaseTestCase,
+                optionflags=flags,
+                checker=testing.outputChecker,
+            )
+        ])
+    return suite
