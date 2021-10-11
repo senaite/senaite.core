191e619d736a90c5b4ddc6dc8f616bd9e04e1f7c
Jordi Puiggené <jp@naralabs.com>
2021-10-11 11:56:02 +0200

    Store the Service Conditions in the Sample

--- src/bika/lims/content/analysisrequest.py
+++ src/bika/lims/content/analysisrequest.py
@@ -1357,6 +1357,12 @@ schema = BikaSchema.copy() + Schema((
             visible={'add': 'edit',}
         ),
     ),
+
+    # Initial conditions for analyses set on Sample registration
+    RecordsField(
+        "ServiceConditions",
+        visible=False,
+    )
 )
 )
 
