1f9527b26107113d92c6350ff420345cbff63fa5
Ramon Bartl <rb@ridingbytes.com>
2021-10-21 17:18:13 +0200

    Implemented UID reference field

--- src/senaite/core/schema/uidreferencefield.py
+++ src/senaite/core/schema/uidreferencefield.py
@@ -1,11 +1,50 @@
 # -*- coding: utf-8 -*-
 
+import six
+
+from bika.lims import api
+from persistent.dict import PersistentDict
+from persistent.list import PersistentList
+from senaite.core import logger
 from senaite.core.schema.fields import BaseField
 from senaite.core.schema.interfaces import IUIDReferenceField
+from zope.annotation.interfaces import IAnnotations
 from zope.interface import implementer
 from zope.schema import ASCIILine
+from Acquisition import aq_base
 from zope.schema import List
 from zope.schema.interfaces import IFromUnicode
+
+BACKREFS_STORAGE = "senaite.core.schema.uidreferencefield.backreferences"
+
+
+def get_brefs(context, relationship, as_objects=False):
+    """Return backreferences of the context
+
+    :returns: List of UIDs that are linked by the relationship
+    """
+    context = aq_base(context)
+    # get the bref annotation storage of the context
+    brefs = get_bref_storage(context)
+    # get the referenced UIDs
+    bref_uids = list(brefs.get(relationship, []))
+
+    if not bref_uids:
+        return []
+
+    if as_objects is True:
+        return [api.get_object(uid) for uid in bref_uids]
+
+    return bref_uids
+
+
+def get_bref_storage(context):
+    """Get the annotation storage for backreferences of the context
+    """
+    annotation = IAnnotations(context)
+    if annotation.get(BACKREFS_STORAGE) is None:
+        annotation[BACKREFS_STORAGE] = PersistentDict()
+    return annotation[BACKREFS_STORAGE]
 
 
 @implementer(IUIDReferenceField, IFromUnicode)
@@ -16,15 +55,174 @@ class UIDReferenceField(List, BaseField):
     value_type = ASCIILine(title=u"UID")
 
     def __init__(self, allowed_types=None, multi_valued=True, **kw):
+        if allowed_types is None:
+            allowed_types = ()
         self.allowed_types = allowed_types
         self.multi_valued = multi_valued
         super(UIDReferenceField, self).__init__(**kw)
 
+    def get_relationship_key(self, context):
+        """Relationship key used for backreferences
+
+        The key used for the annotation storage on the referenced object to
+        remember the current object UID.
+
+        :returns: storage key to lookup back references
+        """
+        portal_type = api.get_portal_type(context)
+        return "%s.%s" % (portal_type, self.__name__)
+
+    def to_uid(self, value):
+        """convert a value to an UID
+
+        :parm value: object/UID/SuperModel
+        :returns: UID
+        """
+        try:
+            return api.get_uid(value)
+        except api.APIError:
+            raise TypeError("Can not get UID of '%s'" % repr(value))
+
+    def get_allowed_types(self):
+        """Returns the allowed reference types
+
+        :returns: tuple of allowed_types
+        """
+        allowed_types = self.allowed_types
+        if not allowed_types:
+            allowed_types = ()
+        elif isinstance(allowed_types, six.string_types):
+            allowed_types = (allowed_types, )
+        return allowed_types
+
     def set(self, object, value):
-        super(UIDReferenceField, self).set(object, value)
+        """Set UID reference
+
+        :param object: the instance of the field
+        :param value: object/UID/SuperModel
+        :type value: list/tuple/str
+        """
+
+        # always handle all values internally as a list
+        if isinstance(value, six.string_types):
+            value = [value]
+        elif api.is_object(value):
+            value = [value]
+
+        # check if the fields accepts single values only
+        if not self.multi_valued and len(value) > 1:
+            raise TypeError("Single valued field accepts at most 1 value")
+
+        # check if the type is allowed
+        allowed_types = self.get_allowed_types()
+        if allowed_types:
+            objs = map(api.get_object, value)
+            types = set(map(api.get_portal_type, objs))
+            if not types.issubset(allowed_types):
+                raise TypeError("Only the following types are allowed: %s"
+                                % ",".join(allowed_types))
+
+        # convert to UIDs
+        uids = []
+        for v in value:
+            uid = self.to_uid(v)
+            uids.append(uid)
+
+        # current set UIDs
+        existing = self.get_raw(object)
+
+        # filter out new/removed UIDs
+        added_uids = [u for u in uids if u not in existing]
+        removed_uids = [u for u in existing if u not in uids]
+
+        # link backreferences of new uids
+        for uid in added_uids:
+            self.link_bref(api.get_object(uid), object)
+
+        # unlink backreferences of removed UIDs
+        for uid in removed_uids:
+            self.unlink_bref(api.get_object(uid), object)
+
+        super(UIDReferenceField, self).set(object, uids)
+
+    def unlink_bref(self, source, target):
+        """Remove backreference from the source to the target
+
+        :param source: the object where the bref is stored (our reference)
+        :param target: the object where the bref points to (our object)
+        :returns: True when the bref was removed, False otherwise
+        """
+        target_uid = api.get_uid(target)
+        # get the storage key
+        key = self.get_relationship_key(target)
+        # get all backreferences from the source
+        brefs = get_bref_storage(source)
+        if key not in brefs:
+            logger.warn(
+                "Referenced object {} has no backreferences for the key {}"
+                .format(repr(source), key))
+            return False
+        if target_uid not in brefs[key]:
+            logger.warn("Target {} was not linked by {}"
+                        .format(repr(target), repr(source)))
+            return False
+        brefs[key].remove(target_uid)
+        return True
+
+    def link_bref(self, source, target):
+        """Add backreference from the source to the target
+
+        :param source: the object where the bref is stored (our reference)
+        :param target: the object where the bref points to (our object)
+        :returns: True when the bref was written
+        """
+        target_uid = api.get_uid(target)
+        # get the annotation storage key
+        key = self.get_relationship_key(target)
+        # get all backreferences
+        brefs = get_bref_storage(source)
+        if key not in brefs:
+            brefs[key] = PersistentList()
+        if target_uid not in brefs[key]:
+            brefs[key].append(target_uid)
+        return True
 
     def get(self, object):
-        return super(UIDReferenceField, self).get(object)
+        """Get referenced objects
+
+        :param object: instance of the field
+        :returns: list of referenced objects
+        """
+        return self._get(object, as_objects=True)
+
+    def get_raw(self, object):
+        """Get referenced UIDs
+
+        :param object: instance of the field
+        :returns: list of referenced UIDs
+        """
+        return self._get(object, as_objects=False)
+
+    def _get(self, object, as_objects=False):
+        """Returns single/multi value
+
+        :param object: instance of the field
+        :param as_objects: Flag for UID/object returns
+        :returns: list of referenced UIDs
+        """
+        uids = super(UIDReferenceField, self).get(object)
+
+        if not uids:
+            uids = []
+
+        if as_objects is True:
+            uids = map(api.get_object_by_uid, uids)
+
+        if self.multi_valued:
+            return uids
+        if len(uids) == 0:
+            return None
+        return uids[0]
 
     def fromUnicode(self, value):
         self.validate(value)
