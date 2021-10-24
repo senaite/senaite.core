8a0990aeae34a6103511d59367ac5765e55096c3
Ramon Bartl <rb@ridingbytes.com>
2021-10-24 10:59:28 +0200

    Show object links in display view

--- src/senaite/core/z3cform/widgets/uidreference.py
+++ src/senaite/core/z3cform/widgets/uidreference.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from bika.lims import api
 from senaite.core.interfaces import ISenaiteFormLayer
 from senaite.core.schema.interfaces import IUIDReferenceField
 from senaite.core.z3cform.interfaces import IUIDReferenceWidget
+from z3c.form import interfaces
 from z3c.form.browser import widget
 from z3c.form.browser.textlines import TextLinesWidget
+from z3c.form.converter import TextLinesConverter
 from z3c.form.interfaces import IFieldWidget
 from z3c.form.widget import FieldWidget
 from zope.component import adapter
 from zope.interface import implementer
+
+
+@adapter(IUIDReferenceField, interfaces.IWidget)
+class UIDReferenceDataConverter(TextLinesConverter):
+
+    def toWidgetValue(self, value):
+        if self.widget.mode == "display":
+            return map(api.get_object, value)
+        return super(UIDReferenceDataConverter, self).toWidgetValue(value)
 
 
 @implementer(IUIDReferenceWidget)
@@ -24,6 +36,14 @@ class UIDReferenceWidget(TextLinesWidget):
     def update(self):
         super(UIDReferenceWidget, self).update()
         widget.addFieldClass(self)
+
+    def get_info_for(self, uid):
+        obj = api.get_object(uid)
+        return {
+            "title": api.get_title(obj),
+            "path": api.get_path(obj),
+            "url": api.get_url(obj),
+        }
 
 
 @adapter(IUIDReferenceField, ISenaiteFormLayer)
