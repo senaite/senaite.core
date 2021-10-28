9d9311931611ebaf18ab31fbedab18bed6a86fa6
Ramon Bartl <rb@ridingbytes.com>
2021-10-28 08:57:07 +0200

    Added docstrings

--- webpack/app/widgets/uidreferencewidget/widget.js
+++ webpack/app/widgets/uidreferencewidget/widget.js
@@ -14,21 +14,22 @@ class UIDReferenceWidgetController extends React.Component {
 
     // Internal state
     this.state = {
-      results: [],
-      searchterm: "",
-      loading: false,
-      count: 0,
-      page: 1,
-      pages: 1,
-      next_url: null,
-      prev_url: null,
-      b_start: 1,
+      results: [],  // `items` list of search results coming from `senaite.jsonapi`
+      searchterm: "",  // the search term that was entered by the user
+      loading: false,  // loading flag when searching for results
+      count: 0,  // count of results (coming from `senaite.jsonapi`)
+      page: 1,  // current page (coming from `senaite.jsonapi`)
+      pages: 1,  // number of pages (coming from `senaite.jsonapi`)
+      next_url: null,  // next page API URL (coming from `senaite.jsonapi`)
+      prev_url: null,  // previous page API URL (coming from `senaite.jsonapi`)
+      b_start: 1,  // batch start for pagination (see `senaite.jsonapi.batch`)
     }
 
-    // root input HTML element
+    // Root input HTML element
     let el = props.root_el;
 
-    // data keys located at the root element
+    // Data keys located at the root element
+    // -> initial values are set from the widget class
     const data_keys = [
       "id",
       "name",
@@ -45,23 +46,24 @@ class UIDReferenceWidgetController extends React.Component {
       "readonly",
     ]
 
-    // query data keys and set state with parsed JSON value
+    // Query data keys and set state with parsed JSON value
     for (let key of data_keys) {
       let value = el.dataset[key];
       this.state[key] = this.parse_json(value);
     }
 
-    // Prepare API
+    // Initialize communication API with the API URL
     this.api = new ReferenceWidgetAPI({
       api_url: this.state.api_url,
     });
 
+    // Bind callbacks to current context
     this.search = this.search.bind(this);
     this.goto_page = this.goto_page.bind(this);
     this.clear_results = this.clear_results.bind(this);
     this.select = this.select.bind(this);
     this.deselect = this.deselect.bind(this);
-    this.on_esc = this.on_esc.bind(this);
+    this.on_keydown = this.on_keydown.bind(this);
     this.on_click = this.on_click.bind(this);
 
     // dev only
@@ -71,26 +73,39 @@ class UIDReferenceWidgetController extends React.Component {
   }
 
   componentDidMount() {
-    document.addEventListener("keydown", this.on_esc, false);
+    // Bind event listeners of the document
+    document.addEventListener("keydown", this.on_keydown, false);
     document.addEventListener("click", this.on_click, false)
   }
 
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.on_esc, false);
+    // Remove event listeners of the document
+    document.removeEventListener("keydown", this.on_keydown, false);
     document.removeEventListener("click", this.on_click, false);
   }
 
   /*
    * JSON parse the given value
+   *
+   * @param {String} value: The JSON value to parse
    */
   parse_json(value) {
     try {
       return JSON.parse(value)
     } catch (error) {
-      console.warn(`Could not parse "${value}" to JSON`);
+      console.error(`Could not parse "${value}" to JSON`);
     }
   }
 
+  /*
+   * Create a query object for the API
+   *
+   * This method prepares a query from the current state variables,
+   * which can be used to call the `api.search` method.
+   *
+   * @param {Object} options: Additional options to add to the query
+   * @returns {Object} The query object
+   */
   make_query(options) {
     options = options || {};
     return Object.assign({
@@ -100,6 +115,12 @@ class UIDReferenceWidgetController extends React.Component {
     }, options, this.state.query);
   }
 
+  /*
+   * Execute a search query and set the results to the state
+   *
+   * @param {Object} url_params: Additional search params for the API search URL
+   * @returns {Promise}
+   */
   fetch_results(url_params) {
     url_params = url_params || {};
     // prepare the server request
@@ -108,36 +129,65 @@ class UIDReferenceWidgetController extends React.Component {
     this.toggle_loading(true);
     let promise = this.api.search(this.state.catalog, query, url_params);
     promise.then(function(data) {
-      console.debug(">>> GOT REFWIDGET SEARCH RESULTS: ", data);
-      self.set_results_data(data)
+      console.debug("ReferenceWidgetController::fetch_results:GOT DATA: ", data);
+      self.set_results_data(data);
       self.toggle_loading(false);
     });
+    return promise;
   }
 
-  search(value) {
-    console.debug("ReferenceWidgetController::search:value:", value);
-    // set the searchterm directly
-    this.state.searchterm = value;
-    this.fetch_results();
+  /*
+   * Execute a search for the given searchterm
+   *
+   * @param {String} searchterm: The value entered into the search field
+   * @returns {Promise}
+   */
+  search(searchterm) {
+    console.debug("ReferenceWidgetController::search:searchterm:", searchterm);
+    // set the searchterm directly to avoid re-rendering
+    this.state.searchterm = searchterm;
+    return this.fetch_results();
   }
 
+  /*
+   * Fetch results of a page
+   *
+   * @param {Integer} page: The page to fetch
+   * @returns {Promise}
+   */
   goto_page(page) {
     page = parseInt(page);
     let limit = parseInt(this.state.limit)
+    // calculate the beginning of the page
+    // Note: this is the count of previous items that are excluded
     let b_start = page * limit - limit;
-    this.fetch_results({b_start: b_start});
+    return this.fetch_results({b_start: b_start});
   }
 
+  /*
+   * Add the UID of a search result to the state
+   *
+   * @param {String} uid: The selected UID
+   * @returns {Array} uids: current selected UIDs
+   */
   select(uid) {
     console.debug("ReferenceWidgetController::select:uid:", uid);
     // create a copy of the selected UIDs
     let uids = [].concat(this.state.uids);
+    // Add the new UID if it is not selected yet
     if (uids.indexOf(uid) == -1) {
       uids.push(uid);
     }
     this.setState({uids: uids});
+    return uids;
   }
 
+  /*
+   * Remove the UID of a reference from the state
+   *
+   * @param {String} uid: The selected UID
+   * @returns {Array} uids: current selected UIDs
+   */
   deselect(uid) {
     console.debug("ReferenceWidgetController::deselect:uid:", uid);
     let uids = [].concat(this.state.uids);
@@ -146,9 +196,15 @@ class UIDReferenceWidgetController extends React.Component {
       uids.splice(pos, 1);
     }
     this.setState({uids: uids});
-    this.clear_results();
+    return uids;
   }
 
+  /*
+   * Toggle loading state
+   *
+   * @param {Boolean} toggle: The loading state to set
+   * @returns {Boolean} toggle: The current loading state
+   */
   toggle_loading(toggle) {
     if (toggle == null) {
       toggle = false;
@@ -159,6 +215,11 @@ class UIDReferenceWidgetController extends React.Component {
     return toggle;
   }
 
+  /*
+   * Set results data coming from `senaite.jsonapi`
+   *
+   * @param {Object} data: JSON search result object returned from `senaite.jsonapi`
+   */
   set_results_data(data) {
     data = data || {};
     let items = data.items || [];
@@ -181,6 +242,9 @@ class UIDReferenceWidgetController extends React.Component {
     });
   }
 
+  /*
+   * Clear results from the state
+   */
   clear_results() {
     this.setState({
       results: [],
@@ -192,13 +256,19 @@ class UIDReferenceWidgetController extends React.Component {
     });
   }
 
-  on_esc(event){
+  /*
+   * ReactJS event handler for keydown event
+   */
+  on_keydown(event){
     // clear results when ESC key is pressed
     if(event.keyCode === 27) {
       this.clear_results();
     }
   }
 
+  /*
+   * ReactJS event handler for click events
+   */
   on_click(event) {
     // clear results when clicked outside of the widget
     let widget = this.props.root_el;
@@ -207,7 +277,6 @@ class UIDReferenceWidgetController extends React.Component {
       this.clear_results();
     }
   }
-
 
   render() {
     return (
