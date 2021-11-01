587ba681fa2280b686f4872adf2a0760a9407eb2
Ramon Bartl <rb@ridingbytes.com>
2021-11-01 20:22:40 +0100

    Patch isFactoryContained for CatalogMultiplex

--- src/senaite/core/patches/archetypes.py
+++ src/senaite/core/patches/archetypes.py
@@ -5,7 +5,6 @@ import re
 from Acquisition import aq_inner
 from Acquisition import aq_parent
 from bika.lims import api
-from Products.Archetypes.utils import isFactoryContained
 from Products.Archetypes.utils import shasattr
 from senaite.core import logger
 
@@ -29,20 +28,3 @@ def isTemporary(self):
     return tmp
 
 
-def indexObject(self):
-    if isFactoryContained(self):
-        return
-    parent = aq_parent(aq_inner(self))
-    # Fix indexing of temporary objects resulting in orphan entries in catalog
-    # https://github.com/senaite/senaite.core/pull/1865
-    if is_tmp_id(self.id) or is_tmp_id(parent.id):
-        logger.debug("Object %s is temporary!" % api.get_path(self))
-        return
-    catalogs = self.getCatalogs()
-    url = api.get_path(self)
-    for c in catalogs:
-        if c.id == "portal_catalog":
-            # use catalog tool queuing system
-            c.indexObject(self)
-            continue
-        c.catalog_object(self, url)
