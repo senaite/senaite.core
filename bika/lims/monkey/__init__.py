f794c8bf937eafa238eb20da883c372b5d610188
Jordi Puiggené <jp@naralabs.com>
2021-11-02 12:59:37 +0100

    Port 1865 to 1.3.x

--- bika/lims/monkey/__init__.py
+++ bika/lims/monkey/__init__.py
@@ -19,3 +19,25 @@
 # Some rights reserved, see README and LICENSE.
 
 import add_senaite_site  # noqa
+
+from Acquisition import aq_base
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from Products.Archetypes import utils
+
+
+def isFactoryContained(obj):
+    """Are we inside the portal_factory?
+    """
+    if obj.isTemporary():
+        return True
+    parent = aq_parent(aq_inner(obj))
+    if parent is None:
+        # We don't have enough context to know where we are
+        return False
+    meta_type = getattr(aq_base(parent), "meta_type", "")
+    return meta_type == "TempFolder"
+
+
+# https://pypi.org/project/collective.monkeypatcher/#patching-module-level-functions
+utils.isFactoryContained = isFactoryContained
