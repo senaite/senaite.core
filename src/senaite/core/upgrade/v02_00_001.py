2eb3ac69035109d32643a1003881ec7a65676f03
Ramon Bartl <rb@ridingbytes.com>
2021-11-09 21:21:29 +0100

    Added migration step

--- src/senaite/core/upgrade/v02_00_001.py
+++ src/senaite/core/upgrade/v02_00_001.py
@@ -20,12 +20,30 @@
 
 from bika.lims import api
 from senaite.core import logger
+from senaite.core.api.catalog import add_column
+from senaite.core.api.catalog import add_index
+from senaite.core.api.catalog import get_columns
+from senaite.core.api.catalog import get_index
+from senaite.core.api.catalog import get_indexes
 from senaite.core.config import PROJECTNAME as product
+from senaite.core.setuphandlers import setup_auditlog_catalog_mappings
+from senaite.core.setuphandlers import setup_core_catalogs
 from senaite.core.upgrade import upgradestep
 from senaite.core.upgrade.utils import UpgradeUtils
 
 version = "2.0.1"  # Remember version number in metadata.xml and setup.py
 profile = "profile-{0}:default".format(product)
+
+MIGRATE_CATALOGS = [
+    ("auditlog_catalog", "senaite_catalog_auditlog"),
+    ("bika_analysis_catalog", "senaite_catalog_analysis"),
+    ("bika_catalog", "senaite_catalog"),
+    ("bika_catalog_analysisrequest_listing", "senaite_catalog_sample"),
+    ("bika_catalog_autoimportlogs_listing", "senaite_catalog_autoimportlog"),
+    ("bika_catalog_report", "senaite_catalog_report"),
+    ("bika_catalog_worksheet_listing", "senaite_catalog_worksheet"),
+    ("bika_setup_catalog", "senaite_catalog_setup"),
+]
 
 
 @upgradestep(product, version)
@@ -35,10 +53,10 @@ def upgrade(tool):
     ut = UpgradeUtils(portal)
     ver_from = ut.getInstalledVersion(product)
 
-    if ut.isOlderVersion(product, version):
-        logger.info("Skipping upgrade of {0}: {1} > {2}".format(
-            product, ver_from, version))
-        return True
+    # if ut.isOlderVersion(product, version):
+    #     logger.info("Skipping upgrade of {0}: {1} > {2}".format(
+    #         product, ver_from, version))
+    #     return True
 
     logger.info("Upgrading {0}: {1} -> {2}".format(product, ver_from, version))
 
@@ -47,6 +65,9 @@ def upgrade(tool):
     setup.runImportStepFromProfile(profile, "workflow")
 
     fix_types_i18n_domain(portal)
+
+    # https://github.com/senaite/senaite.core/pull/1872
+    migrate_catalogs(portal)
 
     logger.info("{0} upgraded to version {1}".format(product, version))
     return True
@@ -62,3 +83,65 @@ def fix_types_i18n_domain(portal):
         if fti.product in products:
             if fti.i18n_domain != "senaite.core":
                 fti.i18n_domain = "senaite.core"
+
+
+def migrate_catalogs(portal):
+    """Migrate catalogs to Senaite
+    """
+    logger.info("Migrate catalogs to Senaite...")
+    # 1. Install new core catalogs
+    setup_core_catalogs(portal)
+    # 2. Setup auditog catalog
+    setup_auditlog_catalog_mappings(portal)
+
+    # 3. Migrate old -> new indexes
+    for src_cat_id, dst_cat_id in MIGRATE_CATALOGS:
+        logger.info("Migrating catalog %s -> %s" %
+                    (src_cat_id, dst_cat_id))
+
+        src_cat = getattr(portal, src_cat_id, None)
+        dst_cat = getattr(portal, dst_cat_id, None)
+
+        if src_cat is None:
+            logger.info("Source catalog '%s' not found [SKIP]")
+            continue
+
+        # ensure indexes
+        for index in get_indexes(src_cat):
+            if index not in get_indexes(dst_cat):
+                index_obj = get_index(src_cat, index)
+                index_type = index_obj.__class__.__name__
+                add_index(dst_cat, index, index_type)
+                logger.info("Added missing index %s('%s') to %s"
+                            % (index_type, index, dst_cat_id))
+
+        # ensure columns
+        for column in get_columns(src_cat):
+            if column not in get_columns(dst_cat):
+                add_column(dst_cat, column)
+                logger.info("Added missing column %s to %s"
+                            % (column, dst_cat_id))
+
+        # copy over internal catalog structure from internal Catalog:
+        #
+        # self.schema = {}   # mapping from attribute name to column number
+        # self.names = ()    # sequence of column names
+        # self.indexes = {}  # mapping from index name to index object
+        dst_cat._catalog.schema = src_cat._catalog.schema
+        dst_cat._catalog.names = src_cat._catalog.names
+        dst_cat._catalog.indexes = src_cat._catalog.indexes
+
+        # self.data = IOBTree()  # mapping of rid to meta_data
+        # self.uids = OIBTree()  # mapping of uid to rid
+        # self.paths = IOBTree()  # mapping of rid to uid
+        dst_cat._catalog.data = src_cat._catalog.data
+        dst_cat._catalog.uids = src_cat._catalog.uids
+        dst_cat._catalog.paths = src_cat._catalog.paths
+        dst_cat._catalog._length = src_cat._catalog._length
+
+        # dst_cat.refreshCatalog()
+
+        # delete old catalog
+        # portal.manage_delObjects([src_cat_id])
+
+    logger.info("Migrate catalogs to Senaite [DONE]")
