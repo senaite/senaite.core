dd92c5e5a8d5cfb5efb5bc20f5386463fa44d949
Jordi Puiggené <jp@naralabs.com>
2022-04-12 17:12:36 +0200

    Ensure data is extracted and encoded correctly

--- src/senaite/core/z3cform/widgets/address.py
+++ src/senaite/core/z3cform/widgets/address.py
@@ -19,24 +19,28 @@
 # Some rights reserved, see README and LICENSE.
 
 import json
+import six
+from bika.lims import api
+from bika.lims import senaiteMessageFactory as _
+from Products.CMFPlone.utils import safe_unicode
 from senaite.core.api import geo
 from senaite.core.interfaces import ISenaiteFormLayer
 from senaite.core.schema.interfaces import IAddressField
 from senaite.core.z3cform.interfaces import IAddressWidget
+from six import string_types
 from z3c.form.browser.widget import HTMLFormElement
-from z3c.form.converter import FieldDataConverter
+from z3c.form.converter import BaseDataConverter
 from z3c.form.interfaces import IFieldWidget
 from z3c.form.interfaces import IWidget
+from z3c.form.interfaces import NO_VALUE
 from z3c.form.widget import FieldWidget
 from z3c.form.widget import Widget
 from zope.component import adapter
 from zope.interface import implementer
 
-from bika.lims import api
-
 
 @adapter(IAddressField, IWidget)
-class AddressDataConverter(FieldDataConverter):
+class AddressDataConverter(BaseDataConverter):
     """Value conversion between field and widget
     """
     def to_list_of_dicts(self, value):
@@ -53,12 +57,42 @@ class AddressDataConverter(FieldDataConverter):
     def toWidgetValue(self, value):
         """Returns the field value with encoded string
         """
-        return self.to_list_of_dicts(value)
+        values = self.to_list_of_dicts(value)
+        return self.to_utf8(values)
 
     def toFieldValue(self, value):
         """Converts from widget value to safe_unicode
         """
-        return self.to_list_of_dicts(value)
+        values = self.to_list_of_dicts(value)
+        return self.to_safe_unicode(values)
+
+    def to_safe_unicode(self, data):
+        """Converts the data to unicode
+        """
+        if isinstance(data, unicode):
+            return data
+        if isinstance(data, list):
+            return [self.to_safe_unicode(item) for item in data]
+        if isinstance(data, dict):
+            return {
+                self.to_safe_unicode(key): self.to_safe_unicode(value)
+                for key, value in six.iteritems(data)
+            }
+        return safe_unicode(data)
+
+    def to_utf8(self, data):
+        """Encodes the data to utf-8
+        """
+        if isinstance(data, unicode):
+            return data.encode("utf-8")
+        if isinstance(data, list):
+            return [self.to_utf8(item) for item in data]
+        if isinstance(data, dict):
+            return {
+                self.to_utf8(key): self.to_utf8(value)
+                for key, value in six.iteritems(data)
+            }
+        return data
 
 
 @implementer(IAddressWidget)
@@ -77,14 +111,19 @@ class AddressWidget(HTMLFormElement, Widget):
         """
         if self.address_format:
             return self.address_format
+
         lines = [
             "<strong>{type}</strong>",
             "{address}",
             "{zip} {city}",
-            "{subdivision2}",
-            "{subdivision1}",
+            "{subdivision2}, {subdivision1}",
             "{country}"
         ]
+
+        # Skip the type if multi-address
+        if not self.field.is_multi_address():
+            lines = lines[1:]
+
         return "<br/>".join(lines)
 
     def get_formatted_address(self, address):
@@ -93,6 +132,9 @@ class AddressWidget(HTMLFormElement, Widget):
         address_format = self.get_address_format()
         lines = address_format.split("<br/>")
         values = map(lambda line: line.format(**address), lines)
+        # Some extra cleanup
+        values = map(lambda line: line.strip(",- "), values)
+        values = filter(lambda line: line != "()", values)
         values = filter(None, values)
         return "<br/>".join(values)
 
@@ -140,6 +182,31 @@ class AddressWidget(HTMLFormElement, Widget):
 
         return countries
 
+    def extract(self, default=NO_VALUE):
+        """Extracts the value based on the request and nothing else
+        """
+        mapping = {}
+        prefix = "{}.".format(self.name)
+        keys = filter(lambda key: key.startswith(prefix), self.request.keys())
+        for key in keys:
+            val = key.replace(prefix, "")
+            idx, subfield_name = val.split(".")
+            if not api.is_floatable(idx):
+                continue
+
+            empty_record = self.field.get_empty_address("")
+            mapping.setdefault(api.to_int(idx), empty_record).update({
+                subfield_name: self.request.get(key)
+            })
+
+        # Return the list of items sorted correctly
+        records = []
+        for index in sorted(mapping.keys()):
+            record = mapping.get(index)
+            records.append(record)
+
+        return records or default
+
     def get_input_widget_attributes(self):
         """Return input widget attributes for the ReactJS component
         """
@@ -154,6 +221,14 @@ class AddressWidget(HTMLFormElement, Widget):
             "data-class": self.klass,
             "data-style": self.style,
             "data-disabled": self.disabled or False,
+            "data-labels": {
+                "country": api.to_utf8(_("Country")),
+                "subdivision1": api.to_utf8(_("State")),
+                "subdivision2": api.to_utf8(_("District")),
+                "city": api.to_utf8(_("City")),
+                "zip": api.to_utf8(_("Postal code")),
+                "address": api.to_utf8(_("Address")),
+            },
         }
 
         # convert all attributes to JSON
