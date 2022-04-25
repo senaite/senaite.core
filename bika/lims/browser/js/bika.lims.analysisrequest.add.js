5271692f67da1dab4611a5dae3006212937de65f
Jordi Puiggené <jp@naralabs.com>
2022-04-25 23:18:30 +0200

    _t( --> _(

--- bika/lims/browser/js/bika.lims.analysisrequest.add.js
+++ bika/lims/browser/js/bika.lims.analysisrequest.add.js
@@ -1337,7 +1337,7 @@
         if (data['errors']) {
           msg = data.errors.message;
           if (msg !== "") {
-            msg = _t("Sorry, an error occured 🙈<p class='code'>" + msg + "</p>");
+            msg = _("Sorry, an error occured 🙈<p class='code'>" + msg + "</p>");
           }
           for (fieldname in data.errors.fielderrors) {
             field = $("#" + fieldname);
