139f878b139541d29e4cad4f4e863c6d1a04f8ea
Lunga Baliwe <lunga001@gmail.com>
2022-05-03 23:32:24 +0200

    Use api.is_ui to check for empty methods Omit get_available_instruments_for and rely on method's getInstruments Remove commented code

--- src/senaite/core/browser/form/adapters/worksheettemplate.py
+++ src/senaite/core/browser/form/adapters/worksheettemplate.py
@@ -15,7 +15,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 51
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-# Copyright 2018-2021 by it's authors.
+# Copyright 2018-2022 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
 from bika.lims import api
@@ -26,7 +26,7 @@ from zope.component import queryMultiAdapter
 
 
 class EditForm(EditFormAdapterBase):
-    """Edit form adapter for Method
+    """Edit form adapter for Worksheet Template
     """
 
     def initialized(self, data):
@@ -38,39 +38,29 @@ class EditForm(EditFormAdapterBase):
 
         # Handle Methods Change
         if name == "RestrictToMethod" and value:
-            # Available Methods
+            value = value[0]
             empty = [{"title": _("No Instrument"), "value": [""]}]
+            if not api.is_uid(value):
+                self.add_update_field("Instruments:list", {
+                    "options": empty})
+                self.add_update_field("Instruments_options", {
+                    "options": empty})
+                self.add_update_field("Instrument", {
+                    "options": empty})
+                return self.data
+
             # Get selected method
-            method = api.get_object_by_uid(value[0])
-            instruments = self.get_available_instruments_for(method)
+            method = api.get_object_by_uid(value)
+            instruments = method.getInstruments()
             i_opts = map(lambda o: dict(
                 title=api.get_title(o), value=api.get_uid(o)), instruments)
 
-            # When methods are selected, we filter other fields accordingly
-            if method:
-                # selected instruments
-                i_sel = map(api.get_uid, instruments)
-                self.add_update_field("Instruments_options", {
-                    "options": i_opts})
-                self.add_update_field("Instruments:list", {
-                    "options": i_opts, "selected": i_sel})
-                self.add_update_field("Instrument", {
-                    "options": empty + i_opts})
-            else:
-                self.add_update_field("Instruments:list", {
-                    "options": []})
-                self.add_update_field("Instruments_options", {
-                    "options": i_opts})
-                self.add_update_field("Instrument", {
-                    "options": empty})
+            i_sel = map(api.get_uid, instruments)
+            self.add_update_field("Instruments_options", {
+                "options": i_opts})
+            self.add_update_field("Instruments:list", {
+                "options": i_opts, "selected": i_sel})
+            self.add_update_field("Instrument", {
+                "options": empty + i_opts})
 
         return self.data
-
-    def get_available_instruments_for(self, method):
-        """Return all available instruments for the given method
-
-        If no methods are given, all active instruments are returned
-        """
-        if method:
-            instruments = method.getInstruments()
-            return instruments
