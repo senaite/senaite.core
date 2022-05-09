09560e6aa9ec361674d306b8e4fcf89c68ef503f
Ramon Bartl <rb@ridingbytes.com>
2022-05-09 16:56:25 +0200

    reindex object slug for UID lookup

--- src/senaite/core/exportimport/genericsetup/structure.py
+++ src/senaite/core/exportimport/genericsetup/structure.py
@@ -479,6 +479,7 @@ def create_or_get(parent, id, uid, portal_type):
         notify(ObjectCreatedEvent(obj))
         parent._setObject(tmp_id, obj)
         obj = parent._getOb(api.get_id(obj))
+        obj.reindexObject()
 
     return obj
 
