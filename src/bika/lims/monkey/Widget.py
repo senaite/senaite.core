4fc3cb3418dff52aaf1c4589e6d097d484fcf38a
Ramon Bartl <rb@ridingbytes.com>
2022-06-22 20:22:43 +0200

    Reduced logging when creating samples

--- src/bika/lims/monkey/Widget.py
+++ src/bika/lims/monkey/Widget.py
@@ -74,7 +74,7 @@ def isVisible(self, instance, mode='view', default="visible", field=None):
             continue
         adapter_state = adapter(instance, mode, field, state)
         adapter_name = adapter.__class__.__name__
-        logger.info(
+        logger.debug(
             "IATWidgetVisibility: <{} for {}.{} mode:{} sort:{}> {} -> {}"
             .format(adapter_name, instance.id, field.getName(), mode,
                     adapter.sort, state, adapter_state))
