4fa0313043de0750c50e845e3d3bfa27215e965f
Ramon Bartl <rb@ridingbytes.com>
2022-07-21 18:05:31 +0200

    Moved to SENAITE Setup

--- src/senaite/core/registry/schema.py
+++ src/senaite/core/registry/schema.py
@@ -1,36 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from bika.lims import api
-from plone.autoform import directives
-from senaite.impress import senaiteMessageFactory as _
 from zope.interface import Interface
-from zope.interface import provider
-from zope.schema.interfaces import IContextAwareDefaultFactory
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from senaite.core.schema.registry import RichTextField
-
-
-@provider(IContextAwareDefaultFactory)
-def default_email_body_sample_publication(context):
-    """Returns the default body text for publication emails
-    """
-    view = api.get_view("senaite_view")
-    if view is None:
-        # Test fixture
-        return ""
-    tpl = ViewPageTemplateFile("templates/email_body_sample_publication.pt")
-    return tpl(view)
 
 
 class ISenaiteRegistry(Interface):
     """Senaite registry schema
     """
-
-    directives.widget("email_body_sample_publication", klass="richTextWidget")
-    email_body_sample_publication = RichTextField(
-        title=_(u"Publication Email Text"),
-        description=_(
-            "The default text that is used for the publication email."),
-        defaultFactory=default_email_body_sample_publication,
-        required=False,
-    )
--- src/senaite/core/schema/registry.py
+++ src/senaite/core/schema/registry.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from senaite.core.schema import RichTextField as BaseRichTextField
 from senaite.core.schema.fields import DataGridField as BaseDataGridField
 from senaite.core.schema.fields import DataGridRow as BaseDataGridRow
-from plone.app.textfield.interfaces import IRichTextValue
 
 try:
     from plone.registry.field import PersistentField
@@ -28,22 +26,3 @@ class DataGridRow(PersistentField, BaseDataGridRow):
     https://community.plone.org/t/there-is-no-persistent-field-equivalent-for-the-field-a-of-type-b
     """
     pass
-
-
-class RichTextField(PersistentField, BaseRichTextField):
-    """Use this field for registry entries
-
-    https://pypi.org/project/plone.registry/#persistent-fields
-    https://community.plone.org/t/there-is-no-persistent-field-equivalent-for-the-field-a-of-type-b
-    """
-
-    def set(self, object, value):
-        """Convert IRichTextValue to plain string for registry
-
-        :param object: the instance of the field
-        :param value: value to set
-        """
-        # Convert value for registry to the raw text
-        if IRichTextValue.providedBy(value):
-            value = value.raw
-        super(RichTextField, self).set(object, value)
