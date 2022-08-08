d89a66f13c3c8652148a1b6ead397994d6ec3fdd
Ramon Bartl <rb@ridingbytes.com>
2022-08-08 08:15:15 +0200

    Field mockup for widgets

--- src/senaite/core/browser/viewlets/sampleheader.py
+++ src/senaite/core/browser/viewlets/sampleheader.py
@@ -2,11 +2,13 @@
 
 from bika.lims import api
 from bika.lims import senaiteMessageFactory as _
-from plone.app.layout.viewlets import ViewletBase
+from itertools import islice
 from bika.lims.api.security import check_permission
+from plone.app.layout.viewlets import ViewletBase
+from Products.Archetypes.interfaces import IField as IATField
 from Products.CMFCore.permissions import ModifyPortalContent
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.Archetypes.interfaces import IField as IATField
+from senaite.core import logger
 from zope.schema.interfaces import IField as IDXField
 
 
@@ -22,42 +24,93 @@ class SampleHeaderViewlet(ViewletBase):
             self.request.response.redirect(self.context.absolute_url())
         return self.template()
 
+    def render_field(self, field):
+        """Render the field
+        """
+        mode = self.get_field_mode(field)
+        return self.context.widget(field.getName(), mode=mode)
+
     @property
     def fields(self):
         """Returns an ordered dict of all schema fields
         """
         return api.get_fields(self.context)
 
+    def grouper(self, iterable, n):
+        for chunk in iter(lambda it=iter(iterable): list(islice(it, n)), []):
+            yield chunk
+
     def get_header_fields(self):
         """Return the (re-arranged) fields
         """
-        prominent = []
-        standard = []
-        hidden = []
-
-        for name, field in self.fields.items():
-            layout = self.get_field_visibility(field)
-
-        return {
-            "prominent": prominent,
-            "standard": standard
-            "hidden": hidden
+        header_fields = {
+            "prominent": [],
+            "visible": [],
+            "hidden": [],
         }
 
-    def get_field_visibility(self, field):
-        """Returns "view" or "edit" modes, together with the place within where
-        this field has to be rendered, based on the permissions the current
-        user has for the context and the field passed in
+        for name, field in self.fields.items():
+            vis = self.get_field_visibility(field)
+            mode = self.get_field_mode(field)
+            if mode not in ["view", "edit"]:
+                continue
+            header_fields[vis].append(field)
+
+        return header_fields
+
+    def get_field_visibility(self, field, default="hidden"):
+        """Returns the field visibility in the header
+
+        Possible values are:
+
+          - prominent: Rendered as a promient field
+          - visible: Rendered as a standard field
+          - hidden: Not displayed
         """
         widget = self.get_widget(field)
-        widget.isVisible(self.context, "header_table")
+        visibility = widget.isVisible(self.context, mode="header_table", field=field)
+        if visibility not in ["prominent", "visible"]:
+            return default
+        return visibility
+
+    def get_field_mode(self, field, default="hidden"):
+        """Returns the field mode in the header
+
+        Possible values are:
+
+          - edit: field is rendered in edit mode
+          - view: field is rendered in view mode
+        """
+        mode = "view"
+        if field.checkPermission("edit", self.context):
+            mode = "edit"
+            if not self.is_edit_allowed():
+                logger.warn("Permission '{}' granted for the edition of '{}', "
+                            "but 'Modify portal content' not granted"
+                            .format(field.write_permission, field.getName()))
+        elif field.checkPermission("view", self.context):
+            mode = "view"
+
+        widget = self.get_widget(field)
+        mode_vis = widget.isVisible(self.context, mode=mode, field=field)
+        if mode_vis != "visible":
+            if mode == "view":
+                return default
+            # The field cannot be rendered in edit mode, but maybe can be
+            # rendered in view mode.
+            mode = "view"
+            view_vis = widget.isVisible(self.context, mode=mode, field=field)
+            if view_vis != "visible":
+                return default
+
+        return mode
 
     def get_widget(self, field):
         """Returns the widget of the field
         """
-        elif IATField.providedBy(field):
+        if IATField.providedBy(field):
             return field.widget
-        if IDXField.providedBy(field):
+        elif IDXField.providedBy(field):
             raise NotImplementedError("DX widgets not yet needed")
         raise TypeError("Field %r is neither a DX nor an AT field")
 
@@ -70,6 +123,3 @@ class SampleHeaderViewlet(ViewletBase):
         """Check permission 'ModifyPortalContent' on the context
         """
         return check_permission(ModifyPortalContent, self.context)
-
-    def render_field(self, field):
-        return "FIELD EDIT/VIEW HTML"
