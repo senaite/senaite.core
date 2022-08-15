43915362d6ed245142181b472fd327bc28ff76a4
Ramon Bartl <rb@ridingbytes.com>
2022-08-15 16:57:31 +0200

    Switched to WF adapter

--- src/senaite/core/adapters/sample.py
+++ src/senaite/core/adapters/sample.py
@@ -2,6 +2,7 @@
 
 from bika.lims import api
 from bika.lims.browser.workflow import RequestContextAware
+from bika.lims.browser.workflow import WorkflowActionGenericAdapter
 from bika.lims.interfaces import IWorkflowActionUIDsAdapter
 from zope.interface import implementer
 
@@ -19,14 +20,14 @@ class WorkflowActionDispatchAdapter(RequestContextAware):
         return self.redirect(redirect_url=url)
 
 
-@implementer(IWorkflowActionUIDsAdapter)
-class WorkflowActionMultiResultsAdapter(RequestContextAware):
-    """Multi results edit sample adapter
+class WorkflowActionMultiResultsAdapter(WorkflowActionGenericAdapter):
+    """Multi results WF transition adapter
     """
 
-    def __call__(self, action, uids):
-        """Redirects the user to the dispatch form
+    def __call__(self, action, objects):
+        """Redirects the user to the multi results form
         """
+        uids = map(api.get_uid, objects)
         url = "{}/multi_results?uids={}".format(
             api.get_url(self.context), ",".join(uids))
         return self.redirect(redirect_url=url)
