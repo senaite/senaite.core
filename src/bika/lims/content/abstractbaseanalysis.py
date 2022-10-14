79d701c928acc57ac37a515e2b4f0266076aa371
Jordi Puiggené <jp@naralabs.com>
2022-10-14 16:16:39 +0200

    Restore getRawMethod original implementation

--- src/bika/lims/content/abstractbaseanalysis.py
+++ src/bika/lims/content/abstractbaseanalysis.py
@@ -305,7 +305,7 @@ Method = UIDReferenceField(
     required=0,
     allowed_types=("Method",),
     vocabulary="_default_method_vocabulary",
-    accessor="getMethodUID",
+    accessor="getRawMethod",
     widget=SelectionWidget(
         format="select",
         label=_("Default Method"),
@@ -927,16 +927,7 @@ class AbstractBaseAnalysis(BaseContent):  # TODO BaseContent?  is really needed?
         """
         return self.getField("Method").get(self)
 
-    @security.public
     def getRawMethod(self):
-        """Returns the UID of the assigned method
-
-        :returns: Method UID
-        """
-        return self.getField("Method").getRaw(self)
-
-    @security.public
-    def getMethodUID(self):
         """Returns the UID of the assigned method
 
         NOTE: This is the default accessor of the `Method` schema field
@@ -945,7 +936,11 @@ class AbstractBaseAnalysis(BaseContent):  # TODO BaseContent?  is really needed?
 
         :returns: Method UID
         """
-        return self.getRawMethod()
+        field = self.getField("Method")
+        method = field.getRaw(self)
+        if not method:
+            return None
+        return method
 
     @security.public
     def getMethodURL(self):
