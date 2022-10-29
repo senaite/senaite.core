6e0f6c00914f53fe261bacd2d3c67d83676a963a
Ramon Bartl <rb@ridingbytes.com>
2022-10-29 16:07:13 +0200

    Pass catalog query/searchterm to widget

--- src/senaite/core/z3cform/widgets/queryselect/widget.py
+++ src/senaite/core/z3cform/widgets/queryselect/widget.py
@@ -1,18 +1,33 @@
 # -*- coding: utf-8 -*-
 
 import json
+import string
 
+import six
+
+from bika.lims import api
+from bika.lims import logger
+from plone.z3cform.fieldsets.interfaces import IDescriptiveGroup
+from Products.CMFPlone.utils import base_hasattr
 from senaite.core.interfaces import ISenaiteFormLayer
 from senaite.core.z3cform.interfaces import IQuerySelectWidget
 from z3c.form.browser import widget
 from z3c.form.interfaces import INPUT_MODE
+from z3c.form.interfaces import IDataConverter
 from z3c.form.interfaces import IFieldWidget
+from z3c.form.interfaces import ISubForm
 from z3c.form.widget import FieldWidget
 from z3c.form.widget import Widget
 from zope.component import adapter
+from zope.component import getUtility
+from zope.component.interfaces import IFactory
 from zope.interface import implementer
 from zope.interface import implementer_only
 from zope.schema.interfaces import IField
+
+DISPLAY_TEMPLATE = "${title}"
+
+_marker = object
 
 
 @implementer_only(IQuerySelectWidget)
@@ -27,12 +42,109 @@ class QuerySelectWidget(widget.HTMLInputWidget, Widget):
         if self.mode == INPUT_MODE:
             self.addClass("form-control form-control-sm")
 
+    def get_form(self):
+        """Return the current form of the widget
+        """
+        form = self.form
+        # form is a fieldset group
+        if IDescriptiveGroup.providedBy(form):
+            form = form.parentForm
+        # form is a subform (e.g. DataGridFieldObjectSubForm)
+        if ISubForm.providedBy(form):
+            form = form.parentForm
+        return form
+
+    def get_context(self):
+        """Get the current context
+
+        NOTE: If we are in the ++add++ form, `self.context` is the container!
+              Therefore, we create one here to have access to the methods.
+        """
+        schema_iface = self.field.interface
+        if schema_iface and schema_iface.providedBy(self.context):
+            return self.context
+
+        # we might be in a subform, so try first to retrieve the object from
+        # the base form itself first
+        form = self.get_form()
+        portal_type = getattr(form, "portal_type", None)
+        context = getattr(form, "context", None)
+        if api.is_object(context):
+            if api.get_portal_type(context) == portal_type:
+                return context
+
+        # Hack alert!
+        # we are in ++add++ form and have no context!
+        # Create a temporary object to be able to access class methods
+        if not portal_type:
+            portal_type = api.get_portal_type(self.context)
+        portal_types = api.get_tool("portal_types")
+        fti = portal_types[portal_type]
+        factory = getUtility(IFactory, fti.factory)
+        context = factory("temporary")
+        # hook into acquisition chain
+        context = context.__of__(self.context)
+        return context
+
+    def attr(self, name, default=None):
+        """Get the named attribute of the widget or the field
+        """
+        value = getattr(self, name, _marker)
+        if value is _marker:
+            return default
+        if isinstance(value, six.string_types):
+            context = self.get_context()
+            if base_hasattr(context, value):
+                attr = getattr(context, value)
+                if callable(attr):
+                    attr = attr()
+                if attr:
+                    value = attr
+        return value
+
+    def get_value(self):
+        """Extract the value from the request or get it from the field
+        """
+        # get the processed value from the `update` method
+        value = self.value
+        # the value might come from the request, e.g. on object creation
+        if isinstance(value, six.string_types):
+            value = IDataConverter(self).toFieldValue(value)
+        # we handle always lists in the templates
+        if value is None:
+            return []
+        if not isinstance(value, (list, tuple)):
+            value = [value]
+        return value
+
+    def get_catalog(self):
+        return self.attr("catalog", "portal_catalog")
+
+    def get_query(self):
+        return self.attr("query", {})
+
+    def get_search_index(self):
+        return self.attr("search_index", "")
+
+    def get_allow_user_value(self):
+        """Allow the user to enter a custom value
+        """
+        return self.attr("allow_user_value", False)
+
+    def get_display_template(self):
+        return self.attr("display_template", DISPLAY_TEMPLATE)
+
     def get_input_widget_attributes(self):
         """Return input widget attributes for the ReactJS component
         """
         attributes = {
             "data-id": self.id,
             "data-name": self.name,
+            "data-api_url": self.get_api_url(),
+            "data-query": self.get_query(),
+            "data-catalog": self.get_catalog(),
+            "data-search_index": self.get_search_index(),
+            "data-allow_user_value": self.get_allow_user_value(),
         }
 
         # convert all attributes to JSON
@@ -40,6 +152,27 @@ class QuerySelectWidget(widget.HTMLInputWidget, Widget):
             attributes[key] = json.dumps(value)
 
         return attributes
+
+    def get_api_url(self):
+        """JSON API URL to use for this widget
+        """
+        portal = api.get_portal()
+        portal_url = api.get_url(portal)
+        api_url = "{}/@@API/senaite/v1".format(portal_url)
+        return api_url
+
+    def render_value(self, uid):
+        """Returns a rendered HTML element for the reference
+        """
+        template = string.Template(self.get_display_template())
+        try:
+            obj_info = self.get_obj_info(uid)
+        except ValueError as e:
+            # Current user might not have privileges to view this object
+            logger.error(e.message)
+            return ""
+
+        return template.safe_substitute(obj_info)
 
 
 @adapter(IField, ISenaiteFormLayer)
--- webpack/app/widgets/queryselect/widget.js
+++ webpack/app/widgets/queryselect/widget.js
@@ -33,9 +33,10 @@ class QuerySelectWidgetController extends React.Component {
       "name",
       "values",
       "api_url",
-      "records",
-      "catalog",
-      "query",
+      "catalog",  // the catalog tool to query
+      "query",  // the base catalog query to use
+      "search_index",  // the search index to use
+      "allow_user_value",  // allow the user to enter custom values
       "columns",
       "display_template",
       "limit",
@@ -59,6 +60,11 @@ class QuerySelectWidgetController extends React.Component {
       api_url: this.state.api_url,
     });
 
+    // Bind callbacks to current context
+    this.search = this.search.bind(this);
+
+    window.qsw = this;
+
     return this;
   }
 
@@ -75,12 +81,112 @@ class QuerySelectWidgetController extends React.Component {
     }
   }
 
+  /*
+   * Create a query object for the API
+   *
+   * This method prepares a query from the current state variables,
+   * which can be used to call the `api.search` method.
+   *
+   * @param {Object} options: Additional options to add to the query
+   * @returns {Object} The query object
+   */
+  make_query(options) {
+    options = options || {};
+
+    let search_index = this.state.search_index || "q";
+    let search_term = this.state.searchterm;
+
+    let query = Object.assign({
+      limit: this.state.limit,
+      complete: 1,
+    }, options, this.state.query);
+
+    // inject the search index
+    query[search_index] = search_term;
+    return query;
+  }
+
+  /*
+   * Execute a search query and set the results to the state
+   *
+   * @param {Object} url_params: Additional search params for the API search URL
+   * @returns {Promise}
+   */
+  fetch_results(url_params) {
+    url_params = url_params || {};
+    // prepare the server request
+    let self = this;
+    let query = this.make_query();
+    this.toggle_loading(true);
+    let promise = this.api.search(this.state.catalog, query, url_params);
+    promise.then(function(data) {
+      console.debug("QuerySelectWidgetController::fetch_results:GOT DATA: ", data);
+      self.set_results_data(data);
+      self.toggle_loading(false);
+    });
+    return promise;
+  }
+
+  /*
+   * Execute a search for the given searchterm
+   *
+   * @param {String} searchterm: The value entered into the search field
+   * @returns {Promise}
+   */
+  search(searchterm) {
+    if (!searchterm && this.state.results.length > 0) {
+      this.state.searchterm = "";
+      return;
+    }
+    console.debug("QuerySelectWidgetController::search:searchterm:", searchterm);
+    // set the searchterm directly to avoid re-rendering
+    this.state.searchterm = searchterm || "";
+    return this.fetch_results();
+  }
+
+  /*
+   * Toggle loading state
+   *
+   * @param {Boolean} toggle: The loading state to set
+   * @returns {Boolean} toggle: The current loading state
+   */
+  toggle_loading(toggle) {
+    if (toggle == null) {
+      toggle = false;
+    }
+    this.setState({
+      loading: toggle
+    });
+    return toggle;
+  }
+
+  /*
+   * Set results data coming from `senaite.jsonapi`
+   *
+   * @param {Object} data: JSON search result object returned from `senaite.jsonapi`
+   */
+  set_results_data(data) {
+    data = data || {};
+    let items = data.items || [];
+
+    this.setState({
+      results: items,
+      count: data.count || 0,
+      page: data.page || 1,
+      pages: data.pages || 1,
+      next_url: data.next || null,
+      prev_url: data.previous || null,
+    });
+  }
+
   render() {
     return (
         <div className="queryselectwidget">
           <SearchField
             className="form-control"
             name="queryselect-search"
+            on_search={this.search}
+            on_focus={this.search}
           />
         </div>
     );
