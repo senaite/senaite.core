6f81e0b55aaf39592e7ba2dd1814032151f18b1e
Jordi Puiggené <jp@naralabs.com>
2022-12-29 15:33:17 +0100

    Added upgrade step

--- src/senaite/core/upgrade/v02_04_000.py
+++ src/senaite/core/upgrade/v02_04_000.py
@@ -27,6 +27,7 @@ from bika.lims.interfaces import IRejected
 from bika.lims.interfaces import IRetracted
 from senaite.core import logger
 from senaite.core.catalog import ANALYSIS_CATALOG
+from senaite.core.catalog import SAMPLE_CATALOG
 from senaite.core.config import PROJECTNAME as product
 from senaite.core.upgrade import upgradestep
 from senaite.core.upgrade.utils import UpgradeUtils
@@ -188,3 +189,40 @@ def fix_traceback_retract_dl(tool):
         obj._p_deactivate()
 
     logger.info("Migrate LDL, UDL and result fields to string [DONE]")
+
+
+def purge_computed_fields_profile(self):
+    """Cleanup of computed fields related with Profiles field and removal of
+    indexes and columns that are no longer required
+    """
+    logger.info("Purge ComputedField from Sample related with Profiles ...")
+    indexes_to_remove = [
+    ]
+    columns_to_remove = [
+        ("getProfilesUID", SAMPLE_CATALOG),
+    ]
+
+    # Purge the catalogs
+    purge_catalogs(indexes_to_remove, columns_to_remove)
+
+    logger.info("Purge ComputedField from Sample related with Profiles [DONE]")
+
+
+def purge_catalogs(indexes_to_remove, columns_to_remove):
+    """Removes the indexes and columns from catalogs
+    """
+    # remove indexes
+    for index_name, catalog_id in indexes_to_remove:
+        cat = api.get_tool(catalog_id)
+        if index_name in cat.indexes():
+            logger.info("Removing '{}' index from '{}'".format(
+                index_name, catalog_id))
+            cat.delIndex(index_name)
+
+    # remove columns
+    for col_name, catalog_id in columns_to_remove:
+        cat = api.get_tool(catalog_id)
+        if col_name in cat.schema():
+            logger.info("Removing '{}' column from '{}'".format(
+                col_name, catalog_id))
+            cat.delColumn(col_name)
