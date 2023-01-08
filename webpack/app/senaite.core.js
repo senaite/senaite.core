807372cda7212f1c19d061dc18d610b118b7462f
Ramon Bartl <rb@ridingbytes.com>
2023-01-08 17:15:54 +0100

    Remove workflow menu update handler
    This one was superseded by the next handler

--- webpack/app/senaite.core.js
+++ webpack/app/senaite.core.js
@@ -48,41 +48,6 @@ document.addEventListener("DOMContentLoaded", () => {
     $("select.selectpicker").selectpicker();
   });
 
-  // Workflow Menu Update for Ajax Transitions
-  // https://github.com/senaite/senaite.app.listing/pull/87
-  document.body.addEventListener("listing:submit", (event) => {
-    let menu = document.getElementById("plone-contentmenu-workflow");
-    // return immediately if no workflow menu is present
-    if (menu === null ) {
-      return false;
-    }
-    // get the base url from the `data-base-url` attribute
-    // -> see IBootstrapView
-    let base_url = document.body.dataset.baseUrl;
-    if (base_url === undefined) {
-      // fallback to the current location URL
-      base_url = location.href.split("#")[0].split("?")[0];
-    }
-    const request = new Request(base_url + "/menu/workflow_menu");
-    fetch(request)
-      .then((response) => {
-        // we might get a 404 when the current page URL ends with a view, e.g.
-        // `WS-ID/manage_results` or `CLIENT-ID/multi_results` etc.
-        if (response.ok) {
-          return response.text();
-        }
-      })
-      .then((html) => {
-        if (!html) {
-          return;
-        }
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(html, "text/html");
-        let el = doc.body.firstChild;
-        menu.replaceWith(el);
-      })
-  });
-
   // Reload the whole view if the status of the view's context has changed
   // due to the transition submission of some items from the listing
   document.body.addEventListener("listing:submit", (event) => {
