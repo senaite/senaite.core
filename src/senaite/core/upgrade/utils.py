0ed043521410f5b23c62b4ce0eb4c1e200058363
Ramon Bartl <rb@ridingbytes.com>
2023-01-30 09:28:38 +0100

    Remove stale catalog brains in upgrade steps

--- src/senaite/core/upgrade/utils.py
+++ src/senaite/core/upgrade/utils.py
@@ -368,3 +368,20 @@ def delete_object(obj):
     uncatalog_object(obj)
     parent = aq_parent(obj)
     parent._delObject(obj.getId(), suppress_events=True)
+
+
+def uncatalog_brain(brain):
+    """Uncatalog a stale catalog entry
+    """
+    if not api.is_brain(brain):
+        return False
+
+    catalog = brain.aq_parent
+    uid = brain.UID
+    path = brain.getPath()
+    logger.warn(80*"*")
+    logger.warn("Removing stale catalog entry for catalog %s: %s -> %s"
+                % (catalog.getId(), uid, path))
+    logger.warn(80*"*")
+    catalog.uncatalog_object(path)
+    return True
