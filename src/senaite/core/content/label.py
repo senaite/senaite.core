2100f7eb4f6c71c53a8b0db4a901bb89f6dadfd2
Ramon Bartl <rb@ridingbytes.com>
2023-03-08 16:19:38 +0100

    Added category getter/setter

--- src/senaite/core/content/label.py
+++ src/senaite/core/content/label.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from bika.lims import api
 from bika.lims import senaiteMessageFactory as _
 from plone.autoform import directives
 from plone.supermodel import model
+from senaite.core.catalog import SETUP_CATALOG
 from senaite.core.content.base import Container
 from senaite.core.interfaces import IHideActionsMenu
 from senaite.core.interfaces import ILabel
@@ -42,3 +44,21 @@ class ILabelSchema(model.Schema):
 class Label(Container):
     """A container for labels
     """
+    _catalogs = [SETUP_CATALOG]
+
+    def getCategory(self):
+        """Return the label category
+        """
+        accessor = self.accessor("category")
+        return accessor(self)
+
+    def setCategory(self, value):
+        """Set the label category
+        """
+        mutator = self.mutator("category")
+        return mutator(self, api.safe_unicode(value))
+
+    def getCategoryTitle(self):
+        """Leverage existing metadata
+        """
+        return self.getCategory()
