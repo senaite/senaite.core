cc8671c9a5a998f336dacdfc381a7fc4502d763e
Ramon Bartl <rb@ridingbytes.com>
2023-03-14 15:42:35 +0100

    Context property lookup by own key

--- src/senaite/core/browser/widgets/queryselect.py
+++ src/senaite/core/browser/widgets/queryselect.py
@@ -22,8 +22,8 @@ import json
 
 import six
 
-from bika.lims import api
 from AccessControl import ClassSecurityInfo
+from bika.lims import api
 from Products.Archetypes.Registry import registerWidget
 from Products.Archetypes.Widget import TypesWidget
 from Products.CMFPlone.utils import base_hasattr
@@ -55,20 +55,34 @@ class QuerySelectWidget(TypesWidget):
         "hide_input_after_select": False,
     })
 
-    def attr(self, context, name, default=None):
-        """Get the named attribute of the widget or from the context
+    def lookup(self, name, field, context, default=None):
+        """Check if the context has an override for the given named property
+
+        The context can either define an attribute or a method with the
+        following naming convention:
+
+            <fieldname>_<propertyname>
+
+        If an attribute or method is found, this value will be returned,
+        otherwise the lookup will return the default value
         """
-        value = getattr(self, name, _marker)
-        if value is _marker:
-            return default
-        if isinstance(value, six.string_types):
-            if base_hasattr(context, value):
-                attr = getattr(context, value)
-                if callable(attr):
-                    attr = attr()
-                if attr:
-                    value = attr
-        return value
+
+        # check if the current context defines an attribute or method for the
+        # given property
+        key = "{}_{}".format(field.getName(), name)
+        if base_hasattr(context, key):
+            attr = getattr(context, key, default)
+            if callable(attr):
+                # call the context method with additional information
+                attr = attr(name=name,
+                            widget=self,
+                            field=field,
+                            context=context,
+                            default=default)
+            return attr
+
+        # return the widget attribute
+        return getattr(self, name, default)
 
     def to_value(self, value):
         """Extract the value from the request or get it from the field
@@ -83,12 +97,12 @@ class QuerySelectWidget(TypesWidget):
             value = [value]
         return value
 
-    def get_api_url(self, context):
+    def get_api_url(self, field, context):
         """JSON API URL to use for this widget
         """
         portal = api.get_portal()
         portal_url = api.get_url(portal)
-        api_url = self.attr(context, "api_url")
+        api_url = self.lookup("api_url", field, context)
         if not api_url:
             api_url = "{}/@@API/senaite/v1".format(portal_url)
         return api_url
@@ -99,23 +113,24 @@ class QuerySelectWidget(TypesWidget):
         attributes = {
             "data-id": field.getName(),
             "data-name": field.getName(),
+            "data-api_url": self.get_api_url(field, context),
+            "data-catalog": self.catalog,
+            "data-query": self.query,
             "data-values": self.to_value(value),
-            "data-value_key": self.attr(context, "value_key"),
-            "data-columns": self.attr(context, "columns"),
-            "data-query": self.attr(context, "query"),
-            "data-multi_valued": self.attr(context, "multi_valued"),
-            "data-api_url": self.get_api_url(context),
-            "data-search_index": self.attr(context, "search_index"),
-            "data-search_wildcard": self.attr(context, "search_wildcard"),
-            "data-limit": self.attr(context, "limit"),
-            "data-catalog": self.attr(context, "catalog"),
-            "data-allow_user_value": self.attr(context, "allow_user_value"),
-            "data-hide_input_after_select": self.attr(
-                context, "hide_input_after_select"),
+            "data-value_key": self.value_key,
+            "data-columns": self.columns,
+            "data-multi_valued": self.multi_valued,
+            "data-search_index": self.search_index,
+            "data-search_wildcard": self.search_wildcard,
+            "data-limit": self.limit,
+            "data-allow_user_value": self.allow_user_value,
+            "data-hide_input_after_select": self.hide_input_after_select,
         }
 
-        # convert all attributes to JSON
         for key, value in attributes.items():
+            # lookup attributes for overrides
+            value = self.lookup(key, field, context, default=value)
+            # convert all attributes to JSON
             attributes[key] = json.dumps(value)
 
         return attributes
