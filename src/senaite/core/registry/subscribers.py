2b5aac8d42a2bf123f0f30a1d50c30f2422765ca
Ramon Bartl <rb@ridingbytes.com>
2023-03-20 14:37:05 +0100

    Initialize labeled types

--- src/senaite/core/registry/subscribers.py
+++ src/senaite/core/registry/subscribers.py
@@ -1,35 +1,54 @@
 # -*- coding: utf-8 -*-
 
 from bika.lims import api
-from Products.CMFPlone.utils import classDoesNotImplement
-from Products.CMFPlone.utils import classImplements
-from senaite.core.interfaces import ICanHaveLabels
-from plone.dexterity.utils import resolveDottedName
-from Products.Archetypes.atapi import listTypes
+from senaite.core.api import label as label_api
+from senaite.core.registry import get_registry_record
+
+labels_initialized = False
 
 
-def get_klass(portal_type):
-    portal_types = api.get_tool("portal_types")
-    fti = portal_types.getTypeInfo(portal_type)
+def update_label_types(object, event):
+    """Update label enabled types
 
-    if fti.product:
-        at_types = listTypes(fti.product)
-        for t in at_types:
-            if not t.get("portal_type") == portal_type:
-                continue
-            return t.get("klass")
-    else:
-        return resolveDottedName(fti.klass)
+    NOTE: The settings are thread local!
 
+    This means that in multi client environments the settings are not reflected
+    in the other instances.
 
-def update_label_registry(object, event):
+    A restart of these instances is required, so that the `init_labels` handler
+    below marks the classes appropriately.
+    """
     # get the types that support labels directly
     enabled_types = object.label_enabled_portal_types
     plone_utils = api.get_tool("plone_utils")
     friendly_types = plone_utils.getUserFriendlyTypes()
     for portal_type in friendly_types:
-        klass = get_klass(portal_type)
         if portal_type not in enabled_types:
-            classDoesNotImplement(klass, ICanHaveLabels)
+            label_api.disable_labels_for_type(portal_type)
         else:
-            classImplements(klass, ICanHaveLabels)
+            label_api.enable_labels_for_type(portal_type)
+
+
+def init_labels(site, event):
+    """Initialize labels for enabled portal types after startup
+
+    This is a multi-subscriber to `zope.component.interfaces.ISite` and
+    `zope.traversing.interfaces.BeforeTraverseEvent`.
+
+    This is required because the Zope Component Architecture (Site Manager) is
+    set for each request and is required here to lookup the registry settings.
+
+    Also see `zope.site.site.threadSiteSubscriber`.
+
+    We use a global variable to ensure it is only run once per instance.
+    """
+    global labels_initialized
+    if labels_initialized:
+        return
+    enabled_types = get_registry_record("label_enabled_portal_types")
+    if not enabled_types:
+        labels_initialized = True
+        return
+    for portal_type in enabled_types:
+        label_api.enable_labels_for_type(portal_type)
+    labels_initialized = True
