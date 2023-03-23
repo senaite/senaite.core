d0d26e2366f92c033bf30e467bd756bce671b394
Ramon Bartl <rb@ridingbytes.com>
2023-03-23 16:23:08 +0100

    Keep behavior assignable adapter

--- src/senaite/core/behaviors/label.py
+++ src/senaite/core/behaviors/label.py
@@ -4,6 +4,10 @@ from AccessControl import ClassSecurityInfo
 from bika.lims import senaiteMessageFactory as _
 from plone.autoform import directives
 from plone.autoform.interfaces import IFormFieldProvider
+from plone.behavior.interfaces import IBehaviorAssignable
+from plone.behavior.registration import BehaviorRegistration
+from plone.dexterity.behavior import DexterityBehaviorAssignable
+from plone.dexterity.schema import SCHEMA_CACHE
 from plone.supermodel import model
 from plone.supermodel.directives import fieldset
 from Products.CMFCore import permissions
@@ -15,6 +19,44 @@ from zope import schema
 from zope.component import adapter
 from zope.interface import implementer
 from zope.interface import provider
+
+
+@implementer(IBehaviorAssignable)
+@adapter(ICanHaveLabels)
+class LabelBehaviorAssignable(DexterityBehaviorAssignable):
+    """Extend label behavior if the context provides the interface `ICanHaveLabels`
+    """
+    def __init__(self, context):
+        self.context = context
+
+    def supports(self, behavior_interface):
+        for behavior in self.enumerateBehaviors():
+            if behavior_interface in behavior.interface._implied:
+                return True
+        return False
+
+    def enumerateBehaviors(self):
+        portal_type = self.context.portal_type
+        behaviors = SCHEMA_CACHE.behavior_registrations(portal_type)
+        registered = False
+        for behavior in behaviors:
+            if behavior.marker == ICanHaveLabels:
+                registered = True
+            yield behavior
+        # additionally yield the schema registration if it was not already
+        # registered via the FTI
+        if not registered:
+            yield self.label_registration
+
+    @property
+    def label_registration(self):
+        return BehaviorRegistration(
+            title="Label schema extender",
+            description="Adds the ability to add/remove labels",
+            interface=ILabelSchema,
+            marker=ICanHaveLabels,
+            factory=LabelSchema,
+        )
 
 
 @provider(IFormFieldProvider)
