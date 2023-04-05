c1306bf7b90525da508fc97aa6cbcf8419b69b38
Ramon Bartl <rb@ridingbytes.com>
2023-04-05 10:45:40 +0200

    Changed method signature

--- src/senaite/core/browser/widgets/referencewidget.py
+++ src/senaite/core/browser/widgets/referencewidget.py
@@ -28,10 +28,11 @@ from bika.lims import logger
 from Products.Archetypes.Registry import registerWidget
 from Products.Archetypes.Widget import StringWidget
 from Products.CMFPlone.utils import base_hasattr
-from senaite.app.supermodel.model import SuperModel
+from senaite.app.supermodel import SuperModel
 
+DEFAULT_SEARCH_CATALOG = "uid_catalog"
 DISPLAY_TEMPLATE = "<a href='${url}' _target='blank'>${title}</a>"
-MAKE_LOWER_CASE = ["Title", "Description"]
+IGNORE_COLUMNS = ["UID"]
 
 
 class ReferenceWidget(StringWidget):
@@ -39,12 +40,27 @@ class ReferenceWidget(StringWidget):
     _properties.update({
         "macro": "senaite_widgets/uidreferencewidget",
 
-        # OLD PROPERTIES BELOW THIS LINE
+        # NEW PROPERTIES
+        "query": {},
+        "limit": 5,
+        "catalog": None,
+        "api_url": None,
+        "disabled": False,
+        "readonly": False,
+        "multi_valued": False,
+        "allow_user_value": False,
+        "search_wildcard": True,
+        "value_key": "uid",
+        "padding": 3,
+        "display_template": None,
+        "hide_input_after_select": False,
+
+        # BBB: OLD PROPERTIES
         "url": "referencewidget_search",
         "catalog_name": "portal_catalog",
         # base_query can be a dict or a callable returning a dict
         "base_query": {},
-        # This will be faster if the columnNames are catalog indexes
+        # columns to display in the search dropdown
         "colModel": [
             {"columnName": "Title", "width": "30", "label": _(
                 "Title"), "align": "left"},
@@ -53,7 +69,6 @@ class ReferenceWidget(StringWidget):
             # UID is required in colModel
             {"columnName": "UID", "hidden": True},
         ],
-        # Default field to put back into input elements
         "ui_item": "Title",
         "search_fields": ("Title",),
         "discard_empty": [],
@@ -74,11 +89,18 @@ class ReferenceWidget(StringWidget):
 
         This method get called from the page template to populate the
         attributes that are used by the ReactJS widget component.
+
+        :param context: The current context of the field
+        :param field: The current field of the widget
+        :param value: The curent field value (list of UIDs)
         """
+        uids = self.get_value(context, field, value)
         attributes = {
             "data-id": field.getName(),
             "data-name": field.getName(),
-            "data-values": self.to_value(value),
+            "data-values": uids,
+            "data-records": dict(zip(uids, map(
+                lambda uid: self.get_obj_info(context, field, uid), uids))),
             "data-value_key": getattr(self, "value_key", "uid"),
             "data-api_url": getattr(self, "url", "referencewidget_search"),
             "data-query": getattr(self, "query", {}),
@@ -94,7 +116,7 @@ class ReferenceWidget(StringWidget):
             "data-disabled": getattr(self, "disabled", False),
             "data-readonly": getattr(self, "readonly", False),
             "data-hide_input_after_select": getattr(
-                self, "hide_user_input_after_select", False),
+                self, "hide_user_input_after_select", True),
         }
 
         for key, value in attributes.items():
@@ -104,21 +126,6 @@ class ReferenceWidget(StringWidget):
             attributes[key] = json.dumps(value)
 
         return attributes
-
-    def get_api_url(self, field, context, default=None):
-        """JSON API URL to use for this widget
-
-        NOTE: we need to call the search view on the correct context to allow
-              context adapter registrations for IReferenceWidgetVocabulary!
-        """
-        # ensure we have an absolute url for the current context
-        url = api.get_url(context)
-        # normalize portal factory urls
-        url = url.split("/portal_factory")[0]
-        # ensure the search path does not contain already the url
-        search_path = self.url.split(url)[-1]
-        # return the absolute search url
-        return "/".join([url, search_path])
 
     def lookup(self, name, context, field, default=None):
         """Check if the context has an override for the given named property
@@ -130,6 +137,12 @@ class ReferenceWidget(StringWidget):
 
         If an attribute or method is found, this value will be returned,
         otherwise the lookup will return the default value
+
+        :param name: The name of a method to lookup
+        :param context: The current context of the field
+        :param field: The current field of the widget
+        :param default: The default property value for the given name
+        :returns: New value for the named property
         """
 
         # check if the current context defines an attribute or method for the
@@ -151,13 +164,52 @@ class ReferenceWidget(StringWidget):
         getter = "get_{}".format(key)
         method = getattr(self, getter, None)
         if callable(method):
-            return method(field, context, default=None)
+            return method(context, field, default=None)
 
         # return the widget attribute
         return getattr(self, name, default)
 
-    def get_display_template(self, field, context, default=None):
+    def get_api_url(self, context, field, default=None):
+        """JSON API URL to use for this widget
+
+        NOTE: we need to call the search view on the correct context to allow
+              context adapter registrations for IReferenceWidgetVocabulary!
+
+        :param context: The current context of the field
+        :param field: The current field of the widget
+        :param default: The default property value
+        :returns: API URL that is contacted when the search changed
+        """
+        # ensure we have an absolute url for the current context
+        url = api.get_url(context)
+        # normalize portal factory urls
+        url = url.split("/portal_factory")[0]
+        # ensure the search path does not contain already the url
+        search_path = self.url.split(url)[-1]
+        # return the absolute search url
+        return "/".join([url, search_path])
+
+    def get_multi_valued(self, context, field, default=None):
+        """Lookup if the field is single or multi valued
+
+        :param context: The current context of the field
+        :param field: The current field of the widget
+        :param default: The default property value
+        :returns: True if the field is multi valued, otherwise False
+        """
+        multi_valued = getattr(field, "multiValued", None)
+        if multi_valued is None:
+            return default
+        return multi_valued
+
+    def get_display_template(self, context, field, default=None):
         """Lookup the display template
+
+        :param context: The current context of the field
+        :param field: The current field of the widget
+        :param default: The default property value
+        :returns: Template that is interpolated by the JS widget with the
+                  mapped values found in records
         """
         # check if the new `display_template` property is set
         prop = getattr(self, "display_template", None)
@@ -166,15 +218,18 @@ class ReferenceWidget(StringWidget):
 
         # BBB: ui_item
         ui_item = getattr(self, "ui_item", None),
-        if ui_item in MAKE_LOWER_CASE:
-            ui_item = ui_item.lower()
         if ui_item is not None:
             return "<a href='${url}' _target='blank'>${%s}</a>" % ui_item
 
         return default
 
-    def get_catalog(self, field, context, default=None):
+    def get_catalog(self, context, field, default=None):
         """Lookup the catalog to query
+
+        :param context: The current context of the field
+        :param field: The current field of the widget
+        :param default: The default property value
+        :returns: Catalog name to query
         """
         # check if the new `catalog` property is set
         prop = getattr(self, "catalog", None)
@@ -182,10 +237,19 @@ class ReferenceWidget(StringWidget):
             return prop
 
         # BBB: catalog_name
-        return getattr(self, "catalog_name", default),
+        catalog_name = getattr(self, "catalog_name", None),
 
-    def get_query(self, field, context, default=None):
+        if catalog_name is None:
+            return DEFAULT_SEARCH_CATALOG
+        return catalog_name
+
+    def get_query(self, context, field, default=None):
         """Lookup the catalog query
+
+        :param context: The current context of the field
+        :param field: The current field of the widget
+        :param default: The default property value
+        :returns: Base catalog query
         """
         prop = getattr(self, "query", None)
         if prop:
@@ -207,8 +271,13 @@ class ReferenceWidget(StringWidget):
 
         return base_query
 
-    def get_columns(self, field, context, default=None):
+    def get_columns(self, context, field, default=None):
         """Lookup the columns to show in the results popup
+
+        :param context: The current context of the field
+        :param field: The current field of the widget
+        :param default: The default property value
+        :returns: List column records to display
         """
         prop = getattr(self, "columns", None)
         if prop is not None:
@@ -223,11 +292,8 @@ class ReferenceWidget(StringWidget):
         columns = []
         for col in col_model:
             name = col.get("columnName")
-            # XXX: Workaround for JSON API schema mapping
-            if name in MAKE_LOWER_CASE:
-                name = name.lower()
-            # skip UID fields
-            if name == "UID":
+            # skip ignored columns
+            if name in IGNORE_COLUMNS:
                 continue
             columns.append({
                 "name": name,
@@ -237,8 +303,13 @@ class ReferenceWidget(StringWidget):
             })
         return columns
 
-    def get_search_index(self, field, context, default=None):
+    def get_search_index(self, context, field, default=None):
         """Lookup the search index for fulltext searches
+
+        :param context: The current context of the field
+        :param field: The current field of the widget
+        :param default: The default property value
+        :returns: ZCText compatible search index
         """
         prop = getattr(self, "search_index", None)
         if prop is not None:
@@ -246,13 +317,20 @@ class ReferenceWidget(StringWidget):
 
         # BBB: search_fields
         search_fields = getattr(self, "search_fields", [])
+        if not isinstance(search_fields, (tuple, list)):
+            search_fields = filter(None, [search_fields])
         if len(search_fields) > 0:
             return search_fields[0]
 
         return default
 
-    def to_value(self, value):
+    def get_value(self, context, field, value=None):
         """Extract the value from the request or get it from the field
+
+        :param context: The current context of the field
+        :param field: The current field of the widget
+        :param value: The current set value
+        :returns: List of UIDs
         """
         # the value might come from the request, e.g. on object creation
         if isinstance(value, six.string_types):
@@ -262,21 +340,37 @@ class ReferenceWidget(StringWidget):
             return []
         if not isinstance(value, (list, tuple)):
             value = [value]
-        return value
+        return map(api.get_uid, value)
 
-    def get_obj_info(self, uid):
+    def get_obj_info(self, context, field, uid):
         """Returns a dictionary with the object info
+
+        NOTE: We just return the required columns
         """
+        info = {"uid": uid}
+
+        # we fetch required attributes from the supermodel
         model = SuperModel(uid)
-        obj_info = model.to_dict()
-        obj_info["uid"] = uid
-        obj_info["url"] = api.get_url(model)
+        if not model.instance:
+            return info
 
-        # extend the brain metadata as well
-        for attr in model.brain.schema():
-            obj_info[attr] = getattr(model.brain, attr, "")
+        # always add the URL to render the link
+        info["url"] = api.get_url(model)
 
-        return obj_info
+        # fetch the columns
+        columns = self.get_columns(context, field, [])
+        names = map(lambda col: col.get("name"), columns)
+        for name in names:
+            if not name:
+                continue
+            attr = getattr(model, name, None)
+            if callable(attr):
+                attr = attr()
+            if not api.is_string(attr):
+                continue
+            info[name] = attr
+
+        return info
 
     def render_reference(self, context, field, uid):
         """Returns a rendered HTML element for the reference
@@ -284,7 +378,7 @@ class ReferenceWidget(StringWidget):
         template = string.Template(
             self.get_display_template(context, field, uid))
         try:
-            obj_info = self.get_obj_info(uid)
+            obj_info = self.get_obj_info(context, field, uid)
         except ValueError as e:
             # Current user might not have privileges to view this object
             logger.error(e.message)
