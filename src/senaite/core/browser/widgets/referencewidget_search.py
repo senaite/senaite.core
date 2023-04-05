16f2d6c0d6b891f048afee58eee02c6998fca24c
Ramon Bartl <rb@ridingbytes.com>
2023-04-05 08:16:39 +0200

    Moved to own module

---
+++ src/senaite/core/browser/widgets/referencewidget_search.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+from bika.lims.browser import BrowserView
+from bika.lims.interfaces import IReferenceWidgetVocabulary
+from plone import protect
+from senaite.jsonapi import request as req
+from senaite.jsonapi.api import make_batch
+from senaite.jsonapi.interfaces import IInfo
+from zope.component import getAdapters
+from zope.interface import implementer
+from zope.publisher.interfaces import IPublishTraverse
+
+
+# Make route provider for senaite.jsonapi
+@implementer(IPublishTraverse)
+class ReferenceWidgetSearch(BrowserView):
+    """Search endpoint for new UID referencewidget
+    """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.traverse_subpath = []
+
+    def publishTraverse(self, request, name):
+        """Called before __call__ for each path name and allows to dispatch
+        subpaths to methods
+        """
+        self.traverse_subpath.append(name)
+        return self
+
+    def search(self):
+        """Returns the list of brains that match with the request criteria
+        """
+        brains = []
+
+        for name, adapter in getAdapters((self.context, self.request),
+                                         IReferenceWidgetVocabulary):
+            brains.extend(adapter())
+        return brains
+
+    def get_info(self, brain):
+        """Extract the data for the result items
+        """
+        info = IInfo(brain).to_dict()
+        return info
+
+    def __call__(self):
+        protect.CheckAuthenticator(self.request)
+
+        # Do the search
+        brains = self.search()
+
+        size = req.get_batch_size()
+        start = req.get_batch_start()
+        batch = make_batch(brains, size, start)
+        data = {
+            "pagesize": batch.get_pagesize(),
+            "next": batch.make_next_url(),
+            "previous": batch.make_prev_url(),
+            "page": batch.get_pagenumber(),
+            "pages": batch.get_numpages(),
+            "count": batch.get_sequence_length(),
+            "items": map(self.get_info, batch.get_batch()),
+        }
+        return json.dumps(data)
