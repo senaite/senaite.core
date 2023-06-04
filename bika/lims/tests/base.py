5f7a5230a615d2e8dee40d1aec0de06aef1b6236
Jordi Puiggené <jp@naralabs.com>
2023-06-04 20:16:23 +0200

    Revisit test's base

--- bika/lims/tests/base.py
+++ bika/lims/tests/base.py
@@ -19,12 +19,16 @@
 # Some rights reserved, see README and LICENSE.
 
 import os
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_PASSWORD
+from plone.app.testing.bbb import PloneTestCase
+from plone.protect.authenticator import AuthenticatorView
+from plone.protect.authenticator import createToken
+from plone.testing.z2 import Browser
 from re import match
 
-from bika.lims.testing import BASE_TESTING, DATA_TESTING
-from plone.app.testing import TEST_USER_NAME, TEST_USER_PASSWORD
-from plone.protect.authenticator import AuthenticatorView
-from plone.testing.z2 import Browser
+from bika.lims.testing import BASE_TESTING
+from bika.lims.testing import DATA_TESTING
 
 try:
     import unittest2 as unittest
@@ -32,7 +36,7 @@ except ImportError:  # Python 2.7
     import unittest
 
 
-class BaseTestCase(unittest.TestCase):
+class BaseTestCase(PloneTestCase):
     """Use for test cases which do not rely on the demo data
     """
     layer = BASE_TESTING
@@ -44,7 +48,8 @@ class BaseTestCase(unittest.TestCase):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
         self.request['ACTUAL_URL'] = self.portal.absolute_url()
-
+        # Disable plone.protect for these tests
+        self.request.form["_authenticator"] = createToken()
         # During testing, CSRF protection causes failures.
         os.environ["PLONE_CSRF_DISABLED"] = "true"
 
@@ -54,16 +59,24 @@ class BaseTestCase(unittest.TestCase):
                    loggedIn=True):
 
         # Instantiate and return a testbrowser for convenience
-        browser = Browser(self.portal)
+        portal_url = self.get_url(self.portal)
+        browser = Browser(self.app, url=portal_url)
         browser.addHeader('Accept-Language', 'en-US')
         browser.handleErrors = False
         if loggedIn:
-            browser.open(self.portal.absolute_url())
+            browser.open(portal_url)
             browser.getControl('Login Name').value = username
             browser.getControl('Password').value = password
             browser.getControl('Log in').click()
             self.assertTrue('You are now logged in' in browser.contents)
         return browser
+
+    def get_url(self, obj):
+        scheme = "http"
+        url = obj.absolute_url()
+        if not url.startswith(scheme):
+            url = "{}://{}".format(scheme, url)
+        return url
 
     def getAuthenticator(self):
         tag = AuthenticatorView('context', 'request').authenticator()
