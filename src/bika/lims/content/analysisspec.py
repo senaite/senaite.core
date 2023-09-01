a61b0c04414b4b243634f7fdae70baf1019a932f
Jordi Puiggené <jp@naralabs.com>
2023-09-01 15:48:28 +0200

    better text, with breaklines

--- src/bika/lims/content/analysisspec.py
+++ src/bika/lims/content/analysisspec.py
@@ -84,15 +84,16 @@ schema = Schema((
                 u"description_analysisspec_resultsrange",
                 default=u"'Min' and 'Max' values indicate a valid results "
                         u"range. Any result outside this results range will "
-                        u"raise an alert. 'Min warn' and 'Max warn' values "
-                        u"indicate a shoulder range. "
-                        u"Any result outside the results range but within the "
-                        u"shoulder range will raise a less severe alert. "
+                        u"raise an alert.<br/>"
+                        u"'Min warn' and 'Max warn' values indicate a "
+                        u"shoulder range. Any result outside the results "
+                        u"range but within the shoulder range will raise a "
+                        u"less severe alert.<br/>"
                         u"If the result is out of range, the value set for "
                         u"'&lt; Min' or '&gt; Max' will be displayed in lists "
                         u"and results reports instead of the real result. In "
                         u"such case, the value set for 'Out of range comment' "
-                        u"will be displayed in results report as well."
+                        u"will be displayed in results report as well"
             )
         ),
     ),
