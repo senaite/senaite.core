7fa58e8f12bc9a7ff8ee552ca0bc47a2b1fbf350
Ramon Bartl <rb@ridingbytes.com>
2023-09-11 12:31:40 +0200

    Changed to selection widget

--- src/bika/lims/content/instrumentscheduledtask.py
+++ src/bika/lims/content/instrumentscheduledtask.py
@@ -29,8 +29,8 @@ from Products.Archetypes import atapi
 from Products.Archetypes.public import BaseFolder
 from Products.Archetypes.public import ComputedField
 from Products.Archetypes.public import ComputedWidget
+from Products.Archetypes.public import SelectionWidget
 from Products.Archetypes.public import DisplayList
-from Products.Archetypes.public import ReferenceWidget
 from Products.Archetypes.public import Schema
 from Products.Archetypes.public import StringField
 from Products.Archetypes.public import StringWidget
@@ -61,7 +61,7 @@ schema = BikaSchema.copy() + Schema((
     StringField(
         "Type",
         vocabulary="getTaskTypes",
-        widget=ReferenceWidget(
+        widget=SelectionWidget(
             checkbox_bound=0,
             label=_("Task type",
                     "Type"),
@@ -105,7 +105,6 @@ class InstrumentScheduledTask(BaseFolder):
     implements(ICancellable)
     security = ClassSecurityInfo()
     schema = schema
-    displayContentsTab = False
     _at_rename_after_creation = True
 
     def _renameAfterCreation(self, check_auto_id=False):
@@ -116,11 +115,11 @@ class InstrumentScheduledTask(BaseFolder):
         """ Return the current list of task types
         """
         types = [
-            ('Calibration', safe_unicode(_('Calibration')).encode('utf-8')),
-            ('Enhancement', safe_unicode(_('Enhancement')).encode('utf-8')),
-            ('Preventive', safe_unicode(_('Preventive')).encode('utf-8')),
-            ('Repair', safe_unicode(_('Repair')).encode('utf-8')),
-            ('Validation', safe_unicode(_('Validation')).encode('utf-8')),
+            ("Calibration", safe_unicode(_("Calibration")).encode("utf-8")),
+            ("Enhancement", safe_unicode(_("Enhancement")).encode("utf-8")),
+            ("Preventive", safe_unicode(_("Preventive")).encode("utf-8")),
+            ("Repair", safe_unicode(_("Repair")).encode("utf-8")),
+            ("Validation", safe_unicode(_("Validation")).encode("utf-8")),
         ]
 
         return DisplayList(types)
