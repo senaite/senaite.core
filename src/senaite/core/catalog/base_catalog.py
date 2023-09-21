b85599756043f0ba4779b69cd760c7ca7a8d8046
Ramon Bartl <rb@ridingbytes.com>
2023-09-21 16:57:27 +0200

    Improve memory usage when rebuilding a catalog (#2387)
    * Deactivate objects after reindexing
    
    * Changelog updated
    
    * Comment changed
    
    ---------
    
    Co-authored-by: Jordi Puiggené <jp@naralabs.com>

--- src/senaite/core/catalog/base_catalog.py
+++ src/senaite/core/catalog/base_catalog.py
@@ -150,7 +150,17 @@ class BaseCatalog(CatalogTool):
                         .format(self.progress_counter, self.id))
 
         if self.progress_counter % 10000 == 0:
+            logger.info("Creating transaction savepoint after {} objects"
+                        .format(self.progress_counter))
             transaction.savepoint(optimistic=True)
+
+    def deactivate_object(self, obj):
+        """Deactivate the object to save memory
+        """
+        try:
+            obj._p_deactivate()
+        except AttributeError:
+            pass
 
     @security.protected(ManageZCatalogEntries)
     def clearFindAndRebuild(self):
@@ -176,6 +186,8 @@ class BaseCatalog(CatalogTool):
                 if self.is_obj_indexable(obj, portal_type, mapped_types):
                     self._reindexObject(obj, idxs=idxs)  # bypass queue
                     self.log_progress()
+                # flush object from memory
+                self.deactivate_object(obj)
             except TypeError:
                 # Catalogs have 'indexObject' as well, but they
                 # take different args, and will fail
