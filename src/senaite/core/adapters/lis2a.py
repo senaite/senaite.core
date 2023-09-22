bba233968c1ff3eb38e5ee049ad7260cbc069661
Ramon Bartl <rb@ridingbytes.com>
2023-09-22 21:35:22 +0200

    Make sample and instrument properties

--- src/senaite/core/adapters/lis2a.py
+++ src/senaite/core/adapters/lis2a.py
@@ -93,8 +93,10 @@ class ASTMImporter(object):
         self.data = data
         self.message = message
         self.request = request
-        self.instrument = None
-        self.importlog = None
+        # internals for properties
+        self._importlog = None
+        self._instrument = None
+        self._sample = None
 
     def import_data(self):
         """Import the instrument data
@@ -102,17 +104,41 @@ class ASTMImporter(object):
         raise NotImplementedError("ASTM import logic must be implemented "
                                   "by a specific instrument importer")
 
-    def get_sample_by_id(self, sid, default=None):
-        """Query a sample by ID or Client Sample ID
-
-        NOTE: This method queries for any review_state!
-              -> Validation needs to be implementd by the importer method
-
-        :param sid: Sample ID or Client Sample ID
-        :param default: Default value if no Sample was found
-        :returns: Sample object
+    @property
+    def instrument(self):
+        """Query the instrument of this import
         """
+        if self._instrument:
+            return self._instrument
 
+        instrument = None
+        # get the instrument name, serial and version from the data
+        name, serial, version = self.get_sender()
+        query = {"portal_type": "Instrument"}
+        results = api.search(query, SETUP_CATALOG)
+
+        for brain in results:
+            obj = api.get_object(brain)
+            model = obj.getModel()
+            serialno = obj.getSerialNo()
+            if model == name and serial is None:
+                instrument = obj
+            elif model == name and serialno == serial:
+                instrument = obj
+
+        self._instrument = instrument
+        return instrument
+
+    @property
+    def sample(self):
+        """Query the sample of this import
+        """
+        if self._sample:
+            return self._sample
+
+        sample = None
+        # get the sample ID from the data
+        sid = self.get_sample_id()
         # Query by Sample ID
         query_sid = {"getId": sid}
         # Query by Client Sample ID
@@ -121,49 +147,33 @@ class ASTMImporter(object):
         results = api.search(query_sid, SAMPLE_CATALOG)
         if not results:
             results = api.search(query_csid, SAMPLE_CATALOG)
-        if len(results) != 1:
-            return default
-        return api.get_object(results[0])
+        if len(results) == 1:
+            sample = api.get_object(results[0])
 
-    def get_instrument(self, name, serial=None, version=None):
-        """Query instrument by model and serial
-
-        NOTE: This method queries for any review_state!
-              -> Validation needs to be implementd by the importer method
-        """
-        query = {"portal_type": "Instrument"}
-        results = api.search(query, SETUP_CATALOG)
-        for brain in results:
-            obj = api.get_object(brain)
-            model = obj.getModel()
-            serialno = obj.getSerialNo()
-            if model == name and serial is None:
-                return obj
-            elif model == name and serialno == serial:
-                return obj
-        return None
+        self._sample = sample
+        return sample
 
     def log(self, message, level="info"):
         """Append log to logs
         """
         timestamp = datetime.now()
         message = "{} {}: {}".format(timestamp, level.upper(), message)
-        if self.instrument and self.importlog is None:
-            self.importlog = api.create(self.instrument, "AutoImportLog")
-            self.importlog.setInstrument(self.instrument)
-            self.importlog.setInterface(", ".join(self.get_sender()))
-            self.importlog.setImportFile("ASTM")
-            self.importlog.setLogTime(timestamp.isoformat())
-        if self.importlog:
-            results = self.importlog.getResults()
+        if self.instrument and self._importlog is None:
+            self._importlog = api.create(self.instrument, "AutoImportLog")
+            self._importlog.setInstrument(self.instrument)
+            self._importlog.setInterface(", ".join(self.get_sender()))
+            self._importlog.setImportFile("ASTM")
+            self._importlog.setLogTime(timestamp.isoformat())
+        if self._importlog:
+            results = self._importlog.getResults()
             if results:
                 results += "\n"
             messages = "{}{}".format(results, message)
-            self.importlog.setResults(messages)
+            self._importlog.setResults(messages)
         return message
 
     def get_sender(self):
-        """Return the sender information
+        """Return the instrument name, serial and version
         """
         header = self.get_header()
         if not header:
@@ -173,6 +183,17 @@ class ASTMImporter(object):
         serial = sender.get("serial", "")
         version = sender.get("version", "")
         return name, serial, version
+
+    def get_sample_id(self, default=None):
+        """Return the sender information
+        """
+        order = self.get_order()
+        if not order:
+            return default
+        sid = order.get("sample_id")
+        if not sid:
+            return default
+        return sid
 
     def create_attachment(self, container, contents, filename=None):
         """Create a new attachment with the given contents
