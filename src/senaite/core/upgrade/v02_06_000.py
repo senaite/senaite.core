55ad2120d2b401b7596619f9dd8d8d343f443cc5
Jordi Puiggené <jp@naralabs.com>
2024-01-30 14:07:32 +0100

    Added upgrade step

--- src/senaite/core/upgrade/v02_06_000.py
+++ src/senaite/core/upgrade/v02_06_000.py
@@ -18,10 +18,13 @@
 # Copyright 2018-2024 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
+from bika.lims import api
 from senaite.core import logger
+from senaite.core.catalog import ANALYSIS_CATALOG
 from senaite.core.config import PROJECTNAME as product
 from senaite.core.upgrade import upgradestep
 from senaite.core.upgrade.utils import UpgradeUtils
+from senaite.core.workflow import ANALYSIS_WORKFLOW
 
 version = "2.6.0"  # Remember version number in metadata.xml and setup.py
 profile = "profile-{0}:default".format(product)
@@ -44,3 +47,41 @@ def upgrade(tool):
 
     logger.info("{0} upgraded to version {1}".format(product, version))
     return True
+
+
+def fix_analysis_reject_permission(tool):
+    """Fixes the analysis reject permission, that was not defined at top-level
+    """
+    portal = api.get_portal()
+    setup = portal.portal_setup
+
+    # Reimport rolemap.xml
+    setup.runImportStepFromProfile(profile, "rolemap")
+
+    # Update role mappings of analyses, but only for those analyses that are
+    # in a state from which the new permission can apply
+    statuses = ["unassigned", "assigned", "to_be_verified"]
+    logger.info("Updating role mappings: Analysis ({}) ..."
+                .format(", ".join(statuses)))
+    query = {"portal_type": "Analysis", "review_state": statuses}
+    brains = api.search(query, ANALYSIS_CATALOG)
+    update_workflow_role_mappings(ANALYSIS_WORKFLOW, brains)
+    logger.info("Updating role mappings: Analysis ({}) [DONE]"
+                .format(", ".join(statuses)))
+
+
+def update_workflow_role_mappings(wf_id, objs_or_brains):
+    """Update the workflow role mappings for the given objects or brains
+    """
+    wf_tool = api.get_tool("portal_workflow")
+    workflow = wf_tool.getWorkflowById(wf_id)
+    total = len(objs_or_brains)
+
+    for num, obj_brain in enumerate(objs_or_brains):
+        if num and num % 100 == 0:
+            logger.info("Updating role mappings {0}/{1}".format(num, total))
+
+        obj = api.get_object(obj_brain)
+        workflow.updateRoleMappingsFor(obj)
+        obj.reindexObject()
+        obj._p_deactivate()
