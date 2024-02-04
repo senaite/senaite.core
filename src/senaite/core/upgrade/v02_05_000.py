94c8c7b957d6ae9fd1453fd21d4ac695552ae0fd
Ramon Bartl <rb@ridingbytes.com>
2024-02-04 19:24:54 +0100

    Avoid traceback in upgrade step 2525
    Traceback (innermost last):
      Module ZPublisher.WSGIPublisher, line 176, in transaction_pubevents
      Module ZPublisher.WSGIPublisher, line 385, in publish_module
      Module ZPublisher.WSGIPublisher, line 288, in publish
      Module ZPublisher.mapply, line 85, in mapply
      Module ZPublisher.WSGIPublisher, line 63, in call_object
      Module Products.GenericSetup.tool, line 1135, in manage_doUpgrades
      Module Products.GenericSetup.upgrade, line 185, in doStep
      Module senaite.core.upgrade.v02_05_000, line 666, in setup_client_landing_page
      Module senaite.core.upgrade, line 39, in wrap_func_args
      Module senaite.core.upgrade.v02_05_000, line 275, in import_registry
      Module Products.GenericSetup.tool, line 375, in runImportStepFromProfile
      Module Products.GenericSetup.tool, line 1323, in _doRunImportStep
       - __traceback_info__: typeinfo
      Module Products.CMFCore.exportimport.typeinfo, line 222, in importTypesTool
      Module Products.GenericSetup.utils, line 933, in importObjects
       - __traceback_info__: portal_types
      Module Products.GenericSetup.utils, line 929, in importObjects
       - __traceback_info__: types/Department
      Module Products.GenericSetup.utils, line 530, in _importBody
      Module Products.CMFCore.exportimport.typeinfo, line 61, in _importNode
      Module Products.GenericSetup.utils, line 762, in _initProperties
    ValueError: undefined property 'add_permission'

--- src/senaite/core/upgrade/v02_05_000.py
+++ src/senaite/core/upgrade/v02_05_000.py
@@ -472,7 +472,6 @@ def remove_legacy_reports(tool):
     logger.info("Removing legacy reports [DONE]")
 
 
-@upgradestep(product, version)
 def import_typeinfo(tool):
     """Import type info profile
     """
