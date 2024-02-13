4913d6b0c19a494f4bb8fe4e6baebf5257a81e28
Ramon Bartl <rb@ridingbytes.com>
2024-02-13 15:48:26 +0100

    Added controlpanel listing

---
+++ src/senaite/core/browser/controlpanel/analysisprofiles/view.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.CORE.
+#
+# SENAITE.CORE is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2018-2024 by it's authors.
+# Some rights reserved, see README and LICENSE.
+
+import collections
+
+from bika.lims import api
+from bika.lims import bikaMessageFactory as _
+from bika.lims.utils import get_link
+from senaite.app.listing.view import ListingView
+from senaite.core.catalog import SETUP_CATALOG
+from senaite.core.permissions import AddAnalysisProfile
+
+
+class AnalysisProfilesView(ListingView):
+    """Controlpanel Listing for Analysis Profiles
+    """
+
+    def __init__(self, context, request):
+        super(AnalysisProfilesView, self).__init__(context, request)
+
+        self.catalog = SETUP_CATALOG
+        self.show_select_row = False
+        self.show_select_column = True
+        self.pagesize = 25
+
+        self.contentFilter = {
+            "portal_type": "AnalysisProfile",
+            "sort_on": "sortable_title",
+            "sort_order": "ascending",
+        }
+
+        self.context_actions = {
+            _("Add"): {
+                "url": "createObject?type_name=AnalysisProfile",
+                "permission": AddAnalysisProfile,
+                "icon": "++resource++bika.lims.images/add.png"}
+        }
+
+        self.title = self.context.translate(_("Analysis Profiles"))
+        self.icon = "{}/{}".format(
+            self.portal_url,
+            "/++resource++bika.lims.images/analysisprofile_big.png"
+        )
+
+        self.columns = collections.OrderedDict((
+            ("Title", {
+                "title": _("Profile"),
+                "index": "sortable_title"}),
+            ("Description", {
+                "title": _("Description"),
+                "index": "Description",
+                "toggle": True,
+            }),
+            ("ProfileKey", {
+                "title": _("Profile Key"),
+                "sortable": False,
+                "toggle": True,
+            }),
+        ))
+
+        self.review_states = [
+            {
+                "id": "default",
+                "title": _("Active"),
+                "contentFilter": {"is_active": True},
+                "transitions": [{"id": "deactivate"}, ],
+                "columns": self.columns.keys(),
+            }, {
+                "id": "inactive",
+                "title": _("Inactive"),
+                "contentFilter": {"is_active": False},
+                "transitions": [{"id": "activate"}, ],
+                "columns": self.columns.keys(),
+            }, {
+                "id": "all",
+                "title": _("All"),
+                "contentFilter": {},
+                "columns": self.columns.keys(),
+            },
+        ]
+
+    def folderitem(self, obj, item, index):
+        """Service triggered each time an item is iterated in folderitems.
+        The use of this service prevents the extra-loops in child objects.
+        :obj: the instance of the class to be foldered
+        :item: dict containing the properties of the object to be used by
+            the template
+        :index: current index of the item
+        """
+        obj = api.get_object(obj)
+        title = obj.Title()
+        description = obj.Description()
+        url = obj.absolute_url()
+
+        item["replace"]["Title"] = get_link(url, value=title)
+        item["Description"] = description
+        item["ProfileKey"] = obj.getProfileKey()
+
+        return item
