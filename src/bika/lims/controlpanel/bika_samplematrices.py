be285be124808d4f3fad3c10abe2a7f02ff0e8ae
Jordi Puiggené <jp@naralabs.com>
2024-02-13 12:45:10 +0100

    Added control panel listing view

--- src/bika/lims/controlpanel/bika_samplematrices.py
+++ src/bika/lims/controlpanel/bika_samplematrices.py
@@ -18,87 +18,14 @@
 # Copyright 2018-2024 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
-import collections
-
-from bika.lims import bikaMessageFactory as _
-from bika.lims.browser.bika_listing import BikaListingView
 from bika.lims.config import PROJECTNAME
 from bika.lims.interfaces import ISampleMatrices
-from senaite.core.permissions import AddSampleMatrix
-from bika.lims.utils import get_link
 from plone.app.folder.folder import ATFolder
 from plone.app.folder.folder import ATFolderSchema
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content import schemata
 from senaite.core.interfaces import IHideActionsMenu
 from zope.interface.declarations import implements
-
-
-class SampleMatricesView(BikaListingView):
-
-    def __init__(self, context, request):
-        super(SampleMatricesView, self).__init__(context, request)
-
-        self.catalog = "senaite_catalog_setup"
-
-        self.contentFilter = {
-            "portal_type": "SampleMatrix",
-            "sort_on": "sortable_title",
-        }
-
-        self.context_actions = {_('Add'): {
-            'url': 'createObject?type_name=SampleMatrix',
-            'permission': AddSampleMatrix,
-            'icon': '++resource++bika.lims.images/add.png'
-        }}
-
-        self.title = self.context.translate(_("Sample Matrices"))
-        self.description = ""
-        self.icon = "{}/{}".format(
-            self.portal_url,
-            "/++resource++bika.lims.images/samplematrix_big.png"
-        )
-
-        self.show_select_row = False
-        self.show_select_column = True
-        self.pagesize = 25
-
-        self.columns = collections.OrderedDict((
-            ("Title", {
-                "title": _("Sample Matrix"),
-                "index": "sortable_title"}),
-            ("Description", {
-                "title": _("Description"),
-                "index": "Description",
-                "toggle": True,
-            }),
-        ))
-
-        self.review_states = [
-            {
-                "id": "default",
-                "title": _("Active"),
-                "contentFilter": {"is_active": True},
-                "transitions": [{"id": "deactivate"}, ],
-                "columns": self.columns.keys(),
-            }, {
-                "id": "inactive",
-                "title": _("Inactive"),
-                "contentFilter": {'is_active': False},
-                "transitions": [{"id": "activate"}, ],
-                "columns": self.columns.keys(),
-            }, {
-                "id": "all",
-                "title": _("All"),
-                "contentFilter": {},
-                "columns": self.columns.keys(),
-            },
-        ]
-
-    def folderitem(self, obj, item, index):
-        item["replace"]["Title"] = get_link(
-            item["url"], item["Title"])
-        return item
 
 
 schema = ATFolderSchema.copy()
--- src/senaite/core/browser/controlpanel/samplematrices/view.py
+++ src/senaite/core/browser/controlpanel/samplematrices/view.py
@@ -26,47 +26,47 @@ from bika.lims.utils import get_link_for
 from senaite.app.listing import ListingView
 from senaite.core.catalog import SETUP_CATALOG
 from senaite.core.i18n import translate
-from senaite.core.permissions import AddSampleCondition
+from senaite.core.permissions import AddSampleMatrix
 
 
-class SampleConditionsView(ListingView):
+class SampleMatricesView(ListingView):
 
     def __init__(self, context, request):
-        super(SampleConditionsView, self).__init__(context, request)
+        super(SampleMatricesView, self).__init__(context, request)
 
         self.catalog = SETUP_CATALOG
         self.show_select_column = True
 
         self.contentFilter = {
-            "portal_type": "SampleCondition",
+            "portal_type": "SampleMatrix",
             "sort_on": "sortable_title",
             "sort_order": "ascending",
         }
 
         self.context_actions = {
-            _(u"listing_sampleconditions_action_add", default=u"Add"): {
-                "url": "createObject?type_name=SampleCondition",
-                "permission": AddSampleCondition,
+            _(u"listing_samplematrices_action_add", default=u"Add"): {
+                "url": "createObject?type_name=SampleMatrix",
+                "permission": AddSampleMatrix,
                 "icon": "++resource++bika.lims.images/add.png"
             }
         }
 
         self.title = translate(_(
-            u"listing_sampleconditions_title",
-            default=u"Sample Conditions")
+            u"listing_samplematrices_title",
+            default=u"Sample Matrices")
         )
-        self.icon = api.get_icon("SampleConditions", html_tag=False)
+        self.icon = api.get_icon("SampleMatrices", html_tag=False)
 
         self.columns = collections.OrderedDict((
             ("Title", {
                 "title": _(
-                    u"listing_sampleconditions_column_title",
-                    default=u"Sample Condition"
+                    u"listing_samplematrices_column_title",
+                    default=u"Sample Matrix"
                 ),
                 "index": "sortable_title"}),
             ("Description", {
                 "title": _(
-                    u"listing_sampleconditions_column_description",
+                    u"listing_samplematrices_column_description",
                     default=u"Description"
                 ),
                 "index": "Description",
@@ -77,7 +77,7 @@ class SampleConditionsView(ListingView):
             {
                 "id": "default",
                 "title": _(
-                    u"listing_sampleconditions_state_active",
+                    u"listing_samplematrices_state_active",
                     default=u"Active"
                 ),
                 "contentFilter": {"is_active": True},
@@ -86,16 +86,16 @@ class SampleConditionsView(ListingView):
             }, {
                 "id": "inactive",
                 "title": _(
-                    u"listing_sampleconditions_state_inactive",
+                    u"listing_samplematrices_state_inactive",
                     default=u"Inactive"
                 ),
-                "contentFilter": {'is_active': False},
+                "contentFilter": {"is_active": False},
                 "transitions": [{"id": "activate"}, ],
                 "columns": self.columns.keys(),
             }, {
                 "id": "all",
                 "title": _(
-                    u"listing_sampleconditions_state_all",
+                    u"listing_samplematrices_state_all",
                     default=u"All"
                 ),
                 "contentFilter": {},
@@ -104,12 +104,5 @@ class SampleConditionsView(ListingView):
         ]
 
     def folderitem(self, obj, item, index):
-        """Service triggered each time an item is iterated in folderitems.
-        The use of this service prevents the extra-loops in child objects.
-        :obj: the instance of the class to be foldered
-        :item: dict containing the properties of the object to be used by
-            the template
-        :index: current index of the item
-        """
         item["replace"]["Title"] = get_link_for(obj)
         return item
