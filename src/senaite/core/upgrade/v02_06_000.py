de28865cc3b791b1412151c613d3a4d1acc31354
Jordi Puiggené <jp@naralabs.com>
2024-02-25 18:19:00 +0100

    Wake-up the object before being un-catalogued

--- src/senaite/core/upgrade/v02_06_000.py
+++ src/senaite/core/upgrade/v02_06_000.py
@@ -533,6 +533,7 @@ def cleanup_uid_catalog(tool):
     for brain in duplicates:
         oid = api.get_id(brain)
         path = api.get_path(brain)
+        obj = api.get_object(brain)
         # uncatalog the object for the current path
         logger.info("Uncatalog brain '%s' at '%s'" % (oid, path))
         catalog.uncatalog_object(path)
@@ -541,13 +542,11 @@ def cleanup_uid_catalog(tool):
         fti = type_info.get(brain.portal_type)
         if fti.product:
             # catalog the object on the relative path
-            obj = api.get_object(brain)
             rel_url = getRelURL(catalog, obj.getPhysicalPath())
             logger.info("Catalog brain '%s' at '%s'" % (oid, rel_url))
             catalog.catalog_object(obj, rel_url)
         else:
             # catalog the object on the absolute path
-            obj = api.get_object(brain)
             abs_url = "/".join(obj.getPhysicalPath())
             logger.info("Catalog brain '%s' at '%s'" % (oid, abs_url))
             catalog.catalog_object(obj, abs_url)
