3ceda0b71c068033f996584582f9f98ab32882ad
Ramon Bartl <rb@ridingbytes.com>
2024-03-28 15:40:07 +0100

    Fall back to ID

--- src/senaite/core/config/widgets.py
+++ src/senaite/core/config/widgets.py
@@ -26,7 +26,7 @@ def get_default_columns():
     """
     return [
         {
-            "name": "title",
+            "name": "Title",
             "width": "50",
             "align": "left",
             "label": _(u"Name"),
