d43d78463a1e0b4cc2de62aca6f9b7f96a8062e2
Ramon Bartl <rb@ridingbytes.com>
2024-03-28 13:59:50 +0100

    Refactored Widget Renderer

--- webpack/app/senaite.core.widgets.js
+++ webpack/app/senaite.core.widgets.js
@@ -1,99 +1,70 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import tinymce from "tinymce";
+import {
+  render_address_widget,
+  render_phone_widget,
+  render_queryselect_widget,
+  render_tinymce_widget,
+  render_uidreference_widget,
+} from "./widgets/renderer.js"
 
-import QuerySelectWidgetController from "./widgets/queryselect/widget.js"
-import AddressWidgetController from "./widgets/addresswidget/widget.js"
-import intlTelInput from "intl-tel-input";
-import "intl-tel-input/build/css/intlTelInput.css";
 
+// Widget Renderers
+const WIDGETS = [
+  // Query Select Widget
+  {
+    selector: ".senaite-queryselect-widget-input",
+    renderer: (el) => render_queryselect_widget(el),
+  },
+  // UID Reference Widget
+  {
+    selector: ".senaite-uidreference-widget-input",
+    renderer: (el) => render_uidreference_widget(el),
+  },
+  // Address Widget
+  {
+    selector: ".senaite-address-widget-input",
+    renderer: (el) => render_address_widget(el),
+  },
+  // Phone Widget
+  {
+    selector: ".senaite-phone-widget-input",
+    renderer: (el) => render_phone_widget(el),
+  },
+  // TinyMCE Widget
+  {
+    selector: "textarea.mce_editable,div.ArchetypesRichWidget textarea,textarea[name='form.widgets.IRichTextBehavior.text'],textarea.richTextWidget",
+    renderer: (el) => render_tinymce_widget(el),
+  }
+]
+
+/** Initialize all widgets below a certain root element
+  * */
+const render_all_widgets = (root_element) => {
+  WIDGETS.forEach((cfg) => {
+    let root = root_element instanceof(Node) ? root_element : document;
+    let elements = root.querySelectorAll(cfg.selector);
+    let renderer = cfg.renderer;
+    elements.forEach((element) => {
+      if (renderer) {
+        renderer(element);
+      } else {
+        throw new Error("Widget renderer required");
+      }
+    });
+  });
+}
+
+// Initialize all widgets when the document is ready
 document.addEventListener("DOMContentLoaded", () => {
   console.info("*** SENAITE CORE WIDGETS JS LOADED ***");
 
-  // TinyMCE
-  let wysiwig_elements = [
-    "textarea.mce_editable",
-    "div.ArchetypesRichWidget textarea",
-    "textarea[name='form.widgets.IRichTextBehavior.text']",
-    "textarea.richTextWidget",
-  ]
-  tinymce.init({
-    height: 300,
-    paste_data_images: true,
-    selector: wysiwig_elements.join(","),
-    plugins: ["paste", "link", "fullscreen", "table", "code"],
-    // NOTE: CSS file must match configuration of entry point in webpack.config.js
-    content_css : "/++plone++senaite.core.static/bundles/senaite.core.css",
-  })
+  // Render all (visble) widgets on the current page
+  render_all_widgets();
 
-  // UIDReferenceWidget
-  let ref_widgets = document.getElementsByClassName("senaite-uidreference-widget-input");
-  for (let widget of ref_widgets) {
-    ReactDOM.render(<QuerySelectWidgetController root_class="uidreferencefield" root_el={widget} />, widget);
-  }
-
-  // QuerySelectWidget
-  let queryselect_widgets = document.getElementsByClassName("senaite-queryselect-widget-input");
-  for (let widget of queryselect_widgets) {
-    ReactDOM.render(<QuerySelectWidgetController root_class="queryselectfield" root_el={widget} />, widget);
-  }
-
-  // AddressWidget
-  let address_widgets = document.getElementsByClassName("senaite-address-widget-input");
-  for (let widget of address_widgets) {
-    ReactDOM.render(<AddressWidgetController root_el={widget} />, widget);
-  }
-
-  // PhoneWidget
-  // https://github.com/jackocnr/intl-tel-input#readme
-  let phone_widgets = document.getElementsByClassName("senaite-phone-widget-input");
-  let error_codes = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
-  let init_phone_input = (el) => {
-    let id = el.dataset.intlTelInputId;
-    let initial_country = el.dataset.initial_country;
-    let preferred_countries = JSON.parse(el.dataset.preferred_countries);
-    let iti = intlTelInput(el, {
-      initialCountry: initial_country,
-      preferredCountries: preferred_countries,
-      // avoid that the dropdown is cropped in records widget
-      dropdownContainer: document.body,
-      // https://github.com/jackocnr/intl-tel-input#utilities-script
-      utilsScript: "++plone++senaite.core.static/modules/intl-tel-input/js/utils.js"
-    });
-    // add event handler only once
-    if (id === undefined) {
-      el.addEventListener("blur", () => {
-        // validation
-        let valid = iti.isValidNumber();
-        let number = iti.getNumber();
-        let field = el.closest(".field");
-        if (valid) {
-          field.classList.remove("error");
-          field.title = "";
-        } else {
-          field.classList.add("error");
-          let error_code = iti.getValidationError();
-          let error_msg = error_codes[error_code];
-          field.title = error_msg;
-        }
-        // always set the number (even if validation failed!)
-        let name = el.dataset.name;
-        let hidden = document.querySelector("input[name='" + name + "']");
-        hidden.value = number;
-      });
-    }
-  }
-  // initialize all phone widgets
-  for (let widget of phone_widgets) {
-    init_phone_input(widget);
-  }
-  // dynamically initialize new phone widgets when used in datagrid fields
+  // DataGrid event handler when a new row was added
   document.body.addEventListener("datagrid:row_added", (event) => {
-    let row = event.detail.row;
-    let input = row.querySelector(".senaite-phone-widget-input");
-    if(input) {
-      init_phone_input(input);
-    }
+    // We need to initialize all widgets within the new row, otherwise they
+    // won't be functional
+    render_all_widgets(event.detail.row);
   });
 
 });
