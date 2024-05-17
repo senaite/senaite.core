048c2eeaf89f3e8fb0569d331862d65539664031
Anton Zhuchkov <antonzh85@gmail.com>
2024-05-17 21:17:00 +0700

    fix typo into upgrade

--- src/senaite/core/setuphandlers.py
+++ src/senaite/core/setuphandlers.py
@@ -254,7 +254,7 @@ def add_senaite_setup_items(portal):
         ("sampletemplates", "Sample Templates", "SampleTemplates"),
         ("manufacturers", "Manufacturers", "Manufacturers"),
         ("containertypes", "Container Types", "ContainerTypes"),
-        ("analysiscategories", "Analysis Categories", "Analysis Categories"),
+        ("analysiscategories", "Analysis Categories", "AnalysisCategories"),
     ]
     setup = api.get_senaite_setup()
     add_dexterity_items(setup, items)
