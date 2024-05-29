720eedc09638d06d547740f46500fa300a925007
Jordi Puiggené <jp@naralabs.com>
2024-05-29 14:18:59 +0200

    Added GPSCoordinates widget and field

---
+++ src/senaite/core/schema/gpscoordinatesfield.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.CORE.
+#
+# SENAITE.CORE is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2018-2024 by it's authors.
+# Some rights reserved, see README and LICENSE.
+
+import copy
+
+from bika.lims import api
+from bika.lims import senaiteMessageFactory as _
+from senaite.core.i18n import translate as t
+from senaite.core.schema.fields import BaseField
+from senaite.core.schema.interfaces import IGPSCoordinatesField
+from zope.interface import implementer
+from zope.schema import Dict
+
+SUBFIELDS = ("latitude", "longitude")
+
+# One meter resolution ~ 5 decimal places
+# ~11 centimeter resolution ~ 6 decimal places
+# One centimeter resolution ~ 7 decimal places
+# One millimeter resolution ~ 8 decimal places
+# https://xkcd.com/2170/
+DEFAULT_PRECISION = 7
+
+LATITUDE_RANGE = (-90, 90)
+LONGITUDE_RANGE = (-180, 180)
+
+
+@implementer(IGPSCoordinatesField)
+class GPSCoordinatesField(Dict, BaseField):
+    """A field that handles GPS coordinates
+    """
+
+    def __init__(self, **kwargs):
+        default = kwargs.get("default")
+        kwargs["default"] = default or dict.fromkeys(SUBFIELDS, "")
+        super(GPSCoordinatesField, self).__init__(**kwargs)
+        self.precision = kwargs.get("precision") or DEFAULT_PRECISION
+
+    def set(self, object, value):
+        """Set the GPS coordinates field value
+        """
+        # ensure the value is a valid dict
+        coordinates = copy.deepcopy(self.default)
+        if value:
+            coordinates.update(value)
+
+        # convert values to strings with the right precision
+        template = "{:.%df}" % self.precision
+        for key in coordinates.keys():
+            val = api.to_float(coordinates[key], default=0)
+            coordinates[key] = template.format(val)
+
+        # store the value
+        super(GPSCoordinatesField, self).set(object, coordinates)
+
+    def _validate(self, value):
+        super(GPSCoordinatesField, self)._validate(value)
+
+        # check latitude degrees are within range
+        lat = value.get("latitude", 0)
+        lat = api.to_float(lat, default=0)
+        if abs(lat) > 90:
+            msg = t(_("Latitude must be within -90 and 90 degrees"))
+            raise ValueError(msg)
+
+        # check longitude degrees are within range
+        lon = value.get("longitude", 0)
+        lon = api.to_float(lon, default=0)
+        if abs(lon) > 180:
+            msg = t(_("Longitude must be within -180 and 180 degrees"))
+            raise ValueError(msg)
