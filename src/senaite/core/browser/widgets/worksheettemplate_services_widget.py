ffd6cc275476357d5ad2c52c5f8cc529d1bafdc6
Anton Zhuchkov <antonzh85@gmail.com>
2024-07-30 11:51:16 +0700

    listing services for ws template

--- src/senaite/core/browser/widgets/worksheettemplate_services_widget.py
+++ src/senaite/core/browser/widgets/worksheettemplate_services_widget.py
@@ -18,55 +18,72 @@
 # Copyright 2018-2024 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
-# import collections
-#
-# from bika.lims import bikaMessageFactory as _
-# from plone.memoize import view
+import collections
 
-from .services_widget import ServicesWidget
+from bika.lims import api
+from bika.lims import senaiteMessageFactory as _
+from bika.lims.utils import get_link_for
+from senaite.core.browser.widgets.services_widget import ServicesWidget
 
 
 class WorksheetTemplateServicesWidget(ServicesWidget):
     """Listing widget for Worksheet Template Services
     """
 
-    # def __init__(self, field, request):
-    #     super(WorksheetTemplateServicesWidget, self).__init__(field, request)
-    #
-    #     if self.context.getRestrictToMethod():
-    #         method = self.context.getMethod()
-    #         if method:
-    #             self.contentFilter.update({
-    #                 "method_available_uid": method.UID()
-    #             })
-    #     self.columns = collections.OrderedDict((
-    #         ("Title", {
-    #             "title": _(
-    #                 u"listing_services_column_title",
-    #                 default=u"Service"
-    #             ),
-    #             "index": "sortable_title",
-    #             "sortable": False
-    #         }),
-    #         ("Keyword", {
-    #             "title": _(
-    #                 u"listing_services_column_keyword",
-    #                 default=u"Keyword"
-    #             ),
-    #             "sortable": False
-    #         }),
-    #         ("Methods", {
-    #             "title": _(
-    #                 u"listing_services_column_methods",
-    #                 default=u"Methods"
-    #             ),
-    #             "sortable": False
-    #         }),
-    #         ("Calculation", {
-    #             "title": _(
-    #                 u"listing_services_column_calculation",
-    #                 default=u"Calculation"
-    #             ),
-    #             "sortable": False
-    #         }),
-    #     ))
+    def update(self):
+        super(WorksheetTemplateServicesWidget, self).update()
+
+        method_uid = self.context.getRawRestrictToMethod()
+        if method_uid:
+            self.contentFilter.update({
+                "method_available_uid": method_uid
+            })
+
+        self.columns = collections.OrderedDict((
+            ("Title", {
+                "title": _(
+                    u"listing_services_column_title",
+                    default=u"Service"
+                ),
+                "index": "sortable_title",
+                "sortable": False
+            }),
+            ("Keyword", {
+                "title": _(
+                    u"listing_services_column_keyword",
+                    default=u"Keyword"
+                ),
+                "sortable": False
+            }),
+            ("Methods", {
+                "title": _(
+                    u"listing_services_column_methods",
+                    default=u"Methods"
+                ),
+                "sortable": False
+            }),
+            ("Calculation", {
+                "title": _(
+                    u"listing_services_column_calculation",
+                    default=u"Calculation"
+                ),
+                "sortable": False
+            }),
+        ))
+
+        self.review_states[0]["columns"] = self.columns.keys()
+
+    def folderitem(self, obj, item, index):
+        item = super(WorksheetTemplateServicesWidget, self).folderitem(
+            obj, item, index)
+
+        obj = api.get_object(obj)
+
+        calculation = obj.getCalculation()
+        if calculation:
+            item["Calculation"] = api.get_title(calculation)
+            item["replace"]["Calculation"] = get_link_for(calculation)
+        else:
+            item["Calculation"] = ""
+
+        return item
