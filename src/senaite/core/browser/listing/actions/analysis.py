ba676524d71ac9cb25a6b14ee1711ff546ebc0c7
Ramon Bartl <rb@ridingbytes.com>
2025-01-29 18:57:01 +0100

    Pass the title in the payload

--- src/senaite/core/browser/listing/actions/analysis.py
+++ src/senaite/core/browser/listing/actions/analysis.py
@@ -10,11 +10,13 @@ class ActionView(BaseActionView):
     def recalculate(self):
         """Recalculate the results
         """
+        title = self.context.Title()
         calc = self.context.getCalculation()
         if not calc:
-            return self.message("No calculation found", False)
+            return self.message("No calculation found", False, title=title)
         success = self.context.calculateResult(override=True)
         if not success:
-            return self.message("Failed to recalculate result", False)
+            return self.message(
+                "Failed to recalculate result", False, title=title)
 
-        return self.message("Result recalucated", True)
+        return self.message("Result recalucated", True, title=title)
