719341234b9425fb763d9e0488dd7f1c780f1a70
Ramon Bartl <rb@ridingbytes.com>
2025-02-05 10:52:16 +0100

    Don't show statusmessage if the sample is already received

--- src/senaite/core/browser/viewlets/sample/not_sampled.py
+++ src/senaite/core/browser/viewlets/sample/not_sampled.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from bika.lims.interfaces import IReceived
 from plone.app.layout.viewlets import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class NotSampledViewlet(ViewletBase):
-    """ Print a viewlet to display a message stating the Sample cannot
-    transition due to an unset DateSampled 
+    """Print a viewlet to display a message stating the Sample cannot
+    transition due to an unset DateSampled
     """
     index = ViewPageTemplateFile("templates/not_sampled.pt")
 
@@ -14,4 +15,7 @@ class NotSampledViewlet(ViewletBase):
         """Returns whether this viewlet must be visible or not
         """
         sample = self.context
+        if IReceived.providedBy(sample):
+            # sample already received
+            return False
         return not sample.getDateSampled()
