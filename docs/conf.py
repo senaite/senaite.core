b061e0b3fa46c38e5b175c6cd4cc338e39c90e35
Jordi Puiggené <jp@naralabs.com>
2025-02-21 12:43:01 +0100

    Copyright 2018-2025

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'senaite.core'
-copyright = u'2018-2024, Riding Bytes & Naralabs'
+copyright = u'2018-2025, Riding Bytes & Naralabs'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
