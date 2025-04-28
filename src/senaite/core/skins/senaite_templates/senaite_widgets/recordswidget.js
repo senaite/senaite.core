2d3f39ebc37fcb5e3412cd6dc01f38011f3b94c9
Ramon Bartl <rb@ridingbytes.com>
2025-04-28 13:20:21 +0200

    Combogrid Widget jQuery 3 compatibility

--- src/senaite/core/skins/senaite_templates/senaite_widgets/recordswidget.js
+++ src/senaite/core/skins/senaite_templates/senaite_widgets/recordswidget.js
@@ -1,125 +1,129 @@
-jQuery(function($){
-    $(document).ready(function(){
-        recordswidget_lookups();
-        recordswidget_loadEventHandlers();
-    });
-});
+/* Used in AT DataGridField, e.g. in Setup -> ID Server */
+jQuery(function($) {
+    function recordswidget_lookups(elements) {
+        var inputs = elements === undefined
+            ? $(".ArchetypesRecordsWidget [combogrid_options]").not(".has_combogrid_widget")
+            : elements;
 
-function recordswidget_lookups(elements){
-    if(elements == undefined){
-        var inputs = $(".ArchetypesRecordsWidget [combogrid_options]").not('.has_combogrid_widget');
-    } else {
-        var inputs = elements;
+        inputs.each(function() {
+            var $element = $(this);
+            var options = $.parseJSON($element.attr("combogrid_options"));
+
+            if (!options) {
+                return;
+            }
+
+            options.select = function(event, ui) {
+                event.preventDefault();
+                var fieldName = $(this).attr("name").split(".")[0];
+                var key = "";
+                if ($(this).attr("name").includes(".")) {
+                    key = $(this).attr("name").split(".")[1].split(":")[0];
+                }
+                var rowNr = parseInt(this.id.split("-").pop(), 10);
+                $(this).val(ui.item[key]);
+
+                var colModel = $.parseJSON($(this).attr("combogrid_options")).colModel || [];
+                colModel.forEach(function(col) {
+                    var colname = col.columnName;
+                    if (colname !== key) {
+                        var $field = $("#" + fieldName + "-" + colname + "-" + rowNr);
+                        if ($field.length === 1) {
+                            $field.val(ui.item[colname]);
+                        }
+                    }
+                });
+            };
+
+            if (window.location.href.includes("portal_factory")) {
+                options.url = window.location.href.split("/portal_factory")[0] + "/" + options.url;
+            }
+
+            options.url += "?_authenticator=" + $("input[name='_authenticator']").val();
+            $element.combogrid(options);
+        });
     }
-    for (var i = inputs.length - 1; i >= 0; i--) {
-        var element = inputs[i];
-        var options = $.parseJSON($(element).attr('combogrid_options'));
-        if(options == '' || options == undefined || options == null){
-            continue;
-        }
-        options.select = function(event, ui){
-            event.preventDefault();
-            // Set value in activated element (must exist in colModel!)
-            var fieldName = $(this).attr('name').split(".")[0];
-            var key = "";
-            if ($(this).attr('name').split(".").length > 1) {
-            	key = $(this).attr('name').split(".")[1].split(":")[0];
+
+    function recordswidget_loadEventHandlers() {
+        $(document).on("focus", ".ArchetypesRecordsWidget [combogrid_options]", function() {
+            var $this = $(this);
+            var options = $.parseJSON($this.attr("combogrid_options"));
+
+            if (!options) {
+                return;
             }
-            var row_nr = parseInt(this.id.split("-").slice(-1)[0]);
-            $(this).val(ui.item[key]);
-            // Set values if colModel matches recordswidget subfield name
-            var colModel = $.parseJSON($(this).attr('combogrid_options')).colModel;
-            for (var i = colModel.length - 1; i >= 0; i--) {
-                var colname = colModel[i]['columnName'];
-                if (colname != key) {
-                    var element = $('#'+fieldName+'-'+colname+'-'+row_nr);
-                    if(element.length == 1){
-                        $(element).val(ui.item[colname]);
+
+            $this.val("");
+
+            var fieldName = $this.attr("name").split(".")[0];
+            var key = "";
+            if ($this.attr("name").includes(".")) {
+                key = $this.attr("name").split(".")[1].split(":")[0];
+            }
+
+            var colModel = options.colModel || [];
+            var rowNr = parseInt(this.id.split("-").pop(), 10);
+
+            colModel.forEach(function(col) {
+                var colname = col.columnName;
+                if (colname !== key) {
+                    var $field = $("#" + fieldName + "-" + colname + "-" + rowNr);
+                    if ($field.length === 1) {
+                        $field.val("");
                     }
                 }
-            }
-        };
-        if(window.location.href.search("portal_factory") > -1){
-            options.url = window.location.href.split("/portal_factory")[0] + "/" + options.url;
-        }
-        options.url = options.url + '?_authenticator=' + $('input[name="_authenticator"]').val();
-        $(element).combogrid(options);
-    };
-}
+            });
+        });
 
-function recordswidget_loadEventHandlers(elements) {
-    $('.ArchetypesRecordsWidget [combogrid_options]').live('focus', function(){
-        /*if ($(this).hasClass('has_combogrid_widget')) {
-            return;
-        }*/
-        var options = $.parseJSON($(this).attr('combogrid_options'));
-        if(options != '' && options != undefined && options != null){
-            // For inputs with combogrids, we want them to clear when focused
-            $(this).val('');
-            // We also want to clear all colModel->subfield completions
-            var fieldName = $(this).attr('name').split(".")[0];
-            var key = "";
-            if ($(this).attr('name').split(".").length > 1) {
-                key = $(this).attr('name').split(".")[1].split(":")[0];
-            }
-            var colModel = $.parseJSON($(this).attr('combogrid_options')).colModel;
-            var row_nr = parseInt(this.id.split("-").slice(-1)[0]);
-            for (var i = colModel.length - 1; i >= 0; i--) {
-                var colname = colModel[i]['columnName'];
-                if (colname != key) {
-                    var element = $('#'+fieldName+'-'+colname+'-'+row_nr);
-                    if(element.length == 1){
-                        $(element).val('');
-                    }
+        $(document).on("click", "input[id$='_more']", function() {
+            var fieldname = $(this).attr("id").split("_")[0];
+            var $table = $("#" + fieldname + "_table");
+            var $rows = $(".records_row_" + fieldname);
+            var $lastRow = $rows.last().clone();
+
+            var $foundInputs = $lastRow.find("input[id^='" + fieldname + "'], select[id^='" + fieldname + "']");
+
+            $foundInputs.each(function() {
+                var idParts = this.id.split("-");
+                var prefix = idParts[0] + "-" + idParts[1];
+                var nr = parseInt(idParts[2], 10) + 1;
+
+                $(this).attr("id", prefix + "-" + nr).val("");
+            });
+
+            // Validate required fields before adding
+            var isValid = true;
+            $foundInputs.each(function() {
+                if ($(this).hasClass("required") && $(this).val() === "") {
+                    window.bika.lims.portalMessage(this.id.split("-")[1] + ": " + _p("Input is required but not given."));
+                    isValid = false;
+                    return false; // break loop
                 }
-            }
-        }
-    });
+            });
 
-    $("input[id$='_more']").click(function(i,e){
-        var fieldname = $(this).attr("id").split("_")[0];
-        var table = $('#'+fieldname+"_table");
-        var rows = $(".records_row_"+fieldname);
-        // clone last row
-        var row = $(rows[rows.length-1]).clone();
-        // after cloning, make sure the new element's IDs are unique
-        var found = $(row).find("input[id^='"+fieldname+"']");
-        if (found.length === 0) {
-            found = $(row).find("select[id^='"+fieldname+"']");
-        }
-        for (var i = found.length - 1; i >= 0; i--) {
-            var ID = found[i].id;
-            var prefix = ID.split("-")[0] + "-" + ID.split("-")[1];
-            var nr = parseInt(ID.split("-")[2]) + 1;
-            $(found[i]).attr('id', prefix + "-" + nr);
-            $(found[i]).val('');
-        };
-        // First check to see that all required subfields are completed,
-        // before allowing a new row to be added
-        for (var i = found.length - 1; i >= 0; i--) {
-            var e = found[i];
-            var required = $(e).hasClass('required');
-            var val = $(e).val();
-            if (required && val == ""){
-                window.bika.lims.portalMessage(e.id.split("-")[1] + ": " + _p("Input is required but not given."));
+            if (!isValid) {
                 return false;
             }
-        };
-        // clear values
-        for(i=0; i<$(row).children().length; i++){
-            var td = $(row).children()[i];
-            var input = $(td).children()[0];
-            $(input).val('');
-            $(input).removeClass("hasDatepicker");
-        }
-        $(row).appendTo($(table));
-        recordswidget_lookups($(row).find('[combogrid_options]'));
-    });
 
-    $(".rw_deletebtn").live('click', function(i,e){
-        var row = $(this).parent().parent();
-        var siblings = $(row).siblings();
-        if (siblings.length < 2) return;
-        $(this).parent().parent().remove();
-    });
-}
+            // Clear values
+            $lastRow.children().each(function() {
+                var $input = $(this).children().first();
+                $input.val("").removeClass("hasDatepicker");
+            });
+
+            $lastRow.appendTo($table);
+            recordswidget_lookups($lastRow.find("[combogrid_options]"));
+        });
+
+        $(document).on("click", ".rw_deletebtn", function() {
+            var $row = $(this).closest("tr");
+            var $siblings = $row.siblings("tr");
+            if ($siblings.length >= 1) {
+                $row.remove();
+            }
+        });
+    }
+
+    recordswidget_lookups();
+    recordswidget_loadEventHandlers();
+});
