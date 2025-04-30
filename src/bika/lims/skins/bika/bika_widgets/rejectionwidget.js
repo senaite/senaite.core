5265b5a974f9d1ea189e2292dd67cb159791c819
Ramon Bartl <rb@ridingbytes.com>
2025-04-30 21:45:42 +0200

    Rejection Widget

--- src/bika/lims/skins/bika/bika_widgets/rejectionwidget.js
+++ src/bika/lims/skins/bika/bika_widgets/rejectionwidget.js
@@ -1,39 +1,31 @@
 jQuery(function($) {
-    function hide_show_options(div_widget) {
-        var isChecked = div_widget.find(".rejectionwidget-checkbox").prop("checked");
-        if (isChecked) {
-            div_widget.find("div.options-set").show();
-        } else {
-            div_widget.find("div.options-set").hide();
-        }
+
+    function toggleOptionsVisibility($widget) {
+        const isChecked = $widget.find(".rejectionwidget-checkbox").prop("checked");
+        $widget.find("div.options-set").toggle(isChecked);
     }
 
-    function hide_show_other(div_widget) {
-        var isChecked = div_widget.find(".rejectionwidget-checkbox-other").prop("checked");
-        if (isChecked) {
-            div_widget.find(".rejectionwidget-input-other").show();
-        } else {
-            div_widget.find(".rejectionwidget-input-other").hide();
-        }
+    function toggleOtherInputVisibility($widget) {
+        const isChecked = $widget.find(".rejectionwidget-checkbox-other").prop("checked");
+        $widget.find(".rejectionwidget-input-other").toggle(isChecked);
     }
 
-    $(document).ready(function() {
-        var widgets = $("div.RejectionWidget");
+    $(function() {
+        const $widgets = $("div.RejectionWidget");
 
-        widgets.each(function() {
-            var $widget = $(this);
-            hide_show_other($widget);
-            hide_show_options($widget);
-        });
-
-        $(document).on("change copy", "input.rejectionwidget-checkbox-other", function() {
-            var $widget = $(this).closest("div.RejectionWidget");
-            hide_show_other($widget);
+        $widgets.each(function() {
+            const $widget = $(this);
+            toggleOptionsVisibility($widget);
+            toggleOtherInputVisibility($widget);
         });
 
         $(document).on("change copy", "input.rejectionwidget-checkbox", function() {
-            var $widget = $(this).closest("div.RejectionWidget");
-            hide_show_options($widget);
+            toggleOptionsVisibility($(this).closest("div.RejectionWidget"));
+        });
+
+        $(document).on("change copy", "input.rejectionwidget-checkbox-other", function() {
+            toggleOtherInputVisibility($(this).closest("div.RejectionWidget"));
         });
     });
+
 });
