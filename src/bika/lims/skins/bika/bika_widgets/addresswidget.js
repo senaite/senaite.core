c329e5b9930e6e1ecd64b49cfbef5716f491d2a1
Ramon Bartl <rb@ridingbytes.com>
2025-04-30 21:01:24 +0200

    AT Address Widget jQuery 3 compatibility

--- src/bika/lims/skins/bika/bika_widgets/addresswidget.js
+++ src/bika/lims/skins/bika/bika_widgets/addresswidget.js
@@ -1,97 +1,99 @@
-jQuery( function($) {
+jQuery(function ($) {
+	$(document).ready(function () {
 
-	$(document).ready(function(){
-
-		function lookups(){
+		function lookups() {
 			// Selecting a Country (populate state field)
-			$("[id*='\\.country']").change(function(){
-				field = $(this).attr('name').split(".")[0];
-				// // removes value from state, city, zip.
-				// $("[id='"+field+"\\.state']").val("");
-				// $("[id='"+field+"\\.district']").val("");
-				// re-populate state dropdown
+			$("[id*='\\.country']").on("change", function () {
+				const field = $(this).attr("name").split(".")[0];
 				populate_state_select(field);
 			});
+
 			// Selecting a State (populate district field)
-			$("[id*='\\.state']").change(function(){
-				field = $(this).attr('name').split(".")[0];
+			$("[id*='\\.state']").on("change", function () {
+				const field = $(this).attr("name").split(".")[0];
 				populate_district_select(field);
 			});
 		}
 
-		function copyAddress(){
-			to_address = $(this).attr('id').split(".")[0];
-			from_address = $(this).val();
-			if (from_address == '') {
-				return
-			}
+		function copyAddress() {
+			const to_address = $(this).attr("id").split(".")[0];
+			const from_address = $(this).val();
+			if (!from_address) return;
 
-			state = $("#" + from_address + "\\.state").clone();
-			$(state).attr('id', to_address + ".state");
-			$(state).attr('name', to_address + ".state:record");
+			// Replace state field
+			const state = $("#" + from_address + "\\.state").clone();
+			state.attr({
+				id: to_address + ".state",
+				name: to_address + ".state:record"
+			});
 			$("#" + to_address + "\\.state").replaceWith(state);
 
-			district = $("#" + from_address + "\\.district").clone();
-			$(district).attr('id', to_address + ".district");
-			$(district).attr('name', to_address + ".district:record");
+			// Replace district field
+			const district = $("#" + from_address + "\\.district").clone();
+			district.attr({
+				id: to_address + ".district",
+				name: to_address + ".district:record"
+			});
 			$("#" + to_address + "\\.district").replaceWith(district);
 
-			$.each(['country', 'state', 'district'], function(i, item){
-				val = $("#" + from_address + "\\." + item).val();
-				$.each($("#" + to_address + "\\." + item + " > option"), function(i,e){
-					if($(e).val() == val){
-						$(e).attr('selected', 'selected');
-					} else {
-						$(e).removeAttr('selected');
-					}
+			// Sync selected options
+			["country", "state", "district"].forEach(function (item) {
+				const val = $("#" + from_address + "\\." + item).val();
+				$("#" + to_address + "\\." + item + " option").each(function () {
+					$(this).prop("selected", $(this).val() === val);
 				});
 			});
 
-			$('#' + to_address + '\\.city').val($('#' + from_address + '\\.city').val()).change();
-			$('#' + to_address + '\\.zip').val($('#' + from_address + '\\.zip').val()).change();
-			$('#' + to_address + '\\.address').val($('#' + from_address + '\\.address').val()).change();
+			// Copy text fields
+			$("#" + to_address + "\\.city").val($("#" + from_address + "\\.city").val()).trigger("change");
+			$("#" + to_address + "\\.zip").val($("#" + from_address + "\\.zip").val()).trigger("change");
+			$("#" + to_address + "\\.address").val($("#" + from_address + "\\.address").val()).trigger("change");
 
-		    lookups();
-
+			// Reapply dependent handlers
+			lookups();
 		}
 
-		function populate_state_select(field){
+		function populate_state_select(field) {
 			$.ajax({
-				type: 'POST',
+				type: "POST",
 				url: portal_url + "/geo_subdivisions",
-				data: {'parent': $("[id='"+field+"\\.country']").val(),
-					   '_authenticator': $('input[name="_authenticator"]').val()},
-				success: function(data,textStatus,$XHR){
-					target = $("[id='"+field+"\\.state']");
-					$(target).empty();
-					$(target).append("<option value=''></option>");
-					$.each(data, function(i,v){
-						$(target).append('<option value="'+v['name']+'">'+v['name']+'</option>');
-					});
-					$("[id='"+field+"\\.district']").empty();
+				data: {
+					parent: $("#" + field + "\\.country").val(),
+					_authenticator: $("input[name='_authenticator']").val()
 				},
-				dataType: "json"
-			});
-		}
-		function populate_district_select(field){
-			$.ajax({
-				type: 'POST',
-				url: portal_url + "/geo_subdivisions",
-				data: {'parent': $("[id='"+field+"\\.state']").val(),
-					   '_authenticator': $('input[name="_authenticator"]').val()},
-				success: function(data,textStatus,$XHR){
-					target = $("[id='"+field+"\\.district']");
-					$(target).empty();
-					$(target).append("<option value=''></option>");
-					$.each(data, function(i,v){
-						$(target).append('<option value="'+v['name']+'">'+v['name']+'</option>');
+				success: function (data) {
+					const target = $("#" + field + "\\.state");
+					target.empty().append("<option value=''></option>");
+					data.forEach(function (v) {
+						target.append('<option value="' + v.name + '">' + v.name + "</option>");
 					});
+					$("#" + field + "\\.district").empty();
 				},
 				dataType: "json"
 			});
 		}
 
-		$("[id*='\\.selection']").change(copyAddress);
+		function populate_district_select(field) {
+			$.ajax({
+				type: "POST",
+				url: portal_url + "/geo_subdivisions",
+				data: {
+					parent: $("#" + field + "\\.state").val(),
+					_authenticator: $("input[name='_authenticator']").val()
+				},
+				success: function (data) {
+					const target = $("#" + field + "\\.district");
+					target.empty().append("<option value=''></option>");
+					data.forEach(function (v) {
+						target.append('<option value="' + v.name + '">' + v.name + "</option>");
+					});
+				},
+				dataType: "json"
+			});
+		}
+
+		// Event bindings
+		$("[id*='\\.selection']").on("change", copyAddress);
 		lookups();
 	});
 });
