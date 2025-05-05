d8b5ed9b7f8bec4ff5ed3276f054f623341c70d9
Ramon Bartl <rb@ridingbytes.com>
2025-05-05 15:29:54 +0200

    Refactored AttachmentsUtils to class

--- src/senaite/core/browser/static/js/bika.lims.utils.attachments.js
+++ src/senaite/core/browser/static/js/bika.lims.utils.attachments.js
@@ -1,45 +1,35 @@
-
-/* Please use this command to compile this file into the parent `js` directory:
-    coffee --no-header -w -o ../ -c bika.lims.utils.attachments.coffee
- */
-
-
 /**
- * Controller class for calculation events
+ * Attachments Controller
+ *
+ * This controller is loaded if the attachments viewlet is displayed, e.g. for samples or worksheets
  */
+window.AttachmentsUtils = class AttachmentsUtils {
+  constructor() {
+    this.load = this.load.bind(this);
+  }
 
-(function() {
-  window.AttachmentsUtils = function() {
-    var that;
-    that = this;
-    that.load = function() {
-      $('#AttachFile,#Service,#Analysis').change(function(event) {
-        var analysis, attachfile, service;
-        attachfile = $('#AttachFile').val();
-        if (attachfile === void 0) {
-          attachfile = '';
-        }
-        service = $('#Service').val();
-        if (service === void 0) {
-          service = '';
-        }
-        analysis = $('#Analysis').val();
-        if (analysis === void 0) {
-          analysis = '';
-        }
-        if (this.id === 'Service') {
-          $('#Analysis').val('');
-        }
-        if (this.id === 'Analysis') {
-          $('#Service').val('');
-        }
-        if (attachfile !== '' && (service !== '' || analysis !== '')) {
-          $('#addButton').removeAttr('disabled');
-        } else {
-          $('#addButton').prop('disabled', true);
-        }
-      });
-    };
-  };
+  load() {
+    // Worksheets need to check these before enabling Add button
+    $("#AttachFile, #Service, #Analysis").on("change", (event) => {
+      const attachfile = $("#AttachFile").val() || "";
+      let service = $("#Service").val() || "";
+      let analysis = $("#Analysis").val() || "";
 
-}).call(this);
+      if (event.target.id === "Service") {
+        $("#Analysis").val("");
+        analysis = "";
+      }
+
+      if (event.target.id === "Analysis") {
+        $("#Service").val("");
+        service = "";
+      }
+
+      if (attachfile !== "" && (service !== "" || analysis !== "")) {
+        $("#addButton").removeAttr("disabled");
+      } else {
+        $("#addButton").prop("disabled", true);
+      }
+    });
+  }
+}
