096c7acfcf1998637df4afb5b839bfb2223a6b40
Anton Zhuchkov <antonzh85@gmail.com>
2025-06-04 17:17:04 +0700

    comment restricted

--- src/bika/lims/utils/analysisrequest.py
+++ src/bika/lims/utils/analysisrequest.py
@@ -279,6 +279,7 @@ def to_service_uids(services=None, values=None):
     # Exclude service uids if not available in categories for Client
     client = values.get("Client")
     available_uids = get_available_service_uids(client)
+    logger.info("Available service uids: {}".format(available_uids))
     # if available_uids:
     #    uids = [uid for uid in uids if uid in available_uids]
 
