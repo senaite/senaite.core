67c7c7acbb60064c6d85654fa59a160e0d37b41c
Aurthur Musendame <musendamea@nmrl.org.zw>
2025-06-13 20:44:27 +0200

    replaced string check with a python3 compatible api string check

--- src/bika/lims/content/abstractanalysis.py
+++ src/bika/lims/content/abstractanalysis.py
@@ -937,7 +937,7 @@ class AbstractAnalysis(AbstractBaseAnalysis):
         # If string-like result, return without any formatting
         if result_type in ["string", "text"]:
             if html:
-                result = result if isinstance(result, basestring) else str(result)
+                result = result if api.is_string(result) else str(result)
                 result = cgi.escape(result)
                 result = result.replace("\n", "<br/>")
             return result
