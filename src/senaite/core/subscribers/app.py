b0c8397d7631e602b74bad8935a9f56985eb8b52
Ramon Bartl <rb@ridingbytes.com>
2025-06-30 10:35:34 +0200

    Removed test module

--- src/senaite/core/subscribers/app.py
+++
@@ -1,9 +0,0 @@
-from OFS.Application import import_product
-
-
-def databaseOpenedWithRootHandler(event):
-    # db = event.database
-    path = "/Products"
-    import sys
-    sys.path.append("/Users/rbartl/develop/buildout/addons")
-    import_product(path, "addon")
