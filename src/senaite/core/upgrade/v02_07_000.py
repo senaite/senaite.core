857c27049ffd744c5f02acd8e0d53be5eff9805c
Ramon Bartl <rb@ridingbytes.com>
2025-07-17 16:52:06 +0200

    Add `has_calculation` index

--- src/senaite/core/upgrade/v02_07_000.py
+++ src/senaite/core/upgrade/v02_07_000.py
@@ -22,7 +22,9 @@
 from bika.lims import api
 from bika.lims.interfaces import IInvalidated
 from senaite.core import logger
+from senaite.core.catalog import ANALYSIS_CATALOG
 from senaite.core.catalog import SAMPLE_CATALOG
+from senaite.core.catalog.analysis_catalog import INDEXES as ANALYSIS_INDEXES
 from senaite.core.config import PROJECTNAME as product
 from senaite.core.interfaces.catalog import ISenaiteCatalogObject
 from senaite.core.setuphandlers import add_catalog_column
@@ -118,3 +120,20 @@ def upgrade_catalog_modified_index(tool):
         "You may need to manually reindex the 'modified' index in existing "
         "catalogs as required."
     )
+
+
+def update_analysis_catalog_indexes(tool):
+    """Update analysis catalog indexes
+    """
+    logger.info("Update analysis catalog indexes ...")
+    to_reindex = []
+    catalog = api.get_tool(ANALYSIS_CATALOG)
+    for record in ANALYSIS_INDEXES:
+        if add_catalog_index(catalog, *record):
+            to_reindex.append(record[0])
+
+    for index_id in to_reindex:
+        logger.info("Reindexing index '%s'" % index_id)
+        catalog.reindexIndex(index_id, api.get_request())
+
+    logger.info("Update analysis catalog indexes [DONE]")
