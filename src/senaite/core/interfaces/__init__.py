3b1c407575fe50dd3d21cfeb5c1add6878c2f5ba
Ramon Bartl <rb@ridingbytes.com>
2025-11-14 13:29:03 +0100

    Removed redefinition of interfaces

--- src/senaite/core/interfaces/__init__.py
+++ src/senaite/core/interfaces/__init__.py
@@ -516,16 +516,6 @@ class IWorksheetTemplate(Interface):
     """
 
 
-class IContacts(Interface):
-    """Marker interface for Contacts container
-    """
-
-
-class IContact(Interface):
-    """Marker interface for a single Contact
-    """
-
-
 class IAfterCreateSampleHook(Interface):
     """Subscription adapter after the sample was created
     """
