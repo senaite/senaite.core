26982f25b47f7ef7a0ddd007a01f285670bc09d6
Tim-IA <95612081+Tim-IA@users.noreply.github.com>
2025-11-17 15:00:00 +0100

    replace setup

--- src/senaite/core/exportimport/setupdata/__init__.py
+++ src/senaite/core/exportimport/setupdata/__init__.py
@@ -357,7 +357,7 @@ class Sub_Groups(WorksheetImporter):
 class Lab_Information(WorksheetImporter):
 
     def Import(self):
-        laboratory = self.context.bika_setup.laboratory
+        laboratory = self.context.setup.laboratory
         values = {}
         for row in self.get_rows(3):
             values[row['Field']] = row['Value']
