dc595ffe2cd36f79edd5340c126be1122f667af5
Ramon Bartl <rb@ridingbytes.com>
2025-11-21 13:30:55 +0100

    updated AT widget

--- src/senaite/core/browser/widgets/multiuploadwidget.py
+++ src/senaite/core/browser/widgets/multiuploadwidget.py
@@ -24,10 +24,6 @@ from bika.lims import api
 from bika.lims import logger
 from Products.Archetypes.Registry import registerWidget
 from senaite.core.browser.widgets.referencewidget import ReferenceWidget
-from senaite.core.interfaces import IMultiUploadFileCreator
-from senaite.core.interfaces import IMultiUploadFileRemover
-from zope.component import getAdapter
-from zope.component import getMultiAdapter
 
 
 class MultiUploadWidget(ReferenceWidget):
@@ -99,14 +95,6 @@ class MultiUploadWidget(ReferenceWidget):
 
         return attributes
 
-    def is_multi_valued(self, field):
-        """Check if the field accepts multiple values
-
-        :param field: The current field
-        :returns: True if multi-valued, False otherwise
-        """
-        return getattr(field, "multiValued", False)
-
     def get_download_url(self, obj):
         """Get the download URL for a File or Image object
 
@@ -146,14 +134,6 @@ class MultiUploadWidget(ReferenceWidget):
 
         return 0
 
-    def get_session(self, request):
-        """Safely get the session from the request
-
-        :param request: The request object
-        :returns: Session object or None if not available (e.g., in tests)
-        """
-        return getattr(request, "SESSION", None)
-
     def get_existing_files_data(self, context, field, value):
         """Get metadata for existing file references
 
@@ -189,14 +169,15 @@ class MultiUploadWidget(ReferenceWidget):
             self, instance, field, form,
             empty_marker=None, emptyReturnsMarker=False,
             validating=True):
-        """Process form data and create File/Image objects
+        """Process form data and return existing UIDs
 
-        This method:
-        1. Gets the main field value (existing UIDs)
-        2. Gets uploaded file IDs from .data field
-        3. Creates File or Image objects for new uploads
-        4. Deletes File/Image objects that were removed
-        5. Returns combined list of existing + new UIDs
+        :param instance: The content instance
+        :param field: The field being processed
+        :param form: The form data
+        :param empty_marker: Empty marker value
+        :param emptyReturnsMarker: Whether to return empty marker
+        :param validating: Whether we are validating
+        :returns: Tuple of (existing_uids, kwargs)
         """
         # Skip processing for temporary instances
         if instance.isTemporary():
@@ -208,185 +189,30 @@ class MultiUploadWidget(ReferenceWidget):
             "process_form() called for field '{}', validating={}"
             .format(field_name, validating))
 
-        # Get current field value (before form processing)
-        old_value = field.get(instance)
-        old_uids = []
-        if old_value:
-            if isinstance(old_value, (list, tuple)):
-                old_uids = [
-                    api.get_uid(obj) for obj in old_value
-                    if api.is_object(obj)]
-            elif api.is_object(old_value):
-                old_uids = [api.get_uid(old_value)]
-
-        # Get the main field value (existing UIDs from React)
-        main_value = form.get(field_name, "")
+        # Get the main field value (existing UIDs from React widget)
         existing_uids = []
-        if main_value:
-            if api.is_string(main_value):
+        value = form.get(field_name, "")
+        if value:
+            if api.is_string(value):
                 # UIDs are separated by \r\n
-                existing_uids = [
-                    uid.strip() for uid in main_value.split("\r\n")
-                    if uid.strip()]
-            elif isinstance(main_value, (list, tuple)):
-                existing_uids = [uid for uid in main_value if uid]
+                existing_uids = [uid.strip() for uid in value.split("\r\n")]
+            elif isinstance(value, (list, tuple)):
+                existing_uids = [uid for uid in value if uid]
 
         logger.info(
-            "process_form for field '{}': old_uids = {}, "
-            "existing_uids from main field = {}"
-            .format(field_name, old_uids, existing_uids))
+            "process_form for field '{}': returning existing UIDs = {}"
+            .format(field_name, existing_uids))
 
-        # Get the JSON data from .data field (new uploads only)
-        # It contains the generated UUIDs for uploaded files
-        data_field = field_name + ".data"
-        data = form.get(data_field, "")
-
-        # Start with existing UIDs
-        uids = list(existing_uids)
-
-        # Parse upload IDs if there are new uploads
-        upload_ids = []
-        if data:
-            try:
-                # Parse JSON data with upload IDs
-                upload_ids = json.loads(data)
-                logger.info(
-                    "process_form for field '{}': upload_ids "
-                    "from .data field = {}"
-                    .format(field_name, upload_ids))
-            except (ValueError, TypeError):
-                logger.error(
-                    "Invalid JSON data for field {}"
-                    .format(field_name))
-                upload_ids = []
-
-        # Get session and uploaded files
-        session = self.get_session(instance.REQUEST)
-        if not session:
-            # No session (e.g., in tests), return existing UIDs only
+        # Upload UUIDs are stored in <fieldname>.data and will be
+        # processed by the event subscriber
+        data_key = "{}.data".format(field_name)
+        if data_key in form:
             logger.info(
-                "No SESSION available, skipping file upload processing")
-            if not self.is_multi_valued(field):
-                return uids[0] if uids else "", {}
-            return uids, {}
+                "Upload UUIDs found in '{}' for event subscriber"
+                .format(data_key))
 
-        uploaded_files = session.get("multiupload_files", {})
-
-        # Track created UIDs to handle multiple calls
-        created_uids_key = "multiupload_created_uids"
-        created_uids_map = session.get(created_uids_key, {})
-
-        # Create File/Image objects for each uploaded file
-        for upload_id in upload_ids:
-            # Check if we already created object for this upload_id
-            if upload_id in created_uids_map:
-                # Reuse the UID from previous process_form call
-                uid = created_uids_map[upload_id]
-                # Verify object still exists (might be rolled back)
-                try:
-                    obj = api.get_object(uid)
-                    uids.append(uid)
-                    logger.info(
-                        "Reusing existing UID {} for upload_id {}"
-                        .format(uid, upload_id))
-                    continue
-                except api.APIError:
-                    logger.warning(
-                        "UID {} no longer exists, recreating for "
-                        "upload_id {}".format(uid, upload_id))
-                    # Remove stale UID and fall through to recreate
-                    del created_uids_map[upload_id]
-                    session[created_uids_key] = created_uids_map
-
-            # Get file data from session dict
-            file_data = uploaded_files.get(upload_id)
-
-            logger.info(
-                "Looking for upload_id {} in session dict"
-                .format(upload_id))
-            if file_data:
-                logger.info(
-                    "✓ Found file data for upload_id {}"
-                    .format(upload_id))
-            else:
-                logger.warning(
-                    "✗ File data NOT found for upload_id {}"
-                    .format(upload_id))
-                logger.info(
-                    "Available upload IDs in session: {}"
-                    .format(uploaded_files.keys()))
-
-            if file_data:
-                data = file_data["data"]
-                filename = api.safe_unicode(file_data["filename"])
-                content_type = file_data["content_type"]
-
-                try:
-                    # Get the file creator adapter
-                    creator = getMultiAdapter(
-                        (instance, field), IMultiUploadFileCreator)
-
-                    # Create the File/Image object
-                    obj = creator.create(
-                        filename, content_type, data)
-
-                    # Get UID
-                    uid = api.get_uid(obj)
-                    uids.append(uid)
-
-                    # Store the UID for this upload_id in session
-                    created_uids_map[upload_id] = uid
-                    session[created_uids_key] = created_uids_map
-
-                    logger.info(
-                        "Created and mapped: upload_id {} -> UID {}"
-                        .format(upload_id, uid))
-
-                except Exception as e:
-                    import traceback
-                    logger.error(
-                        u"Error creating object {}: {}"
-                        .format(filename, api.safe_unicode(str(e))))
-                    logger.error(traceback.format_exc())
-                    continue
-            else:
-                logger.warning(
-                    "Upload file data for ID {} not found, skipping"
-                    .format(upload_id))
-
-        logger.info(
-            "process_form for field '{}': final UIDs = {}"
-            .format(field_name, uids))
-
-        # Delete File/Image objects that were removed
-        logger.info(
-            "Checking deletion: validating={}, old_uids={}, uids={}"
-            .format(validating, old_uids, uids))
-
-        if old_uids:
-            removed_uids = set(old_uids) - set(uids)
-            logger.info("Removed UIDs: {}".format(removed_uids))
-            if removed_uids:
-                self.delete_removed_files(instance, removed_uids)
-
-        # Handle multi-valued vs single-valued
-        if not self.is_multi_valued(field):
-            return uids[0] if uids else "", {}
-
-        return uids, {}
-
-    def delete_removed_files(self, container, uids):
-        """Delete File/Image objects removed from the field
-
-        :param container: The parent container
-        :param uids: List of UIDs to delete
-        """
-        if not uids:
-            return
-
-        # Remove files using IMultiUploadFileRemover adapter
-        remover = getAdapter(container, IMultiUploadFileRemover)
-        remover.remove(uids)
+        # Return only existing UIDs
+        return existing_uids, {}
 
 
 registerWidget(
