ae76096228cfb30e0e3d696f909701d7f5c3a6fb
Ramon Bartl <rb@ridingbytes.com>
2025-11-24 22:35:40 +0100

    Use api.delete instead

--- src/senaite/core/adapters/multiupload.py
+++ src/senaite/core/adapters/multiupload.py
@@ -18,14 +18,17 @@
 # Copyright 2018-2025 by it's authors.
 # Some rights reserved, see README and LICENSE.
 
+import transaction
 from bika.lims import api
 from bika.lims import logger
 from plone.dexterity.utils import createContentInContainer
 from plone.namedfile.file import NamedBlobFile
 from plone.namedfile.file import NamedBlobImage
+from senaite.core.events.attachments import AttachmentsDeletedEvent
 from senaite.core.interfaces import IMultiUploadFileCreator
 from senaite.core.interfaces import IMultiUploadFileRemover
 from zope.component import adapter
+from zope.event import notify
 from zope.interface import Interface
 from zope.interface import implementer
 
@@ -89,6 +92,7 @@ class DefaultFileCreator(object):
         }
 
         try:
+            transaction.savepoint()
             api.snapshot.pause_snapshots_for(self.context)
             obj = createContentInContainer(
                 self.context,
@@ -135,53 +139,40 @@ class DefaultFileRemover(object):
         """
         if not uids:
             return
+        if not isinstance(uids, (list, set, tuple)):
+            uids = [uids]
 
         logger.info("Deleting {} removed file(s) from {}".format(
             len(uids), api.get_path(self.container)))
 
-        # Use privileged context to delete files
-        with api.security.as_privileged_user():
-            for uid in uids:
-                try:
-                    obj = api.get_object(uid)
-                    parent = api.get_parent(obj)
+        # deleted objects for event notification
+        deleted = []
 
-                    # Verify the object lives in this container
-                    if parent != self.container:
-                        logger.warning(
-                            "Skipping deletion of {}: not in container "
-                            "(parent: {}, expected: {})".format(
-                                uid, api.get_path(parent),
-                                api.get_path(self.container)))
-                        continue
+        for uid in uids:
+            try:
+                transaction.savepoint()
+                obj = api.get_object(uid)
+                # Store info before deletion for logging
+                obj_id = api.get_id(obj)
+                obj_type = api.get_portal_type(obj)
+                obj_path = api.get_path(obj)
+                # Delete the object
+                api.delete(obj, check_permissions=False)
+                logger.info(u"Deleted {} object: {} (was at: {})".format(
+                    obj_type, obj_id, obj_path))
+                deleted.append(obj)
+            except api.APIError as e:
+                logger.error("Error deleting object {}: {}".format(
+                    repr(obj), str(e)))
+                import traceback
+                logger.error(traceback.format_exc())
+                continue
+            except Exception as e:
+                logger.error("Unexpected error deleting object {}: {}".format(
+                    repr(obj), str(e)))
+                import traceback
+                logger.error(traceback.format_exc())
+                continue
 
-                    # Verify it's a SimpleFile or SimpleImage
-                    portal_type = api.get_portal_type(obj)
-                    if portal_type not in ["SimpleFile", "SimpleImage"]:
-                        logger.warning(
-                            "Skipping deletion of {}: not a SimpleFile/"
-                            "SimpleImage (type: {})".format(uid, portal_type))
-                        continue
-
-                    # Store info before deletion for logging
-                    obj_title = api.get_title(obj)
-                    obj_type = api.get_portal_type(obj)
-                    obj_path = api.get_path(obj)
-
-                    # Delete the object
-                    api.delete(obj)
-                    logger.info(u"Deleted {} object: {} (was at: {})".format(
-                        obj_type, api.safe_unicode(obj_title), obj_path))
-
-                except api.APIError as e:
-                    logger.error("Error deleting object {}: {}".format(
-                        uid, str(e)))
-                    import traceback
-                    logger.error(traceback.format_exc())
-                    continue
-                except Exception as e:
-                    logger.error("Unexpected error deleting object {}: {}".format(
-                        uid, str(e)))
-                    import traceback
-                    logger.error(traceback.format_exc())
-                    continue
+        # Notify about the removed attachments
+        notify(AttachmentsDeletedEvent(self.container, deleted))
