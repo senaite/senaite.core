0c8c8bd3dea6e71accc50e0edec2931793e993f9
Tim-IA <95612081+Tim-IA@users.noreply.github.com>
2026-04-07 15:22:07 +0200

    Remove the u prefix

--- src/senaite/core/content/laboratory.py
+++ src/senaite/core/content/laboratory.py
@@ -46,9 +46,9 @@ DEFAULT_ACCREDITATION_PAGE_HEADER = """${lab_name} has been accredited as
 ${accreditation_standard} conformant by ${accreditation_body_abbr},
 ${accreditation_body_name}<br/><br/> ${accreditation_body_abbr} is the single
 national accreditation body assessing testing and calibration laboratories for
-compliance to the ISO/IEC 17025 standard.<br/><br/>\n The following analysis
-services have been included in the ${accreditation_body_abbr} schedule of
-Accreditation for this Laboratory:
+compliance to the ISO/IEC 17025 standard.<br/><br/>
+The following analysis services have been included in the ${accreditation_body_abbr}
+schedule of Accreditation for this Laboratory:
 """
 
 
