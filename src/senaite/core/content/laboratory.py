0d8709572b9e864720cde08fa5e30a9dc69e5804
Tim-IA <95612081+Tim-IA@users.noreply.github.com>
2026-04-07 15:38:20 +0200

    replace curret accreditation page header

--- src/senaite/core/content/laboratory.py
+++ src/senaite/core/content/laboratory.py
@@ -49,7 +49,7 @@ national accreditation body assessing testing and calibration laboratories for
 compliance to the ISO/IEC 17025 standard.<br/><br/>
 The following analysis services have been included in the ${accreditation_body_abbr}
 schedule of Accreditation for this Laboratory:
-"""
+""".encode('utf-8')
 
 
 class ILaboratorySchema(IOrganizationSchema):
